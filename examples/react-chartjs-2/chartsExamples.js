a45147874907f589cba3c4078a146eedca1a991a
Artyom Keydunov <artyom.keydunov@gmail.com>
2018-09-24 14:23:37 -0700

    Refactor example for chartjs2

--- examples/react-chartjs-2/chartsExamples.js
+++ examples/react-chartjs-2/chartsExamples.js
@@ -9,6 +9,28 @@ const chartTypesToComponents = {
   pie: Pie
 };
 
+const exampleTemplate = (query, type) => (
+`
+import { default as cubejs, chartjsConfig } from 'cubejs-client';
+import { QueryRenderer } from '@cubejs-client/react';
+import { Line } from 'react-chartjs-2';
+
+const query =
+${JSON.stringify(query, null, 2)}
+
+ReactDOM.render(
+  <QueryRenderer
+    query={query}
+    cubejsApi={cubejs('API_KEY')}
+    render={ ({ resultSet }) => {
+      const { data, options } = chartjsConfig('${type}', resultSet)
+      return <${chartTypesToComponents[type].name} data={data} options={options} />
+    }
+  />
+)
+`
+)
+
 class Chart extends React.Component {
   render() {
     const ChartComponent = chartTypesToComponents[this.props.type];
@@ -26,58 +48,48 @@ const renderChart = (resultSet, type) => {
   return <Chart data={data} type={type} options={options}  />
 }
 
+const renderExample = (type, query) => {
+  return () => {
+     return (<Example
+       query={query}
+       codeExample={exampleTemplate(query, type)}
+       render={ ({ resultSet }) => renderChart(resultSet, type) }
+     />);
+  }
+}
+
 const chartsExamples = {
   line: {
     title: 'Line',
-    render: () => (
-      <Example
-        query={{
-          measures: ['Stories.count'], timeDimensions: [{dimension: 'Stories.time', granularity: 'month', dateRange: ["2015-01-01", "2015-08-01"] }]
-        }}
-        codeExample={
-          `
-import { default as cubejs, chartjsConfig } from 'cubejs-client';
-import { QueryRenderer } from '@cubejs-client/react';
-import { Line } from 'react-chartjs-2';
-
-ReactDOM.render(
-  <QueryRenderer
-    query={{
-      measures: ['Stories.count'], dimensions: ['Stories.date.month']
-    }}
-    cubejsApi={cubejs('API_KEY')}
-    render={ ({ resultSet }) => {
-      const { data, options } = chartjsConfig('line', resultSet)
-      return <Line data={data} options={options} />
-    }
-  />
-)
-          `
+    render: renderExample('line', {
+      measures: ['Stories.count'],
+      timeDimensions: [
+        {
+          dimension: 'Stories.time',
+          granularity: 'month',
+          dateRange: ["2015-01-01", "2015-08-01"]
         }
-        render={ ({ resultSet }) => renderChart(resultSet, 'line') }
-      />
-    )
+      ]
+    })
   },
   bar: {
     title: 'Bar',
-    render: () => (
-      <Example
-        query={{
-          measures: ['Stories.count', 'Stories.totalScore'],
-          timeDimensions: [{ dimension: 'Stories.time', granularity: 'month', dateRange: ["2015-01-01", "2015-08-01"] }]
-        }}
-        render={ ({ resultSet }) => renderChart(resultSet, 'bar') }
-      />
-    )
+    render: renderExample('bar', {
+      measures: ['Stories.count', 'Stories.totalScore'],
+      timeDimensions: [
+        {
+          dimension: 'Stories.time',
+          granularity: 'month',
+          dateRange: ["2015-01-01", "2015-08-01"] }
+      ]
+    })
   },
   pie: {
     title: 'Pie',
-    render: () => (
-      <Example
-        query={{ measures: ['Stories.count'], dimensions: ['Stories.category'] }}
-        render={ ({ resultSet }) => renderChart(resultSet, 'pie') }
-      />
-    )
+    render: renderExample('pie', {
+      measures: ['Stories.count'],
+      dimensions: ['Stories.category']
+    })
   }
 };
 
