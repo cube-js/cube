798c5d780a93041581fc6daa994acfee5d3c58f1
Artyom Keydunov <artyom.keydunov@gmail.com>
2018-09-27 15:16:14 +0300

    More Examples

--- examples/react-chartjs-2/index.js
+++ examples/react-chartjs-2/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Layout, Row, Col, Menu } from 'antd';
+import { Layout, Row, Col, Menu, Icon } from 'antd';
 import chartsExamples from './chartsExamples';
 import 'antd/dist/antd.css';
 import './style.css';
+
 
 const { Header, Footer, Sider, Content } = Layout;
 class App extends React.Component {
@@ -29,8 +30,20 @@ class App extends React.Component {
             defaultSelectedKeys={[this.state.activeChart]}
             style={{ lineHeight: '64px' }}
           >
-            <Menu.Item key="line">Line</Menu.Item>
-            <Menu.Item key="bar">Bar</Menu.Item>
+            <Menu.SubMenu title={<span className="submenu-title-wrapper"><Icon type="line-chart" />Line</span>}>
+              <Menu.Item key="line">Time series</Menu.Item>
+              <Menu.Item key="lineMulti">Multi axis</Menu.Item>
+            </Menu.SubMenu>
+            <Menu.SubMenu
+              title={
+                <span className="submenu-title-wrapper">
+                  <Icon type="bar-chart" />Bar
+                </span>
+              }
+            >
+              <Menu.Item key="bar">Basic</Menu.Item>
+              <Menu.Item key="barStacked">Stacked</Menu.Item>
+            </Menu.SubMenu>
             <Menu.Item key="pie">Pie</Menu.Item>
           </Menu>
         </Header>
