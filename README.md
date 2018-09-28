1e180c875448be1735aaaab5158030678782cceb
Pavel Tiunov <pavel.tiunov@gmail.com>
2018-09-28 11:28:33 +0300

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 
 <p align="center"><a href="https://www.statsbot.co"><img src="https://i.imgur.com/zYHXm4o.png" alt="Cube.js" width="340px"></a></p>
+
+## Installation
+
+Vanilla JS:
+
+```
+npm i --save @cubejs-client/core
+```
+
+React:
+
+```
+npm i --save @cubejs-client/core
+npm i --save @cubejs-client/react
+```
+
+## Getting Started
+
+Instantiate Cube.js API:
+
+```
+const cubejsApi = cubejs('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpIjozODU5NH0.5wEbQo-VG2DEjR2nBpRpoJeIcE_oJqnrm78yUo9lasw');
+```
+
+Please email info@statsbot.co to obtain API key.
+
+Use load API to fetch data:
+
+```
+cubejsApi.load({
+  measures: ['Stories.count'],
+  timeDimensions: [{
+    dimension: 'Stories.time',
+    dateRange: ['2015-01-01', '2015-12-31'],
+    granularity: 'month'
+  }]
+})
+  .then(r => {
+    const context = document.getElementById("myChart");
+    new Chart(context, chartjsConfig(r));
+  });
+```
+
+## API
+
+### cubejs(apiKey)
+
+Create instance of CubejsApi.
+
+- `apiKey` - API key used to authorize requests and determine SQL database you're accessing. Please email info@statsbot.co to obtain API key.
--- examples/vanilla.js
+++ examples/vanilla.js
@@ -1,40 +1,15 @@
 'use strict';
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: false };
-    this.api = cubejs('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpIjozODU5NH0.5wEbQo-VG2DEjR2nBpRpoJeIcE_oJqnrm78yUo9lasw');
-  }
-
-  render() {
-    if (this.state.result) {
-      return this.state.result;
-    }
-    if (this.state.loading) {
-      return 'Loading...';
-    }
-
-    return <button onClick={() => this.load()}>Load</button>;
-  }
-
-  load() {
-    this.setState({ loading: true });
-    this.api.load({
-      measures: ["Stories.count"],
-      timeDimensions: [{
-        dimension: "Stories.time",
-        dateRange: ["2015-01-01", "2016-01-01"],
-        granularity: 'month'
-      }]
-    })
-      .then(r => {
-        const context = document.getElementById("myChart");
-        const config = chartjsConfig(r);
-        this.setState({ result: JSON.stringify(config) });
-        new Chart(context, config);
-      })
-  }
-}
-
-ReactDOM.render(<LikeButton/>, document.getElementById("root"));
+const cubejsApi = cubejs('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpIjozODU5NH0.5wEbQo-VG2DEjR2nBpRpoJeIcE_oJqnrm78yUo9lasw');
+cubejsApi.load({
+  measures: ["Stories.count"],
+  timeDimensions: [{
+    dimension: "Stories.time",
+    dateRange: ["2015-01-01", "2016-01-01"],
+    granularity: 'month'
+  }]
+})
+  .then(r => {
+    const context = document.getElementById("myChart");
+    new Chart(context, chartjsConfig(r));
+  });
