3b6c3ad3b5834793811a4d2e7561500f1a6cfc82
Pavel Tiunov <pavel.tiunov@gmail.com>
2018-12-12 20:48:42 +0300

    AWS Web analytics example: source and descriptions

--- examples/aws-web-analytics/analytics-dashboard/src/App.js
+++ examples/aws-web-analytics/analytics-dashboard/src/App.js
@@ -7,7 +7,9 @@ import { QueryRenderer } from '@cubejs-client/react';
 import { Spin } from 'antd';
 import { Chart, Axis, Tooltip, Geom, Coord, Legend } from 'bizcharts';
 import moment from 'moment';
+import GithubCorner from 'react-github-corner';
 import { trackPageView } from './track';
+import PrismCode from './PrismCode';
 
 const dateRange = [
   moment().subtract(14,'d').format('YYYY-MM-DD'),
@@ -62,6 +64,117 @@ const renderPieChart = (resultSet, measure) => (
   </Chart>
 );
 
+const trackingCode = `import { fetch } from 'whatwg-fetch';
+import cookie from 'component-cookie';
+import uuidv4 from 'uuid/v4';
+
+const trackPageView = () => {
+  if (!cookie('aws_web_uid')) {
+    cookie('aws_web_uid', uuidv4());
+  }
+  fetch(
+    'https://4bfydqnx8i.execute-api.us-east-1.amazonaws.com/dev/collect',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        url: window.location.href,
+        referrer: document.referrer,
+        anonymousId: cookie('aws_web_uid'),
+        eventType: 'pageView'
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+}`;
+
+const lambdaCode = `const AWS = require('aws-sdk');
+const { promisify } = require('util');
+
+const kinesis = new AWS.Kinesis();
+
+const putRecord = promisify(kinesis.putRecord.bind(kinesis));
+
+const response = (body, status) => {
+  return {
+    statusCode: status || 200,
+    body: body && JSON.stringify(body),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Content-Type': 'application/json'
+    }
+  }
+}
+
+module.exports.collect = async (event, context) => {
+  const body = JSON.parse(event.body);
+  if (!body.anonymousId || !body.url || !body.eventType) {
+    return response({
+      error: 'anonymousId, url and eventType required'
+    }, 400);
+  }
+
+  await putRecord({
+    Data: JSON.stringify({
+      anonymous_id: body.anonymousId,
+      url: body.url,
+      event_type: body.eventType,
+      referrer: body.referrer,
+      timestamp: (new Date()).toISOString(),
+      source_ip: event.requestContext.identity.sourceIp,
+      user_agent: event.requestContext.identity.userAgent
+    }) + '\\n',
+    PartitionKey: body.anonymousId,
+    StreamName: 'event-collection'
+  });
+
+  return response();
+};`;
+
+const schemaCode = `cube(\`PageViews\`, {
+  sql: \`select * from aws_web_analytics.aws_web_analytics_event_collection\`,
+
+  measures: {
+    count: {
+      type: \`count\`
+    },
+
+    userCount: {
+      sql: \`anonymous_id\`,
+      type: \`countDistinct\`,
+    }
+  },
+
+  dimensions: {
+    url: {
+      sql: \`url\`,
+      type: \`string\`
+    },
+
+    anonymousId: {
+      sql: \`anonymous_id\`,
+      type: \`string\`
+    },
+
+    eventType: {
+      sql: \`event_type\`,
+      type: \`string\`
+    },
+
+    referrer: {
+      sql: \`referrer\`,
+      type: \`string\`
+    },
+
+    timestamp: {
+      sql: \`from_iso8601_timestamp(timestamp)\`,
+      type: \`time\`
+    }
+  }
+});`;
+
 const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpIjo0MDY3OH0.Vd-Qu4dZ95rVy9pKkyzy6Uxc5D-VOdTidCWYUVhKpYU';
 
 class App extends Component {
@@ -70,13 +183,13 @@ class App extends Component {
   }
 
   render() {
-    return (
+    return [
       <Layout>
           <Header>
             <h2 style={{ color: '#fff' }}>AWS Web Analytics Dashboard</h2>
           </Header>
           <Content style={{ padding: '25px', margin: '25px' }}>
-            <Row type="flex" justify="space-around" align="middle" gutter={24}>
+            <Row type="flex" justify="space-around" align="top" gutter={24}>
               <Col lg={12} md={24}>
                 <Card title="Page Views" style={{ marginBottom: '24px' }}>
                   <QueryRenderer
@@ -170,15 +283,31 @@ class App extends Component {
                   />
                 </Card>
               </Col>
-              <Col span={24}>
+              <Col lg={12} md={24}>
                 <Card title="Architecture" style={{ marginBottom: '24px', textAlign: 'center' }}>
                   <img src="/architecture.png" style={{ width: '100%', maxWidth: '500px' }}/>
                 </Card>
               </Col>
+              <Col lg={12} md={24}>
+                <Card title="Page contains this tracking code" style={{ marginBottom: '24px' }}>
+                  <PrismCode code={trackingCode}/>
+                </Card>
+              </Col>
+              <Col lg={12} md={24}>
+                <Card title="Events collected using this Lambda function" style={{ marginBottom: '24px' }}>
+                  <PrismCode code={lambdaCode}/>
+                </Card>
+              </Col>
+              <Col lg={12} md={24}>
+                <Card title="Cube.js schema used for analytic querying" style={{ marginBottom: '24px' }}>
+                  <PrismCode code={schemaCode}/>
+                </Card>
+              </Col>
             </Row>
           </Content>
-        </Layout>
-    );
+        </Layout>,
+        <GithubCorner size={120} href="https://github.com/statsbotco/cubejs-client/tree/master/examples/aws-web-analytics" />
+    ];
   }
 }
 
