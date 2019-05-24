545a02037e1f27272d19f0a027101d575d673b57
Pavel Tiunov <pavel.tiunov@gmail.com>
2019-05-24 13:05:10 +0300

    feat: better npm fail message in Playground

--- packages/cubejs-server-core/core/DevServer.js
+++ packages/cubejs-server-core/core/DevServer.js
@@ -9,7 +9,7 @@ const executeCommand = (command, args, options) => {
   return new Promise((resolve, reject) => {
     child.on('close', code => {
       if (code !== 0) {
-        reject(new Error(`${command} ${args.join(' ')} failed with exit code ${code}`));
+        reject(new Error(`${command} ${args.join(' ')} failed with exit code ${code}. Please check your console.`));
         return;
       }
       resolve();
