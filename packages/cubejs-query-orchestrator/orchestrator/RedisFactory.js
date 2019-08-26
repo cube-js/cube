55858cf7260017b76c9b36a8af8949c75330f73c
Pavel Tiunov <pavel.tiunov@gmail.com>
2019-08-26 14:48:02 -0700

    feat: `REDIS_TLS=true` env variable support

--- packages/cubejs-query-orchestrator/orchestrator/RedisFactory.js
+++ packages/cubejs-query-orchestrator/orchestrator/RedisFactory.js
@@ -4,11 +4,22 @@ const { promisify } = require('util');
 module.exports = function createRedisClient(url) {
   redis.Multi.prototype.execAsync = promisify(redis.Multi.prototype.exec);
 
-  const client = redis.createClient(url);
+  let options;
+
+  if (process.env.REDIS_TLS === 'true') {
+    options = {
+      url,
+      tls: {}
+    };
+  }
+
+  const client = redis.createClient(options || url);
 
   ['brpop', 'del', 'get', 'hget', 'rpop', 'set', 'zadd', 'zrange', 'zrangebyscore'].forEach(
-    k => client[`${k}Async`] = promisify(client[k])
+    k => {
+      client[`${k}Async`] = promisify(client[k]);
+    }
   );
 
   return client;
-}
+};
