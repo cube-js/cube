3870ba07314d31ca8f03e61e6c37304acadb7c78
philippefutureboy <philippe@human.space>
2019-09-26 15:39:35 -0400

    chore(packages/cubejs-server): Removed dependency on config/env script

--- packages/cubejs-server/scripts/test.js
+++ packages/cubejs-server/scripts/test.js
@@ -13,7 +13,7 @@ process.on("unhandledRejection", (err) => {
 });
 
 // Ensure environment variables are read.
-require("../config/env");
+require('dotenv').config()
 
 const jest = require("jest");
 const execSync = require("child_process").execSync;
