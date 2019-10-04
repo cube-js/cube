fd6ea4d019fbd2ab4ef2f5a9ef944e3e38288e4e
Pavel Tiunov <pavel.tiunov@gmail.com>
2019-10-03 20:36:23 -0700

    Bump playground version

--- packages/cubejs-server-core/core/DevServer.js
+++ packages/cubejs-server-core/core/DevServer.js
@@ -1,5 +1,5 @@
 /* eslint-disable global-require */
-// Playground version: 0.10.55
+// Playground version: 0.10.56
 const fs = require('fs-extra');
 const path = require('path');
 const spawn = require('cross-spawn');
