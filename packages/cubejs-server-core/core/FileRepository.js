40f520ec71af5a65de41b6bbef8dbf152e060cbd
Lucas Silva <lucas_s.s@hotmail.com>
2019-10-09 17:57:23 -0300

    add recursive read files into path

--- packages/cubejs-server-core/core/FileRepository.js
+++ packages/cubejs-server-core/core/FileRepository.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const fs = require('fs-extra');
-const R = require('ramda');
+const path = require("path");
+const fs = require("fs-extra");
+const R = require("ramda");
 
 class FileRepository {
   constructor(repositoryPath) {
@@ -25,7 +25,6 @@ class FileRepository {
   async dataSchemaFiles(includeDependencies) {
     const self = this;
     const files = await self.getFiles(self.localPath());
-
     let result = await Promise.all(
       files
         .filter(file => R.endsWith(".js", file))
@@ -41,34 +40,40 @@ class FileRepository {
   }
 
   async readModules() {
-    const packageJson = JSON.parse(await fs.readFile('package.json', 'utf-8'));
-    const files = await Promise.all(Object.keys(packageJson.dependencies).map(async module => {
-      if (R.endsWith('-schema', module)) {
-        return this.readModuleFiles(path.join('node_modules', module));
-      }
-      return [];
-    }));
+    const packageJson = JSON.parse(await fs.readFile("package.json", "utf-8"));
+    const files = await Promise.all(
+      Object.keys(packageJson.dependencies).map(async module => {
+        if (R.endsWith("-schema", module)) {
+          return this.readModuleFiles(path.join("node_modules", module));
+        }
+        return [];
+      })
+    );
     return files.reduce((a, b) => a.concat(b));
   }
 
   async readModuleFiles(modulePath) {
     const files = await fs.readdir(modulePath);
-    return (await Promise.all(files
-      .map(async file => {
+    return (await Promise.all(
+      files.map(async file => {
         const fileName = path.join(modulePath, file);
         const stats = await fs.lstat(fileName);
         if (stats.isDirectory()) {
           return this.readModuleFiles(fileName);
-        } else if (R.endsWith('.js', file)) {
+        } else if (R.endsWith(".js", file)) {
           const content = await fs.readFile(fileName, "utf-8");
-          return [{
-            fileName, content, readOnly: true
-          }]
+          return [
+            {
+              fileName,
+              content,
+              readOnly: true
+            }
+          ];
         } else {
           return [];
         }
       })
-    )).reduce((a, b) => a.concat(b), [])
+    )).reduce((a, b) => a.concat(b), []);
   }
 }
 
