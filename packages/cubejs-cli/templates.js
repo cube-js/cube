fd9d872073f064ef33bc12161c3937f060d36ece
Pavel Tiunov <pavel.tiunov@gmail.com>
2019-11-01 10:11:11 -0700

    fix: Catch unhandled rejections on server starts

--- packages/cubejs-cli/templates.js
+++ packages/cubejs-cli/templates.js
@@ -4,6 +4,9 @@ const server = new CubejsServer();
 
 server.listen().then(({ port }) => {
   console.log(\`🚀 Cube.js server is listening on \${port}\`);
+}).catch(e => {
+  console.error('Fatal error during server start: ');
+  console.error(e.stack || e);
 });
 `;
 
