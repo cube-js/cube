6281fdba16f58117925109f1ee12efd879166480
Justin-ZS <jzstxdy@gmail.com>
2019-11-17 17:40:53 +0800

    test: add simple tests for the pre-aggregation in time hierarchy

--- packages/cubejs-schema-compiler/test/PreAggregationsTest.js
+++ packages/cubejs-schema-compiler/test/PreAggregationsTest.js
@@ -590,3 +590,157 @@ describe('PreAggregations', function test() {
     });
   });
 });
+
+
+describe('PreAggregations in time hierarchy', function test() {
+  this.timeout(20000);
+
+  after(async () => {
+    await dbRunner.tearDown();
+  });
+
+  const { compiler, joinGraph, cubeEvaluator } = prepareCompiler(`
+    cube(\`visitors\`, {
+      sql: \`
+      select * from visitors
+      \`,
+
+      measures: {
+        count: {
+          type: 'count'
+        }
+      },
+
+      dimensions: {
+        createdAt: {
+          type: 'time',
+          sql: 'created_at'
+        },
+      },
+      
+      preAggregations: {
+        month: {
+          type: 'rollup',
+          measureReferences: [count],
+          timeDimensionReference: createdAt,
+          granularity: 'month',
+        },
+        day: {
+          type: 'rollup',
+          measureReferences: [count],
+          timeDimensionReference: createdAt,
+          granularity: 'day',
+        },
+      }
+    })
+    `);
+
+  function replaceTableName(query, preAggregation, suffix) {
+    const [toReplace, params] = query;
+    console.log(toReplace);
+    preAggregation = Array.isArray(preAggregation) ? preAggregation : [preAggregation];
+    return [
+      preAggregation.reduce((replacedQuery, desc) =>
+        replacedQuery.replace(new RegExp(desc.tableName, 'g'), desc.tableName + '_' + suffix), toReplace
+      ),
+      params
+    ];
+  }
+
+  function tempTablePreAggregations(preAggregationsDescriptions) {
+    return R.unnest(preAggregationsDescriptions.map(desc =>
+      desc.invalidateKeyQueries.concat([
+        [desc.loadSql[0].replace('CREATE TABLE', 'CREATE TEMP TABLE'), desc.loadSql[1]]
+      ])
+    ));
+  }
+
+  it('query on year match to pre-agg on month', () => {
+    return compiler.compile().then(() => {
+      const query = new PostgresQuery({ joinGraph, cubeEvaluator, compiler }, {
+        measures: [
+          'visitors.count'
+        ],
+        dimensions: [],
+        timezone: 'America/Los_Angeles',
+        timeDimensions: [{
+          dimension: 'visitors.createdAt',
+          granularity: 'year',
+          dateRange: ['2016-12-30', '2018-12-30']
+        }],
+        preAggregationsSchema: '',
+        order: [],
+      });
+
+      const queryAndParams = query.buildSqlAndParams();
+
+      query.preAggregations.preAggregationForQuery.preAggregation.granularity.should.be.equal('month');
+
+      console.log(queryAndParams);
+      const preAggregationsDescription = query.preAggregations.preAggregationsDescription();
+      console.log(preAggregationsDescription);
+
+      const queries = tempTablePreAggregations(preAggregationsDescription);
+
+      console.log(JSON.stringify(queries.concat(queryAndParams)));
+
+      return dbRunner.testQueries(
+        queries.concat([queryAndParams]).map(q => replaceTableName(q, preAggregationsDescription, 1))
+      ).then(res => {
+        console.log(JSON.stringify(res));
+        res.should.be.deepEqual(
+          [
+            {
+              "visitors__count": "5",
+              "visitors__created_at_year": "2017-01-01T00:00:00.000Z"
+            },
+          ]
+        );
+      });
+    });
+  });
+  it('query on week match to pre-agg on day', () => {
+    return compiler.compile().then(() => {
+      const query = new PostgresQuery({ joinGraph, cubeEvaluator, compiler }, {
+        measures: [
+          'visitors.count'
+        ],
+        dimensions: [],
+        timezone: 'America/Los_Angeles',
+        timeDimensions: [{
+          dimension: 'visitors.createdAt',
+          granularity: 'week',
+          dateRange: ['2017-01-02', '2019-02-08']
+        }],
+        preAggregationsSchema: '',
+        order: [],
+      });
+
+      const queryAndParams = query.buildSqlAndParams();
+
+      query.preAggregations.preAggregationForQuery.preAggregation.granularity.should.be.equal('day');
+
+      console.log(queryAndParams);
+      const preAggregationsDescription = query.preAggregations.preAggregationsDescription();
+      console.log(preAggregationsDescription);
+
+      const queries = tempTablePreAggregations(preAggregationsDescription);
+
+      console.log(JSON.stringify(queries.concat(queryAndParams)));
+
+      return dbRunner.testQueries(
+        queries.concat([queryAndParams]).map(q => replaceTableName(q, preAggregationsDescription, 1))
+      ).then(res => {
+        console.log(JSON.stringify(res));
+        res.should.be.deepEqual(
+          [
+            {
+              "visitors__count": "5",
+              "visitors__created_at_week": "2017-01-02T00:00:00.000Z"
+            },
+          ]
+        );
+      });
+    });
+  });
+});
