94337d8cdcc4adb1dfef9d7daaa07c608747ba6e
Pavel Tiunov <pavel.tiunov@gmail.com>
2019-12-15 22:49:59 -0800

    Bump commit

--- packages/cubejs-serverless/index.js
+++ packages/cubejs-serverless/index.js
@@ -3,8 +3,6 @@ const ProviderToHandlerPackage = {
   google: '@cubejs-backend/serverless-google'
 };
 
-// bump commit
-
 const platform = process.env.CUBEJS_SERVERLESS_PLATFORM || process.env.SERVERLESS_EXPRESS_PLATFORM || 'aws';
 const handlerPackage = ProviderToHandlerPackage[platform];
 
