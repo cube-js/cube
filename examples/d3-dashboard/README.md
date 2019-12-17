5f51ac1c801134896136030813d05d7e38fe8a2f
Artyom Keydunov <artyom.keydunov@gmail.com>
2019-12-16 22:22:00 -0800

    D3 dashboard example

---
+++ examples/d3-dashboard/README.md
@@ -0,0 +1,4 @@
+### D3 Dashboard Example
+
+A dashboard app built with Cube.js and D3. It was generated in Cube.js Playground with
+React, Material UI and D3 templates.
---
+++ examples/d3-dashboard/dashboard-app/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./body.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { Layout } from "antd";
+import cubejs from "@cubejs-client/core";
+import { CubeProvider } from "@cubejs-client/react";
+import Header from "./components/Header";
+const API_URL = "http://localhost:4000";
+const CUBEJS_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NzYzMDIyNjIsImV4cCI6MTU3NjM4ODY2Mn0._lhZS_L33fK8U_30X0iH7DTQiPRJYdDgnj5Xefzrgok";
+const cubejsApi = cubejs(CUBEJS_TOKEN, {
+  apiUrl: `${API_URL}/cubejs-api/v1`
+});
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  }
+}));
+
+const AppLayout = ({ children }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Header />
+      <div>{children}</div>
+    </div>
+  );
+};
+
+const App = ({ children }) => (
+  <CubeProvider cubejsApi={cubejsApi}>
+    <AppLayout>{children}</AppLayout>
+  </CubeProvider>
+);
+
+export default App;
