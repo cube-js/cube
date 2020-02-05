66e2fe5a4faa8e25bcc872a215479b3bde4fc774
Luc Vauvillier <luc.vauvillier@gmail.com>
2020-02-05 10:47:34 +0100

    fix: Fix typescript type definition

--- packages/cubejs-server-core/core/index.d.ts
+++ packages/cubejs-server-core/core/index.d.ts
@@ -1,42 +1,91 @@
 declare module '@cubejs-backend/server-core' {
   export function create(options?: CreateOptions): any;
 
-  export function createDriver(dbType: DatabaseType): DriverFactory;
-
   export interface CreateOptions {
-    apiSecret?: string;
-    basePath?: string;
-    checkAuthMiddleware?: (req: any, res: any, next: any) => any;
-    queryTransformer?: (query: any, context: any) => any;
-    extendContext?: (req: any) => any;
-    contextToAppId?: any;
-    contextToDataSourceId?: (context: any) => string;
-    devServer?: boolean;
-    dbType?: DatabaseType;
-    driverFactory?: DriverFactory;
-    externalDriverFactory?: DriverFactory;
-    logger?: (msg: string, params: any) => void;
-    orchestratorOptions?: any;
-    preAggregationsSchema?: string | (() => string)
-    repositoryFactory?: (context: any) => SchemaFileRepository;
+    dbType?: DatabaseType | ((context: RequestContext) => DatabaseType);
+    externalDbType?: DatabaseType | ((context: RequestContext) => DatabaseType);
     schemaPath?: string;
+    basePath?: string;
+    devServer?: boolean;
+    apiSecret?: string;
+    logger?: (msg: string, params: any) => void;
+    driverFactory?: (context: DriverContext) => any;
+    externalDriverFactory?: (context: RequestContext) => any;
+    contextToAppId?: (context: RequestContext) => string;
+    contextToDataSourceId?: (context: RequestContext) => string;
+    repositoryFactory?: (context: RequestContext) => SchemaFileRepository;
+    checkAuthMiddleware?: (req: any, res: any, next: any) => any;
+    queryTransformer?: (query: any, context: RequestContext) => any;
+    preAggregationsSchema?: String | ((context: RequestContext) => string);
+    schemaVersion?: (context: RequestContext) => string;
+    extendContext?: (req: any) => any;
+    scheduledRefreshTimer?: boolean | number;
     compilerCacheSize?: number;
     maxCompilerCacheKeepAlive?: number;
     updateCompilerCacheKeepAlive?: boolean;
-    schemaVersion?: (context: any) => string;
+    telemetry?: boolean;
+    allowUngroupedWithoutPrimaryKey?: boolean;
+    orchestratorOptions?: OrchestratorOptions;
   }
 
-  export interface DriverFactory {
+  export interface OrchestratorOptions {
+    redisPrefix: string;
+    queryCacheOptions: QueryCacheOptions;
+    preAggregationsOptions: PreAggregationsOptions;
   }
 
-  export type DatabaseType = 'athena' | 'bigquery' | 'clickhouse' | 'jdbc' | 'mongobi' | 'mssql' | 'mysql' | 'postgres' | 'redshift';
-
-  export interface SchemaFileRepository {
-    dataSchemaFiles(): Promise<SchemaFileContent[]>;
+  export interface QueryCacheOptions {
+    refreshKeyRenewalThreshold: number;
+    backgroundRenew: boolean;
+    queueOptions: QueueOptions;
   }
 
-  export interface SchemaFileContent {
+  export interface PreAggregationsOptions {
+    queueOptions: QueueOptions;
+  }
+
+  export interface QueueOptions {
+    concurrency: number;
+    continueWaitTimeout: number;
+    executionTimeout: number;
+    orphanedTimeout: number;
+    heartBeatInterval: number;
+  }
+
+  export interface RequestContext {
+    authInfo: any;
+    requestId: string;
+  }
+
+  export interface DriverContext extends RequestContext {
+    dataSource: string;
+  }
+
+  export interface FileContent {
     fileName: string;
     content: string;
   }
+
+  export interface SchemaFileRepository {
+    dataSchemaFiles: () => Promise<FileContent[]>;
+  }
+
+  export interface DriverFactory {}
+
+  export type DatabaseType =
+    | "athena"
+    | "bigquery"
+    | "clickhouse"
+    | "jdbc"
+    | "hive"
+    | "mongobi"
+    | "mssql"
+    | "mysql"
+    | "odelasticsearch"
+    | "oracle"
+    | "postgres"
+    | "prestodb"
+    | "redshift"
+    | "snowflake"
+    | "sqlite";
 }
