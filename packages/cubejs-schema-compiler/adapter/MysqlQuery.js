dc553b954e2bb65c4dc7c833bea18e462aa114dd
Pavel Tiunov <pavel.tiunov@gmail.com>
2020-02-10 23:36:23 -0800

    fix: Invalid Date: Incorrect MySQL minutes granularity

--- packages/cubejs-schema-compiler/adapter/MysqlQuery.js
+++ packages/cubejs-schema-compiler/adapter/MysqlQuery.js
@@ -7,8 +7,8 @@ const GRANULARITY_TO_INTERVAL = {
   day: (date) => `DATE_FORMAT(${date}, '%Y-%m-%dT00:00:00.000')`,
   week: (date) => `DATE_FORMAT(date_add('1900-01-01', interval TIMESTAMPDIFF(WEEK, '1900-01-01', ${date}) WEEK), '%Y-%m-%dT00:00:00.000')`,
   hour: (date) => `DATE_FORMAT(${date}, '%Y-%m-%dT%H:00:00.000')`,
-  minute: (date) => `DATE_FORMAT(${date}, '%Y-%m-%dT%H:%M:00.000')`,
-  second: (date) => `DATE_FORMAT(${date}, '%Y-%m-%dT%H:%M:%S.000')`,
+  minute: (date) => `DATE_FORMAT(${date}, '%Y-%m-%dT%H:%i:00.000')`,
+  second: (date) => `DATE_FORMAT(${date}, '%Y-%m-%dT%H:%i:%S.000')`,
   month: (date) => `DATE_FORMAT(${date}, '%Y-%m-01T00:00:00.000')`,
   year: (date) => `DATE_FORMAT(${date}, '%Y-01-01T00:00:00.000')`
 };
