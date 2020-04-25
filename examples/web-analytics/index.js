a03a8632600b4ec28a5a40212b45c2eb5b9a6905
jcw- <jc.w@getjobber.com>
2020-04-25 02:41:53 -0600

    Update examples to log version

--- examples/web-analytics/index.js
+++ examples/web-analytics/index.js
@@ -1,16 +1,16 @@
 const CubejsServerCore = require('@cubejs-backend/server-core');
 const MySQLDriver = require('@cubejs-backend/mysql-driver');
 const express = require('express');
-const bodyParser = require("body-parser");
-const path = require("path");
-const http = require("http");
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
 const serveStatic = require('serve-static');
 const session = require('express-session');
 const passport = require('passport');
 require('dotenv').config();
 
-var app = express();
-app.use(bodyParser.json({ limit: "50mb" }));
+const app = express();
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(require('cors')());
 
 const cubejsServer = CubejsServerCore.create({
@@ -35,29 +35,25 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 if (process.env.GOOGLE_AUTH_DOMAIN) {
-  const GoogleStrategy = require('passport-google-oauth20').Strategy;
+  const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
+  const authURL = process.env.NODE_ENV === 'production' ? `${process.env.GOOGLE_AUTH_REDIRECT}` : 'http://localhost:4000';
   passport.use(new GoogleStrategy({
-      clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
-      callbackURL: (
-        process.env.NODE_ENV === 'production' ?
-        `${process.env.GOOGLE_AUTH_REDIRECT}` :
-        'http://localhost:4000'
-      ) + '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, cb) => {
-      if (profile.emails && profile.emails.find(e => e.value.match(new RegExp(`@${process.env.GOOGLE_AUTH_DOMAIN}$`)) && e.verified)) {
-        return cb(null, profile);
-      }
-      return cb(`${profile.emails && profile.emails[0] && profile.emails[0].value} not within @${process.env.GOOGLE_AUTH_DOMAIN}`);
+    clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+    callbackURL: `${authURL}/auth/google/callback`
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    if (profile.emails && profile.emails.find(e => e.value.match(new RegExp(`@${process.env.GOOGLE_AUTH_DOMAIN}$`)) && e.verified)) {
+      return cb(null, profile);
     }
-  ));
+    return cb(`${profile.emails && profile.emails[0] && profile.emails[0].value} not within @${process.env.GOOGLE_AUTH_DOMAIN}`);
+  }));
 
-  passport.serializeUser(function(user, done) {
+  passport.serializeUser((user, done) => {
     done(null, user);
   });
 
-  passport.deserializeUser(function(user, done) {
+  passport.deserializeUser((user, done) => {
     done(null, user);
   });
 
@@ -66,11 +62,10 @@ if (process.env.GOOGLE_AUTH_DOMAIN) {
 
   app.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/auth/google' }),
-    function(req, res) {
+    (req, res) => {
       // Successful authentication, redirect home.
       res.redirect('/');
-    }
-  );
+    });
 
   app.use((req, res, next) => {
     if (!req.user) {
@@ -91,5 +86,5 @@ const port = process.env.PORT || 4000;
 const server = http.createServer({}, app);
 
 server.listen(port, () => {
-  console.log(`🚀 Cube.js server is listening on ${port}`);
+  console.log(`🚀 Cube.js server (${CubejsServerCore.version()}) is listening on ${port}`);
 });
