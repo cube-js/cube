9619c20c1d4960522114d5387768fe107ca17821
jcw- <jc.w@getjobber.com>
2020-04-25 01:24:31 -0600

    Use version returned from listen

--- packages/cubejs-server/bin/dev-server.js
+++ packages/cubejs-server/bin/dev-server.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 const CubejsServer = require('../');
-const cubeVersion = require('../package.json').version;
 
 const server = new CubejsServer();
 
-server.listen().then(({ port }) => {
-  console.log(`🚀 Cube.js server (${cubeVersion}) is listening on ${port}`);
+server.listen().then(({ version, port }) => {
+  console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
 }).catch(e => {
   console.error('Fatal error during server start: ');
   console.error(e.stack || e);
