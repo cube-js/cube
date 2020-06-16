76498a9514c78e8fd376bbde8281e1c4c9ceca1b
Leonid Yakovlev <leonid@cube.dev>
2020-06-16 12:56:43 +0300

    Examples: Material UI Dashboard | BrowserRouter added /#/

--- examples/react-material-dashboard/dashboard-app/src/App.js
+++ examples/react-material-dashboard/dashboard-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { Chart } from 'react-chartjs-2';
 import { ThemeProvider } from '@material-ui/styles';
@@ -19,9 +19,9 @@ export default class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <Router history={browserHistory}>
+        <BrowserRouter history={browserHistory} basename="/#/">
           <Routes />
-        </Router>
+        </BrowserRouter>
       </ThemeProvider>
     );
   }
