e2083c84d6408e6f24d5c4a149c8e5e9175591b2
Alex.V <vaspiring@gmail.com>
2020-06-24 21:10:50 +0200

    feat(cubejs-client-react): Exposing updateQuery method (#751)
    * feat(cubejs-client-react): Exposing updateQuery method
    
    * linter fix

--- packages/cubejs-client-react/src/QueryBuilder.jsx
+++ packages/cubejs-client-react/src/QueryBuilder.jsx
@@ -179,7 +179,7 @@ export default class QueryBuilder extends React.Component {
         (m) => m.type === 'time'
       ),
       availableSegments: (meta && meta.membersForQuery(query, 'segments')) || [],
-
+      updateQuery: (queryUpdate) => this.updateQuery(queryUpdate),
       updateMeasures: updateMethods('measures'),
       updateDimensions: updateMethods('dimensions'),
       updateSegments: updateMethods('segments'),
