558d4e25e0e55373258aad7721b6b3821b7b202a
A.Vasilev <vaspiring@gmail.com>
2020-07-01 13:37:07 +0300

    docs(cubejs-client-react): Migration to typedoc

---
+++ packages/cubejs-client-react/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * @title @cubejs-client/react
+ * @permalink /@cubejs-client-react
+ * @category Cube.js Frontend
+ * @subcategory Reference
+ * @menuOrder 3
+ * @description `@cubejs-client/react` provides React Components for easy Cube.js integration in a React app.
+ */
+
+declare module '@cubejs-client/react' {
+
+}
