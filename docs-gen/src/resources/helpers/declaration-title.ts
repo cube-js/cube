9960392944d089df3721271c187fbdcdeac18421
A.Vasilev <vaspiring@gmail.com>
2020-07-03 17:36:05 +0300

    fix(docs-gen): improvements

--- docs-gen/src/resources/helpers/declaration-title.ts
+++ docs-gen/src/resources/helpers/declaration-title.ts
@@ -1,7 +1,9 @@
-import { DeclarationReflection, ReflectionKind } from 'typedoc';
+import { DeclarationReflection, ReflectionKind, ParameterType } from 'typedoc';
 import { heading } from './heading';
 import { memberSymbol } from './member-symbol';
 import { type } from './type';
+import { ReferenceType, Type, TypeParameterReflection, IntrinsicType } from 'typedoc/dist/lib/models';
+import { TypeReference } from 'typescript';
 
 export function declarationTitle(this: DeclarationReflection, showSymbol: boolean) {
   // if (this.type?.type !== 'union' && this.type?.type !== 'tuple' && this.kind !== ReflectionKind.EnumMember) {
@@ -25,11 +27,25 @@ export function declarationTitle(this: DeclarationReflection, showSymbol: boolea
   }
 
   md.push(`**${this.name}**${isOptional ? '? ' : ''}`);
-  
+
+  if (this.typeHierarchy?.types.length) {
+    const [parent] = this.typeHierarchy.types;
+    if (parent instanceof ReferenceType) {
+      const name = parent.reflection === undefined ? parent.symbolFullyQualifiedName : parent.name;
+
+      md.push('extends');
+      md.push(`**${name}**`);
+
+      if (parent.typeArguments) {
+        md.push(`‹${parent.typeArguments.map((typeArgument) => type.call(typeArgument)).join(', ')}›`.trim());
+      }
+    }
+  }
+
   // We want to display enum members like:
   // • DAY = "day"
   if (this.kind !== ReflectionKind.EnumMember) {
-    md.push(':');
+    md[md.length - 1] += ':';
   }
 
   if (this.type) {
