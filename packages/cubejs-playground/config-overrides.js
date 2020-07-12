5ee6b3ceadd1043475db2d1e96cac26f406999e7
Pavel Tiunov <pavel.tiunov@gmail.com>
2020-07-12 12:39:53 -0700

    Build playground on test

--- packages/cubejs-playground/config-overrides.js
+++ packages/cubejs-playground/config-overrides.js
@@ -6,30 +6,9 @@ module.exports = function override(config, env) {
   config.optimization = {
     minimizer: [
       new TerserPlugin({
-        terserOptions: {
-          parse: {
-            ecma: 8,
-          },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            comparisons: false,
-            inline: 2,
-            drop_console: true,
-          },
-          mangle: {
-            safari10: true,
-          },
-          output: {
-            ecma: 5,
-            comments: false,
-            ascii_only: true,
-          },
-        },
-        parallel: 2,
         cache: true,
-        sourceMap: false,
-        extractComments: false,
+        parallel: true,
+        chunkFilter: (chunk) => chunk.name.indexOf('babel') === -1 && chunk.name.indexOf('vendors') === -1
       })
     ],
     splitChunks: {
