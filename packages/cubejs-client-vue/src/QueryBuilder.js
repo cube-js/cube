32402e6aaaded72dcd1f45313f691d31e09b20b5
Leonid Yakovlev <leonid@cube.dev>
2020-07-14 11:21:45 +0300

    fix(cubejs-client-vue): added deep watch at query props object in Vue QueryBuilder (#818)
    * fix: added deep watch at query props object in Vue QueryBuilder
    
    * fix: build

--- packages/cubejs-client-vue/src/QueryBuilder.js
+++ packages/cubejs-client-vue/src/QueryBuilder.js
@@ -391,15 +391,17 @@ export default {
   },
 
   watch: {
-    query() {
-      if (!this.meta) {
-        // this is ok as if meta has not been loaded by the time query prop has changed,
-        // then the promise for loading meta (found in mounted()) will call
-        // copyQueryFromProps and will therefore update anyway.
-        return;
+    query: {
+      deep: true,
+      handler() {
+        if (!this.meta) {
+          // this is ok as if meta has not been loaded by the time query prop has changed,
+          // then the promise for loading meta (found in mounted()) will call
+          // copyQueryFromProps and will therefore update anyway.
+          return;
+        }
+        this.copyQueryFromProps();
       }
-
-      this.copyQueryFromProps();
     }
   }
 };
