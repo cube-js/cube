28e8a4a0bcc7eaf7232a9f9b119acbf1c8bb204f
Pavel Tiunov <pavel.tiunov@gmail.com>
2020-07-15 11:16:31 -0700

    docs: async apiToken function

--- packages/cubejs-client-core/index.d.ts
+++ packages/cubejs-client-core/index.d.ts
@@ -708,7 +708,7 @@ declare module '@cubejs-client/core' {
    * );
    * ```
    *
-   * @param apiToken - [API token](security) is used to authorize requests and determine SQL database you're accessing. In the development mode, Cube.js Backend will print the API token to the console on on startup. Can be an async function without arguments that returns the API token.
+   * @param apiToken - [API token](security) is used to authorize requests and determine SQL database you're accessing. In the development mode, Cube.js Backend will print the API token to the console on on startup. In case of async function `authorization` is updated for `options.transport` on each request.
    * @order 1
    */
   export default function cubejs(apiToken: string | (() => Promise<string>), options: CubeJSApiOptions): CubejsApi;
