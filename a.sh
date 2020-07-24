54703485d750b8be82cf4322a2c89fb52dcb02f5
DonDonDonDonDonDon <751931087@qq.com>
2020-07-24 09:05:34 +0800

    []

--- a.sh
+++ a.sh
@@ -1,22 +1,2 @@
-#!/bin/sh
 
-git filter-branch --env-filter '
-
-OLD_EMAIL="751931087@qq.com"
-CORRECT_NAME="DonDonDonDonDonDon"
-CORRECT_EMAIL="751931087@qq.com"
-
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-export GIT_COMMITTER_NAME="$CORRECT_NAME"
-export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
-' --tag-name-filter cat -- --branches --tags
-
-
-
+asdfas
