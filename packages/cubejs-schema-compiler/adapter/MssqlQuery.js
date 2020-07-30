854e51f194c05cd184c38f4fe21e75cf5d424f4a
Joshua D. Mentzer <mentzerj@trinity-health.org>
2020-07-30 15:44:26 -0400

    feat(mssql-driver): add readonly aggregation for mssql sources

--- packages/cubejs-schema-compiler/adapter/MssqlQuery.js
+++ packages/cubejs-schema-compiler/adapter/MssqlQuery.js
@@ -29,13 +29,13 @@ class MssqlParamAllocator extends ParamAllocator {
 }
 
 const GRANULARITY_TO_INTERVAL = {
-  day: (date) => `FORMAT(${date}, 'yyyy-MM-ddT00:00:00.000')`,
-  week: (date) => `FORMAT(dateadd(week, DATEDIFF(week, '1900-01-01', ${date}), '1900-01-01'), 'yyyy-MM-ddT00:00:00.000')`,
-  hour: (date) => `FORMAT(${date}, 'yyyy-MM-ddTHH:00:00.000')`,
-  minute: (date) => `FORMAT(${date}, 'yyyy-MM-ddTHH:mm:00.000')`,
-  second: (date) => `FORMAT(${date}, 'yyyy-MM-ddTHH:mm:ss.000')`,
-  month: (date) => `FORMAT(${date}, 'yyyy-MM-01T00:00:00.000')`,
-  year: (date) => `FORMAT(${date}, 'yyyy-01-01T00:00:00.000')`
+  day: (date) => `dateadd(day, DATEDIFF(day, 0, ${date}), 0)`,
+  week: (date) => `dateadd(week, DATEDIFF(week, 0, ${date}), 0)`,
+  hour: (date) => `dateadd(hour, DATEDIFF(hour, 0, ${date}), 0)`,
+  minute: (date) => `dateadd(minute, DATEDIFF(minute, 0, ${date}), 0)`,
+  second: (date) => `CAST(FORMAT(${date}, 'yyyy-MM-ddTHH:mm:ss.000') AS datetime)`, // until SQL 2016, this causes an int overflow; in SQL 2016 these calls can be changed to DATEDIFF_BIG
+  month: (date) => `dateadd(month, DATEDIFF(month, 0, ${date}), 0)`,
+  year: (date) => `dateadd(year, DATEDIFF(year, 0, ${date}), 0)`,
 };
 
 class MssqlFilter extends BaseFilter {
