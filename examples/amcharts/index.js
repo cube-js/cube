a38e028fa6cdebc631523b577568e2ce2e6d869b
Igor Lukanin <me@igorlukanin.io>
2020-08-04 22:51:06 +0300

    Import data from Slack export file to embedded SQLite database. Create cubes for that data

--- examples/amcharts/index.js
+++ examples/amcharts/index.js
@@ -1,10 +1,15 @@
+const importSlackArchive = require('./import');
 const CubejsServer = require('@cubejs-backend/server');
 
-const server = new CubejsServer();
+(async function() {
+    await importSlackArchive(process.argv[2]);
 
-server.listen().then(({ version, port }) => {
-  console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
-}).catch(e => {
-  console.error('Fatal error during server start: ');
-  console.error(e.stack || e);
-});
+    const server = new CubejsServer();
+
+    server.listen().then(({ version, port }) => {
+        console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
+    }).catch(e => {
+        console.error('Fatal error during server start: ');
+        console.error(e.stack || e);
+    });
+})();
