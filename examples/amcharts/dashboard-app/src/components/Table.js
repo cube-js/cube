5875fb8cc443684fc988a0aaa7ff1357ada1a641
Yulia Kotova <kalipso@pretty-girl.ru>
2020-08-13 18:51:29 +0300

    examples: slack analytics

--- examples/amcharts/dashboard-app/src/components/Table.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Table, Tag, Space } from 'antd';
-
-export default (props) => {
-  return <Table columns={props.data.column} dataSource={props.data.data} />;
-};
