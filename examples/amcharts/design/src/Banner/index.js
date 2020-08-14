469bfe40d74cf424219f27313999553bc3a64fc4
Igor Lukanin <me@igorlukanin.io>
2020-08-14 16:01:48 +0300

    Tune banner to match Slack style

--- examples/amcharts/design/src/Banner/index.js
+++ examples/amcharts/design/src/Banner/index.js
@@ -5,7 +5,7 @@ export default function Banner() {
   return (
     <div className={styles.root}>
       <div className={styles.banner}>
-        <p><em>Slave Vibe</em> is created and powered by <em>Cube.js</em>, an&nbsp;open source analytical
+        <p><em>Slack Vibe</em> is created and powered by <em>Cube.js</em>, an&nbsp;open source analytical
           data access layer for modern web applications.</p>
         <p>Learn more at <a href='https://cube.dev' target='_blank' rel='noopener noreferrer'>cube.dev</a> and <a
           href='https://github.com/cube-js/cube.js' target='_blank' rel='noopener noreferrer'>GitHub</a>.</p>
