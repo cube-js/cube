26b329eec12144441387ff1a6e976c35f6f9e291
Guðjón Ragnar Brynjarsson <gudjon.8@gmail.com>
2020-09-02 13:58:55 +0000

    fix: Export the TimeDimensionGranularity type (#1044). Thanks to @gudjonragnar
    Commit 3396fb changed TimeDimensionGranularities enum to TimeDimensionGranularity type
    and removed the export. This reintroduces the export of the type.

--- packages/cubejs-client-core/index.d.ts
+++ packages/cubejs-client-core/index.d.ts
@@ -593,7 +593,7 @@ declare module '@cubejs-client/core' {
 
 
 
-  type TimeDimensionGranularity = 'hour' | 'day' | 'week' | 'month' | 'year';
+  export type TimeDimensionGranularity = 'hour' | 'day' | 'week' | 'month' | 'year';
 
   export type TimeDimension = {
     dimension: string;
