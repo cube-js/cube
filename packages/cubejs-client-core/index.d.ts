a48e7e73faa725ac75aad6eb62de48f7a31d2c1e
Aaron Hakala <aaron.hakala@gmail.com>
2020-09-03 15:25:05 +0300

    fix: cube-client-core resolveMember return type
    Add correct return type to Meta.resolveMember. The previous type of `Object` was incorrect.

--- packages/cubejs-client-core/index.d.ts
+++ packages/cubejs-client-core/index.d.ts
@@ -673,6 +673,14 @@ declare module '@cubejs-client/core' {
   type TCubeDimension = TCubeMember & {
     suggestFilterValues: boolean;
   };
+
+  type TCubeSegment = Pick<TCubeMember, "name" | 'shortTitle' | "title">
+
+  type TCubeMemberByType<T> =
+    T extends "measures" ? TCubeMeasure :
+    T extends "dimensions" ? TCubeDimension :
+    T extends "segments" ? TCubeSegment :
+    never
   
   type TDryRunResponse = {
     queryType: QueryType;
@@ -709,7 +717,7 @@ declare module '@cubejs-client/core' {
      * @param memberName - Fully qualified member name in a form `Cube.memberName`
      * @return An object containing meta information about member
      */
-    resolveMember(memberName: string, memberType: MemberType): Object;
+    resolveMember<T extends MemberType>(memberName: string, memberType: T): { title: string, error: string } | TCubeMemberByType<T>;
     defaultTimeDimensionNameFor(memberName: string): string;
     filterOperatorsForMember(memberName: string, memberType: MemberType): any;
   }
