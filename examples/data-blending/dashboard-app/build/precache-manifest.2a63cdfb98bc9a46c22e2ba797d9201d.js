390ba70eba060541c9a758f5c535e501a0af799d
Yulia Kotova <kalipso@pretty-girl.ru>
2020-09-16 18:20:33 +0300

    examples__data-blending

---
+++ examples/data-blending/dashboard-app/build/precache-manifest.2a63cdfb98bc9a46c22e2ba797d9201d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1e28e3ac23e679fc652b389cb95159",
+    "url": "/index.html"
+  },
+  {
+    "revision": "659de82943ab34fcbc07",
+    "url": "/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "da28ed415b8cb8913a34",
+    "url": "/static/css/main.42f26f63.chunk.css"
+  },
+  {
+    "revision": "659de82943ab34fcbc07",
+    "url": "/static/js/2.3a2af167.chunk.js"
+  },
+  {
+    "revision": "4b70c65191312d918c98e88b463903ae",
+    "url": "/static/js/2.3a2af167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da28ed415b8cb8913a34",
+    "url": "/static/js/main.c676e228.chunk.js"
+  },
+  {
+    "revision": "882b77b6f1eb68438514",
+    "url": "/static/js/runtime-main.74c2df34.js"
+  },
+  {
+    "revision": "d3e9f6739b9b797bdeeae9b8018ef475",
+    "url": "/static/media/logo.d3e9f673.svg"
+  }
+]);
---
+++ examples/data-blending/dashboard-app/src/components/Dashboard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import SchemaCube from './SchemaCube';
+import Query from './Query';
+
+import QueryOneTable from './QueryOneTable';
+
+export default () => {
+  return (
+    <React.Fragment>
+      <Row gutter={20} className='dashboard__row'>
+        <Col sm={24} lg={12}>
+          <div className='dashboard__cell'>
+            <Query />
+          </div>
+        </Col>
+        <Col sm={24} lg={12}>
+          <div className='dashboard__cell'>
+            <SchemaCube />
+          </div>
+        </Col>
+      </Row>
+      <Row gutter={20} className='dashboard__row'>
+        <Col sm={24} lg={12}>
+          <div className='dashboard__cell'>
+            <QueryOneTable />
+          </div>
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
+};
