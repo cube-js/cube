4efe012ca7e732695d17f2dd136b53e15e091e38
Igor Lukanin <me@igorlukanin.io>
2020-09-16 14:16:00 +0500

    Remove italic font

--- examples/slack-vibe/frontend/src/Banner/index.js
+++ examples/slack-vibe/frontend/src/Banner/index.js
@@ -5,7 +5,7 @@ export default function Banner() {
   return (
     <div className={styles.root}>
       <div className={styles.banner}>
-        <div><em>Slack Vibe</em> is created and powered by <em>Cube.js</em>, an&nbsp;open source analytical
+        <div>Slack Vibe is created and powered by Cube.js, an&nbsp;open source analytical
           data access layer for modern web applications.</div>
         <ul className={styles.buttons}>
           <li>
