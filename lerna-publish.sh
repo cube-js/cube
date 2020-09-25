faee6aa2df8d470007eee4d77a17cb8e174cb10b
A.Vasilev <vaspiring@gmail.com>
2020-09-25 15:38:45 +0300

    lerna publish rollback

--- lerna-publish.sh
+++ lerna-publish.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 . .gh-token
-echo $GH_TOKEN
-# BUMP=$1
-# if [ "x$BUMP" == "x" ]; then
-#   BUMP=patch
-# fi
-# yarn lerna version --github-release --conventional-commits $BUMP
+BUMP=$1
+if [ "x$BUMP" == "x" ]; then
+  BUMP=patch
+fi
+yarn lerna version --github-release --conventional-commits $BUMP
