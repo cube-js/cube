85ca240e46213f0e3e13dbddd7229d88f6f74dd4
Pavel Tiunov <pavel.tiunov@gmail.com>
2020-10-17 12:12:08 -0700

    feat: Cube Store driver

---
+++ packages/cubejs-cubestore-driver/driver/CubeStoreDriver.js
@@ -0,0 +1,208 @@
+const mysql = require('mysql');
+const genericPool = require('generic-pool');
+const { promisify } = require('util');
+const BaseDriver = require('@cubejs-backend/query-orchestrator/driver/BaseDriver');
+const fs = require('fs');
+const CubeStoreQuery = require('./CubeStoreQuery');
+
+const GenericTypeToCubeStore = {
+  string: 'varchar(255)',
+  text: 'varchar(255)'
+};
+
+class CubeStoreDriver extends BaseDriver {
+  constructor(config) {
+    super();
+    const { pool, ...restConfig } = config || {};
+    let ssl;
+
+    const sslOptions = [
+      { name: 'ca', value: 'CUBEJS_DB_SSL_CA' },
+      { name: 'cert', value: 'CUBEJS_DB_SSL_CERT' },
+      { name: 'key', value: 'CUBEJS_DB_SSL_KEY' },
+      { name: 'ciphers', value: 'CUBEJS_DB_SSL_CIPHERS' },
+      { name: 'passphrase', value: 'CUBEJS_DB_SSL_PASSPHRASE' },
+    ];
+
+    if (
+      process.env.CUBEJS_DB_SSL ||
+      process.env.CUBEJS_DB_SSL_REJECT_UNAUTHORIZED ||
+      sslOptions.find(o => !!process.env[o.value])
+    ) {
+      ssl = sslOptions.reduce(
+        (agg, { name, value }) => ({
+          ...agg,
+          ...(process.env[value] ? { [name]: fs.readFileSync(process.env[value]) } : {}),
+        }),
+        {}
+      );
+
+      if (process.env.CUBEJS_DB_SSL_REJECT_UNAUTHORIZED) {
+        ssl.rejectUnauthorized =
+          process.env.CUBEJS_DB_SSL_REJECT_UNAUTHORIZED.toLowerCase() === 'true';
+      }
+    }
+
+    this.config = {
+      host: process.env.CUBEJS_DB_HOST,
+      database: process.env.CUBEJS_DB_NAME,
+      port: process.env.CUBEJS_DB_PORT,
+      user: process.env.CUBEJS_DB_USER,
+      password: process.env.CUBEJS_DB_PASS,
+      socketPath: process.env.CUBEJS_DB_SOCKET_PATH,
+      timezone: 'Z',
+      ssl,
+      ...restConfig,
+    };
+    this.pool = genericPool.createPool({
+      create: async () => {
+        const conn = mysql.createConnection(this.config);
+        const connect = promisify(conn.connect.bind(conn));
+
+        if (conn.on) {
+          conn.on('error', () => {
+            conn.destroy();
+          });
+        }
+        conn.execute = promisify(conn.query.bind(conn));
+
+        await connect();
+
+        return conn;
+      },
+      destroy: (connection) => promisify(connection.end.bind(connection))(),
+      validate: async (connection) => {
+        try {
+          await connection.execute('SELECT 1');
+        } catch (e) {
+          return false;
+        }
+        return true;
+      }
+    }, {
+      min: 0,
+      max: process.env.CUBEJS_DB_MAX_POOL && parseInt(process.env.CUBEJS_DB_MAX_POOL, 10) || 8,
+      evictionRunIntervalMillis: 10000,
+      softIdleTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      testOnBorrow: true,
+      acquireTimeoutMillis: 20000,
+      ...pool
+    });
+  }
+
+  withConnection(fn) {
+    const self = this;
+    const connectionPromise = this.pool.acquire();
+
+    let cancelled = false;
+    const cancelObj = {};
+    const promise = connectionPromise.then(async conn => {
+      const [{ connectionId }] = await conn.execute('select connection_id() as connectionId');
+      cancelObj.cancel = async () => {
+        cancelled = true;
+        await self.withConnection(async processConnection => {
+          await processConnection.execute(`KILL ${connectionId}`);
+        });
+      };
+      return fn(conn)
+        .then(res => this.pool.release(conn).then(() => {
+          if (cancelled) {
+            throw new Error('Query cancelled');
+          }
+          return res;
+        }))
+        .catch((err) => this.pool.release(conn).then(() => {
+          if (cancelled) {
+            throw new Error('Query cancelled');
+          }
+          throw err;
+        }));
+    });
+    promise.cancel = () => cancelObj.cancel();
+    return promise;
+  }
+
+  async testConnection() {
+    // eslint-disable-next-line no-underscore-dangle
+    const conn = await this.pool._factory.create();
+    try {
+      return await conn.execute('SELECT 1');
+    } finally {
+      // eslint-disable-next-line no-underscore-dangle
+      await this.pool._factory.destroy(conn);
+    }
+  }
+
+  query(query, values) {
+    return this.withConnection(db => db.execute(query, values)
+      .then(res => res));
+  }
+
+  async release() {
+    await this.pool.drain();
+    await this.pool.clear();
+  }
+
+  informationSchemaQuery() {
+    return `${super.informationSchemaQuery()} AND columns.table_schema = '${this.config.database}'`;
+  }
+
+  quoteIdentifier(identifier) {
+    return `\`${identifier}\``;
+  }
+
+  fromGenericType(columnType) {
+    return GenericTypeToCubeStore[columnType] || super.fromGenericType(columnType);
+  }
+
+  toColumnValue(value, genericType) {
+    if (genericType === 'timestamp' && typeof value === 'string') {
+      return value && value.replace('Z', '');
+    }
+    if (genericType === 'boolean' && typeof value === 'string') {
+      if (value.toLowerCase() === 'true') {
+        return true;
+      }
+      if (value.toLowerCase() === 'false') {
+        return false;
+      }
+    }
+    return super.toColumnValue(value, genericType);
+  }
+
+  async uploadTable(table, columns, tableData) {
+    if (!tableData.rows) {
+      throw new Error(`${this.constructor} driver supports only rows upload`);
+    }
+    await this.createTable(table, columns);
+    try {
+      const batchSize = 1000; // TODO make dynamic?
+      for (let j = 0; j < Math.ceil(tableData.rows.length / batchSize); j++) {
+        const currentBatchSize = Math.min(tableData.rows.length - j * batchSize, batchSize);
+        const indexArray = Array.from({ length: currentBatchSize }, (v, i) => i);
+        const valueParamPlaceholders =
+          indexArray.map(i => `(${columns.map((c, paramIndex) => this.param(paramIndex + i * columns.length)).join(', ')})`).join(', ');
+        const params = indexArray.map(i => columns
+          .map(c => this.toColumnValue(tableData.rows[i + j * batchSize][c.name], c.type)))
+          .reduce((a, b) => a.concat(b), []);
+
+        await this.query(
+          `INSERT INTO ${table}
+        (${columns.map(c => this.quoteIdentifier(c.name)).join(', ')})
+        VALUES ${valueParamPlaceholders}`,
+          params
+        );
+      }
+    } catch (e) {
+      await this.dropTable(table);
+      throw e;
+    }
+  }
+
+  static dialectClass() {
+    return CubeStoreQuery;
+  }
+}
+
+module.exports = CubeStoreDriver;
---
+++ packages/cubejs-cubestore-driver/driver/CubeStoreQuery.js
@@ -0,0 +1,92 @@
+const moment = require('moment-timezone');
+
+const BaseQuery = require('@cubejs-backend/schema-compiler/adapter/BaseQuery');
+const BaseFilter = require('@cubejs-backend/schema-compiler/adapter/BaseFilter');
+const UserError = require('@cubejs-backend/schema-compiler/compiler/UserError');
+
+const GRANULARITY_TO_INTERVAL = {
+  day: 'day',
+  week: 'week',
+  hour: 'hour',
+  minute: 'minute',
+  second: 'second',
+  month: 'month',
+  year: 'year'
+};
+
+class CubeStoreFilter extends BaseFilter {
+  likeIgnoreCase(column, not) {
+    return `${column}${not ? ' NOT' : ''} LIKE CONCAT('%', ?, '%')`;
+  }
+}
+
+class CubeStoreQuery extends BaseQuery {
+  newFilter(filter) {
+    return new CubeStoreFilter(this, filter);
+  }
+
+  convertTz(field) {
+    return `CONVERT_TZ(${field}, '${moment().tz(this.timezone).format('Z')}')`;
+  }
+
+  timeStampParam() {
+    return `to_timestamp(?)`;
+  }
+
+  timeStampCast(value) {
+    return `CAST(${value} as TIMESTAMP)`; // TODO
+  }
+
+  inDbTimeZone(date) {
+    return this.inIntegrationTimeZone(date).clone().utc().format(moment.HTML5_FMT.DATETIME_LOCAL_MS);
+  }
+
+  dateTimeCast(value) {
+    return `TIMESTAMP(${value})`;
+  }
+
+  subtractInterval(date, interval) {
+    return `DATE_SUB(${date}, INTERVAL ${interval})`;
+  }
+
+  addInterval(date, interval) {
+    return `DATE_ADD(${date}, INTERVAL ${interval})`;
+  }
+
+  timeGroupedColumn(granularity, dimension) {
+    return `date_trunc(${dimension}, '${GRANULARITY_TO_INTERVAL[granularity]}')`;
+  }
+
+  escapeColumnName(name) {
+    return `\`${name}\``;
+  }
+
+  seriesSql(timeDimension) {
+    const values = timeDimension.timeSeries().map(
+      ([from, to]) => `select '${from}' f, '${to}' t`
+    ).join(' UNION ALL ');
+    return `SELECT TIMESTAMP(dates.f) date_from, TIMESTAMP(dates.t) date_to FROM (${values}) AS dates`;
+  }
+
+  concatStringsSql(strings) {
+    return `CONCAT(${strings.join(', ')})`;
+  }
+
+  unixTimestampSql() {
+    return `UNIX_TIMESTAMP()`;
+  }
+
+  wrapSegmentForDimensionSelect(sql) {
+    return `IF(${sql}, 1, 0)`;
+  }
+
+  preAggregationTableName(cube, preAggregationName, skipSchema) {
+    const name = super.preAggregationTableName(cube, preAggregationName, skipSchema);
+    if (name.length > 64) {
+      throw new UserError(`MySQL can not work with table names that longer than 64 symbols. Consider using the 'sqlAlias' attribute in your cube and in your pre-aggregation definition for ${name}.`);
+    }
+    return name;
+  }
+}
+
+module.exports = CubeStoreQuery;
