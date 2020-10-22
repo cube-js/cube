6df205c5149280e883c76e689b573a0f2a9c749f
Dmitry Patsura <talk@dmtry.me>
2020-10-22 15:54:44 +0300

    feat(cubejs-cli): Deprecation notice for Node.js < 10

--- packages/cubejs-cli/src/index.ts
+++ packages/cubejs-cli/src/index.ts
@@ -14,14 +14,21 @@ const major = parseInt(<string> semver[0], 10);
 if (major < 8) {
   console.error(
     chalk.red(
-      'You are running Node ' +
-      currentNodeVersion +
-      '.\n' +
-      'Cube.js CLI requires Node 8 or higher. \n' +
-      'Please update your version of Node.'
+      'You are running Node.js ' + currentNodeVersion + '.\n' +
+      'Cube.js CLI requires Node.js 8 or higher. \n' +
+      'Please update your version of Node.js.'
     )
   );
   process.exit(1);
+}
+
+if (major < 10) {
+  process.emitWarning(
+    chalk.red(
+      'You are running Node.js ' + currentNodeVersion + '.\n' +
+      'Support for Node.js 8 will be removed soon. Please upgrade to Node.js 10 or higher.'
+    )
+  );
 }
 
 require('./cli');
