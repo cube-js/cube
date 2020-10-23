ad8f6c7ea7928cd372d6e8a3f15caeeb241221a9
Dmitry Patsura <talk@dmtry.me>
2020-10-23 11:41:26 +0300

    feat(@cubejs-backend/server): Introduce cubejs-server

--- packages/cubejs-server/src/index.ts
+++ packages/cubejs-server/src/index.ts
@@ -1,6 +1,7 @@
 import { CubejsServer } from './server';
 
 export * from './server';
+export { run } from '@oclif/command';
 
 // Internal staff, don't show it as a public api
 // export * from './command/server';
