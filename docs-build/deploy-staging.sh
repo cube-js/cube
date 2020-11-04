cf13f805f32ac8941e709ec58afd4332299ad320
A.Vasilev <vaspiring@gmail.com>
2020-11-04 21:08:11 +0300

    test

--- docs-build/deploy-staging.sh
+++ docs-build/deploy-staging.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo 'testing...'
+echo $NODE_ENV
+echo $ALGOLIA_API_KEY
+echo $PATH_PREFIX
+
 /bin/bash ./build.sh \
 && aws s3 sync public/ s3://cubejs-docs-staging/docs \
 && echo "Deployed staging at: http://cubejs-docs-staging.s3-website-us-east-1.amazonaws.com/docs/"
--- docs-build/docs-gen.sh
+++ docs-build/docs-gen.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo $NODE_ENV
-echo $ALGOLIA_API_KEY
-echo $PATH_PREFIX
-
 cd ../docs-gen && yarn && yarn generate
