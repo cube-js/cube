7702316c5ba80e16ca9c9fe8a7e39b1e89fdfddf
A.Vasilev <vaspiring@gmail.com>
2020-11-04 22:12:45 +0300

    fix

--- docs-build/deploy-staging.sh
+++ docs-build/deploy-staging.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo 'testing...'
-echo $NODE_ENV
-echo $ALGOLIA_API_KEY
-echo $PATH_PREFIX
+echo PATH_PREFIX=$PATH_PREFIX > .env.production
+echo ALGOLIA_API_KEY=$ALGOLIA_API_KEY >> .env.production
+echo ALGOLIA_INDEX_NAME=$ALGOLIA_INDEX_NAME >> .env.production
 
 /bin/bash ./build.sh \
 && aws s3 sync public/ s3://cubejs-docs-staging/docs \
