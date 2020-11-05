32e74f033eb3172ee2ae28f080d3525fa8c15423
Igor Lukanin <me@igorlukanin.io>
2020-11-05 17:10:48 +0300

    Dashboard: logo and banner

---
+++ examples/hacktoberfest/dashboard/src/App/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export default function App() {
+  return (
+    <>
+      <div className={styles.banner}>
+        <a href='https://cube.dev' target='_blank' rel='noreferrer'>
+          This data story is powered by Cube.js,
+          an open source analytical API platform
+        </a>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.logo}>
+          <div className={styles.image} />
+          <div className={styles.text}>
+            <h2>The open source story of</h2>
+            <h1>Hacktoberfest 2020</h1>
+            <h2>told with data and admiration</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
