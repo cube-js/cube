570b90341e87b458ef12873ce43f01b630abc8ac
Dmitry Patsura <talk@dmtry.me>
2020-11-07 01:25:26 +0300

    fix(examples/drill-down): Automatically deploy

--- examples/drill-downs/dashboard-app/src/components/DrillDownExample.js
+++ examples/drill-downs/dashboard-app/src/components/DrillDownExample.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { useCubeQuery } from "@cubejs-client/react";
 import moment from "moment";
--- examples/drill-downs/dashboard-app/src/components/Header.js
+++ examples/drill-downs/dashboard-app/src/components/Header.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import logo from "./cubejs-logo-white.svg";
 
 const useStyles = makeStyles((theme) => ({
@@ -32,7 +28,7 @@ const Header = ({ location }) => {
     <AppBar position="static" className={classes.appBar}>
       <Toolbar variant="dense" className={classes.toolBar}>
         <div className={classes.logo}>
-          <img src={logo} />
+          <img src={logo} alt="Logotype" />
           <span className={classes.exampleName}>Drill Downs</span>
         </div>
       </Toolbar>
