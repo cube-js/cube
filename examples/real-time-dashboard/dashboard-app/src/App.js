54303d88604593f48661f5980fe105d1f1bea8b4
Dmitry Patsura <talk@dmtry.me>
2020-11-09 16:38:08 +0300

    feat(examples/real-time-dashboard): Automatically deploy

--- examples/real-time-dashboard/dashboard-app/src/App.js
+++ examples/real-time-dashboard/dashboard-app/src/App.js
@@ -6,17 +6,11 @@ import { CubeProvider } from "@cubejs-client/react";
 import Header from "./components/Header";
 import WebSocketTransport from "@cubejs-client/ws-transport";
 import tracker from "./tracker";
-const CUBEJS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NzM2MjI5MjR9.iD_sSpXRBQSO0ywh5zSPHpHV-1O37oQMakrkG03kR-o";
-let WS_URL;
-if (process.env.NODE_ENV === 'production') {
-  WS_URL = window.location.origin.replace('http', 'ws').replace('https', 'wss')
-} else {
-  WS_URL = "ws://localhost:4000/"
-}
+
 const cubejsApi = cubejs({
   transport: new WebSocketTransport({
-    authorization: CUBEJS_TOKEN,
-    apiUrl: WS_URL
+    authorization: process.env.REACT_APP_CUBEJS_TOKEN,
+    apiUrl: process.env.REACT_APP_API_URL
   })
 });
 
