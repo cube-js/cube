bde3ad8ef9f5d6dbe5b07a0e496709adedc8abf7
Dmitry Patsura <talk@dmtry.me>
2020-11-09 19:09:13 +0300

    fix(examples/real-time-dashboard): Configure collect entrypoint by REACT_APP_COLLECT_URL ENV

--- examples/real-time-dashboard/dashboard-app/src/tracker.js
+++ examples/real-time-dashboard/dashboard-app/src/tracker.js
@@ -2,32 +2,26 @@ import cookie from "component-cookie";
 import uuidv4 from "uuid/v4";
 import { fetch } from "whatwg-fetch";
 
-let URL;
-if (process.env.NODE_ENV === 'production') {
-  URL = window.location.origin
-} else {
-  URL = "http://localhost:4000"
-}
-
 const COOKIE_NAME = "real_time_dashboard_uid";
-const track = (eventName) => {
+
+const track = async (eventName) => {
   if (!cookie(COOKIE_NAME)) {
     cookie(COOKIE_NAME, uuidv4());
   }
 
-  fetch(`${URL}/collect`, {
-    method: "POST",
+  return fetch(process.env.REACT_APP_COLLECT_URL, {
+    method: 'POST',
     body: JSON.stringify({
       anonymousId: cookie(COOKIE_NAME),
       eventType: eventName
     }),
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     }
   });
 };
 
 export default {
-  pageview: () => track("pageView"),
+  pageview: () => track('pageView'),
   event: (eventName) => track(eventName)
 }
