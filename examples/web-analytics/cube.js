827bbffcaf3c9c97400e03318227394c68f9d1fc
Pavel Tiunov <pavel.tiunov@gmail.com>
2020-11-10 00:15:34 -0800

    Web analytics pre-aggregations schema env variable

--- examples/web-analytics/cube.js
+++ examples/web-analytics/cube.js
@@ -1,4 +1,4 @@
 // Server options go here: https://cube.dev/docs/config#options-reference
 module.exports = {
-  preAggregationsSchema: 'wa_pre_aggregations',
+  preAggregationsSchema: process.env.PRE_AGGREGATIONS_SCHEMA || 'wa_pre_aggregations',
 };
