8418ed4d9b2e2f94216dc8ef27e3166a6ae6671f
Hassan Khan <hassan@cube.dev>
2020-11-12 14:11:01 +0000

    chore(examples): remove invalid examples

--- examples/express-analytics-dashboard/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
