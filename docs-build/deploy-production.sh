dbd2a4f4cd4b90f81fb5f7f8e0aa8f8892c474eb
Dmitry Patsura <talk@dmtry.me>
2020-11-12 14:53:48 +0300

    misc(ci): Simplify docs-build

--- docs-build/deploy-production.sh
+++ docs-build/deploy-production.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo PATH_PREFIX=$PATH_PREFIX > .env.production
-echo ALGOLIA_API_KEY=$ALGOLIA_API_KEY >> .env.production
-echo ALGOLIA_INDEX_NAME=$ALGOLIA_INDEX_NAME >> .env.production
-
 /bin/bash ./build.sh \
 && aws s3 sync public/ s3://cubejs/docs \
 && aws cloudfront create-invalidation --distribution-id E32Q4UMUFUPI8O --paths /docs/*
