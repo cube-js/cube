7bfc134effae6627c7256c682107431033363d44
Igor Lukanin <me@igorlukanin.io>
2020-11-19 16:13:02 +0300

    Cube.js server

--- examples/hacktoberfest/schema/Repos.js
+++ examples/hacktoberfest/schema/Repos.js
@@ -1,191 +1,164 @@
 cube(`Repos`, {
-  sql: `SELECT * FROM public.repos`,
+  sql: `
+  SELECT
+    base.repo.full_name AS full_name,
+    base.repo.html_url AS url,
+    CAST(FROM_ISO8601_TIMESTAMP(base.repo.created_at) AS TIMESTAMP) AS created_at,
+    COALESCE(base.repo.language, 'Unknown') AS language,
+    COALESCE(base.repo.license.spdx_id, 'Unknown') AS license,
+    COALESCE(base.repo.stargazers_count, 0) AS star_count,
+    COALESCE(base.repo.watchers_count, 0) AS watcher_count,
+    COALESCE(base.repo.forks, 0) AS fork_count,
+    COALESCE(base.repo.size, 0) AS size,
+    IF(base.repo.fork, '1', '0') AS is_fork,
+    base.repo.default_branch AS default_branch
+  FROM cubedev_examples_hacktoberfest.api_accepted
+  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11`,
+
+  refreshKey: {
+    every: `1 hour`,
+  },
+
+  preAggregations: {
+    main: {
+      type: `originalSql`,
+    },
+    rollup: {
+      type: `rollup`,
+      useOriginalSqlPreAggregations: true,
+      measureReferences: [
+        count,
+        maxStarCount,
+      ],
+      dimensionReferences: [
+        fullName,
+        url,
+        isFork,
+        language,
+      ],
+    },
+  },
 
   joins: {
-
+    PullRequests: {
+      relationship: `hasMany`,
+      sql: `${Repos}.full_name = ${PullRequests}.repo_name`,
+    },
   },
 
   measures: {
     count: {
       type: `count`,
-      drillMembers: [name, id, fullName, createdAt, updatedAt]
-    },
-    uniqueCount: {
-      sql: `repo_id`,
-      type: "countDistinct"
     },
 
-    openIssuesCount: {
-      sql: `open_issues_count`,
-      type: `sum`
+    avgStarCount: {
+      sql: `${starCount}`,
+      type: `avg`,
     },
 
-    stargazersCount: {
-      sql: `stargazers_count`,
-      type: `sum`
+    maxStarCount: {
+      sql: `${starCount}`,
+      type: `max`,
     },
 
-    watchersCount: {
-      sql: `watchers_count`,
-      type: `sum`
+    medianStarCount: {
+      sql: `APPROX_PERCENTILE(${starCount}, 0.5)`,
+      type: `number`,
     },
 
-    forksCount: {
-      sql: `forks_count`,
-      type: `sum`
-    }
+    avgWatcherCount: {
+      sql: `${watcherCount}`,
+      type: `avg`,
+    },
+
+    maxWatcherCount: {
+      sql: `${watcherCount}`,
+      type: `max`,
+    },
+
+    medianWatcherCount: {
+      sql: `APPROX_PERCENTILE(${watcherCount}, 0.5)`,
+      type: `number`,
+    },
+
+    avgForkCount: {
+      sql: `${forkCount}`,
+      type: `avg`,
+    },
+
+    maxForkCount: {
+      sql: `${forkCount}`,
+      type: `max`,
+    },
+
+    medianForkCount: {
+      sql: `APPROX_PERCENTILE(${forkCount}, 0.5)`,
+      type: `number`,
+    },
   },
 
   dimensions: {
-    htmlUrl: {
-      sql: `html_url`,
-      type: `string`
-    },
-
-    name: {
-      sql: `name`,
-      type: `string`
-    },
-
-    hasWiki: {
-      sql: `has_wiki`,
-      type: `string`
-    },
-
-    ownerUrl: {
-      sql: `owner_url`,
-      type: `string`
-    },
-
-    hasPages: {
-      sql: `has_pages`,
-      type: `string`
-    },
-
-    ownerLogin: {
-      sql: `owner_login`,
-      type: `string`
-    },
-
-    hasDownloads: {
-      sql: `has_downloads`,
-      type: `string`
-    },
-
-    license: {
-      sql: `license`,
-      type: `string`
-    },
-
-    description: {
-      sql: `description`,
-      type: `string`
-    },
-
-    archived: {
-      sql: `archived`,
-      type: `string`
-    },
-
-    issuesUrl: {
-      sql: `issues_url`,
-      type: `string`
-    },
-
-    hasIssues: {
-      sql: `has_issues`,
-      type: `string`
-    },
-
-    language: {
-      sql: `language`,
-      type: `string`
-    },
-
-    mirrorUrl: {
-      sql: `mirror_url`,
-      type: `string`
-    },
-
-    gitUrl: {
-      sql: `git_url`,
-      type: `string`
-    },
-
-    subscribersUrl: {
-      sql: `subscribers_url`,
-      type: `string`
-    },
-
-    contributorsUrl: {
-      sql: `contributors_url`,
-      type: `string`
-    },
-
-    id: {
-      sql: `id`,
-      type: `number`,
-      primaryKey: true
-    },
-
-    commitsUrl: {
-      sql: `commits_url`,
-      type: `string`
-    },
-
     fullName: {
       sql: `full_name`,
-      type: `string`
-    },
-
-    pullsUrl: {
-      sql: `pulls_url`,
-      type: `string`
-    },
-
-    disabled: {
-      sql: `disabled`,
-      type: `string`
+      type: `string`,
+      primaryKey: true,
+      shown: true,
     },
 
     url: {
       sql: `url`,
-      type: `string`
-    },
-
-    fork: {
-      sql: `fork`,
-      type: `string`
-    },
-
-    hasProjects: {
-      sql: `has_projects`,
-      type: `string`
-    },
-
-    languagesUrl: {
-      sql: `languages_url`,
-      type: `string`
-    },
-
-    private: {
-      sql: `private`,
-      type: `string`
-    },
-
-    defaultBranch: {
-      sql: `default_branch`,
-      type: `string`
+      type: `string`,
     },
 
     createdAt: {
       sql: `created_at`,
-      type: `time`
+      type: `time`,
     },
 
-    updatedAt: {
-      sql: `updated_at`,
-      type: `time`
-    }
-  }
+    language: {
+      sql: `language`,
+      type: `string`,
+    },
+
+    license: {
+      sql: `license`,
+      type: `string`,
+    },
+
+    starCount: {
+      sql: `star_count`,
+      type: `number`,
+    },
+
+    watcherCount: {
+      sql: `watcher_count`,
+      type: `number`,
+    },
+
+    forkCount: {
+      sql: `fork_count`,
+      type: `number`,
+    },
+
+    size: {
+      sql: `size`,
+      type: `number`,
+    },
+
+    defaultBranch: {
+      sql: `default_branch`,
+      type: `string`,
+    },
+
+    isFork: {
+      sql: `is_fork`,
+      type: `string`,
+    },
+
+    pullRequestCount: {
+      sql: `${PullRequests.count}`,
+      type: `number`,
+      subQuery: true,
+    },
+  },
 });
