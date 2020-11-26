8c371add2821a851bc51e00fb24e7ad2d8620345
Dmitry Patsura <talk@dmtry.me>
2020-11-26 16:20:46 +0300

    feat: Specify CORS for server/serverless (#1455)
    * feat: Specify CORS for server/serverless
    
    * feat(@cubejs-backend/server): Allow passing cors options inside CreateOptions

--- packages/cubejs-serverless/Handlers.js
+++ packages/cubejs-serverless/Handlers.js
@@ -85,7 +85,10 @@ class Handlers {
   getApiHandler() {
     if (!this.apiHandler) {
       const app = express();
-      app.use(cors());
+      app.use(cors({
+        allowedHeaders: 'authorization,x-request-id',
+      }));
+
       this.serverCore.initApp(app);
       this.apiHandler = handler(app);
     }
