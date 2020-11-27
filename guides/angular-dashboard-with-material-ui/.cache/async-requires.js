229497395d8cf299fe574f39b1b6c87b0d6b132a
Igor Lukanin <me@igorlukanin.io>
2020-11-27 15:42:17 +0300

    Move files and remove Amplify stuff

--- guides/angular-dashboard-with-material-ui/.cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => (m && m.default) || m
-
-exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": () => import("./../../caches/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---cache-caches-gatsby-plugin-offline-app-shell-js" */),
-  "component---guides-base-src-templates-post-jsx": () => import("./../../../../guides-base/src/templates/post.jsx" /* webpackChunkName: "component---guides-base-src-templates-post-jsx" */),
-  "component---src-pages-index-jsx": () => import("./../../../src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */)
-}
-
