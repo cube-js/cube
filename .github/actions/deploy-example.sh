7830f18f865923278c5e42d3958b807d3403fb2b
Igor Lukanin <me@igorlukanin.io>
2020-11-29 02:47:31 +0300

    Test

--- .github/actions/deploy-example.sh
+++ .github/actions/deploy-example.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-cd $1
+WORKING_DIRECTORY=$1
+FRONTEND_SUBDIRECTORY=${2:-dashboard-app}
+FRONTEND_DEPLOY_SUBDIRECTORY=${3:-build}
+
+cd $WORKING_DIRECTORY
 yarn install
 npm install -g cubejs-cli
 cubejs deploy
 
-cd ${2:-dashboard-app}
+cd $FRONTEND_SUBDIRECTORY
 yarn install
 yarn build
 npm install -g netlify-cli
-netlify deploy --dir=${3:-build} --prod
+netlify deploy --dir=$FRONTEND_DEPLOY_SUBDIRECTORY --prod
