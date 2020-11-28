1ec4b9a2cf67020ee716c04f29ae51d0b2f702d3
Igor Lukanin <me@igorlukanin.io>
2020-11-29 00:50:44 +0300

    Fix

--- examples/angular-dashboard-with-material-ui/dashboard-app/src/app/app.module.ts
+++ examples/angular-dashboard-with-material-ui/dashboard-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { CubejsClientModule } from "@cubejs-client/ngx";
-import { MatCardModule } from "@angular/material/Card";
+import { MatCardModule } from "@angular/material/card";
 
 import { AppComponent } from "./app.component";
 import { ChartsModule } from "ng2-charts";
