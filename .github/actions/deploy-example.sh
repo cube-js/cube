0f5911c5cd4f28560f58cabc228b9ea1421cc434
Igor Lukanin <me@igorlukanin.io>
2020-11-29 03:09:51 +0300

    Test

--- .github/actions/deploy-example.sh
+++ .github/actions/deploy-example.sh
@@ -3,6 +3,8 @@
 FRONTEND_SUBDIRECTORY=${FRONTEND_SUBDIRECTORY:-dashboard-app}
 FRONTEND_DEPLOY_SUBDIRECTORY=${FRONTEND_DEPLOY_SUBDIRECTORY:-build}
 
+npm config set loglevel warn
+
 yarn install
 npm install -g cubejs-cli
 cubejs deploy
