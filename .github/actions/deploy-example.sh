a77e49cf80e15c06e2ab06da4dfa23397fdf6dd5
Igor Lukanin <me@igorlukanin.io>
2020-11-29 04:47:06 +0300

    Fix

--- .github/actions/deploy-example.sh
+++ .github/actions/deploy-example.sh
@@ -3,6 +3,8 @@
 # Exit on first error
 set -e
 
+npm config set loglevel error
+
 # Required environment variable
 EXAMPLE_SLUG=${EXAMPLE_SLUG}
 
@@ -10,8 +12,6 @@ EXAMPLE_DIRECTORY=examples/${EXAMPLE_SLUG}
 EXAMPLE_FRONTEND_SKIP=${EXAMPLE_FRONTEND_SKIP:-0}
 EXAMPLE_FRONTEND_SUBDIRECTORY=${EXAMPLE_FRONTEND_SUBDIRECTORY:-dashboard-app}
 EXAMPLE_FRONTEND_BUILD_SUBDIRECTORY=${EXAMPLE_FRONTEND_BUILD_SUBDIRECTORY:-build}
-
-npm config set loglevel error
 
 cd "$EXAMPLE_DIRECTORY"
 yarn install
