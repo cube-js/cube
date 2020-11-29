bd328407b7f3a659708b2c790151290d60883036
Igor Lukanin <me@igorlukanin.io>
2020-11-29 03:38:13 +0300

    Refactor

--- .github/actions/deploy-example.sh
+++ .github/actions/deploy-example.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-FRONTEND_SUBDIRECTORY=${FRONTEND_SUBDIRECTORY:-dashboard-app}
-FRONTEND_DEPLOY_SUBDIRECTORY=${FRONTEND_DEPLOY_SUBDIRECTORY:-build}
+# Exit on first error
+set -e
+
+# Required environment variable
+EXAMPLE_SLUG=${EXAMPLE_SLUG}
+
+EXAMPLE_DIRECTORY=eamples/${EXAMPLE_SLUG}
+EXAMPLE_FRONTEND_SKIP=${EXAMPLE_FRONTEND_SKIP:-}
+EXAMPLE_FRONTEND_SUBDIRECTORY=${EXAMPLE_FRONTEND_SUBDIRECTORY:-dashboard-app}
+EXAMPLE_FRONTEND_BUILD_SUBDIRECTORY=${EXAMPLE_FRONTEND_BUILD_SUBDIRECTORY:-build}
 
 npm config set loglevel error
 
+cd $EXAMPLE_DIRECTORY
 yarn install
 npm install -g cubejs-cli
 cubejs deploy
 
-cd $FRONTEND_SUBDIRECTORY
+cd $EXAMPLE_FRONTEND_SUBDIRECTORY
 yarn install
 yarn build
 npm install -g netlify-cli
-netlify deploy --dir=$FRONTEND_DEPLOY_SUBDIRECTORY --prod
+netlify deploy --dir=$EXAMPLE_FRONTEND_BUILD_SUBDIRECTORY --prod
