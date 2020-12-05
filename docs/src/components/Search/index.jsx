5dff98a39f5ca7992a436a9568dc3937b44353d1
Hassan Khan <hassan@cube.dev>
2020-12-05 10:00:28 +0000

    docs(@cubejs-docs/site): update search box styling

--- docs/src/components/Search/index.jsx
+++ docs/src/components/Search/index.jsx
@@ -73,7 +73,7 @@ export default class Search extends Component {
 
   render() {
     return (
-      <div>
+      <div className={styles.searchBoxWrapper}>
         {this.state.open &&
           <Helmet
             bodyAttributes={{ class: cx(styles.noscroll, {
