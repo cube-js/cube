728110ed0ffe5697bd5e47e3920bf2e5377a0ffd
Dmitry Patsura <zaets28rus@gmail.com>
2020-12-14 18:51:13 +0300

    feat(@cubejs-backend/query-orchestrator): Introduce AsyncRedisClient type

--- packages/cubejs-query-orchestrator/src/orchestrator/RedisFactory.ts
+++ packages/cubejs-query-orchestrator/src/orchestrator/RedisFactory.ts
@@ -1,7 +1,51 @@
-import redis, { ClientOpts } from 'redis';
+import redis, { ClientOpts, RedisClient, Commands } from 'redis';
 import { promisify } from 'util';
 
-export function createRedisClient(url: string) {
+export interface AsyncRedisClient extends RedisClient {
+  brpopAsync: Commands<Promise<any>>['brpop'],
+  delAsync: Commands<Promise<any>>['del'],
+  getAsync: Commands<Promise<any>>['get'],
+  hgetAsync: Commands<Promise<any>>['hget'],
+  rpopAsync: Commands<Promise<any>>['rpop'],
+  setAsync: Commands<Promise<any>>['set'],
+  zaddAsync: Commands<Promise<any>>['zadd'],
+  zrangeAsync: Commands<Promise<any>>['zrange'],
+  zrangebyscoreAsync: Commands<Promise<any>>['zrangebyscore'],
+  keysAsync: Commands<Promise<any>>['keys'],
+  watchAsync: Commands<Promise<any>>['watch'],
+  unwatchAsync: Commands<Promise<any>>['unwatch'],
+  incrAsync: Commands<Promise<any>>['incr'],
+  decrAsync: Commands<Promise<any>>['decr'],
+  lpushAsync: Commands<Promise<any>>['lpush'],
+}
+
+function decorateRedisClient(client: RedisClient): AsyncRedisClient {
+  [
+    'brpop',
+    'del',
+    'get',
+    'hget',
+    'rpop',
+    'set',
+    'zadd',
+    'zrange',
+    'zrangebyscore',
+    'keys',
+    'watch',
+    'unwatch',
+    'incr',
+    'decr',
+    'lpush',
+  ].forEach(
+    k => {
+      client[`${k}Async`] = promisify(client[k]);
+    }
+  );
+
+  return <any>client;
+}
+
+export function createRedisClient(url: string, opts: ClientOpts = {}) {
   redis.Multi.prototype.execAsync = function execAsync() {
     return new Promise((resolve, reject) => this.exec((err, res) => (
       err ? reject(err) : resolve(res)
@@ -20,29 +64,10 @@ export function createRedisClient(url: string) {
     options.password = process.env.REDIS_PASSWORD;
   }
 
-  const client = redis.createClient(options);
-
-  [
-    'brpop',
-    'del',
-    'get',
-    'hget',
-    'rpop',
-    'set',
-    'zadd',
-    'zrange',
-    'zrangebyscore',
-    'keys',
-    'watch',
-    'unwatch',
-    'incr',
-    'decr',
-    'lpush'
-  ].forEach(
-    k => {
-      client[`${k}Async`] = promisify(client[k]);
-    }
+  return decorateRedisClient(
+    redis.createClient({
+      ...options,
+      ...opts,
+    })
   );
-
-  return client;
 }
