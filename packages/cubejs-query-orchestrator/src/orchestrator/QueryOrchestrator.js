c537db02a908f9e8a95bf209c21f84d43a1d7a95
Dmitry Patsura <zaets28rus@gmail.com>
2020-12-14 17:17:13 +0300

    misc(@cubejs-backend/query-orchestrator): Use redisPoolOptions (explicitly prefix)

--- packages/cubejs-query-orchestrator/src/orchestrator/QueryOrchestrator.js
+++ packages/cubejs-query-orchestrator/src/orchestrator/QueryOrchestrator.js
@@ -17,7 +17,7 @@ export class QueryOrchestrator {
     if (cacheAndQueueDriver !== 'redis' && cacheAndQueueDriver !== 'memory') {
       throw new Error('Only \'redis\' or \'memory\' are supported for cacheAndQueueDriver option');
     }
-    const redisPool = cacheAndQueueDriver === 'redis' ? new RedisPool(options.poolOptions) : undefined;
+    const redisPool = cacheAndQueueDriver === 'redis' ? new RedisPool(options.redisPoolOptions) : undefined;
 
     this.redisPool = redisPool;
     this.queryCache = new QueryCache(
