a705a2ed6885d5c08e654945682054a1421dfb51
Dmitry Patsura <talk@dmtry.me>
2020-12-14 13:06:31 +0300

    fix(@cubejs-backend/schema-compiler): CubeCheckDuplicatePropTranspiler - dont crash on not StringLiterals (#1582)

--- packages/cubejs-schema-compiler/compiler/CubeCheckDuplicatePropTranspiler.js
+++ packages/cubejs-schema-compiler/compiler/CubeCheckDuplicatePropTranspiler.js
@@ -1,5 +1,7 @@
 const TYPE = {
-  OBJECT_EXPRESSION: 'ObjectExpression'
+  OBJECT_EXPRESSION: 'ObjectExpression',
+  STRING_LITERAL: 'StringLiteral',
+  IDENTIFIER: 'Identifier'
 };
 
 class CubeCheckDuplicatePropTranspiler {
@@ -8,25 +10,47 @@ class CubeCheckDuplicatePropTranspiler {
       CallExpression: path => {
         if (path.node.callee.name === 'cube') {
           path.node.arguments.forEach(arg => {
-            if (arg && arg.type === TYPE.OBJECT_EXPRESSION) this.checkExpression(arg);
+            if (arg && arg.type === TYPE.OBJECT_EXPRESSION) {
+              this.checkExpression(arg);
+            }
           });
         }
       }
     };
   }
 
+  compileExpression(expr) {
+    if (expr.type === TYPE.IDENTIFIER) {
+      return expr.name;
+    }
+
+    if (expr.type === TYPE.STRING_LITERAL && expr.value) {
+      return expr.value;
+    }
+
+    return null;
+  }
+
   checkExpression(astObjectExpression) {
     const unique = new Set();
+
     astObjectExpression.properties.forEach(prop => {
       const { value, key, loc } = prop || {};
       if (value && key) {
-        if (value.type === TYPE.OBJECT_EXPRESSION) this.checkExpression(value);
-        if (unique.has(key.name)) {
-          const error = new SyntaxError(`Duplicate property parsing ${key.name}`);
-          error.loc = loc.start;
-          throw error;
+        if (value.type === TYPE.OBJECT_EXPRESSION) {
+          this.checkExpression(value);
         }
-        unique.add(key.name);
+
+        const keyName = this.compileExpression(key);
+        if (keyName) {
+          if (unique.has(keyName)) {
+            const error = new SyntaxError(`Duplicate property parsing ${keyName}`);
+            error.loc = loc.start;
+            throw error;
+          }
+
+          unique.add(keyName);
+        }
       }
     });
   }
