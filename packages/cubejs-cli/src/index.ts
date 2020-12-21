99ea6d041746f9eca5e31e6fcf118dc69c3913dd
Dmitry Patsura <zaets28rus@gmail.com>
2020-12-21 12:21:05 +0300

    feat!: Drop Node.js 8 support, 10.8 is a minimal version

--- packages/cubejs-cli/src/index.ts
+++ packages/cubejs-cli/src/index.ts
@@ -6,25 +6,17 @@ import chalk from 'chalk';
 const currentNodeVersion = process.versions.node;
 const semver = currentNodeVersion.split('.');
 const major = parseInt(<string> semver[0], 10);
+const minor = parseInt(<string> semver[1], 10);
 
-if (major < 8) {
+if (major < 10 || major === 10 && minor < 8) {
   console.error(
     chalk.red(
       `You are running Node.js ${currentNodeVersion}.\n` +
-      'Cube.js CLI requires Node.js 8 or higher. \n' +
+      'Cube.js CLI requires Node.js 10.8.0 or higher. \n' +
       'Please update your version of Node.js.'
     )
   );
   process.exit(1);
-}
-
-if (major < 10) {
-  process.emitWarning(
-    chalk.red(
-      `You are running Node.js ${currentNodeVersion}.\n` +
-      'Support for Node.js 8 will be removed soon. Please upgrade to Node.js 10 or higher.'
-    )
-  );
 }
 
 require('./cli');
