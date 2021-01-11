88b3643a08c778cd2d52d5574cd6cdbbfccc5da3
Dmitry Patsura <zaets28rus@gmail.com>
2021-01-11 17:47:25 +0300

    misc: Use TS shared for whole monorepo

--- packages/cubejs-druid-driver/custom.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@cubejs-backend/schema-compiler/adapter/BaseQuery' {
-  abstract class BaseQuery {
-    protected readonly timezone: string;
-
-    public escapeColumnName(identifier: string): string;
-  }
-
-  export default BaseQuery;
-}
---
+++ packages/cubejs-druid-driver/src/custom.d.ts
@@ -0,0 +1,5 @@
+declare module '@cubejs-backend/schema-compiler/adapter/BaseQuery' {
+  declare var BaseQuery: any;
+
+  export default BaseQuery;
+}
