d5d109f49e499e04eb1392cd4821b63ec5b5ce94
Johan Rylander <johan.rylander@svt.se>
2021-01-20 22:09:00 +0100

    Explicitly wait for connection to redis before using it

--- packages/cubejs-query-orchestrator/src/orchestrator/RedisFactory.ts
+++ packages/cubejs-query-orchestrator/src/orchestrator/RedisFactory.ts
@@ -1,14 +1,17 @@
 import Redis, { RedisOptions } from 'ioredis';
 
 export async function createRedisClient(url: string): Promise<Redis.Redis> {
-  const options: RedisOptions = {};
-
   const [host, portStr] = (process.env.REDIS_SENTINEL || url || 'localhost').replace('redis://', '').split(':');
   const port = portStr ? Number(portStr) : 6379;
+
+  const options: RedisOptions = {
+    lazyConnect: true
+  };
 
   if (process.env.REDIS_SENTINEL) {
     options.sentinels = [{ host, port }];
     options.name = 'mymaster';
+    options.enableOfflineQueue = false;
   } else {
     options.host = host;
     options.port = port;
@@ -22,5 +25,7 @@ export async function createRedisClient(url: string): Promise<Redis.Redis> {
     options.password = process.env.REDIS_PASSWORD;
   }
 
-  return new Redis(options);
+  const client = new Redis(options);
+
+  return client.connect().then(() => client);
 }
