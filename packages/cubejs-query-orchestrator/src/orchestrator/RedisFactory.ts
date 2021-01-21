138f22faf5cba96e0df5572c526c19fec4b8f33d
Johan Rylander <johan.rylander@svt.se>
2021-01-21 14:09:49 +0100

    Log redis connections

--- packages/cubejs-query-orchestrator/src/orchestrator/RedisFactory.ts
+++ packages/cubejs-query-orchestrator/src/orchestrator/RedisFactory.ts
@@ -5,6 +5,7 @@ export async function createRedisClient(url: string): Promise<Redis.Redis> {
   const port = portStr ? Number(portStr) : 6379;
 
   const options: RedisOptions = {
+    enableReadyCheck: true,
     lazyConnect: true
   };
 
@@ -26,6 +27,30 @@ export async function createRedisClient(url: string): Promise<Redis.Redis> {
   }
 
   const client = new Redis(options);
+
+  client.on("connect", () => {
+    console.debug(`Redis connection established`);
+  });
+
+  client.on("ready", () => {
+    console.debug(`Redis ready`);
+  });
+
+  client.on("close", () => {
+    console.debug(`Redis connection closed`);
+  });
+
+  client.on("end", () => {
+    console.debug(`Redis connection ended`);
+  });
+
+  client.on("error", (e) => {
+    console.error(`Redis connection failed: `, e);
+  });
+
+  client.on('reconnecting', (times) => {
+    console.warn('Redis connection is being reconnected, attempt no: ', times);
+  });
 
   return client.connect().then(() => client);
 }
