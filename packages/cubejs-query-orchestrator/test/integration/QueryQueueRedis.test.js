f0dafe92bdb7b229ee77dd20ba9b13eafb728357
Johan Rylander <johan.rylander@svt.se>
2021-01-22 18:18:54 +0100

    Use better name for redis sentinel in tests

--- packages/cubejs-query-orchestrator/test/integration/QueryQueueRedis.test.js
+++ packages/cubejs-query-orchestrator/test/integration/QueryQueueRedis.test.js
@@ -4,8 +4,8 @@ const { RedisPool } = require('../../src/orchestrator/RedisPool');
 [false, true].forEach((sentinelFlag) => {
     process.env.FLAG_ENABLE_REDIS_SENTINEL = sentinelFlag
 
-    QueryQueueTest(`RedisPool, redis sentinel=${process.env.FLAG_ENABLE_REDIS_SENTINEL}`,
+    QueryQueueTest(`RedisPool, sentinel ${process.env.FLAG_ENABLE_REDIS_SENTINEL ? 'enabled' : 'disabled'}`,
         { cacheAndQueueDriver: 'redis', redisPool: new RedisPool() });
-    QueryQueueTest(`RedisNoPool, redis sentinel=${process.env.FLAG_ENABLE_REDIS_SENTINEL}`,
+    QueryQueueTest(`RedisNoPool, sentinel ${process.env.FLAG_ENABLE_REDIS_SENTINEL ? 'enabled' : 'disabled'}`,
         { cacheAndQueueDriver: 'redis', redisPool: new RedisPool({ poolMin: 0, poolMax: 0 }) });
 });
