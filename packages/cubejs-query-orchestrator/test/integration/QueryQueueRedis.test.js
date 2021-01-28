cdb5921b096608deca9d8815e5fc7ae6ae11377b
Johan Rylander <johan.rylander@svt.se>
2021-01-28 17:14:17 +0100

    DRY env vars

--- packages/cubejs-query-orchestrator/test/integration/QueryQueueRedis.test.js
+++ packages/cubejs-query-orchestrator/test/integration/QueryQueueRedis.test.js
@@ -1,13 +1,11 @@
 const QueryQueueTest = require('../unit/QueryQueue.test');
 const { RedisPool } = require('../../src/orchestrator/RedisPool');
 
-const config = require('../../config');
-
 [false, true].forEach((sentinelFlag) => {
-  config.FLAG_ENABLE_REDIS_SENTINEL = sentinelFlag;
+  process.env.FLAG_ENABLE_REDIS_SENTINEL = sentinelFlag;
 
-  QueryQueueTest(`RedisPool, sentinel ${config.FLAG_ENABLE_REDIS_SENTINEL ? 'enabled' : 'disabled'}`,
+  QueryQueueTest(`RedisPool, sentinel ${process.env.FLAG_ENABLE_REDIS_SENTINEL ? 'enabled' : 'disabled'}`,
     { cacheAndQueueDriver: 'redis', redisPool: new RedisPool() });
-  QueryQueueTest(`RedisNoPool, sentinel ${config.FLAG_ENABLE_REDIS_SENTINEL ? 'enabled' : 'disabled'}`,
+  QueryQueueTest(`RedisNoPool, sentinel ${process.env.FLAG_ENABLE_REDIS_SENTINEL ? 'enabled' : 'disabled'}`,
     { cacheAndQueueDriver: 'redis', redisPool: new RedisPool({ poolMin: 0, poolMax: 0 }) });
 });
