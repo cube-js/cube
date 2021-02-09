dfd42a9a84659d0add0998bafcc029a988c9d37c
rong fengliang <1141591465@qq.com>
2021-02-09 09:01:18 +0800

    Update Limit.js

--- packages/cubejs-playground/src/components/Settings/Limit.js
+++ packages/cubejs-playground/src/components/Settings/Limit.js
@@ -1,13 +1,13 @@
 import { InputNumber } from 'antd';
 
-export default function Limit({ limit = 200, onUpdate }) {
+export default function Limit({ limit = 5000, onUpdate }) {
   return (
     <label>
       Limit{' '}
       <InputNumber
         prefix="Limit"
         value={limit}
-        step={100}
+        step={500}
         onChange={(value) =>
           onUpdate({
             limit: value,
