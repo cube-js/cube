2b8b549b022e357d85b9f4549a4ff61c9d39fbeb
Dmitry Patsura <talk@dmtry.me>
2021-02-10 19:55:18 +0300

    fix(query-orchestrator): detect negative int/decimal as strings (#2051)
    * fix(query-orchestrator): detect negative int/decimal as strings

--- packages/cubejs-query-orchestrator/test/unit/BaseDriver.test.ts
+++ packages/cubejs-query-orchestrator/test/unit/BaseDriver.test.ts
@@ -15,7 +15,12 @@ describe('BaseDriver', () => {
     const rows = [{
       bigint: 21474836479,
       int: 1,
-      intstr: '1',
+      int_as_str: '1',
+      int_as_str_zero: '0',
+      int_as_str_negative: '-1',
+      decimal_as_str: '1.000000000001',
+      decimal_as_str_zero: '0.0000000',
+      decimal_as_str_negative: '-1.000000000001',
       string: 'str',
     }];
 
@@ -24,7 +29,12 @@ describe('BaseDriver', () => {
     expect((await driver.downloadQueryResults()).types).toEqual([
       { name: 'bigint', type: 'bigint' },
       { name: 'int', type: 'int' },
-      { name: 'intstr', type: 'int' },
+      { name: 'int_as_str', type: 'int' },
+      { name: 'int_as_str_zero', type: 'int' },
+      { name: 'int_as_str_negative', type: 'int' },
+      { name: 'decimal_as_str', type: 'decimal' },
+      { name: 'decimal_as_str_zero', type: 'decimal' },
+      { name: 'decimal_as_str_negative', type: 'decimal' },
       { name: 'string', type: 'string' }
     ]);
   });
