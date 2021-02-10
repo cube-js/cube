637e07c24cb04e87c409c4a20c6f8ede30f2098d
Dmitry Patsura <zaets28rus@gmail.com>
2021-02-10 19:33:03 +0300

    fix(query-orchestrator): detect negative int/decimal as strings

--- packages/cubejs-query-orchestrator/test/unit/BaseDriver.test.ts
+++ packages/cubejs-query-orchestrator/test/unit/BaseDriver.test.ts
@@ -16,8 +16,10 @@ describe('BaseDriver', () => {
       bigint: 21474836479,
       int: 1,
       int_as_str: '1',
+      int_as_str_zero: '0',
       int_as_str_negative: '-1',
       decimal_as_str: '1.000000000001',
+      decimal_as_str_zero: '0.0000000',
       decimal_as_str_negative: '-1.000000000001',
       string: 'str',
     }];
@@ -28,8 +30,10 @@ describe('BaseDriver', () => {
       { name: 'bigint', type: 'bigint' },
       { name: 'int', type: 'int' },
       { name: 'int_as_str', type: 'int' },
+      { name: 'int_as_str_zero', type: 'int' },
       { name: 'int_as_str_negative', type: 'int' },
       { name: 'decimal_as_str', type: 'decimal' },
+      { name: 'decimal_as_str_zero', type: 'decimal' },
       { name: 'decimal_as_str_negative', type: 'decimal' },
       { name: 'string', type: 'string' }
     ]);
