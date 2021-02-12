cef9251908cc259671c314694ac36ef01558d151
Alex.V <vaspiring@gmail.com>
2021-02-12 16:03:52 +0300

    fix(@cubejs-client/playground): context refactor, piechart fix (#2076)
    * fix(@cubejs-client/playground): context refactor, piechart fix
    
    * lint fix

--- packages/cubejs-playground/charts-gen/dev/SourceContainer.js
+++ packages/cubejs-playground/charts-gen/dev/SourceContainer.js
@@ -3,7 +3,10 @@ const { toPairs, fromPairs } = require('ramda');
 class SourceContainer {
   constructor(sourceFiles) {
     this.fileToTargetSource = {};
-    this.fileContent = fromPairs(sourceFiles.map(({ fileName, content }) => [fileName, content]));
+    this.fileContent = fromPairs(
+      sourceFiles.map(({ fileName, content }) => [fileName, content])
+    );
+    
     this.importDependencies = {};
   }
 
@@ -18,12 +21,32 @@ class SourceContainer {
   add(fileName, content) {
     this.fileContent[fileName] = content;
   }
-  
+
   addImportDependencies(importDependencies = {}) {
+    // if some template returns a dependency with a specified version
+    // it should have a priority over the same dependency with the `latest` version
+    const specificDependencies = fromPairs(
+      Object.keys(importDependencies)
+        .map((name) => {
+          const version =
+            this.importDependencies[name] &&
+            this.importDependencies[name] !== 'latest'
+              ? this.importDependencies[name]
+              : importDependencies[name];
+          if (importDependencies[name]) {
+            return [name, version];
+          }
+
+          return null;
+        })
+        .filter(Boolean)
+    );
+
     // todo: version validation
     this.importDependencies = {
       ...this.importDependencies,
-      ...importDependencies
+      ...importDependencies,
+      ...specificDependencies,
     };
   }
 
--- packages/cubejs-playground/src/hooks/index.js
+++ packages/cubejs-playground/src/hooks/index.js
@@ -5,8 +5,6 @@ export const AppContext = createContext({
   isPreAggregationBuildInProgress: false,
 });
 
-export { default as useSlowQuery } from './slow-query';
-export { default as useIsPreAggregationBuildInProgress } from './is-pre-aggregation-build-in-progress';
 export { default as useDeepCompareMemoize } from './deep-compare-memoize';
 export { default as useSecurityContext } from './security-context';
 export { default as useCubejsApi } from './cubejs-api';
