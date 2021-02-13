585686ac4c648efaeab7069cdf2841ef4a2b2e41
Dmitry Patsura <zaets28rus@gmail.com>
2021-02-13 17:40:29 +0300

    examples(auth0): Setup for production

--- examples/auth0/dashboard-app/src/init-cubejs-api.js
+++ examples/auth0/dashboard-app/src/init-cubejs-api.js
@@ -1,11 +1,11 @@
 import cubejs from '@cubejs-client/core';
- 
-const API_URL = 'http://localhost:4000';
- 
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/cubejs-api/v1';
+
 export default (accessToken) => {
  return cubejs({
-  apiUrl: `${API_URL}/cubejs-api/v1`,
-  headers: { 
+  apiUrl: `${API_URL}`,
+  headers: {
     Authorization: `Bearer ${accessToken}`
   },
  });
