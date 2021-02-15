612812c2e26cce5837ff391c2658da4a9e75dc44
Leonid Yakovlev <leonid@cube.dev>
2021-02-15 18:59:36 +0300

    vue-query-builder: update grid layout

--- examples/vue-query-builder/dashboard-app/src/pages/dashboard/Dashboard.vue
+++ examples/vue-query-builder/dashboard-app/src/pages/dashboard/Dashboard.vue
@@ -1,90 +1,71 @@
 <template>
   <v-container fluid class="text-center background pa-0">
-    <v-snackbar
-      color="success"
-      top
-      v-model="snackbar"
-    >
-      {{ text }}
-
-      <template v-slot:action="{ attrs }">
-        <v-btn
-          text
-          v-bind="attrs"
-          @click="snackbar = false"
-        >
-          Close
-        </v-btn>
-      </template>
-    </v-snackbar>
-    The Dashboard
     <grid-layout
-      :layout.sync="layout"
+      v-if="layout.length"
+      :layout="layout"
       :col-num="12"
-      :row-height="430"
-      :is-draggable="true"
-      :is-resizable="true"
-      :is-mirrored="false"
+      :row-height="30"
       :vertical-compact="true"
-      :margin="[10, 10]"
       :use-css-transforms="true"
-      :preventCollision="false"
-      :responsive="true"
+      @layout-updated="layoutUpdatedEvent"
     >
-        <grid-item
-                   v-for="(item, index) in dashboardItems"
-                   :key="item.id"
-                   :x="layout[index].x"
-                   :y="layout[index].y"
-                   :w="layout[index].w"
-                   :h="layout[index].h"
-                   :i="layout[index].i">
-          <query-renderer :cubejsApi="cubejsApi" :query="JSON.parse(item.vizState).query">
-            <template #default="{ resultSet }">
-              <v-card :loading="!resultSet" class="px-4 py-2">
-                <v-card-title>{{item.name}}</v-card-title>
-                <template v-if="resultSet">
-                  <line-chart v-if="item.type === 'line'" legend="bottom" :data="series(resultSet)"></line-chart>
-                  <area-chart v-else-if="item.type === 'area'" legend="bottom" :data="series(resultSet)"></area-chart>
-                  <pie-chart v-else-if="item.type === 'pie'" :data="pairs(resultSet)"></pie-chart>
-                  <column-chart v-else-if="item.type === 'bar'" :data="seriesPairs(resultSet)"></column-chart>
-                  <div v-else-if="item.type === 'number'">
-                    <div v-for="item in resultSet.series()" :key="item.key">
-                      {{ item.series[0].value }}
-                    </div>
-                  </div>
-                  <Table v-else :data="resultSet"></Table>
-                </template>
+      <grid-item
+        v-for="item in layout"
+        :key="item.i"
+        :x="layout.x"
+        :y="layout.y"
+        :w="layout.w"
+        :h="layout.h"
+        :i="layout.i"
+      >
+        {{item}}
+        <!--<query-renderer :cubejsApi="cubejsApi" :query="JSON.parse(item.vizState).query">-->
+          <!--<template #default="{ resultSet }">-->
+            <!--<v-card :loading="!resultSet" class="px-4 py-2">-->
+              <!--<v-card-title>{{item.name}}</v-card-title>-->
+              <!--<template v-if="resultSet">-->
+                <!--<line-chart v-if="item.type === 'line'" legend="bottom" :data="series(resultSet)"></line-chart>-->
+                <!--<area-chart v-else-if="item.type === 'area'" legend="bottom" :data="series(resultSet)"></area-chart>-->
+                <!--<pie-chart v-else-if="item.type === 'pie'" :data="pairs(resultSet)"></pie-chart>-->
+                <!--<column-chart v-else-if="item.type === 'bar'" :data="seriesPairs(resultSet)"></column-chart>-->
+                <!--<div v-else-if="item.type === 'number'">-->
+                  <!--<div v-for="item in resultSet.series()" :key="item.key">-->
+                    <!--{{ item.series[0].value }}-->
+                  <!--</div>-->
+                <!--</div>-->
+                <!--<Table v-else :data="resultSet"></Table>-->
+              <!--</template>-->
 
-                <v-card-actions>
-                  <v-btn
-                    text
-                    @click="deleteDashboardItem(item.id)"
-                  >
-                    Delete
-                  </v-btn>
-                </v-card-actions>
+              <!--<v-card-actions>-->
+                <!--<v-btn-->
+                  <!--text-->
+                  <!--@click="deleteDashboardItem(item.id)"-->
+                <!--&gt;-->
+                  <!--Delete-->
+                <!--</v-btn>-->
+              <!--</v-card-actions>-->
 
-                <template slot="progress">
-                  <v-progress-linear
-                    color="deep-purple"
-                    height="10"
-                    indeterminate
-                  ></v-progress-linear>
-                </template>
-              </v-card>
-            </template>
-          </query-renderer>
-        </grid-item>
+              <!--<template slot="progress">-->
+                <!--<v-progress-linear-->
+                  <!--color="deep-purple"-->
+                  <!--height="10"-->
+                  <!--indeterminate-->
+                <!--&gt;</v-progress-linear>-->
+              <!--</template>-->
+            <!--</v-card>-->
+          <!--</template>-->
+        <!--</query-renderer>-->
+      </grid-item>
     </grid-layout>
   </v-container>
 </template>
 
 <script>
   import gql from "graphql-tag";
+  import { UPDATE_DASHBOARD_ITEM, DELETE_DASHBOARD_ITEM } from "../../graphql/mutations";
   import { QueryRenderer } from "@cubejs-client/vue";
-  import Table from '../explore/components/Table';
-  import VueGridLayout from 'vue-grid-layout';
+  import Table from "../explore/components/Table";
+  import VueGridLayout from "vue-grid-layout";
 
   export default {
     name: "Dashboard",
@@ -109,7 +90,11 @@
         `,
         pollInterval: 1000,
         deep: true,
-        update: data => data["dashboardItems"]
+        update: data => {
+          console.log(this);
+          console.log(data);
+          return data["dashboardItems"]
+        }
       }
     },
 
@@ -122,12 +107,8 @@
     data() {
       return {
         snackbar: false,
-        text: '',
-        layout: [
-          {"x":0,"y":0,"w":6,"h":1,"i":"0"},
-          {"x":6,"y":0,"w":6,"h":1,"i":"1"},
-        ],
-        index: 0,
+        dashboardItems: [],
+        layout: []
       };
     },
     methods: {
@@ -153,45 +134,47 @@
       },
       async deleteDashboardItem(id) {
         const response = await this.$apollo.mutate({
-          mutation: gql`
-          mutation($id: String!) {
-            deleteDashboardItem(id: $id) {
-              id
-            }
-          }
-        `,
+          mutation: DELETE_DASHBOARD_ITEM,
           variables: {
             id
-          },
+          }
         });
-        this.openSnackBar('Deleted');
-        console.log('>', response);
+        console.log(">", response);
       },
-      openSnackBar(text) {
-        this.text = text;
-        this.snackbar = true;
-        setTimeout(() => {
-          this.text = '';
-          this.snackbar = false;
-        }, 2000);
-      },
-      addItem: function () {
-        // Add a new item. It must have a unique key!
-        this.layout.push({
-          x: (this.layout.length * 2) % (this.colNum || 12),
-          y: this.layout.length + (this.colNum || 12), // puts it at the bottom
-          w: 2,
-          h: 2,
-          i: this.index,
+      layoutUpdatedEvent(newLayout) {
+        newLayout.forEach(l => {
+          const item = this.dashboardItems.find(i => i.id.toString() === l.i);
+          const toUpdate = JSON.stringify({
+            x: l.x,
+            y: l.y,
+            w: l.w,
+            h: l.h
+          });
+          if (item && toUpdate !== item.layout) {
+            const newItem = {...item}
+            const id = newItem.id;
+            delete newItem['id'];
+            delete newItem['__typename'];
+            newItem.layout = toUpdate;
+            this.$apollo.mutate({
+              // Query
+              mutation: UPDATE_DASHBOARD_ITEM,
+              // Parameters
+              variables: {
+                id,
+                input: newItem
+              },
+            }).then((data) => {
+              // Result
+              console.log(data)
+            }).catch((error) => {
+              // Error
+              console.error(error)
+            })
+          }
         });
-        // Increment the counter to ensure key is always unique.
-        this.index++;
-      },
-      removeItem: function (val) {
-        const index = this.layout.map(item => item.i).indexOf(val);
-        this.layout.splice(index, 1);
-      },
-    }
+      }
+    },
   };
 </script>
 
