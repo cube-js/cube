12473cae59e906e52d75f1b15295d2dc1b84143b
Artyom Keydunov <artyom.keydunov@gmail.com>
2021-02-23 12:41:27 -0800

    examples: update auth0 config

--- examples/auth0/cube.js
+++ examples/auth0/cube.js
@@ -1,9 +1,3 @@
 // Server options go here: https://cube.dev/docs/config#options-reference
 module.exports = {
-  jwt: {
-    /**
-     * Please specify your namespace, if you are using custom Rule to add claims to you JWT's payload
-     */
-    claimsNamespace: 'http://localhost:4000/'
-  }
 };
