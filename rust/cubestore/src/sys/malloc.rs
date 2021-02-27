34977b5205134ade419513458068dd9e755ec062
Pavel Tiunov <pavel.tiunov@gmail.com>
2021-02-26 19:02:40 -0800

    fix(cubestore): Malloc trim is broken for docker

--- rust/cubestore/src/sys/malloc.rs
+++ rust/cubestore/src/sys/malloc.rs
@@ -8,17 +8,17 @@
 ///
 /// Use this function after code that produces considerable amount of memory allocations that
 /// **have been already freed**.
-#[cfg(all(target_os = "linux", target_env = "gnu"))] // Musl doesnt support malloc_trim, probably only gnu has it.
+#[cfg(all(target_os = "linux", not(target_env = "musl")))] // Musl doesnt support malloc_trim, probably only gnu has it.
 pub fn trim_allocs() {
     unsafe {
         malloc_trim(0);
     }
 }
 
-#[cfg(not(all(target_os = "linux", target_env = "gnu")))]
+#[cfg(any(not(target_os = "linux"), target_env = "musl"))]
 pub fn trim_allocs() {}
 
-#[cfg(all(target_os = "linux", target_env = "gnu"))] // Musl doesnt support malloc_trim, probably only gnu has it.
+#[cfg(all(target_os = "linux", not(target_env = "musl")))] // Musl doesnt support malloc_trim, probably only gnu has it.
 extern "C" {
     fn malloc_trim(pad: usize) -> i32;
 }
