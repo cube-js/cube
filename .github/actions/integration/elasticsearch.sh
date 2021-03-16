31b9a0934f575d38be7b6d51b54a263d3b8a3782
Dmitry Patsura <talk@dmtry.me>
2021-03-16 14:18:08 +0300

    misc(ci): Rework integration tests to matrix

---
+++ .github/actions/integration/elasticsearch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eo pipefail
+
+export TEST_ELASTIC_OPENDISTRO_VERSION=1.13.1
+
+echo "::group::ElasticSearch Open Distro ${TEST_ELASTIC_OPENDISTRO_VERSION}";
+docker pull amazon/opendistro-for-elasticsearch:${TEST_ELASTIC_OPENDISTRO_VERSION}
+yarn lerna run --concurrency 1 --stream --no-prefix integration:elastic
+echo "::endgroup::"
