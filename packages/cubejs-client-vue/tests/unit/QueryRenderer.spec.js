dc6742f69796ef220b9156d1d6d59588c167f8ad
Vasilev <vaspiring@gmail.com>
2021-03-16 21:23:22 +0300

    tests fix

--- packages/cubejs-client-vue/tests/unit/QueryRenderer.spec.js
+++ packages/cubejs-client-vue/tests/unit/QueryRenderer.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount, mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import flushPromises from 'flush-promises';
 
 import QueryRenderer from '../../src/QueryRenderer';
