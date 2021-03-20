5754f15fa597da3b485b23465e6ba86099a63c4f
Dmitry Patsura <talk@dmtry.me>
2021-03-20 14:21:45 +0300

    misc: migrate testcontainers to ^7.5

--- packages/cubejs-schema-compiler/test/integration/mysql/MySqlDbRunner.js
+++ packages/cubejs-schema-compiler/test/integration/mysql/MySqlDbRunner.js
@@ -78,7 +78,7 @@ export class MySqlDbRunner extends BaseDbRunner {
   async containerLazyInit() {
     const version = process.env.TEST_MYSQL_VERSION || '5.7';
 
-    return new GenericContainer('mysql', version)
+    return new GenericContainer(`mysql:${version}`)
       .withEnv('MYSQL_ROOT_PASSWORD', this.password())
       .withExposedPorts(this.port())
       // workaround for MySQL 8 unsupported auth
