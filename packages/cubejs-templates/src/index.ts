1ee498bb8dc950577e0527ec5bbf5e1238d63b06
Vasilev <vaspiring@gmail.com>
2021-04-01 17:56:13 +0300

    remove porevo

--- packages/cubejs-templates/src/index.ts
+++ packages/cubejs-templates/src/index.ts
@@ -1,7 +1,6 @@
-export { default as AppContainer } from './AppContainer';
-export { default as DependencyTree } from './DependencyTree';
-export { default as PackageFetcher } from './PackageFetcher';
-export { default as DevPackageFetcher } from './DevPackageFetcher';
-export { default as SourceContainer } from './SourceContainer';
-
+export * from './AppContainer';
+export * from './DependencyTree';
+export * from './PackageFetcher';
+export * from './DevPackageFetcher';
+export * from './SourceContainer';
 export * from './utils';
