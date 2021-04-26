2686ddcf51e1fde0f7b7c717481721eadff5702c
Alex Vasilev <vaspiring@gmail.com>
2021-04-26 19:28:20 +0300

    readibility

--- packages/cubejs-testing/src/utils.ts
+++ packages/cubejs-testing/src/utils.ts
@@ -3,7 +3,9 @@ import { platform } from 'os';
 export function getLocalHostnameByOs() {
   if (platform() === 'win32') {
     return 'docker.for.win.localhost';
-  } else if (platform() === 'darwin') {
+  }
+
+  if (platform() === 'darwin') {
     return 'host.docker.internal';
   }
 
