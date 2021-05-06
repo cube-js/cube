77cfb822b24060eb32d7ebc163d983d7a055b0f1
rong fengliang <1141591465@qq.com>
2021-05-06 15:25:57 +0800

    fix test

--- rust/cubestore/src/sql/mod.rs
+++ rust/cubestore/src/sql/mod.rs
@@ -1241,6 +1241,8 @@ mod tests {
                 c.compaction_chunks_count_threshold = 100;
                 c.store_provider = FileStoreProvider::S3 {
                     region: "us-west-2".to_string(),
+                    endpoint:None,
+                    path_style:false,
                     bucket_name: "cube-store-ci-test".to_string(),
                     sub_path: Some("high_frequency_inserts_s3".to_string()),
                 };
@@ -1256,6 +1258,8 @@ mod tests {
                         c.server_name = "127.0.0.1:4306".to_string();
                         c.store_provider = FileStoreProvider::S3 {
                             region: "us-west-2".to_string(),
+                            endpoint:None,
+                            path_style:false,
                             bucket_name: "cube-store-ci-test".to_string(),
                             sub_path: Some("high_frequency_inserts_s3".to_string()),
                         };
@@ -1558,6 +1562,8 @@ mod tests {
                 c.compaction_chunks_count_threshold = 100;
                 c.store_provider = FileStoreProvider::S3 {
                     region: "us-west-2".to_string(),
+                    path_style:false,
+                    endpoint:None,
                     bucket_name: "cube-store-ci-test".to_string(),
                     sub_path: Some("create_table_with_location_cluster".to_string()),
                 };
@@ -1574,6 +1580,8 @@ mod tests {
                         c.server_name = "127.0.0.1:24306".to_string();
                         c.store_provider = FileStoreProvider::S3 {
                             region: "us-west-2".to_string(),
+                            endpoint:None,
+                            path_style:false,
                             bucket_name: "cube-store-ci-test".to_string(),
                             sub_path: Some("create_table_with_location_cluster".to_string()),
                         };
