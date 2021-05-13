de31373d9829a6924d7edc04b96464ffa417d920
Alex.V <vaspiring@gmail.com>
2021-05-13 10:45:19 +0300

    fix(@cubejs-client/core): response error handling (#2703)
    * fix(@cubejs-client/core): response error handling
    
    * test fix
    
    * fixes

--- packages/cubejs-client-core/src/index.js
+++ packages/cubejs-client-core/src/index.js
@@ -108,11 +108,14 @@ class CubejsApi {
         return continueWait(true);
       }
 
-      if (response.status < 200 || response.status > 299) {
-        throw new Error(`Request error. Response status: ${response.status}`);
+      let body = {};
+
+      try {
+        body = await response.clone().json();
+      } catch (_) {
+        body.error = await response.text();
       }
 
-      const body = await response.json();
       if (body.error === 'Continue wait') {
         await checkMutex();
         if (options.progressCallback) {
@@ -120,6 +123,7 @@ class CubejsApi {
         }
         return continueWait();
       }
+
       if (response.status !== 200) {
         await checkMutex();
         if (!options.subscribe && requestInstance.unsubscribe) {
