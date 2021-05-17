c58b46752b680692b817124d4e233252c27b85ed
Dmitry Patsura <talk@dmtry.me>
2021-05-17 20:21:43 +0300

    feat(postgres-driver): Migrate driver to TypeScript

--- packages/cubejs-postgres-driver/src/PostgresDriver.ts
+++ packages/cubejs-postgres-driver/src/PostgresDriver.ts
@@ -1,36 +1,46 @@
-const pg = require('pg');
-const { types } = require('pg');
-const moment = require('moment');
-const { BaseDriver } = require('@cubejs-backend/query-orchestrator');
+import { types, Pool, PoolConfig } from 'pg';
+import moment from 'moment';
+import {
+  BaseDriver,
+  DownloadQueryResults, DownloadTableMemoryData, DriverInterface,
+  GenericDataBaseType, IndexesSQL, TableStructure,
+} from '@cubejs-backend/query-orchestrator';
 
-const { Pool } = pg;
-
-const GenericTypeToPostgres = {
+const GenericTypeToPostgres: Record<GenericDataBaseType, string> = {
   string: 'text',
   double: 'decimal'
 };
 
-const DataTypeMapping = {};
+const DataTypeMapping: Record<string, any> = {};
+
 Object.entries(types.builtins).forEach(pair => {
   const [key, value] = pair;
   DataTypeMapping[value] = key;
 });
 
 const timestampDataTypes = [1114, 1184];
+const timestampTypeParser = (val: any) => moment.utc(val).format(moment.HTML5_FMT.DATETIME_LOCAL_MS);
 
-const timestampTypeParser = val => moment.utc(val).format(moment.HTML5_FMT.DATETIME_LOCAL_MS);
+export type PostgresDriverConfiguration = Partial<PoolConfig> & {
+  storeTimezone?: string,
+  executionTimeout?: number,
+  readOnly?: number,
+};
 
-class PostgresDriver extends BaseDriver {
-  constructor(config) {
+export class PostgresDriver extends BaseDriver implements DriverInterface {
+  protected readonly pool: Pool;
+
+  public constructor(
+    protected readonly config: Partial<PostgresDriverConfiguration> = {}
+  ) {
     super();
-    this.config = config || {};
 
     this.pool = new Pool({
       max: process.env.CUBEJS_DB_MAX_POOL && parseInt(process.env.CUBEJS_DB_MAX_POOL, 10) || 8,
       idleTimeoutMillis: 30000,
       host: process.env.CUBEJS_DB_HOST,
       database: process.env.CUBEJS_DB_NAME,
-      port: process.env.CUBEJS_DB_PORT,
+      port: <any>process.env.CUBEJS_DB_PORT,
       user: process.env.CUBEJS_DB_USER,
       password: process.env.CUBEJS_DB_PASS,
       ssl: this.getSslOptions(),
@@ -41,22 +51,26 @@ class PostgresDriver extends BaseDriver {
     });
   }
 
-  async testConnection() {
+  public async testConnection(): Promise<void> {
     try {
-      return await this.pool.query('SELECT $1::int AS number', ['1']);
+      await this.pool.query('SELECT $1::int AS number', ['1']);
     } catch (e) {
       if (e.toString().indexOf('no pg_hba.conf entry for host') !== -1) {
         throw new Error(`Please use CUBEJS_DB_SSL=true to connect: ${e.toString()}`);
       }
+
       throw e;
     }
   }
 
-  async queryResponse(query, values) {
+  protected async queryResponse(query: string, values: unknown[]) {
     const client = await this.pool.connect();
     try {
       await client.query(`SET TIME ZONE '${this.config.storeTimezone || 'UTC'}'`);
-      await client.query(`set statement_timeout to ${(this.config.hasOwnProperty('executionTimeout')) ? this.config.executionTimeout * 1000 : 600000}`);
+
+      const statementTimeout: number = this.config.executionTimeout ? this.config.executionTimeout * 1000 : 600000;
+      await client.query(`set statement_timeout to ${statementTimeout}`);
+
       const res = await client.query({
         text: query,
         values: values || [],
@@ -69,7 +83,7 @@ class PostgresDriver extends BaseDriver {
               parser = timestampTypeParser;
             }
 
-            return val => parser(val);
+            return (val: any) => parser(val);
           },
         },
       });
@@ -79,11 +93,12 @@ class PostgresDriver extends BaseDriver {
     }
   }
 
-  async query(query, values) {
-    return (await this.queryResponse(query, values)).rows;
+  public async query(query: string, values: unknown[]) {
+    const result = await this.queryResponse(query, values);
+    return result.rows;
   }
 
-  async downloadQueryResults(query, values) {
+  public async downloadQueryResults(query: string, values: unknown[], options: DownloadQueryResults) {
     const res = await this.queryResponse(query, values);
     return {
       rows: res.rows,
@@ -94,15 +109,17 @@ class PostgresDriver extends BaseDriver {
     };
   }
 
-  readOnly() {
+  public readOnly() {
     return !!this.config.readOnly;
   }
 
-  async uploadTableWithIndexes(table, columns, tableData, indexesSql) {
+  public async uploadTableWithIndexes(table: string, columns: TableStructure, tableData: DownloadTableMemoryData, indexesSql: IndexesSQL) {
     if (!tableData.rows) {
       throw new Error(`${this.constructor} driver supports only rows upload`);
     }
+
     await this.createTable(table, columns);
+
     try {
       await this.query(
         `INSERT INTO ${table}
@@ -110,6 +127,7 @@ class PostgresDriver extends BaseDriver {
       SELECT * FROM UNNEST (${columns.map((c, columnIndex) => `${this.param(columnIndex)}::${this.fromGenericType(c.type)}[]`).join(', ')})`,
         columns.map(c => tableData.rows.map(r => r[c.name]))
       );
+
       for (let i = 0; i < indexesSql.length; i++) {
         const [query, p] = indexesSql[i].sql;
         await this.query(query, p);
@@ -120,17 +138,15 @@ class PostgresDriver extends BaseDriver {
     }
   }
 
-  release() {
+  public release() {
     return this.pool.end();
   }
 
-  param(paramIndex) {
+  public param(paramIndex: number) {
     return `$${paramIndex + 1}`;
   }
 
-  fromGenericType(columnType) {
+  public fromGenericType(columnType: string) {
     return GenericTypeToPostgres[columnType] || super.fromGenericType(columnType);
   }
 }
-
-module.exports = PostgresDriver;
