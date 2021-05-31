6e4b6a12d5737e59f9df5da713cc41c5cf4eecb6
Dmitry Patsura <talk@dmtry.me>
2021-06-01 00:55:32 +0300

    fix(mssql-driver): Support case sensitive collation

--- packages/cubejs-mssql-driver/driver/MSSqlDriver.js
+++ packages/cubejs-mssql-driver/driver/MSSqlDriver.js
@@ -67,9 +67,32 @@ class MSSqlDriver extends BaseDriver {
     return `@_${paramIndex + 1}`;
   }
 
+  async tableColumnTypes(table) {
+    const [schema, name] = table.split('.');
+
+    const columns = await this.query(
+      `SELECT column_name as ${this.quoteIdentifier('column_name')},
+             table_name as ${this.quoteIdentifier('table_name')},
+             table_schema as ${this.quoteIdentifier('table_schema')},
+             data_type  as ${this.quoteIdentifier('data_type')}
+      FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE table_name = ${this.param(0)} AND table_schema = ${this.param(1)}`,
+      [name, schema]
+    );
+
+    return columns.map(c => ({ name: c.column_name, type: this.toGenericType(c.data_type) }));
+  }
+
+  getTablesQuery(schemaName) {
+    return this.query(
+      `SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = ${this.param(0)}`,
+      [schemaName]
+    );
+  }
+
   createSchemaIfNotExists(schemaName) {
     return this.query(
-      `SELECT schema_name FROM information_schema.schemata WHERE schema_name = ${this.param(0)}`,
+      `SELECT schema_name FROM INFORMATION_SCHEMA.SCHEMATA WHERE schema_name = ${this.param(0)}`,
       [schemaName]
     ).then((schemas) => {
       if (schemas.length === 0) {
@@ -82,12 +105,12 @@ class MSSqlDriver extends BaseDriver {
   informationSchemaQuery() {
     // fix The multi-part identifier "columns.data_type" could not be bound
     return `
-      SELECT columns.column_name as ${this.quoteIdentifier('column_name')},
-        columns.table_name as ${this.quoteIdentifier('table_name')},
-        columns.table_schema as ${this.quoteIdentifier('table_schema')},
-        columns.data_type as ${this.quoteIdentifier('data_type')}
-      FROM information_schema.columns columns
-      WHERE columns.table_schema NOT IN ('information_schema', 'sys')
+      SELECT column_name as ${this.quoteIdentifier('column_name')},
+        table_name as ${this.quoteIdentifier('table_name')},
+        table_schema as ${this.quoteIdentifier('table_schema')},
+        data_type as ${this.quoteIdentifier('data_type')}
+      FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE table_schema NOT IN ('information_schema', 'sys')
     `;
   }
 
