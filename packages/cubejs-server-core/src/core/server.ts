9a177b00b8c54ff1d7f392811ba295f3bd202073
Alex Vasilev <vaspiring@gmail.com>
2021-06-21 17:48:39 +0300

    chore: reload dbType

--- packages/cubejs-server-core/src/core/server.ts
+++ packages/cubejs-server-core/src/core/server.ts
@@ -479,8 +479,9 @@ export class CubejsServerCore {
   }
 
   protected reloadEnvVariables() {
-    this.options.dbType = this.options.dbType || <DatabaseType | undefined>process.env.CUBEJS_DB_TYPE;
-    this.options.externalDbType = this.options.externalDbType || <DatabaseType|undefined>process.env.CUBEJS_EXT_DB_TYPE;
+    // `CUBEJS_DB_TYPE` has priority because the dbType can change in the Connection Wizard
+    this.options.dbType = <DatabaseType | undefined>process.env.CUBEJS_DB_TYPE || this.options.dbType;
+    this.options.externalDbType = this.options.externalDbType || <DatabaseType | undefined>process.env.CUBEJS_EXT_DB_TYPE;
 
     this.driver = null;
     this.contextToDbType = wrapToFnIfNeeded(this.options.dbType);
