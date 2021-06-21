ed13768d842392491a2545ac2f465d24e43986ba
Dmitry Patsura <talk@dmtry.me>
2021-06-21 21:31:17 +0300

    feat(mssql-driver): Use DATETIME2 for timeStampCast

--- packages/cubejs-schema-compiler/src/adapter/MssqlQuery.js
+++ packages/cubejs-schema-compiler/src/adapter/MssqlQuery.js
@@ -32,7 +32,7 @@ const GRANULARITY_TO_INTERVAL = {
   week: (date) => `dateadd(week, DATEDIFF(week, 0, ${date}), 0)`,
   hour: (date) => `dateadd(hour, DATEDIFF(hour, 0, ${date}), 0)`,
   minute: (date) => `dateadd(minute, DATEDIFF(minute, 0, ${date}), 0)`,
-  second: (date) => `CAST(FORMAT(${date}, 'yyyy-MM-ddTHH:mm:ss.000') AS datetime)`, // until SQL 2016, this causes an int overflow; in SQL 2016 these calls can be changed to DATEDIFF_BIG
+  second: (date) => `CAST(FORMAT(${date}, 'yyyy-MM-ddTHH:mm:ss.000') AS DATETIME2)`, // until SQL 2016, this causes an int overflow; in SQL 2016 these calls can be changed to DATEDIFF_BIG
   month: (date) => `dateadd(month, DATEDIFF(month, 0, ${date}), 0)`,
   year: (date) => `dateadd(year, DATEDIFF(year, 0, ${date}), 0)`,
 };
@@ -58,7 +58,7 @@ export class MssqlQuery extends BaseQuery {
   }
 
   timeStampCast(value) {
-    return `CAST(${value} AS DATETIME)`; // TODO
+    return `CAST(${value} AS DATETIME2)`; // TODO
   }
 
   dateTimeCast(value) {
