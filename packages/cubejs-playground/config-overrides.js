483057a1f4611e372da437b041a084e34d5a74ef
Alex Vasilev <vaspiring@gmail.com>
2021-07-27 19:18:07 +0300

    chore: use the official @rollup/plugin-typescript to speed up builds (#3150)

--- packages/cubejs-playground/config-overrides.js
+++ packages/cubejs-playground/config-overrides.js
@@ -4,44 +4,12 @@ const { addLessLoader } = require('customize-cra');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const rewireYarnWorkspaces = require('react-app-rewire-yarn-workspaces');
 
-const { LESS_VARIABLES } = require('./src/variables');
+const { LESS_VARIABLES } = require('./variables');
 
 module.exports = function override(config, env) {
-  config.optimization = {
-    minimizer: [
-      new TerserPlugin({
-        cache: true,
-        parallel: 2
-      })
-    ],
-    splitChunks: {
-      chunks: 'all',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        babel: {
-          test: /babel/,
-          priority: -5
-        },
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          priority: -20
-        }
-      }
-    }
-  };
   config.stats = 'verbose';
   config.plugins = config.plugins.concat([
     new webpack.ProgressPlugin(),
-    // to fix No module factory available for dependency type: CssDependency
     new MiniCssExtractPlugin(),
   ]);
   if (env === 'production') {
