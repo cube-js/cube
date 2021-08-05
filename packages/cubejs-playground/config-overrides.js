95e05ac67f0c1a77cebe8a27a2c43e99da07f4dd
Alex Vasilev <vaspiring@gmail.com>
2021-08-05 13:17:36 +0300

    chore: pre-build antd styles (#3193)

--- packages/cubejs-playground/config-overrides.js
+++ packages/cubejs-playground/config-overrides.js
@@ -1,26 +1,25 @@
-const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
-const { addLessLoader } = require('customize-cra');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const { addLessLoader } = require('customize-cra');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const rewireYarnWorkspaces = require('react-app-rewire-yarn-workspaces');
 
-const { LESS_VARIABLES } = require('./variables');
+// const { LESS_VARIABLES } = require('./variables');
 
 module.exports = function override(config, env) {
   config.stats = 'verbose';
   config.plugins = config.plugins.concat([
     new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin(),
+    // new MiniCssExtractPlugin(),
   ]);
   if (env === 'production') {
     config.devtool = false;
   }
-  config = addLessLoader({
-    lessOptions: {
-      modifyVars: LESS_VARIABLES,
-      javascriptEnabled: true,
-    },
-  })(config);
+  // config = addLessLoader({
+  //     lessOptions: {
+  //       modifyVars: LESS_VARIABLES,
+  //       javascriptEnabled: true,
+  //     },
+  // })(config);
 
   return rewireYarnWorkspaces(config, env);
 };
