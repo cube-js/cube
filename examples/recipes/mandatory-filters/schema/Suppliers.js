3b150273945e17034b43254c2712b0b4d9f7bbf1
Anton Rychkov <antrychkov@gmail.com>
2021-08-17 09:07:35 +0500

    add template

---
+++ examples/recipes/mandatory-filters/schema/Suppliers.js
@@ -0,0 +1,49 @@
+cube(`Suppliers`, {
+  sql: `SELECT * FROM public.suppliers`,
+  
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
+  },
+  
+  joins: {
+    
+  },
+  
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [id, createdAt]
+    }
+  },
+  
+  dimensions: {
+    id: {
+      sql: `id`,
+      type: `number`,
+      primaryKey: true
+    },
+    
+    address: {
+      sql: `address`,
+      type: `string`
+    },
+    
+    email: {
+      sql: `email`,
+      type: `string`
+    },
+    
+    company: {
+      sql: `company`,
+      type: `string`
+    },
+    
+    createdAt: {
+      sql: `created_at`,
+      type: `time`
+    }
+  },
+  
+  dataSource: `default`
+});
