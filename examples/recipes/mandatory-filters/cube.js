f329a645f76fccb5bd6a57c9056a465e55bb140c
Anton Rychkov <antrychkov@gmail.com>
2021-08-17 10:46:12 +0500

    add filters template

--- examples/recipes/mandatory-filters/cube.js
+++ examples/recipes/mandatory-filters/cube.js
@@ -4,15 +4,21 @@ module.exports = {
             return Object.keys(obj).length === 0;
         }
 
-        let cube = !isEmpty(query.dimensions) ? query.dimensions[0].split('.')[0] : ''
+        let arr = Array.from(query.dimensions, element => element.split('.')[0])
 
-        
-        
-        query.filters.push({
-            member: `${cube}.createdAt`,
-            operator: 'afterDate',
-            values: ['2021-12-01'],
-        });
+        // let cube = !isEmpty(query.dimensions) ? query.dimensions[0].split('.')[0] : ''
+
+        const reducerFn = (elem) => {
+            return {
+                member: `${elem}.createdAt`,
+                operator: 'afterDate',
+                values: ['2019-12-30'],
+                }
+            };
+
+        query.filters.push(
+            arr.reduce(reducerFn)
+        );
 
         return query;
     },
