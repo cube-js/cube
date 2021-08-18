f5b2e8785ceb10c685bdfffe7276c1495d128b5d
Anton Rychkov <antrychkov@gmail.com>
2021-08-18 13:00:16 +0500

    rename container, move files to folder

--- examples/recipes/role-based-access/queries/run.sh
+++ examples/recipes/role-based-access/queries/run.sh
@@ -8,7 +8,7 @@ readyzUrl=readyz
 operatorToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoib3BlcmF0b3IiLCJpYXQiOjE2Mjg3NDUwNDUsImV4cCI6MTgwMTU0NTA0NX0.VErb2t7Bc43ryRwaOiEgXuU5KiolCT-69eI_i2pRq4o
 managerToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoibWFuYWdlciIsImlhdCI6MTYyODc0NTAxMSwiZXhwIjoxODAxNTQ1MDExfQ.1cOAjRHhrFKD7Tg3g57ppVm5nX4eI0zSk8JMbinfzTk
 
-query=$(cat curl/query.json)
+query=$(cat query/queries/query.json)
 
 # wait for the Cube API ready
 until curl -s "$host":"$port"/"$readyzUrl"  > /dev/null; do
