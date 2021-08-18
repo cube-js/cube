ca7c4af33db944dd692f694aa8c5e4f7f2972f2e
Anton Rychkov <antrychkov@gmail.com>
2021-08-18 11:16:37 +0500

    runnable recipe

--- recipes/column-based-multi-tenancy/cube.js
+++ recipes/column-based-multi-tenancy/cube.js
@@ -1,9 +1,8 @@
 module.exports = {
     queryRewrite: (query, { securityContext }) => {
         const queryDimensions = Array.from(query.dimensions, element => element.split('.')[0]);
-        const queyMeasures =  Array.from(query.measures, element => element.split('.')[0]);
 
-        if (queryDimensions.includes('Products') || queyMeasures.includes('Products')) {
+        if (queryDimensions.includes('Products')) {
             query.filters.push(
                 {
                     member: `Suppliers.email`,
