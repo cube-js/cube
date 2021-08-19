c90b140453e18c65a5df6c1389fe9b4546a973a6
Anton Rychkov <antrychkov@gmail.com>
2021-08-19 08:17:43 +0500

    review fix

--- examples/recipes/role-based-access/schema/Orders.js
+++ examples/recipes/role-based-access/schema/Orders.js
@@ -4,25 +4,13 @@ cube(`Orders`, {
   measures: {
     count: {
       type: `count`,
-      drillMembers: [id, createdAt]
     }
   },
   
   dimensions: {
-    id: {
-      sql: `id`,
-      type: `number`,
-      primaryKey: true
-    },
-    
     status: {
       sql: `status`,
       type: `string`
-    },
-    
-    createdAt: {
-      sql: `created_at`,
-      type: `time`
     }
   }
 });
