b193b048db4f104683ca5dc518b09647a8435a4e
Alex Vasilev <vaspiring@gmail.com>
2021-08-20 14:27:55 +0300

    feat(@cubejs-backend/dremio-driver): support quarter granularity

--- packages/cubejs-schema-compiler/src/adapter/MysqlQuery.js
+++ packages/cubejs-schema-compiler/src/adapter/MysqlQuery.js
@@ -6,7 +6,7 @@ import { UserError } from '../compiler/UserError';
 
 const GRANULARITY_TO_INTERVAL = {
   day: (date) => `DATE_FORMAT(${date}, '%Y-%m-%dT00:00:00.000')`,
-  week: (date) => `DATE_FORMAT(date_add('1900-01-01', interval TIMESTAMPDIFF(WEEK, '1900-01-01', ${date}) WEEK), '%Y-%m-%dT00:00:00.000')`,
+  week: (date) => `DATE_FORMAT(DATE_ADD('1900-01-01', INTERVAL TIMESTAMPDIFF(WEEK, '1900-01-01', ${date}) WEEK), '%Y-%m-%dT00:00:00.000')`,
   hour: (date) => `DATE_FORMAT(${date}, '%Y-%m-%dT%H:00:00.000')`,
   minute: (date) => `DATE_FORMAT(${date}, '%Y-%m-%dT%H:%i:00.000')`,
   second: (date) => `DATE_FORMAT(${date}, '%Y-%m-%dT%H:%i:%S.000')`,
