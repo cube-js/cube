d0bc4e5e4c71d3c2765de60b40eb3adcb12e5ab1
Igor Lukanin <me@igorlukanin.io>
2021-08-20 17:09:17 +0400

    Finish

--- examples/recipes/non-additivity/queries/run.sh
+++ examples/recipes/non-additivity/queries/run.sh
@@ -7,9 +7,9 @@ readyzUrl=readyz
 
 token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjEwMDAwMDAwMDAsImV4cCI6NTAwMDAwMDAwMH0.OHZOpOBVKr-sCwn8sbZ5UFsqI3uCs6e4omT7P6WVMFw
 
-statsQuery=$(cat query/queries/stats.json)
 matchingNonAdditiveQuery=$(cat query/queries/matching-non-additive.json)
 nonMatchingNonAdditiveQuery=$(cat query/queries/non-matching-non-additive.json)
+matchingRefactoredQuery=$(cat query/queries/matching-refactored.json)
 
 # Wait for the Cube API to become ready
 until curl -s "$host":"$port"/"$readyzUrl" > /dev/null; do
@@ -19,6 +19,7 @@ done
 # Send the queries
 curl "$host":"$port"/"$loadUrl" -H "Authorization: ${token}" -G -s --data-urlencode "query=${matchingNonAdditiveQuery}" -o matchingNonAdditiveResponse.json
 curl "$host":"$port"/"$loadUrl" -H "Authorization: ${token}" -G -s --data-urlencode "query=${nonMatchingNonAdditiveQuery}" -o nonMatchingNonAdditiveResponse.json
+curl "$host":"$port"/"$loadUrl" -H "Authorization: ${token}" -G -s --data-urlencode "query=${matchingRefactoredQuery}" -o matchingRefactoredResponse.json
 
 echo "Matching (thus, accelerated) non-additive query:"
 echo "$matchingNonAdditiveQuery" | jq '.'
@@ -30,3 +31,9 @@ echo "Non-matching (thus, non-accelerated) non-additive query:"
 echo "$nonMatchingNonAdditiveQuery" | jq '.'
 echo "Matched pre-aggregation:"
 jq ".usedPreAggregations" nonMatchingNonAdditiveResponse.json
+
+echo ""
+echo "Matching (thus, accelerated) query against the refactored cube:"
+echo "$matchingRefactoredQuery" | jq '.'
+echo "Matched pre-aggregation:"
+jq ".usedPreAggregations" matchingRefactoredResponse.json
