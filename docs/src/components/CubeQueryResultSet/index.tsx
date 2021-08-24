1d0f740d50296de5bdeb82c12e91cafc2643b611
Leonid Yakovlev <leonid@cube.dev>
2021-08-24 17:20:10 +0300

    fix(docs): add queryType params

--- docs/src/components/CubeQueryResultSet/index.tsx
+++ docs/src/components/CubeQueryResultSet/index.tsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import cubejs, {CubejsApi} from '@cubejs-client/core';
+import cubejs, { CubejsApi } from '@cubejs-client/core';
 
 const CubeQueryResultSet = (props: propsType) => {
-  const { api, token, query } = props;
+  const { api, token, query, queryType = 'load' } = props;
   const [code, setCode] = useState('');
-  const defaultToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Mjc0NjM2MDZ9.1boj2JrVcsxVkbQsZxuOP21VDxNQyHpxrh3go45k9pc";
+  const defaultToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Mjc0NjM2MDZ9.1boj2JrVcsxVkbQsZxuOP21VDxNQyHpxrh3go45k9pc';
 
-  const cubejsApi = cubejs(
-  token || defaultToken,
-  { apiUrl: api }
-  );
+  const cubejsApi = cubejs(token || defaultToken, { apiUrl: api });
 
   useEffect(() => {
-    fetchDataFromCube(cubejsApi,query,setCode);
+    fetchDataFromCube(cubejsApi, query, queryType, setCode);
   }, []);
 
   return (
@@ -29,15 +27,24 @@ export default CubeQueryResultSet;
 async function fetchDataFromCube(
   cubejsApi: CubejsApi,
   query: object,
-  setCode: (text: string) => void,
+  queryType: string,
+  setCode: (text: string) => void
 ) {
   try {
-    const resultSet = await cubejsApi.load(query);
-    const result = resultSet?.rawData()
-    if (result) {
-      const resultCode = result.length === 1 ? result?.[0] : result;
-      setCode(JSON.stringify(resultCode, null, 2));
+    if (queryType === 'load') {
+      const resultSet = await cubejsApi.load(query);
+      const result = resultSet?.rawData();
+      if (result) {
+        const resultCode = result.length === 1 ? result?.[0] : result;
+        setCode(JSON.stringify(resultCode, null, 2));
+      }
     }
+
+    if (queryType === 'meta') {
+      const resultSet = await cubejsApi.meta(query);
+      setCode(JSON.stringify(resultSet, null, 2));
+    }
+
     highlightCode();
   } catch (e) {
     console.log(e);
@@ -52,4 +59,5 @@ interface propsType {
   api: string;
   token: string;
   query: object;
+  queryType: string | undefined;
 }
