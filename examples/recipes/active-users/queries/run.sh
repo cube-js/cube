5af4a4e037b40a3a389f01d6f5e3390d0f01d171
Anton Rychkov <antrychkov@gmail.com>
2021-08-25 10:55:13 +0500

    add auth token

--- examples/recipes/active-users/queries/run.sh
+++ examples/recipes/active-users/queries/run.sh
@@ -5,6 +5,8 @@ port=4000
 loadUrl=cubejs-api/v1/load
 readyzUrl=readyz
 
+token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoib3BlcmF0b3IiLCJpYXQiOjE2Mjg3NDUwNDUsImV4cCI6MTgwMTU0NTA0NX0.VErb2t7Bc43ryRwaOiEgXuU5KiolCT-69eI_i2pRq4o
+
 query=$(cat query/queries/query.json)
 
 # Wait for the Cube API to become ready
@@ -13,7 +15,7 @@ until curl -s "$host":"$port"/"$readyzUrl"  > /dev/null; do
 done
 
 # Send the query
-curl "$host":"$port"/"$loadUrl" -G -s --data-urlencode "query=${query}" -o usersResponse.json
+curl "$host":"$port"/"$loadUrl" -H "Authorization: ${token}" -G -s --data-urlencode "query=${query}" -o usersResponse.json
 
 echo "Daily, weekly, and monthly active users"
 jq ".data" usersResponse.json
