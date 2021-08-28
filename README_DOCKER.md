c7407ecf94d6506dbea70da40c8e71a1992fa40d
Shlok Kapoor <shlokkapoor@Shloks-MacBook-Pro.local>
2021-08-28 06:51:12 +0530

    tsQuery string

---
+++ README_DOCKER.md
@@ -0,0 +1,2 @@
+sudo docker build -t shlokkapoor/cubejs:latest -f ./packages/cubejs-docker/dev.Dockerfile .
+sudo docker push shlokkapoor/cubejs:latest
--- packages/cubejs-schema-compiler/src/adapter/BaseFilter.js
+++ packages/cubejs-schema-compiler/src/adapter/BaseFilter.js
@@ -243,7 +243,7 @@ export class BaseFilter extends BaseDimension {
   }
 
   tsQueryWhere(column) {
-    return `${column} @@ to_tsquery('${this.firstParameter()}')`;
+    return `${column} @@ to_tsquery(STR(${this.firstParameter()}))`;
   }
 
   expressionEqualsWhere(column) {
