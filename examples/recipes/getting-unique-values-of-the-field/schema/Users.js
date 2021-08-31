a3ce738b23102092bc1c19aded7aa0568f76945a
Anton Rychkov <antrychkov@gmail.com>
2021-08-31 23:08:05 +0500

    remove id dimension

--- examples/recipes/getting-unique-values-of-the-field/schema/Users.js
+++ examples/recipes/getting-unique-values-of-the-field/schema/Users.js
@@ -2,12 +2,6 @@ cube(`Users`, {
   sql: `SELECT * FROM public.users`,
 
   dimensions: {
-    id: {
-      sql: `id`,
-      type: `number`,
-      primaryKey: true,
-    },
-
     city: {
       sql: `city`,
       type: `string`,
