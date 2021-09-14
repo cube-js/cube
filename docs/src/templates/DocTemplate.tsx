16d3f522cb5786cfb0b627b857b8add2b3045fb8
Leonid Yakovlev <leonid@cube.dev>
2021-09-14 17:12:57 +0300

    add styles and remove unused plugin

--- docs/src/templates/DocTemplate.tsx
+++ docs/src/templates/DocTemplate.tsx
@@ -13,7 +13,7 @@ import get from 'lodash/get';
 import last from 'lodash/last';
 import { renameCategory } from '../rename-category';
 
-import 'gatsby-remark-mathjax-ssr/mathjax.css';
+import 'katex/dist/katex.min.css';
 
 import FeedbackBlock from '../components/FeedbackBlock'
 import ScrollLink, {
