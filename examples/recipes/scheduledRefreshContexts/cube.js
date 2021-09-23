fbd3cad058320c2d7cea69abd25e13ad81a5462a
Anton Rychkov <antrychkov@gmail.com>
2021-09-23 12:24:58 +0500

    update cube.js config

--- examples/recipes/scheduledRefreshContexts/cube.js
+++ examples/recipes/scheduledRefreshContexts/cube.js
@@ -1,40 +1,23 @@
-const PostgresDriver = require('@cubejs-backend/postgres-driver');
-
 module.exports = {
   // Provides distinct identifiers for each tenant which are used as caching keys
-  contextToAppId: ({ securityContext }) =>
-    `CUBEJS_APP_${securityContext.env}`,
+  contextToAppId: ({ securityContext }) => `CUBEJS_APP_${securityContext.env}`,
 
-    scheduledRefreshContexts: async () => [
-      {
-        securityContext: {
-          env: 'testing'
-        },
+  // Defines contexts for scheduled pre-aggregation updates
+  scheduledRefreshContexts: async () => [
+    {
+      securityContext: {
+        env: 'testing',
       },
-      {
-        securityContext: {
-          env: 'staging'
-        },
+    },
+    {
+      securityContext: {
+        env: 'staging',
       },
-      {
-        securityContext: {
-          env: 'production'
-        },
-      }
-    ],
-
-  // Selects the database connection configuration based on the tenant name
-  driverFactory: ({ securityContext }) => {
-    if (!securityContext.env) {
-      throw new Error('No env found in Security Context!')
-    } else {
-      return new PostgresDriver({
-        database: 'localDB',
-        host: 'postgres',
-        user: 'postgres',
-        password: 'example',
-        port: '5432',
-      });
-    }
-  },
+    },
+    {
+      securityContext: {
+        env: 'production',
+      },
+    },
+  ]
 };
