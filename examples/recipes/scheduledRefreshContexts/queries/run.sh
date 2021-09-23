13dde4cc4c57f0c408e86686e770ef54fcf25e4c
Anton Rychkov <antrychkov@gmail.com>
2021-09-23 15:01:32 +0500

    run queries in container

--- examples/recipes/scheduledRefreshContexts/queries/run.sh
+++ examples/recipes/scheduledRefreshContexts/queries/run.sh
@@ -16,8 +16,9 @@ port=4000
 loadUrl=cubejs-api/v1/load
 readyzUrl=readyz
 
-avocadoToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGVuYW50IjoiQXZvY2FkbyIsImlhdCI6MTAwMDAwMDAwMCwiZXhwIjo1MDAwMDAwMDAwfQ.0I17p0gd6cKEqYHzwwaI2tbrQASSf7C7cIO8Jnh_WqE
-mangoToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGVuYW50IjoiTWFuZ28iLCJpYXQiOjEwMDAwMDAwMDAsImV4cCI6NTAwMDAwMDAwMH0.RBhdka-5MVR-y1vzbMFZrmfdX0PL_l1vb5G7OjM8hjU
+testingToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW52IjoidGVzdGluZyIsImlhdCI6MTAwMDAwMDAwMCwiZXhwIjo1MDAwMDAwMDAwfQ.RNr0eInvGE3X3pivhcmP60XStKuNWl7lV0xZO30yUmQ
+stagingToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW52Ijoic3RhZ2luZyIsImlhdCI6MTAwMDAwMDAwMCwiZXhwIjo1MDAwMDAwMDAwfQ.o_kQ5kqF98ufgAouUNF4PSXMjdL8mKQmLm6FQZeGdZE
+productionToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW52IjoicHJvZHVjdGlvbiIsImlhdCI6MTAwMDAwMDAwMCwiZXhwIjo1MDAwMDAwMDAwfQ.vdO5Z4ttp2x1_d6vpR3wcfaUj9AxXRMQ_iqKSyTlP94
 
 query=$(cat query/queries/query.json)
 
@@ -27,20 +28,38 @@ until curl -s "$host":"$port"/"$readyzUrl" > /dev/null; do
 done
 
 # Send the query
-curl "$host":"$port"/"$loadUrl" -H "Authorization: ${avocadoToken}" -G -s --data-urlencode "query=${query}" -o avocadoResponse.json
-curl "$host":"$port"/"$loadUrl" -H "Authorization: ${mangoToken}" -G -s --data-urlencode "query=${query}" -o mangoResponse.json
+curl "$host":"$port"/"$loadUrl" -H "Authorization: ${testingToken}" -G -s --data-urlencode "query=${query}" -o testingResponse.json
+curl "$host":"$port"/"$loadUrl" -H "Authorization: ${stagingToken}" -G -s --data-urlencode "query=${query}" -o stagingResponse.json
+curl "$host":"$port"/"$loadUrl" -H "Authorization: ${productionToken}" -G -s --data-urlencode "query=${query}" -o productionResponse.json
 
 
-echo "Avocado JWT payload:"
-decode_jwt $avocadoToken
+echo "Testing env JWT payload:"
+decode_jwt $testingToken
 
-echo "Avocado products:"
-jq ".data" avocadoResponse.json
+echo "Response from query to testing:"
+jq ".data" testingResponse.json
+
+echo "Names of the used pre-aggregations:"
+jq ".usedPreAggregations" testingResponse.json
 
 echo "---------"
 
-echo "Mango JWT payload:"
-decode_jwt $mangoToken
+echo "Staging env JWT payload:"
+decode_jwt $stagingToken
 
-echo "Mango products:"
-jq ".data" mangoResponse.json
+echo "Response from query to staging:"
+jq ".data" stagingResponse.json
+
+echo "Names of the used pre-aggregations:"
+jq ".usedPreAggregations" stagingResponse.json
+
+echo "---------"
+
+echo "Production env JWT payload:"
+decode_jwt $productionToken
+
+echo "Response from query to production:"
+jq ".data" productionResponse.json
+
+echo "Names of the used pre-aggregations:"
+jq ".usedPreAggregations" productionResponse.json
