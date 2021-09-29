074d68e1d02dadc51e0ef4f229b7023014690de8
Anton Rychkov <antrychkov@gmail.com>
2021-09-29 10:26:30 +0500

    update

--- examples/recipes/refreshing-select-partitions/queries/run.sh
+++ examples/recipes/refreshing-select-partitions/queries/run.sh
@@ -7,7 +7,8 @@ readyzUrl=readyz
 
 token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjEwMDAwMDAwMDAsImV4cCI6NTAwMDAwMDAwMH0.OHZOpOBVKr-sCwn8sbZ5UFsqI3uCs6e4omT7P6WVMFw
 
-query=$(cat query/queries/orders.json)
+ordersQuery=$(cat query/queries/orders.json)
+updatedOrdersQuery=$(cat query/queries/updated-orders.json)
 
 # Wait for the Cube API to become ready
 until curl -s "$host":"$port"/"$readyzUrl" > /dev/null; do
@@ -15,28 +16,20 @@ until curl -s "$host":"$port"/"$readyzUrl" > /dev/null; do
 done
 
 # Send the query
-curl "$host":"$port"/"$loadUrl" -H "Authorization: ${token}" -G -s --data-urlencode "query=${query}" -o firstResponse.json
-curl "$host":"$port"/"$loadUrl" -H "Authorization: ${token}" -G -s --data-urlencode "query=${query}" -o secondResponse.json
-curl "$host":"$port"/"$loadUrl" -H "Authorization: ${token}" -G -s --data-urlencode "query=${query}" -o thirdResponse.json
+curl "$host":"$port"/"$loadUrl" -H "Authorization: ${token}" -G -s --data-urlencode "query=${ordersQuery}" -o ordersResponse.json
+curl "$host":"$port"/"$loadUrl" -H "Authorization: ${token}" -G -s --data-urlencode "query=${updatedOrdersQuery}" -o updatedOrdersResponse.json
 
-echo "First:"
-jq ".data" firstResponse.json
 
-echo "First pre-aggregations:"
-jq ".usedPreAggregations" firstResponse.json
+echo "Orders:"
+jq ".data" ordersResponse.json
 
-echo "---------"
-
-echo "Second:"
-jq ".data" secondResponse.json
-
-echo "Second pre-aggregations:"
-jq ".usedPreAggregations" secondResponse.json
+echo "Orders pre-aggregations:"
+jq ".usedPreAggregations" ordersResponse.json
 
 echo "---------"
 
-echo "Third:"
-jq ".data" thirdResponse.json
+echo "Updated orders:"
+jq ".data" updatedOrdersResponse.json
 
-echo "Third pre-aggregations:"
-jq ".usedPreAggregations" thirdResponse.json
+echo "Updated Orders pre-aggregations:"
+jq ".usedPreAggregations" updatedOrdersResponse.json
