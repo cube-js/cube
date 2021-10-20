8f31cc61870fdeeaae85dbe7a6f788e4e43ca02a
Dmitry Patsura <talk@dmtry.me>
2021-10-20 14:59:07 +0300

    test(native): Auth failed with wrong password

--- packages/cubejs-backend-native/test/sql.test.js
+++ packages/cubejs-backend-native/test/sql.test.js
@@ -11,80 +11,29 @@ describe('SQLInteface', () => {
     native.setLogLevel('trace');
   });
 
-  // it('Failed auth', async () => {
-  //   const load = async (extra) => {
-  //     console.log('[js] load',  {
-  //       extra,
-  //     });
-
-  //     throw new Error('Unsupported');
-  //   };
-
-  //   const meta = async (extra) => {
-  //       console.log('[js] meta',  {
-  //         extra,
-  //       });
-
-  //       throw new Error('Unsupported');
-  //   };
-
-  //   const checkAuth = jest.fn(async (extra) => {
-  //     console.log('[js] checkAuth',  {
-  //       extra,
-  //     });
-
-  //     return false;
-  //   });
-
-  //   await native.registerInterface({
-  //     checkAuth,
-  //     load,
-  //     meta,
-  //   });
-
-  //   const connection = mysql.createConnection({
-  //     host : 'localhost',
-  //     user: 'eyJhbGciOiJIUzI1NiJ9.e30.pLPm89qEsoPg-66NIfEJjRQFiW5PYyjfferd4sBx5IU'
-  //   });
-  //   const pingAsync = util.promisify(connection.ping.bind(connection));
-    
-  //   try {
-  //     await pingAsync();
-
-  //     throw new Error('must throw error');
-  //   } catch (e) {
-  //     expect(e.message).toContain('ER_PASSWORD_NO_MATCH: Incorrect user name or password');
-  //   }
-
-  //   expect(checkAuth.mock.calls.length).toEqual(1);
-  //   expect(checkAuth.mock.calls[0][0]).toEqual('eyJhbGciOiJIUzI1NiJ9.e30.pLPm89qEsoPg-66NIfEJjRQFiW5PYyjfferd4sBx5IU');
-
-  //   connection.destroy();
-  // });
-
   it('SHOW FULL TABLES FROM `db`', async () => {
-    const load = jest.fn(async (extra) => {
+    const load = jest.fn(async (payload) => {
       console.log('[js] load',  {
-        extra,
+        payload,
       });
 
       throw new Error('Unsupported');
     });
 
-    const meta = jest.fn(async (extra) => {
+    const meta = jest.fn(async (payload) => {
       console.log('[js] meta',  {
-        extra,
+        payload,
       });
 
       return meta_fixture;
     });
 
-    const checkAuth = jest.fn(async (extra) => {
+    const checkAuth = jest.fn(async (payload) => {
       console.log('[js] checkAuth',  {
-        extra,
+        payload,
       });
 
-      return true;
+      return payload.authorization === 'eyJhbGciOiJIUzI1NiJ9.e30.pLPm89qEsoPg-66NIfEJjRQFiW5PYyjfferd4sBx5IU';
     });
 
     await native.registerInterface({
@@ -92,6 +41,33 @@ describe('SQLInteface', () => {
       load,
       meta,
     });
+
+    const testConnectionFailed = async (user, expectedAuthorization) =>{
+      const connection = mysql.createConnection({
+        host : 'localhost',
+        user,
+      });
+      const pingAsync = util.promisify(connection.ping.bind(connection));
+
+      try {
+        await pingAsync();
+
+        throw new Error('must throw error');
+      } catch (e) {
+        expect(e.message).toContain('ER_PASSWORD_NO_MATCH: Incorrect user name or password');
+      }
+
+      expect(checkAuth.mock.calls.length).toEqual(1);
+      expect(checkAuth.mock.calls[0][0]).toEqual({
+        authorization: expectedAuthorization,
+      });
+    };
+
+    await testConnectionFailed(undefined, null);
+    checkAuth.mockClear();
+
+    await testConnectionFailed('WRONG JWT, AUTH MUST BE REJECTED', 'WRONG JWT, AUTH MUST BE REJECTED');
+    checkAuth.mockClear();
 
     const connection = mysql.createConnection({
       host : 'localhost',
