092e2ccb245294957d8b43b522a5cc4a77406ca1
Ilya Biryukov <iu.biryukov@gmail.com>
2021-10-21 17:58:06 +0300

    chore(cubestore): allow to specify self-referenced workers
    This is a debugging feature. If you specify `@loop:<anything>` in
    `CUBESTORE_WORKERS`, the corresponding worker will be emulated by
    running all requests in the router process.
    
    This helps with debugging how the queries and ingestion is split across
    workers without actually managing separate instances.

--- rust/cubestore/src/cluster/mod.rs
+++ rust/cubestore/src/cluster/mod.rs
@@ -1232,7 +1232,7 @@ impl ClusterImpl {
         node_name: &str,
         m: NetworkMessage,
     ) -> Result<NetworkMessage, CubeError> {
-        if self.server_name == node_name {
+        if self.server_name == node_name || is_self_reference(node_name) {
             // TODO: query_timeout currently used for all messages.
             // TODO timeout config
             Ok(timeout(
@@ -1256,7 +1256,7 @@ impl ClusterImpl {
         m: NetworkMessage,
     ) -> Result<Box<dyn WorkerConnection>, CubeError> {
         assert!(m.is_streaming_request());
-        if self.server_name == node_name {
+        if self.server_name == node_name || is_self_reference(node_name) {
             let c: Box<dyn WorkerConnection> = Box::new(LoopbackConnection {
                 stream: ClusterImpl::start_stream_on_worker(self.clone(), m).await,
             });
@@ -1526,4 +1526,8 @@ impl MessageStream for QueryStream {
         let finished = batch.is_none();
         (NetworkMessage::SelectResultBatch(Ok(batch)), finished)
     }
+}
+
+fn is_self_reference(name: &str) -> bool {
+    name.starts_with("@loop:")
 }
