09204dc348c60c8eb1e89ea2e178ce44e446a6af
Dmitry Patsura <talk@dmtry.me>
2021-10-27 14:41:48 +0300

    feat(cubesql): Use real Query Engine for simple queries

---
+++ rust/cubesql/src/compile/parser.rs
@@ -0,0 +1,25 @@
+use sqlparser::dialect::Dialect;
+
+#[derive(Debug)]
+pub struct MySqlDialectWithBackTicks {}
+
+impl Dialect for MySqlDialectWithBackTicks {
+    fn is_delimited_identifier_start(&self, ch: char) -> bool {
+        ch == '"' || ch == '`'
+    }
+
+    fn is_identifier_start(&self, ch: char) -> bool {
+        // See https://dev.mysql.com/doc/refman/8.0/en/identifiers.html.
+        // We don't yet support identifiers beginning with numbers, as that
+        // makes it hard to distinguish numeric literals.
+        (ch >= 'a' && ch <= 'z')
+            || (ch >= 'A' && ch <= 'Z')
+            || ch == '_'
+            || ch == '$'
+            || (ch >= '\u{0080}' && ch <= '\u{ffff}')
+    }
+
+    fn is_identifier_part(&self, ch: char) -> bool {
+        self.is_identifier_start(ch) || (ch >= '0' && ch <= '9')
+    }
+}
