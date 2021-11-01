c381570b786d27c49deb701c43858cd6e2facf02
Dmitry Patsura <talk@dmtry.me>
2021-11-01 17:36:44 +0300

    feat(cubeclient): Granularity is an optional field

--- rust/cubeclient/src/models/v1_load_result_annotation.rs
+++ rust/cubeclient/src/models/v1_load_result_annotation.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
 pub struct V1LoadResultAnnotation {
     #[serde(rename = "measures")]
     pub measures: serde_json::Value,
