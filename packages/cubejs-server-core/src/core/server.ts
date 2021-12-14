f2a2fb9a092f61aa110da4cd44db69857bc35391
Pavel Tiunov <pavel.tiunov@gmail.com>
2021-12-14 09:40:15 -0800

    feat: Reuse queue by default for multi-tenancy instead of creating it per tenant
    BREAKING CHANGE: Before this change querying queue was created for each tenant

--- packages/cubejs-server-core/src/core/server.ts
+++ packages/cubejs-server-core/src/core/server.ts
@@ -144,7 +144,7 @@ export class CubejsServerCore {
       throw new Error('contextToDataSourceId has been deprecated and removed. Use contextToOrchestratorId instead.');
     }
 
-    this.contextToOrchestratorId = this.options.contextToOrchestratorId || this.contextToAppId;
+    this.contextToOrchestratorId = this.options.contextToOrchestratorId || (() => 'STANDALONE');
 
     // proactively free up old cache values occasionally
     if (this.options.maxCompilerCacheKeepAlive) {
