d1b06aa8e142074d879833bcf37e1d2d1b616599
Alex Vasilev <vaspiring@gmail.com>
2021-12-28 03:37:23 +0300

    chore: unhardcore show variables queries

--- rust/cubesql/cubesql/src/mysql/mod.rs
+++ rust/cubesql/cubesql/src/mysql/mod.rs
@@ -28,7 +28,7 @@ use crate::mysql::dataframe::batch_to_dataframe;
 use crate::schema::SchemaService;
 use crate::schema::V1CubeMetaExt;
 use crate::CubeError;
-use sqlparser::ast::{ShowCreateObject, Statement};
+use sqlparser::ast::{ShowCreateObject, ShowStatementFilter, Statement};
 
 pub mod dataframe;
 
@@ -50,7 +50,6 @@ enum QueryResponse {
 impl Backend {
     async fn execute_query<'a>(&'a mut self, query: &'a str) -> Result<QueryResponse, CubeError> {
         let _start = SystemTime::now();
-
         let query = query.replace("SELECT FROM", "SELECT * FROM");
         debug!("QUERY: {}", query);
 
@@ -70,71 +69,62 @@ impl Backend {
             _ => false,
         };
 
+        let stmt = parse_sql_to_statement(&query)?;
+
+        if let Statement::ShowVariables {
+            filter: Some(ShowStatementFilter::Like(value)),
+        } = stmt
+        {
+            let columns = vec![
+                dataframe::Column::new(
+                    "Variable_name".to_string(),
+                    ColumnType::MYSQL_TYPE_STRING,
+                    ColumnFlags::empty(),
+                ),
+                dataframe::Column::new(
+                    "Value".to_string(),
+                    ColumnType::MYSQL_TYPE_STRING,
+                    ColumnFlags::empty(),
+                ),
+            ];
+
+            match value.as_str() {
+                "aurora\\_version" => return Ok(QueryResponse::ResultSet(
+                    StatusFlags::empty(),
+                    Arc::new(dataframe::DataFrame::new(
+                        columns,
+                        vec![],
+                    )),
+                )),
+                "sql_mode" => return Ok(QueryResponse::ResultSet(
+                    StatusFlags::empty(),
+                    Arc::new(dataframe::DataFrame::new(
+                        columns,
+                        vec![dataframe::Row::new(vec![
+                            dataframe::TableValue::String("sql_mode".to_string()),
+                            dataframe::TableValue::String("ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION".to_string())
+                        ])]
+                    )),
+                )),
+                "lower_case_table_names" => return Ok(QueryResponse::ResultSet(
+                    StatusFlags::empty(),
+                    Arc::new(dataframe::DataFrame::new(
+                        columns,
+                        vec![dataframe::Row::new(vec![
+                            dataframe::TableValue::String("lower_case_table_names".to_string()),
+                            dataframe::TableValue::Int64(0)
+                        ])]
+                    )),
+                )),
+                _ => return Err(CubeError::internal("Unsupported query".to_string()))
+            }
+        }
+
         if query_lower.eq("set autocommit=1, sql_mode = concat(@@sql_mode,',strict_trans_tables')")
         {
             return Ok(QueryResponse::Ok(
                 StatusFlags::SERVER_STATUS_AUTOCOMMIT | StatusFlags::SERVER_SESSION_STATE_CHANGED,
             ));
-        } else if query_lower.eq("show variables like 'aurora\\_version'") {
-            return Ok(QueryResponse::ResultSet(
-                StatusFlags::empty(),
-                Arc::new(dataframe::DataFrame::new(
-                    vec![
-                        dataframe::Column::new(
-                            "Variable_name".to_string(),
-                            ColumnType::MYSQL_TYPE_STRING,
-                            ColumnFlags::empty(),
-                        ),
-                        dataframe::Column::new(
-                            "Value".to_string(),
-                            ColumnType::MYSQL_TYPE_STRING,
-                            ColumnFlags::empty(),
-                        ),
-                    ],
-                    vec![],
-                )),
-            ));
-        }
-        if query_lower.eq("show variables like 'sql_mode'") {
-            return Ok(
-                QueryResponse::ResultSet(StatusFlags::empty(), Arc::new(
-                    dataframe::DataFrame::new(
-                        vec![dataframe::Column::new(
-                            "Variable_name".to_string(),
-                            ColumnType::MYSQL_TYPE_STRING,
-                            ColumnFlags::empty(),
-                        ), dataframe::Column::new(
-                            "Value".to_string(),
-                            ColumnType::MYSQL_TYPE_STRING,
-                            ColumnFlags::empty(),
-                        )],
-                        vec![dataframe::Row::new(vec![
-                            dataframe::TableValue::String("sql_mode".to_string()),
-                            dataframe::TableValue::String("ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION".to_string())
-                        ])]
-                    )
-                )),
-            )
-        } else if query_lower.eq("show variables like 'lower_case_table_names'") {
-            return Ok(
-                QueryResponse::ResultSet(StatusFlags::empty(), Arc::new(
-                    dataframe::DataFrame::new(
-                        vec![dataframe::Column::new(
-                            "Variable_name".to_string(),
-                            ColumnType::MYSQL_TYPE_STRING,
-                            ColumnFlags::empty(),
-                        ), dataframe::Column::new(
-                            "Value".to_string(),
-                            ColumnType::MYSQL_TYPE_LONGLONG,
-                            ColumnFlags::empty(),
-                        )],
-                        vec![dataframe::Row::new(vec![
-                            dataframe::TableValue::String("lower_case_table_names".to_string()),
-                            dataframe::TableValue::Int64(0)
-                        ])]
-                    )
-                )),
-            )
         } else if query_lower.eq("show collation where charset = 'utf8mb4' and collation = 'utf8mb4_bin'") {
             return Ok(
                 QueryResponse::ResultSet(StatusFlags::empty(), Arc::new(
@@ -180,7 +170,7 @@ impl Backend {
                     )
                 )),
             )
-        }else if query_lower.eq("select cast('test plain returns' as char(60)) as anon_1") {
+        } else if query_lower.eq("select cast('test plain returns' as char(60)) as anon_1") {
             return Ok(
                 QueryResponse::ResultSet(StatusFlags::empty(), Arc::new(
                     dataframe::DataFrame::new(
