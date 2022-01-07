6235bae67debd81ec5942b28871a7ebf2f95f686
Cristian Petrescu-Prahova <cristipp@cristipp.dev>
2022-01-07 15:13:58 -0800

    ok

--- packages/cubejs-query-orchestrator/test/unit/PreAggregations.test.js
+++ packages/cubejs-query-orchestrator/test/unit/PreAggregations.test.js
@@ -104,7 +104,7 @@ describe('PreAggregations', () => {
   basicQueryExternalWithRenew.renewQuery = true;
 
   beforeEach(() => {
-    mockDriver = new MockDriver({now: 12345000});
+    mockDriver = new MockDriver({ now: 12345000 });
     mockExternalDriver = new MockDriver();
     mockDriverFactory = async () => mockDriver;
     mockDriverReadOnlyFactory = async () => {
