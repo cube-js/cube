f55c383db4b9ebfb3fe6c5568f1e2f353af81262
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-01-26 18:49:48 -0800

    ok

--- packages/cubejs-backend-shared/src/helpers.ts
+++ packages/cubejs-backend-shared/src/helpers.ts
@@ -34,14 +34,3 @@ export async function executeCommand(
     });
   });
 }
-
-export function assertNonNullable<T>(name: string, x: T): asserts x is NonNullable<T> {
-  if (x === undefined || x === null) {
-    throw new Error(`${name} is not defined.`);
-  }
-}
-
-export function checkNonNullable<T>(name: string, x: T): NonNullable<T> {
-  assertNonNullable(name, x);
-  return x!;
-}
