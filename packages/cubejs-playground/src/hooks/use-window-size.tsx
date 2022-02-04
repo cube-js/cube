cbe7127918ba756ea9927bf3e5a025cfdea76024
Andrey Yamanov <tenphi@gmail.com>
2022-02-04 13:45:51 +0300

    Revert "Revert "fix(@cubejs-client/playground): auto size chart window (#4017)""
    This reverts commit 664f9532f82dc282e1f5132295ab79094808bb03.

---
+++ packages/cubejs-playground/src/hooks/use-window-size.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+export function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  } as { width?: number; height?: number });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
