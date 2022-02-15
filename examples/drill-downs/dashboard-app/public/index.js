33d53b02e9fbafbfc60f84eb4976a3dfa14946b8
Carina <revuwem@gmail.com>
2022-02-15 11:50:02 +0300

    examples(drill-downs): build examples nav

--- examples/drill-downs/dashboard-app/public/index.js
+++ examples/drill-downs/dashboard-app/public/index.js
@@ -1,22 +1,38 @@
+// fetch examples nav items
+const navConfigPath = "nav.config.json";
+const populateExamplesNav = (data) => {
+    // generate nav options from data items
+    const navOptions = data
+        .map(item=>
+            `<li class="dropdown-list-item"><a href="${item.url}">${item.name}</a></li>`)
+        .join("");
+    
+    // set options to menu select
+    const menu = document.getElementsByClassName("menu-list")[0]
+    menu.innerHTML= navOptions
+}
+
+fetch(navConfigPath).then(res => res.json())
+                        .then(data=>populateExamplesNav(data))
+                        .catch();
+
 // feedback handler
 const feedbackLikeBtn = document.getElementById("feedback-like")
 const feedbackDislikeBtn = document.getElementById("feedback-dislike")
 const feedbackBlock = document.getElementsByClassName("feedback")[0]
 const thanksBlock = document.getElementsByClassName("thanks")[0]
 
-function replaceFeedbackBlocks(){
+function replaceFeedbackBlocks() {
     feedbackBlock.classList.add("d-none")
     thanksBlock.classList.remove("d-none")
 }
 
-// add like event listener
-feedbackLikeBtn.addEventListener("click", ()=>{
-    console.log('you selected like')
+feedbackLikeBtn.addEventListener("click", () => {
+    // push vote to feedback log
     replaceFeedbackBlocks()
 })
 
-// add dislike event listener
-feedbackDislikeBtn.addEventListener("click", ()=>{
-    console.log('you selected dislike')
+feedbackDislikeBtn.addEventListener("click", () => {
+    // push vote to feedback log
     replaceFeedbackBlocks()
 })
