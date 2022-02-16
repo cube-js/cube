c30b024146676a4ca49188136168f15c3de4a75e
Carina <revuwem@gmail.com>
2022-02-16 14:27:09 +0300

    examples(drill-downs): setup feedback tracking

--- examples/drill-downs/dashboard-app/public/index.js
+++ examples/drill-downs/dashboard-app/public/index.js
@@ -1,20 +1,23 @@
+const cubeTracking = require("cubedev-tracking")
+const { event: cubeTrackingEvent } = cubeTracking
+
 // fetch examples nav items
 const navConfigPath = "nav.config.json";
 const populateExamplesNav = (data) => {
     // generate nav options from data items
     const navOptions = data
-        .map(item=>
+        .map(item =>
             `<li class="dropdown-list-item"><a href="${item.url}">${item.name}</a></li>`)
         .join("");
-    
+
     // set options to menu select
     const menu = document.getElementsByClassName("menu-list")[0]
-    menu.innerHTML= navOptions
+    menu.innerHTML = navOptions
 }
 
 fetch(navConfigPath).then(res => res.json())
-                        .then(data=>populateExamplesNav(data))
-                        .catch();
+    .then(data => populateExamplesNav(data))
+    .catch();
 
 // feedback handler
 const feedbackLikeBtn = document.getElementById("feedback-like")
@@ -27,12 +30,24 @@ function replaceFeedbackBlocks() {
     thanksBlock.classList.remove("d-none")
 }
 
+function getFeedbackState(status) {
+    return {
+        page: window.location.host,
+        date: new Date().toISOString(),
+        feedback: status,
+    }
+}
+
 feedbackLikeBtn.addEventListener("click", () => {
-    // push vote to feedback log
+    // push vote to feedback log    
+    const feedbackState = getFeedbackState("like");
+    cubeTrackingEvent("example_feedback_like", feedbackState)
     replaceFeedbackBlocks()
 })
 
 feedbackDislikeBtn.addEventListener("click", () => {
     // push vote to feedback log
+    const feedbackState = getFeedbackState("dislike")
+    cubeTrackingEvent("example_feedback_like", feedbackState)
     replaceFeedbackBlocks()
 })
