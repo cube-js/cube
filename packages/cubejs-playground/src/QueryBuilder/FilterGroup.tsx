e3d17ae7b2b8340ee39ea6464773b2676dc5a9f6
Alex Vasilev <vaspiring@gmail.com>
2022-02-18 11:55:34 +0300

    fix(playground): prevent params to shift around when removing filters

--- packages/cubejs-playground/src/QueryBuilder/FilterGroup.tsx
+++ packages/cubejs-playground/src/QueryBuilder/FilterGroup.tsx
@@ -42,7 +42,7 @@ const FilterGroup = ({
       );
 
       return (
-        <Fragment key={m.index}>
+        <Fragment key={m.member}>
           {isMissing ? (
             <MissingMemberTooltip>{buttonGroup}</MissingMemberTooltip>
           ) : (
