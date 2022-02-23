06b539bf13bffe597f855018d8d80fe383812df3
Andrey Pechkurov <apechkurov@gmail.com>
2022-02-23 10:29:09 +0300

    Remove unused cubes in tests

--- packages/cubejs-questdb-driver/test/QuestQuery.test.ts
+++ packages/cubejs-questdb-driver/test/QuestQuery.test.ts
@@ -38,57 +38,6 @@ describe('QuestQuery', () => {
           sql: 'name'
         }
       }
-    })
-
-    cube(\`Deals\`, {
-      sql: \`select * from deals\`,
-    
-      measures: {
-        amount: {
-          sql: \`amount\`,
-          type: \`sum\`
-        }
-      },
-
-      dimensions: {
-        salesManagerId: {
-          sql: \`sales_manager_id\`,
-          type: 'string',
-          primaryKey: true
-        }
-      }
-    })
-    
-    cube(\`SalesManagers\`, {
-      sql: \`select * from sales_managers\`,
-    
-      joins: {
-        Deals: {
-          relationship: \`hasMany\`,
-          sql: \`\${SalesManagers}.id = \${Deals}.sales_manager_id\`
-        }
-      },
-      
-      measures: {
-        averageDealAmount: {
-          sql: \`\${dealsAmount}\`,
-          type: \`avg\`
-        }
-      },
-    
-      dimensions: {
-        id: {
-          sql: \`id\`,
-          type: \`string\`,
-          primaryKey: true
-        },
-    
-        dealsAmount: {
-          sql: \`\${Deals.amount}\`,
-          type: \`number\`,
-          subQuery: true
-        }
-      }
     });
     `, []);
 
