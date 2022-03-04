527b7a8cda3a8c71b24b4d81c56807717af92229
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-03-04 11:24:46 -0800

    chore(testing): Splits @cubejs/testing-shared from e2e tests in @cubejs/testing, removes @cubejs/testing from dev.Dockerfile

--- packages/cubejs-testing-shared/src/testing/query-test.abstract.ts
+++ packages/cubejs-testing-shared/src/testing/query-test.abstract.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { jest, describe, it, beforeAll, afterAll } from '@jest/globals';
 import { BaseDriver } from '@cubejs-backend/query-orchestrator';
 import { BaseQuery, prepareCompiler as originalPrepareCompiler } from '@cubejs-backend/schema-compiler';
 import { StartedTestContainer } from 'testcontainers';
