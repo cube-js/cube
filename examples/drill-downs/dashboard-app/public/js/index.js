e8d48d701db3c512bd38a630053541a4bb9188b9
Carina <revuwem@gmail.com>
2022-03-05 15:13:00 +0300

    examples(drill-downs): refactor file structure

--- examples/drill-downs/dashboard-app/public/js/index.js
+++ examples/drill-downs/dashboard-app/public/js/index.js
@@ -4,68 +4,6 @@ const minDesktopWidth = getComputedStyle(document.documentElement)
                             .replace("px", "");
 const menuOverflowMaxHeight = getComputedStyle(document.documentElement)
                                 .getPropertyValue("--menu-overflow-max-height")
-// DOM
-const menuList = document.getElementById("menu-list")
-const menuCurrent = document.getElementById("menu-current")
-const menuButton = document.getElementById("menu-button")
-
-// fetch examples nav items
-const NAV_CONFIG_PATH = "examples-nav.config.json";
-// fetch nav items from config file
-
-
-const loadNavItems = () => fetch(NAV_CONFIG_PATH).then(res => res.json())
-.then(data => populateExamplesNav(data))
-.catch();
-
-setTimeout(()=>loadNavItems(), 700)
-
-const populateExamplesNav = (data) => {
-    // find current nav item index
-    const currentNavItemIndex = data.map(item => item.url).indexOf(window.location.href)
-    // remove current nav item from data
-    const currentNavItem = data.splice(currentNavItemIndex, 1)[0]
-
-    // generate nav options from data items
-    const navItems = data
-        .map(item =>
-            `<li class="dropdown-list-item"><a class="dropdown-link" href="${item.url}">${item.name}</a></li>`)
-        .join("");
-
-    // remove loader
-    menuButton.classList.toggle("load")
-    // set options to menu select
-    menuList.innerHTML = navItems
-    // set current item name as menu button text
-    menuCurrent.innerHTML = currentNavItem.name
-
-    // apply dropdown accessibility only when dropdown-list-items are rendered
-    applyDropdownAccessibility()
-
-    // if there is more then 8 menu items
-    // set such a height so that the user understands 
-    // that it is possible to scroll down
-    if (data.length > 7 && window.innerWidth >= minDesktopWidth) {
-        // menuList.style.maxHeight = menuOverflowMaxHeight
-        menuList.classList.add("overflow")
-    }
-
-}
-
-// dropdown menu accessibilty
-function applyDropdownAccessibility() {
-    const dropdownLinks = document.querySelectorAll(".dropdown-link")
-    dropdownLinks.forEach(link => {
-        link.addEventListener("focus", function () {
-            dropdownMenuBtn.setAttribute("aria-expanded", true)
-        })
-    })
-
-    const lastDropdownLinkItem = dropdownLinks.length - 1
-    dropdownLinks[lastDropdownLinkItem].addEventListener("blur", function () {
-        dropdownMenuBtn.setAttribute("aria-expanded", false)
-    })
-}
 
 // dropdown menu functionality
 const dropdownMenuBtn = document.querySelector(".dropdown-button")
