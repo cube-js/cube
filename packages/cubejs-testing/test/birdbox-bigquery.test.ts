7153341b975300254597718cb756ed0cef72ba2c
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-03-09 21:20:29 -0800

    ok

--- packages/cubejs-testing/test/birdbox-bigquery.test.ts
+++ packages/cubejs-testing/test/birdbox-bigquery.test.ts
@@ -2,5 +2,5 @@ import { createDriverTestCase } from './birdbox-driver.test';
 
 createDriverTestCase(
   'bigquery',
-  ['CUBEJS_DB_BQ_PROJECT_ID', 'CUBEJS_DB_BQ_EXPORT_BUCKET', 'CUBEJS_DB_BQ_CREDENTIALS']
+  ['CUBEJS_DB_BQ_PROJECT_ID', 'CUBEJS_DB_EXPORT_BUCKET', 'CUBEJS_DB_BQ_CREDENTIALS']
 );
