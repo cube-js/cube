605eee6b4431efaa4ee88b0cb779f91177d4dcbb
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-03-10 10:42:54 -0800

    ok

--- .github/actions/integration/athena.sh
+++ .github/actions/integration/athena.sh
@@ -5,6 +5,7 @@ set -eo pipefail
 export DEBUG=testcontainers
 
 echo "::group::Athena [cloud]"
+# export CUBEJS_DB_EXPORT_BUCKET=${CUBEJS_DB_ATHENA_EXPORT_BUCKET}
 export CUBEJS_DB_EXPORT_BUCKET=s3://cubejs-opensource/testing/export
 yarn lerna run --concurrency 1 --stream --no-prefix integration:athena
 echo "::endgroup::"
