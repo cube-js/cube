9e0477e93ed76dc8068cd4de09776ec34fc1aaff
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-03-10 10:40:22 -0800

    ok

--- .github/actions/integration/bigquery.sh
+++ .github/actions/integration/bigquery.sh
@@ -5,6 +5,6 @@ set -eo pipefail
 export DEBUG=testcontainers
 
 echo "::group::BigQuery [cloud]"
-export CUBEJS_DB_EXPORT_BUCKET=cube-open-source-export-bucket
+export CUBEJS_DB_EXPORT_BUCKET=${CUBEJS_DB_BQ_EXPORT_BUCKET}
 yarn lerna run --concurrency 1 --stream --no-prefix integration:bigquery
 echo "::endgroup::"
