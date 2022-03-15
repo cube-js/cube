6fb0835647eeb2b8f6b3f05f9d90164382327a46
Dmitry Patsura <talk@dmtry.me>
2022-03-15 17:31:30 +0300

    feat(cubesql): Enable PostgresServer via env variable

--- rust/cubesql/cubesql/e2e/main.rs
+++ rust/cubesql/cubesql/e2e/main.rs
@@ -1,8 +1,12 @@
 use std::fmt::Debug;
 
+use cubesql::telemetry::ReportingLogger;
+use log::Level;
+use simple_logger::SimpleLogger;
 use tests::{
     basic::{AsyncTestConstructorResult, AsyncTestSuite},
     mysql::MySqlIntegrationTestSuite,
+    postgres::PostgresIntegrationTestSuite,
 };
 
 pub mod tests;
@@ -30,9 +34,19 @@ impl TestsRunner {
 fn main() {
     let rt = tokio::runtime::Runtime::new().unwrap();
 
+    let log_level = Level::Trace;
+
+    let logger = SimpleLogger::new()
+        .with_level(Level::Error.to_level_filter())
+        .with_module_level("cubeclient", log_level.to_level_filter())
+        .with_module_level("cubesql", log_level.to_level_filter());
+
+    ReportingLogger::init(Box::new(logger), log_level.to_level_filter()).unwrap();
+
     rt.block_on(async {
         let mut runner = TestsRunner::new();
         runner.register_suite(MySqlIntegrationTestSuite::before_all().await);
+        runner.register_suite(PostgresIntegrationTestSuite::before_all().await);
 
         for suites in runner.suites.iter_mut() {
             suites.run().await.unwrap();
