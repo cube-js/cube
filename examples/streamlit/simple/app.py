b1bc10d5fbf86c695d24b48d1b0fa56443401320
Artyom Keydunov <artyom.keydunov@gmail.com>
2022-03-16 18:51:10 -0700

    Update simple/app.py

--- examples/streamlit/simple/app.py
+++ examples/streamlit/simple/app.py
@@ -12,7 +12,7 @@ Query Cube via SQL API from Streamlit.
 
 """
 
-connection_string = 'mysql+pymysql://cube@conservation-seeley:b0ae7aa12cc92ec8ea10d197ce8efd0f@conservation-seeley.sql.aws-us-east-2.cubecloudapp.dev/db';
+connection_string = 'TODO';
 with streamlit.echo():
     conn = create_engine(connection_string)
     df = pandas.read_sql_query('select count, status from Orders', conn)
