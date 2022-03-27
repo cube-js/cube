pxaris/cube.js

feat(cubesql): Rewrite engine first steps

rust/cubesql/cubesql/src/compile/engine/provider.rs
@@ -15,7 +15,17 @@ use super::information_schema::{
     schemata::InfoSchemaSchemataProvider, statistics::InfoSchemaStatisticsProvider,
     tables::InfoSchemaTableProvider, variables::PerfSchemaVariablesProvider,
 };
+use crate::transport::V1CubeMetaExt;
+use crate::CubeError;
+use async_trait::async_trait;
+use datafusion::arrow::datatypes::{DataType, Field, Schema, SchemaRef, TimeUnit};
+use datafusion::datasource::TableProvider;
+use datafusion::error::DataFusionError;
+use datafusion::logical_plan::Expr;
+use datafusion::physical_plan::ExecutionPlan;
+use std::any::Any;
 
+#[derive(Clone)]
 pub struct CubeContext<'a> {
     /// Internal state for the context (default)
     pub state: &'a ExecutionContextState,
@@ -26,6 +36,37 @@ pub struct CubeContext<'a> {
 impl<'a> CubeContext<'a> {
     pub fn new(state: &'a ExecutionContextState, cubes: &'a Vec<V1CubeMeta>) -> Self {
         Self { state, cubes }
+    }
+
+    pub fn table_name_by_table_provider(
+        &self,
+        table_provider: Arc<dyn datasource::TableProvider>,
+    ) -> Result<String, CubeError> {
+        let any = table_provider.as_any();
+        Ok(if let Some(t) = any.downcast_ref::<CubeTableProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<InfoSchemaTableProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<InfoSchemaColumnsProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<InfoSchemaStatisticsProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<InfoSchemaKeyColumnUsageProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<InfoSchemaSchemataProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<InfoSchemaReferentialConstraintsProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<InfoSchemaCollationsProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<PerfSchemaVariablesProvider>() {
+            t.table_name().to_string()
+        } else {
+            return Err(CubeError::internal(format!(
+                "Unknown table provider with schema: {:?}",
+                table_provider.schema()
+            )));
+        })
     }
 }
 
@@ -43,10 +84,13 @@ impl<'a> ContextProvider for CubeContext<'a> {
                 schema,
                 table,
             } => Some(format!("{}.{}.{}", catalog, schema, table)),
-            _ => None,
+            datafusion::catalog::TableReference::Bare { table } => Some(table.to_string()),
         };
 
         if let Some(tp) = table_path {
+            if let Some(cube) = self.cubes.iter().find(|c| c.name.eq_ignore_ascii_case(&tp)) {
+                return Some(Arc::new(CubeTableProvider::new(cube.clone()))); // TODO .clone()
+            }
             if tp.eq_ignore_ascii_case("information_schema.tables") {
                 return Some(Arc::new(InfoSchemaTableProvider::new(self.cubes)));
             }
@@ -76,11 +120,15 @@ impl<'a> ContextProvider for CubeContext<'a> {
             }
 
             if tp.eq_ignore_ascii_case("performance_schema.global_variables") {
-                return Some(Arc::new(PerfSchemaVariablesProvider::new()));
+                return Some(Arc::new(PerfSchemaVariablesProvider::new(
+                    "performance_schema.global_variables".to_string(),
+                )));
             }
 
             if tp.eq_ignore_ascii_case("performance_schema.session_variables") {
-                return Some(Arc::new(PerfSchemaVariablesProvider::new()));
+                return Some(Arc::new(PerfSchemaVariablesProvider::new(
+                    "performance_schema.session_variables".to_string(),
+                )));
             }
         };
 
@@ -93,5 +141,66 @@ impl<'a> ContextProvider for CubeContext<'a> {
 
     fn get_aggregate_meta(&self, name: &str) -> Option<Arc<AggregateUDF>> {
         self.state.aggregate_functions.get(name).cloned()
+    }
+}
+
+pub trait TableName {
+    fn table_name(&self) -> &str;
+}
+
+pub struct CubeTableProvider {
+    cube: V1CubeMeta,
+}
+
+impl CubeTableProvider {
+    pub fn new(cube: V1CubeMeta) -> Self {
+        Self { cube }
+    }
+}
+
+impl TableName for CubeTableProvider {
+    fn table_name(&self) -> &str {
+        &self.cube.name
+    }
+}
+
+#[async_trait]
+impl TableProvider for CubeTableProvider {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn schema(&self) -> SchemaRef {
+        Arc::new(Schema::new(
+            self.cube
+                .get_columns()
+                .into_iter()
+                .map(|c| {
+                    Field::new(
+                        c.get_name(),
+                        match c.get_data_type().as_str() {
+                            "datetime" => DataType::Timestamp(TimeUnit::Millisecond, None),
+                            "boolean" => DataType::Boolean,
+                            "int" => DataType::Int64,
+                            _ => DataType::Utf8,
+                        },
+                        true,
+                    )
+                })
+                .collect(),
+        ))
+    }
+
+    async fn scan(
+        &self,
+        _projection: &Option<Vec<usize>>,
+        _batch_size: usize,
+        _filters: &[Expr],
+        _limit: Option<usize>,
+    ) -> Result<Arc<dyn ExecutionPlan>, DataFusionError> {
+        Err(DataFusionError::Plan(format!(
+            "Not rewritten table scan node for '{}' cube",
+            self.cube.name
+        )))
     }
 }

cube-js/cube

rust/cubesql/cubesql/src/compile/engine/provider.rs
@@ -29,7 +29,19 @@ use super::information_schema::postgres::{
     tables::InfoSchemaTableProvider as PostgresSchemaTableProvider, PgCatalogNamespaceProvider,
     PgCatalogTableProvider, PgCatalogTypeProvider,
 };
+use crate::compile::engine::information_schema::mysql::ext::CubeColumnMySqlExt;
+use crate::transport::V1CubeMetaExt;
+use crate::CubeError;
+use async_trait::async_trait;
+use cubeclient::models::V1CubeMeta;
+use datafusion::arrow::datatypes::{DataType, Field, Schema, SchemaRef, TimeUnit};
+use datafusion::datasource::TableProvider;
+use datafusion::error::DataFusionError;
+use datafusion::logical_plan::Expr;
+use datafusion::physical_plan::ExecutionPlan;
+use std::any::Any;
 
+#[derive(Clone)]
 pub struct CubeContext<'a> {
     /// Internal state for the context (default)
     pub state: &'a ExecutionContextState,
@@ -53,6 +65,37 @@ impl<'a> CubeContext<'a> {
             session_state,
         }
     }
+
+    pub fn table_name_by_table_provider(
+        &self,
+        table_provider: Arc<dyn datasource::TableProvider>,
+    ) -> Result<String, CubeError> {
+        let any = table_provider.as_any();
+        Ok(if let Some(t) = any.downcast_ref::<CubeTableProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<MySqlSchemaTableProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<MySqlSchemaColumnsProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<MySqlSchemaStatisticsProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<MySqlSchemaKeyColumnUsageProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<MySqlSchemaSchemataProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<MySqlSchemaReferentialConstraintsProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<MySqlSchemaCollationsProvider>() {
+            t.table_name().to_string()
+        } else if let Some(t) = any.downcast_ref::<MySqlPerfSchemaVariablesProvider>() {
+            t.table_name().to_string()
+        } else {
+            return Err(CubeError::internal(format!(
+                "Unknown table provider with schema: {:?}",
+                table_provider.schema()
+            )));
+        })
+    }
 }
 
 impl<'a> ContextProvider for CubeContext<'a> {
@@ -69,11 +112,11 @@ impl<'a> ContextProvider for CubeContext<'a> {
                 schema,
                 table,
             } => Some(format!("{}.{}.{}", catalog, schema, table)),
-            _ => None,
+            datafusion::catalog::TableReference::Bare { table } => Some(table.to_string()),
         };
 
         if let Some(tp) = table_path {
-            return self.session_state.protocol.get_provider(self.clone(), tp);
+            return self.session_state.protocol.get_provider(&self.clone(), tp);
         }
 
         None
@@ -105,6 +148,15 @@ impl DatabaseProtocol {
         context: &CubeContext,
         tp: String,
     ) -> Option<std::sync::Arc<dyn datasource::TableProvider>> {
+        if let Some(cube) = context
+            .meta
+            .cubes
+            .iter()
+            .find(|c| c.name.eq_ignore_ascii_case(&tp))
+        {
+            return Some(Arc::new(CubeTableProvider::new(cube.clone()))); // TODO .clone()
+        }
+
         if tp.eq_ignore_ascii_case("information_schema.tables") {
             return Some(Arc::new(MySqlSchemaTableProvider::new(&context.meta.cubes)));
         }
@@ -143,6 +195,7 @@ impl DatabaseProtocol {
 
         if tp.eq_ignore_ascii_case("performance_schema.global_variables") {
             return Some(Arc::new(MySqlPerfSchemaVariablesProvider::new(
+                "performance_schema.global_variables".to_string(),
                 context
                     .sessions
                     .server
@@ -152,6 +205,7 @@ impl DatabaseProtocol {
 
         if tp.eq_ignore_ascii_case("performance_schema.session_variables") {
             return Some(Arc::new(MySqlPerfSchemaVariablesProvider::new(
+                "performance_schema.session_variables".to_string(),
                 context.session_state.all_variables(),
             )));
         }
@@ -189,5 +243,66 @@ impl DatabaseProtocol {
         }
 
         None
+    }
+}
+
+pub trait TableName {
+    fn table_name(&self) -> &str;
+}
+
+pub struct CubeTableProvider {
+    cube: V1CubeMeta,
+}
+
+impl CubeTableProvider {
+    pub fn new(cube: V1CubeMeta) -> Self {
+        Self { cube }
+    }
+}
+
+impl TableName for CubeTableProvider {
+    fn table_name(&self) -> &str {
+        &self.cube.name
+    }
+}
+
+#[async_trait]
+impl TableProvider for CubeTableProvider {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn schema(&self) -> SchemaRef {
+        Arc::new(Schema::new(
+            self.cube
+                .get_columns()
+                .into_iter()
+                .map(|c| {
+                    Field::new(
+                        c.get_name(),
+                        match c.get_data_type().as_str() {
+                            "datetime" => DataType::Timestamp(TimeUnit::Millisecond, None),
+                            "boolean" => DataType::Boolean,
+                            "int" => DataType::Int64,
+                            _ => DataType::Utf8,
+                        },
+                        true,
+                    )
+                })
+                .collect(),
+        ))
+    }
+
+    async fn scan(
+        &self,
+        _projection: &Option<Vec<usize>>,
+        _batch_size: usize,
+        _filters: &[Expr],
+        _limit: Option<usize>,
+    ) -> Result<Arc<dyn ExecutionPlan>, DataFusionError> {
+        Err(DataFusionError::Plan(format!(
+            "Not rewritten table scan node for '{}' cube",
+            self.cube.name
+        )))
     }
 }