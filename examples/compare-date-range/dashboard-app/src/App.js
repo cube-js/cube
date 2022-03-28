ccf43e2ffe5412e0ad384f4709a43fb6ef28c4fb
Carina <revuwem@gmail.com>
2022-03-29 01:12:29 +0300

    examples(compare-date-range): refactor react app

--- examples/compare-date-range/dashboard-app/src/App.js
+++ examples/compare-date-range/dashboard-app/src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Layout } from 'antd';
 import cubejs from '@cubejs-client/core';
 import { CubeProvider } from '@cubejs-client/react';
-import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 
 const cubejsApi = cubejs(process.env.REACT_APP_CUBEJS_TOKEN, {
   apiUrl: process.env.REACT_APP_API_URL
 });
 
-const AppLayout = () => (
-  <Layout>
-    <Header />
-    <Layout.Content className='examples__container'>
-      <Dashboard />
-    </Layout.Content>
-  </Layout>
+const AppLayout = ({ children }) => (
+  <div className="app">{children}</div>
 );
 
 const App = () => (
   <CubeProvider cubejsApi={cubejsApi}>
-    <AppLayout></AppLayout>
+    <AppLayout>
+      <Dashboard />
+    </AppLayout>
   </CubeProvider>
 );
 
