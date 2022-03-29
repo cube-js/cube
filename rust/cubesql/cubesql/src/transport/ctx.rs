88db9eab3854a89cd93cfdce3a9fad9a180f3b45
George <g.andronchik@gmail.com>
2022-03-29 18:42:48 +0800

    feat(cubesql): Global Meta Tables

--- rust/cubesql/cubesql/src/transport/ctx.rs
+++ rust/cubesql/cubesql/src/transport/ctx.rs
@@ -3,9 +3,24 @@ use cubeclient::models::V1CubeMeta;
 #[derive(Debug)]
 pub struct MetaContext {
     pub cubes: Vec<V1CubeMeta>,
+    pub tables: Vec<CubeMetaTable>,
+}
+
+#[derive(Debug, Clone)]
+pub struct CubeMetaTable {
+    oid: u32,
+    name: String,
 }
 
 impl MetaContext {
+    pub fn new(cubes: Vec<V1CubeMeta>) -> Self {
+        // 18000 - max system table oid
+        let mut oid: u32 = 18000;
+        let tables: Vec<CubeMetaTable> = cubes.iter().map(|cube| { oid += 10; CubeMetaTable { oid, name: cube.name.clone() } }).collect();
+
+        Self { cubes, tables }
+    }
+
     pub fn find_cube_with_name(&self, name: String) -> Option<V1CubeMeta> {
         for cube in self.cubes.iter() {
             if cube.name.eq(&name) {
@@ -14,5 +29,13 @@ impl MetaContext {
         }
 
         None
+    }
+
+    pub fn find_cube_table_with_oid(&self, oid: u32) -> Option<CubeMetaTable> {
+        self.tables.iter().find(|table| table.oid == oid).cloned()
+    }
+
+    pub fn find_cube_table_with_name(&self, name: String) -> Option<CubeMetaTable> {
+        self.tables.iter().find(|table| table.name == name).cloned()
     }
 }
