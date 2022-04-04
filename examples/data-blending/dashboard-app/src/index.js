8a190183d8dee3a9ae3fb7241edf1c9de262ec63
Carina <revuwem@gmail.com>
2022-04-04 11:17:51 +0300

    examples(data-blending): add cube-example-wrapper

--- examples/data-blending/dashboard-app/src/index.js
+++ examples/data-blending/dashboard-app/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import CubeExampleWrapper from "cube-example-wrapper";
+import "cube-example-wrapper/public/style.css";
+
+CubeExampleWrapper.description = {
+  title: "Data Blending",
+  text: "This example shows Data Blending vizualization built with Cube.js and React",
+  tutorialLabel: "story",
+  tutorialSrc: "https://cube.dev/blog/introducing-data-blending-api/",
+  sourceCodeSrc: "https://github.com/cube-js/cube.js/tree/master/examples/data-blending",
+};
+CubeExampleWrapper.render("root");
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App></App>
