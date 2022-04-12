5130e473b7433c475a75750249c914a4a34828a1
Carina <revuwem@gmail.com>
2022-04-12 19:24:35 +0300

    examples(data-blending): fix cube wrapper usage

--- examples/data-blending/dashboard-app/src/index.js
+++ examples/data-blending/dashboard-app/src/index.js
@@ -2,22 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-import CubeExampleWrapper from "cube-example-wrapper";
-import "cube-example-wrapper/public/style.css";
-import "cube-example-wrapper/public/bundle.js";
+import Wrapper from "cube-example-wrapper"
 
 const root = document.getElementById('root')
 
-CubeExampleWrapper.description = {
+const exampleDescription = {
   title: "Data Blending",
   text: "This example shows Data Blending vizualization built with Cube.js and React",
   tutorialLabel: "story",
   tutorialSrc: "https://cube.dev/blog/introducing-data-blending-api/",
   sourceCodeSrc: "https://github.com/cube-js/cube.js/tree/master/examples/data-blending",
 };
-CubeExampleWrapper.render(root);
-
+const cubeExampleWrapper = new Wrapper(exampleDescription)
+cubeExampleWrapper.render(root);
 
 ReactDOM.render(
   <React.StrictMode>
