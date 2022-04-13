f1f54f1046317d4fda4208a8b19eaa11fb5c132f
Alex Qyoun-ae <4062971+MazterQyou@users.noreply.github.com>
2022-04-13 20:08:56 +0300

    feat(cubesql): Support information_schema._pg_numeric_precision UDF

--- rust/cubesql/cubesql/src/compile/mod.rs
+++ rust/cubesql/cubesql/src/compile/mod.rs
@@ -50,8 +50,8 @@ use self::engine::udf::{
     create_connection_id_udf, create_convert_tz_udf, create_current_schema_udf,
     create_current_schemas_udf, create_current_user_udf, create_db_udf, create_format_type_udf,
     create_if_udf, create_instr_udf, create_isnull_udf, create_least_udf, create_locate_udf,
-    create_pg_datetime_precision_udf, create_time_format_udf, create_timediff_udf,
-    create_ucase_udf, create_user_udf, create_version_udf,
+    create_pg_datetime_precision_udf, create_pg_numeric_precision_udf, create_time_format_udf,
+    create_timediff_udf, create_ucase_udf, create_user_udf, create_version_udf,
 };
 use self::parser::parse_sql_to_statement;
 use crate::compile::engine::udf::{
@@ -2219,6 +2219,7 @@ WHERE `TABLE_SCHEMA` = '{}'",
         ctx.register_udf(create_format_type_udf("format_type"));
         ctx.register_udf(create_format_type_udf("pg_catalog.format_type"));
         ctx.register_udf(create_pg_datetime_precision_udf());
+        ctx.register_udf(create_pg_numeric_precision_udf());
         // udaf
         ctx.register_udaf(create_measure_udaf());
 
@@ -5351,6 +5352,34 @@ mod tests {
             execute_query(
                 "
                 SELECT t.oid, information_schema._pg_datetime_precision(t.oid, 3) p
+                FROM pg_catalog.pg_type t
+                ORDER BY t.oid ASC;
+                "
+                .to_string(),
+                DatabaseProtocol::PostgreSQL
+            )
+            .await?
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_pg_numeric_precision_postgres() -> Result<(), CubeError> {
+        insta::assert_snapshot!(
+            "pg_numeric_precision_simple",
+            execute_query(
+                "SELECT information_schema._pg_numeric_precision(1700, 3);".to_string(),
+                DatabaseProtocol::PostgreSQL
+            )
+            .await?
+        );
+
+        insta::assert_snapshot!(
+            "pg_numeric_precision_types",
+            execute_query(
+                "
+                SELECT t.oid, information_schema._pg_numeric_precision(t.oid, 3) p
                 FROM pg_catalog.pg_type t
                 ORDER BY t.oid ASC;
                 "
