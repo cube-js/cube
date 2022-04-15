4b4dd60256dcbd36d049fb02078459a7873f1ecd
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-04-14 20:54:22 -0700

    fix(athena): Support plain bucket for CUBEJS_DB_EXPORT_BUCKET (#4390)
    * ok
    
    * ok
    
    * ok
    
    * ok
    
    * ok
    
    * ok
    
    * ok
    
    * ok
    
    * ok
    
    * ok

--- .github/actions/integration/athena.sh
+++ .github/actions/integration/athena.sh
@@ -7,7 +7,9 @@ export DEBUG=testcontainers
 echo "::group::Athena [cloud]"
 export CUBEJS_AWS_REGION=us-east-1
 export CUBEJS_AWS_S3_OUTPUT_LOCATION=s3://cubejs-opensource/testing/output
-export CUBEJS_DB_EXPORT_BUCKET=s3://cubejs-opensource/testing/export
+export CUBEJS_DB_EXPORT_BUCKET=s3://cubejs-opensource/testing/export/
+yarn lerna run --concurrency 1 --stream --no-prefix integration:athena
+export CUBEJS_DB_EXPORT_BUCKET=cubejs-opensource
 yarn lerna run --concurrency 1 --stream --no-prefix integration:athena
 # yarn lerna run --concurrency 1 --stream --no-prefix birdbox:athena
 echo "::endgroup::"
