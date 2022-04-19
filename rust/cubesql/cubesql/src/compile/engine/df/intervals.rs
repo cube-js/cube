d02de0f5fee7c82fcb75ba0ebe022527a58d52b1
Dmitry Patsura <talk@dmtry.me>
2022-04-20 00:38:57 +0300

    chore(cubesql): Support IntervalUnit::MonthDayNano, NULLs handling

--- rust/cubesql/cubesql/src/compile/engine/df/intervals.rs
+++ rust/cubesql/cubesql/src/compile/engine/df/intervals.rs
@@ -2,16 +2,16 @@
 macro_rules! make_string_interval_year_month {
     ($array: ident, $row: ident) => {{
         let s = if $array.is_null($row) {
-            "NULL".to_string()
+            None
         } else {
             let interval = $array.value($row) as f64;
             let years = (interval / 12_f64).floor();
             let month = interval - (years * 12_f64);
 
-            format!(
+            Some(format!(
                 "{} years {} mons 0 days 0 hours 0 mins 0.00 secs",
                 years, month,
-            )
+            ))
         };
 
         s
@@ -22,7 +22,7 @@ macro_rules! make_string_interval_year_month {
 macro_rules! make_string_interval_day_time {
     ($array: ident, $row: ident) => {{
         let s = if $array.is_null($row) {
-            "NULL".to_string()
+            None
         } else {
             let value: u64 = $array.value($row) as u64;
 
@@ -36,14 +36,52 @@ macro_rules! make_string_interval_day_time {
             let secs = secs - (mins * 60);
             let mins = mins - (hours * 60);
 
-            format!(
+            Some(format!(
                 "0 years 0 mons {} days {} hours {} mins {}.{:02} secs",
                 days_parts,
                 hours,
                 mins,
                 secs,
                 (milliseconds_part % 1000),
-            )
+            ))
+        };
+
+        s
+    }};
+}
+
+#[macro_export]
+macro_rules! make_string_interval_month_day_nano {
+    ($array: ident, $row: ident) => {{
+        let s = if $array.is_null($row) {
+            None
+        } else {
+            let value: u128 = $array.value($row) as u128;
+
+            let months_part: i32 = ((value & 0xFFFFFFFF000000000000000000000000) >> 96) as i32;
+            let years = months_part / 12_i32;
+            let month = months_part - (years * 12_i32);
+
+            let days_part: i32 = ((value & 0xFFFFFFFF0000000000000000) >> 64) as i32;
+            let nanoseconds_part: i64 = (value & 0xFFFFFFFFFFFFFFFF) as i64;
+
+            let secs = nanoseconds_part / 1000000000;
+            let mins = secs / 60;
+            let hours = mins / 60;
+
+            let secs = secs - (mins * 60);
+            let mins = mins - (hours * 60);
+
+            Some(format!(
+                "{} years {} mons {} days {} hours {} mins {}.{:02} secs",
+                years,
+                month,
+                days_part,
+                hours,
+                mins,
+                secs,
+                (nanoseconds_part % 1000000000),
+            ))
         };
 
         s
