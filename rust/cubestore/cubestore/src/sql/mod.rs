63125162338a73215f35e675474d97ac05791373
Alexandr Romanenko <waralex@gmail.com>
2022-04-22 11:39:42 +0300

    in work

--- rust/cubestore/cubestore/src/sql/mod.rs
+++ rust/cubestore/cubestore/src/sql/mod.rs
@@ -3022,13 +3022,15 @@ mod tests {
                                     cnt int,
                                     max_id int
                                   )
-                    AGGREGATES (sum(cnt), max(max_id))
+                    AGGREGATIONS (sum(cnt), max(max_id))
                     INDEX index1 (platform, age)
-                    AGGREGATE INDEX sum_index (age, gender)
+                   /* AGGREGATE INDEX sum_index (age, gender)*/
                     LOCATION {}",
                     paths.into_iter().map(|p| format!("'{}'", p)).join(",")
                 );
                 service.exec_query(&query).await.unwrap();
+                let result = service.exec_query("SELECT count(*) as cnt from foo.Orders").await.unwrap();
+                println!("res {:?}", result);
             })
             .await;
     }
