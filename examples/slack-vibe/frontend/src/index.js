879365284b4e30319fe7efa6f8b1720f34117790
Carina <revuwem@gmail.com>
2022-04-29 19:18:03 +0400

    create wrapper

--- examples/slack-vibe/frontend/src/index.js
+++ examples/slack-vibe/frontend/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import createExampleWrapper from 'cube-example-wrapper'
+
+const exampleDescription={
+  title: "Slack Vibe  🎉",
+  text: `
+    An open source dashboard of public activity in a Slack workspace 
+    of an open community or a private team
+  `
+}
+
+createExampleWrapper(exampleDescription)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
