4b4fc0b9ab89bb30d2e9d23569fa880b409354b5
Igor Lukanin <me@igorlukanin.io>
2022-04-29 11:52:53 +0400

    Install the wrapper

--- examples/hacktoberfest/dashboard/src/index.js
+++ examples/hacktoberfest/dashboard/src/index.js
@@ -1,3 +1,4 @@
+import createExampleWrapper from "cube-example-wrapper";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import cubejs from '@cubejs-client/core';
@@ -6,6 +7,10 @@ import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
 
+createExampleWrapper({
+  title: "demo title",
+  text: `demo description text`,
+});
 
 const api = cubejs(process.env.REACT_APP_CUBE_TOKEN, {
   apiUrl: `${process.env.REACT_APP_CUBE_API}`,
