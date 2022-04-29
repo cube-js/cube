688c22bcd9bd9239db96aee1eab22253715d3a65
Carina <revuwem@gmail.com>
2022-04-29 19:19:55 +0400

    fix UploadView

--- examples/slack-vibe/frontend/src/UploadView/index.js
+++ examples/slack-vibe/frontend/src/UploadView/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.module.css';
-import Header from '../Header';
-import Banner from '../Banner';
 import UploadBlock from '../UploadBlock';
 
 function UploadView(props) {
@@ -11,11 +9,7 @@ function UploadView(props) {
   return (
     <div className={styles.root}>
       <div className={styles.content}>
-        <Header />
         <UploadBlock onUpload={onUpload} />
-      </div>
-      <div className={styles.sidebar}>
-        <Banner />
       </div>
     </div>
   );
