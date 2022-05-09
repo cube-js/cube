ee9f91e7db379b30a2440e261a0f25d28271b791
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-05-09 12:31:24 -0700

    chore(schema): rollupJoin.scheduledRefresh can only be false (#4501)

--- packages/cubejs-schema-compiler/src/compiler/CubeSymbols.js
+++ packages/cubejs-schema-compiler/src/compiler/CubeSymbols.js
@@ -125,7 +125,11 @@ export class CubeSymbols {
       }
 
       if (preAggregation.scheduledRefresh === undefined) {
-        preAggregation.scheduledRefresh = getEnv('scheduledRefreshDefault');
+        if (preAggregation.type === 'rollupJoin') {
+          preAggregation.scheduledRefresh = false;
+        } else {
+          preAggregation.scheduledRefresh = getEnv('scheduledRefreshDefault');
+        }
       }
 
       if (preAggregation.external === undefined) {
