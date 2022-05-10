65a5e390922e339d995c0be0bdd6eb500bba6a65
Alex Qyoun-ae <4062971+MazterQyou@users.noreply.github.com>
2022-05-11 02:42:24 +0400

    feat(cubesql): Support `quarter` field in `date_part` SQL function

--- rust/cubesql/cubesql/src/sql/postgres/pg_type.rs
+++ rust/cubesql/cubesql/src/sql/postgres/pg_type.rs
@@ -4,13 +4,15 @@ use pg_srv::{protocol, PgTypeId, ProtocolError};
 pub fn df_type_to_pg_tid(dt: &DataType) -> Result<PgTypeId, ProtocolError> {
     match dt {
         DataType::Boolean => Ok(PgTypeId::BOOL),
-        DataType::Int16 => Ok(PgTypeId::INT2),
-        DataType::Int32 => Ok(PgTypeId::INT4),
+        // TODO: @ovr — table values <int8 are not supported at the moment
+        DataType::Int16 => Ok(PgTypeId::INT8),
+        DataType::Int32 => Ok(PgTypeId::INT8),
         DataType::Int64 => Ok(PgTypeId::INT8),
         DataType::UInt16 => Ok(PgTypeId::INT8),
         DataType::UInt32 => Ok(PgTypeId::INT8),
         DataType::UInt64 => Ok(PgTypeId::INT8),
-        DataType::Float32 => Ok(PgTypeId::FLOAT4),
+        // TODO: @ovr — table values <float8 are not supported at the moment
+        DataType::Float32 => Ok(PgTypeId::FLOAT8),
         DataType::Float64 => Ok(PgTypeId::FLOAT8),
         DataType::Utf8 | DataType::LargeUtf8 => Ok(PgTypeId::TEXT),
         DataType::Timestamp(_, tz) => match tz {
