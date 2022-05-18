5fa8492cac84a740320f5b5ee6f60bdc26b80709
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-05-17 20:42:02 -0700

    ok

--- rust/cubestore/cubestore-sql-tests/tests/in_process.rs
+++ rust/cubestore/cubestore-sql-tests/tests/in_process.rs
@@ -2,6 +2,7 @@
 use cubestore::config::Config;
 use cubestore_sql_tests::run_sql_tests;
 use tokio::runtime::Builder;
+use cubestore::metastore::MetaStoreTable;
 
 fn main() {
     run_sql_tests("in_process", vec![], |test_name, test_fn| {
@@ -11,6 +12,9 @@ fn main() {
         let test_name = test_name.to_owned() + "-1p";
         r.block_on(Config::run_test(&test_name, |services| async move {
             test_fn(Box::new(services.sql_service)).await;
+            println!("TTT {:#?}", services.meta_store.tables_table().all_rows().await);
+            println!("III {:#?}", services.meta_store.index_table().all_rows().await);
+            println!("PPP {:#?}", services.meta_store.partition_table().all_rows().await)
         }));
     });
 }
