f415719d6e86260a435686f777dc1f6e69d8eac6
Sasha Sushko <sushko@outlook.com>
2022-05-27 22:45:25 +0500

    Add Cube Example Wrapper to Supabase webapp

--- examples/supabase/dashboard-app/src/index.js
+++ examples/supabase/dashboard-app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import 'antd/dist/antd.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import createExampleWrapper from "cube-example-wrapper";
+
+createExampleWrapper({ title: "Supabase + Cube" });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
