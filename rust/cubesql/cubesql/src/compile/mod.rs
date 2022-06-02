1e9ff49981eb5ad7f8dc7bd9322087c4b8a49799
George Andronchik <g.andronchik@gmail.com>
2022-06-02 22:34:05 +0800

    test(cubesql): Verify Metabase queries (#4675)

--- rust/cubesql/cubesql/src/compile/mod.rs
+++ rust/cubesql/cubesql/src/compile/mod.rs
@@ -8220,4 +8220,116 @@ ORDER BY \"COUNT(count)\" DESC"
 
         Ok(())
     }
+
+    #[tokio::test]
+    async fn test_metabase_table_cat_query() -> Result<(), CubeError> {
+        insta::assert_snapshot!(
+            "metabase_table_cat_query",
+            execute_query(
+                "
+                SELECT  result.table_cat,
+                        result.table_schem,
+                        result.table_name,
+                        result.column_name,
+                        result.key_seq,
+                        result.pk_name
+                    FROM (
+                        SELECT  NULL AS table_cat,
+                                n.nspname AS table_schem,
+                                ct.relname AS table_name,
+                                a.attname AS column_name,
+                                (information_schema._pg_expandarray(i.indkey)).n as key_seq,
+                                ci.relname AS pk_name,
+                                information_schema._pg_expandarray(i.indkey) AS keys,
+                                a.attnum AS a_attnum
+                            FROM   pg_catalog.pg_class ct
+                            JOIN   pg_catalog.pg_attribute a ON(ct.oid = a.attrelid)
+                            JOIN   pg_catalog.pg_namespace n ON (ct.relnamespace = n.oid)
+                            JOIN   pg_catalog.pg_index i ON (a.attrelid = i.indrelid)
+                            JOIN   pg_catalog.pg_class ci ON (ci.oid = i.indexrelid)
+                        WHERE true AND ct.relname = 'actor' AND i.indisprimary) result
+                WHERE result.a_attnum = (result.keys).x
+                ORDER BY result.table_name, result.pk_name, result.key_seq;
+                "
+                .to_string(),
+                DatabaseProtocol::PostgreSQL
+            )
+            .await?
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_metabase_pktable_cat_query() -> Result<(), CubeError> {
+        insta::assert_snapshot!(
+            "metabase_pktable_cat_query",
+            execute_query(
+                "
+                SELECT  NULL::text  AS pktable_cat,
+                        pkn.nspname AS pktable_schem,
+                        pkc.relname AS pktable_name,
+                        pka.attname AS pkcolumn_name,
+                        NULL::text  AS fktable_cat,
+                        fkn.nspname AS fktable_schem,
+                        fkc.relname AS fktable_name,
+                        fka.attname AS fkcolumn_name,
+                        pos.n       AS key_seq,
+                        CASE con.confupdtype
+                            WHEN 'c' THEN 0
+                            WHEN 'n' THEN 2
+                            WHEN 'd' THEN 4
+                            WHEN 'r' THEN 1
+                            WHEN 'p' THEN 1
+                            WHEN 'a' THEN 3
+                            ELSE NULL
+                        END AS update_rule,
+                        CASE con.confdeltype
+                            WHEN 'c' THEN 0
+                            WHEN 'n' THEN 2
+                            WHEN 'd' THEN 4
+                            WHEN 'r' THEN 1
+                            WHEN 'p' THEN 1
+                            WHEN 'a' THEN 3
+                            ELSE NULL
+                        END AS delete_rule,
+                        con.conname  AS fk_name,
+                        pkic.relname AS pk_name,
+                        CASE
+                            WHEN con.condeferrable AND con.condeferred THEN 5
+                            WHEN con.condeferrable THEN 6
+                            ELSE 7
+                        END AS deferrability
+                    FROM    pg_catalog.pg_namespace pkn,
+                            pg_catalog.pg_class pkc,
+                            pg_catalog.pg_attribute pka,
+                            pg_catalog.pg_namespace fkn,
+                            pg_catalog.pg_class fkc,
+                            pg_catalog.pg_attribute fka,
+                            pg_catalog.pg_constraint con,
+                            pg_catalog.generate_series(1, 32) pos(n),
+                            pg_catalog.pg_class pkic
+                WHERE   pkn.oid = pkc.relnamespace
+                AND     pkc.oid = pka.attrelid
+                AND     pka.attnum = con.confkey[pos.n]
+                AND     con.confrelid = pkc.oid
+                AND     fkn.oid = fkc.relnamespace
+                AND     fkc.oid = fka.attrelid
+                AND     fka.attnum = con.conkey[pos.n]
+                AND     con.conrelid = fkc.oid
+                AND     con.contype = 'f'
+                AND     (pkic.relkind = 'i' OR pkic.relkind = 'I')
+                AND     pkic.oid = con.conindid
+                AND     fkn.nspname = 'public'
+                AND     fkc.relname = 'actor'
+                ORDER BY pkn.nspname, pkc.relname, con.conname, pos.n;
+                "
+                .to_string(),
+                DatabaseProtocol::PostgreSQL
+            )
+            .await?
+        );
+
+        Ok(())
+    }
 }
