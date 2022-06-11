d53f916414e217f27cbf3a59a50092805888eaba
Dmitriy Rusov <rusov.d.s@gmail.com>
2022-06-11 11:42:14 +0300

    chore(server-core): Use http agent with keep-alive and maxSockets (#4751)

--- packages/cubejs-server-core/src/core/agentCollect.ts
+++ packages/cubejs-server-core/src/core/agentCollect.ts
@@ -1,4 +1,6 @@
 import { getEnv } from '@cubejs-backend/shared';
+import http from 'http';
+import https from 'https';
 import fetch from 'node-fetch';
 import crypto from 'crypto';
 import WebSocket from 'ws';
@@ -102,9 +104,17 @@ class WebSocketTransport implements AgentTransport {
 }
 
 class HttpTransport implements AgentTransport {
+  private agent: http.Agent | https.Agent;
+
   public constructor(
-    private endpointUrl: string
-  ) {}
+    private readonly endpointUrl: string
+  ) {
+    const AgentClass = endpointUrl.startsWith('https') ? https.Agent : http.Agent;
+    this.agent = new AgentClass({
+      keepAlive: true,
+      maxSockets: getEnv('agentMaxSockets')
+    });
+  }
 
   public ready() {
     return true;
@@ -112,6 +122,7 @@ class HttpTransport implements AgentTransport {
 
   public async send(data: any[]) {
     const result = await fetch(this.endpointUrl, {
+      agent: this.agent,
       method: 'post',
       body: JSON.stringify(data),
       headers: { 'Content-Type': 'application/json' },
