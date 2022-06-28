645253f9b21ef08f7fc908e6577878f97b3ef6b0
Dmitry Patsura <talk@dmtry.me>
2022-06-28 17:13:40 +0300

    feat(cubesql): Send parameters at once (initial handshake) for pg-wire (#4812)

--- rust/cubesql/cubesql/src/sql/postgres/shim.rs
+++ rust/cubesql/cubesql/src/sql/postgres/shim.rs
@@ -342,6 +342,15 @@ impl AsyncPostgresShim {
         Ok(())
     }
 
+    pub async fn write_multi<Message: protocol::Serialize>(
+        &mut self,
+        message: Vec<Message>,
+    ) -> Result<(), ConnectionError> {
+        buffer::write_messages(&mut self.socket, message).await?;
+
+        Ok(())
+    }
+
     pub async fn write<Message: protocol::Serialize>(
         &mut self,
         message: Message,
@@ -446,23 +455,22 @@ impl AsyncPostgresShim {
     }
 
     pub async fn ready(&mut self) -> Result<(), ConnectionError> {
-        let params = [
-            ("server_version".to_string(), "14.2 (Cube SQL)".to_string()),
-            ("server_encoding".to_string(), "UTF8".to_string()),
-            ("client_encoding".to_string(), "UTF8".to_string()),
-            ("DateStyle".to_string(), "ISO".to_string()),
+        let params = vec![
+            protocol::ParameterStatus::new(
+                "server_version".to_string(),
+                "14.2 (Cube SQL)".to_string(),
+            ),
+            protocol::ParameterStatus::new("server_encoding".to_string(), "UTF8".to_string()),
+            protocol::ParameterStatus::new("client_encoding".to_string(), "UTF8".to_string()),
+            protocol::ParameterStatus::new("DateStyle".to_string(), "ISO".to_string()),
             // Reports whether PostgreSQL was built with support for 64-bit-integer dates and times.
-            ("integer_datetimes".to_string(), "on".to_string()),
-            ("TimeZone".to_string(), "Etc/UTC".to_string()),
+            protocol::ParameterStatus::new("integer_datetimes".to_string(), "on".to_string()),
+            protocol::ParameterStatus::new("TimeZone".to_string(), "Etc/UTC".to_string()),
             // TODO: Right now we dont support intervals, but it's an important thing
-            // ("IntervalStyle".to_string(), "postgres".to_string()),
+            // protocol::ParameterStatus::new("IntervalStyle".to_string(), "postgres".to_string()),
         ];
 
-        for (key, value) in params {
-            self.write(protocol::ParameterStatus::new(key, value))
-                .await?;
-        }
-
+        self.write_multi(params).await?;
         self.write(protocol::ReadyForQuery::new(
             protocol::TransactionStatus::Idle,
         ))
