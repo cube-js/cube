babe594d2ad1d623cc9e945d04c3d2c25abc3fe0
Hassan Khan <hassan@cube.dev>
2022-07-12 00:24:58 +0100

    docs: fix card grids

--- docs/src/components/Grid/GridItem.tsx
+++ docs/src/components/Grid/GridItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { COL_CLASS_MAP, GridContext } from './Grid';
+import { GridContext } from './Grid';
 
 export type GridItemProps = {
   imageUrl: string;
@@ -8,30 +8,22 @@ export type GridItemProps = {
   url: string;
 };
 
-const WRAPPER_CLASS_MAP: Record<number, string> = {
-  2: 'ant-col ant-col-xs-24 ant-col-sm-24 ant-col-md-24 ant-col-xl-12',
-  3: 'ant-col ant-col-xs-24 ant-col-sm-24 ant-col-md-24 ant-col-lg-12 ant-col-xl-8',
-}
-
-export const GridItem = ({ imageUrl, title, url  }: GridItemProps) => {
+export const GridItem = ({
+  imageUrl,
+  title,
+  url,
+}: GridItemProps) => {
   return (
     <GridContext.Consumer>
       {(value) => {
         const settings = JSON.parse(value);
-        const classPrefix = COL_CLASS_MAP[settings.cols];
-        const [ height, width ] = settings.imageSize;
-
-        const wrapperClassName = [
-          `${classPrefix}Item`,
-          WRAPPER_CLASS_MAP[settings.cols],
-          settings.slim ? `${classPrefix}ItemSlim` : ''
-        ].join(' ');
+        const [ width, height ] = settings.imageSize;
 
         return (
-          <div className={wrapperClassName}>
+          <div className="grid-item">
             <a href={url}>
-              <div className={`${classPrefix}ItemContent`}>
-                <div className={`${classPrefix}ItemImage`}>
+              <div>
+                <div className="grid-item-image">
                   <img
                     src={imageUrl}
                     alt={title}
@@ -39,11 +31,11 @@ export const GridItem = ({ imageUrl, title, url  }: GridItemProps) => {
                     height={height}
                   />
                 </div>
-                <div className={`${classPrefix}ItemLink`}>{title}</div>
+                <div className="grid-item-title">{title}</div>
               </div>
             </a>
           </div>
-        )
+        );
       }}
     </GridContext.Consumer>
   );
