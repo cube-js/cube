408c0c6ef2580ca60fcde9adf5df3ab61e718e0e
Ivan Tsenilov <arantirex@gmail.com>
2022-07-14 16:35:36 +0300

    examples(graphql-api-metrics-dashboard): update deps and README (#4799)
    * examples(graphql-api-metrics-dashboard): update deps, README
    
    * Edit description
    
    Co-authored-by: Anastasiya Kataeva <kataeva@Anastasiyas-MacBook-Pro.local>

--- examples/graphql-api-metrics-dashboard/src/index.js
+++ examples/graphql-api-metrics-dashboard/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,17 +8,25 @@ import { ApolloProvider } from '@apollo/client';
 import createExampleWrapper from "cube-example-wrapper";
 
 const exampleDescription = {
-  title: "GraphQL API Metrics Dashboard",
-  text: `A dashboard app built with Cube's GraphQL API, Postgres, Chart.js, and react-chartjs-2.`,
+  title: "Chart.js Metrics Dashboard",
+  text: `
+    <p>This live demo shows a metrics dashboard built with Cube's GraphQL API, PostgreSQL as a database, Chart.js and react-chartjs-2 for data visualization, and Cube.</p>
+    <p>
+      Follow 
+      the <a href="https://cube.dev/blog/graphql-postgres-metrics-dashboard-with-cube">tutorial</a>
+      or explore 
+      the <a href="https://github.com/cube-js/cube.js/tree/master/examples/graphql-api-metrics-dashboard">source code</a>
+      to learn more.
+    </p>`,
 };
 
 createExampleWrapper(exampleDescription);
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <ApolloProvider client={clientWithCubeCloud}>
     <App />
-  </ApolloProvider>,
-  document.getElementById('root')
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
