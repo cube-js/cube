9ea0ff8cc796d904351e31083a85e0a352ce89ba
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-07-28 11:45:39 -0700

    ok

--- packages/cubejs-testing-shared/src/db/crate.ts
+++ packages/cubejs-testing-shared/src/db/crate.ts
@@ -4,7 +4,7 @@ import { DbRunnerAbstract, DBRunnerContainerOptions } from './db-runner.abstract
 
 export class CrateDBRunner extends DbRunnerAbstract {
   public static startContainer(options: DBRunnerContainerOptions) {
-    const version = process.env.TEST_QUEST_DB_VERSION || '5.0.0';
+    const version = process.env.TEST_CRATE_DB_VERSION || '5.0.0';
 
     const container = new GenericContainer(`crate/crate:${version}`)
       .withExposedPorts(5432)
