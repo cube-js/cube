2f898af9919695117cdb148a536523f889de34bc
Cristian Petrescu-Prahova <cristipp@cube.dev>
2022-08-16 14:18:56 -0700

    ok

--- examples/lambda-benchmark/basic.sh
+++ examples/lambda-benchmark/basic.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+export CUBESTORE_DIR=basic
 export CUBEJS_TEST_PORT=4001
 export CUBESTORE_TEST_PORT=3001
-export CUBEJS_TEST_USE_LAMBDA=
+# export CUBEJS_TEST_USE_LAMBDA=
 
 docker-compose -p cubejs-basic -f cube/docker-compose.yml up
