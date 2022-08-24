d572d5ba74f3e665044f704520d3e62df493e36b
Igor Lukanin <me@igorlukanin.io>
2022-08-24 04:02:48 +0400

    Add Firebolt example

---
+++ examples/wikipedia-outliers/dashboard-app/src/emoji.js
@@ -0,0 +1,21 @@
+import { countryCodeEmoji } from 'country-code-emoji'
+
+const countryCodeMap = {
+  'EN': 'US',
+  'JA': 'JP',
+  'ZH': 'CN',
+  'FA': 'IR',
+  'UK': 'UA',
+  'KO': 'KR',
+  'CS': 'CZ',
+  'HE': 'IL',
+  'EL': 'GR',
+  'DA': 'DK',
+  'HI': 'IN'
+}
+
+export function getEmoji(region) {
+  let code = countryCodeMap[region] || region
+
+  return countryCodeEmoji(code)
+}
