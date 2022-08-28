78524873d71c21711e5138b48deec4f54cf6e955
Pavel Tiunov <pavel.tiunov@gmail.com>
2022-08-27 20:46:50 -0700

    chore(cubestore): Do not intersect inline table ids with partition ones and make proper node distribution -- InlineCompoundPartition docs

--- rust/cubestore/cubestore/src/queryplanner/query_executor.rs
+++ rust/cubestore/cubestore/src/queryplanner/query_executor.rs
@@ -897,6 +897,8 @@ pub struct ClusterSendExec {
 pub type PartitionWithFilters = (u64, RowRange);
 pub type InlineTableId = u64;
 
+/// Compound structure to assign inline tables to specific partitions so they can get to the same worker.
+/// It's helpful to do so in order to avoid allocation of additional worker allocations for inline table micro reads.
 #[derive(Clone, Serialize, Deserialize, Debug, Eq, PartialEq)]
 pub enum InlineCompoundPartition {
     Partition(IdRow<Partition>),
