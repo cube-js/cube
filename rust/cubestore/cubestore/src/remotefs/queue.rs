289252aed2948ea01eab6df551fe84cd851cf699
Alexandr Romanenko <alex.romanenko@cube.dev>
2022-09-06 19:18:47 +0300

    fix

--- rust/cubestore/cubestore/src/remotefs/queue.rs
+++ rust/cubestore/cubestore/src/remotefs/queue.rs
@@ -357,18 +357,28 @@ impl RemoteFs for QueueRemoteFs {
                     match result {
                         Ok(f) => {
                             let local_path = self.local_file(remote_path).await?;
-                            let metadata = tokio::fs::metadata(&local_path).await?;
-                            if let Err(e) = QueueRemoteFs::check_file_size(
-                                remote_path,
-                                expected_file_size,
-                                &local_path,
-                                metadata,
-                            )
-                            .await
-                            {
-                                return Err(e);
+                            let metadata = tokio::fs::metadata(&local_path).await;
+                            match metadata {
+                                Ok(metadata) => {
+                                    if let Err(e) = QueueRemoteFs::check_file_size(
+                                        remote_path,
+                                        expected_file_size,
+                                        &local_path,
+                                        metadata,
+                                    )
+                                    .await
+                                    {
+                                        return Err(e);
+                                    }
+                                    return Ok(f);
+                                }
+                                Err(e) => {
+                                    return Err(CubeError::internal(format!(
+                                        "Error while reading local file {}: {}",
+                                        local_path, e
+                                    )))
+                                }
                             }
-                            return Ok(f);
                         }
                         Err(err) => {
                             //Check if file doesn't exists in remoteFs
