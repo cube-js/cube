ce52300432382781de6f0106a3a20a8f090115ac
Alex Vasilev <vaspiring@gmail.com>
2022-09-11 09:16:31 +0300

    ff

--- packages/cubejs-playground/postbuild.js
+++ packages/cubejs-playground/postbuild.js
@@ -1,22 +1,24 @@
-// const fs = require('fs');
-// const path = require('path');
+console.log('Postbuild...', __dirname);
 
-// const distFolder = path.resolve(__dirname, 'lib');
+const fs = require('fs');
+const path = require('path');
 
-console.log('Postbuild...');
+const distFolder = path.resolve(__dirname, 'lib');
 
-// ['cloud', 'rollup-designer'].forEach((name) => {
-//   fs.writeFileSync(
-//     path.join(distFolder, name, 'package.json'),
-//     JSON.stringify(
-//       {
-//         name: `@cubejs-client/playground/${name}`,
-//         type: 'module',
-//         module: 'index.js',
-//         types: `${name}/index.d.ts`,
-//       },
-//       null,
-//       2
-//     )
-//   );
-// });
+console.log(`Postbuild... ${distFolder}`);
+
+['cloud', 'rollup-designer'].forEach((name) => {
+  fs.writeFileSync(
+    path.join(distFolder, name, 'package.json'),
+    JSON.stringify(
+      {
+        name: `@cubejs-client/playground/${name}`,
+        type: 'module',
+        module: 'index.js',
+        types: `${name}/index.d.ts`,
+      },
+      null,
+      2
+    )
+  );
+});
