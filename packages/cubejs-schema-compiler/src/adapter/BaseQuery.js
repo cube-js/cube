3c179bc52ddf96161d69869c55d0fbb6b12cc2ab
Pavel Tiunov <pavel.tiunov@gmail.com>
2022-09-11 20:25:53 -0700

    chore: Cube Views implementation -- fix Cube Store rolling window queries

--- packages/cubejs-schema-compiler/src/adapter/BaseQuery.js
+++ packages/cubejs-schema-compiler/src/adapter/BaseQuery.js
@@ -1615,10 +1615,6 @@ class BaseQuery {
   }
 
   dimensionSql(dimension) {
-    const context = this.safeEvaluateSymbolContext();
-    if (context.wrapQuery) {
-      return this.escapeColumnName(dimension.unescapedAliasName(context.wrappedGranularity));
-    }
     return this.evaluateSymbolSql(dimension.path()[0], dimension.path()[1], dimension.dimensionDefinition());
   }
 
