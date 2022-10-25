c44ba3619077b0332be10005660cdde74776c280
Adnan Rahic <ado.raha198@gmail.com>
2022-10-25 15:07:36 +0200

    examples(metabase): added initial config

---
+++ examples/metabase/services/postgres/00_metabase_create_user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e # exit if a command exits with a not-zero exit code
+
+POSTGRES="psql -U postgres"
+
+# create a shared role to read & write general datasets into postgres
+echo "Creating database role: metabase"
+$POSTGRES <<-EOSQL
+CREATE USER metabase WITH
+    LOGIN
+    NOSUPERUSER
+    NOCREATEDB
+    NOCREATEROLE
+    NOINHERIT
+    NOREPLICATION
+    PASSWORD '$METABASE_PASSWORD';
+EOSQL
---
+++ examples/metabase/services/postgres/01_metabase_create_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e # exit immediately if a command exits with a non-zero status.
+
+POSTGRES="psql --username postgres"
+
+# create database for superset
+echo "Creating database: metabase"
+$POSTGRES <<EOSQL
+CREATE DATABASE metabase OWNER metabase;
+EOSQL
