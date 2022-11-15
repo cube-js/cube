b08e1c406d6e902bddbea522d2055a354649a242
buntarb <buntarb@gmail.com>
2022-11-14 21:19:23 -0800

    wip

--- packages/cubejs-databricks-jdbc-driver/src/installer.ts
+++ packages/cubejs-databricks-jdbc-driver/src/installer.ts
@@ -19,19 +19,19 @@ export async function downloadJDBCDriver(): Promise<string | null> {
   const driverAccepted = acceptedByEnv();
 
   if (driverAccepted) {
-    console.log('Downloading SimbaSparkJDBC42-2.6.17.1021');
+    console.log('Downloading DatabricksJDBC42-2.6.29.1051');
 
     await downloadAndExtractFile(
-      'https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/jdbc/2.6.17/SimbaSparkJDBC42-2.6.17.1021.zip',
+      'https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/jdbc/2.6.29/DatabricksJDBC42-2.6.29.1051.zip',
       {
         showProgress: true,
         cwd: path.resolve(path.join(__dirname, '..', 'download')),
       }
     );
 
-    console.log('Release notes: https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/jdbc/2.6.17/docs/release-notes.txt');
+    console.log('Release notes: https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/jdbc/2.6.29/docs/release-notes.txt');
 
-    return path.resolve(path.join(__dirname, '..', 'download', 'SparkJDBC42.jar'));
+    return path.resolve(path.join(__dirname, '..', 'download', 'DatabricksJDBC42.jar'));
   }
 
   return null;
