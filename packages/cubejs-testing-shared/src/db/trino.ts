d634d1940a66a1b43bc906e90f5007ae002fd1ef
Yurii Boriu <yurii.boriu@bringg.com>
2022-12-06 20:26:41 +0200

    lint fixes

--- packages/cubejs-testing-shared/src/db/trino.ts
+++ packages/cubejs-testing-shared/src/db/trino.ts
@@ -7,21 +7,21 @@ type TrinoStartOptions = DBRunnerContainerOptions & {
 };
 
 export class TrinoDBRunner extends DbRunnerAbstract {
-    public static startContainer(options: TrinoStartOptions) {
-        const version = process.env.TEST_TRINO_VERSION || options.version || '403';
+  public static startContainer(options: TrinoStartOptions) {
+    const version = process.env.TEST_TRINO_VERSION || options.version || '403';
 
-        const container = new GenericContainer(`trinodb/trino:${version}`)
-            .withExposedPorts(8080)
-            .withWaitStrategy(Wait.forLogMessage("======== SERVER STARTED ========"))
-            .withStartupTimeout(30 * 1000);
+    const container = new GenericContainer(`trinodb/trino:${version}`)
+      .withExposedPorts(8080)
+      .withWaitStrategy(Wait.forLogMessage('======== SERVER STARTED ========'))
+      .withStartupTimeout(30 * 1000);
 
-        if (options.volumes) {
-            // eslint-disable-next-line no-restricted-syntax
-            for (const { source, target, bindMode } of options.volumes) {
-                container.withBindMount(source, target, bindMode);
-            }
-        }
-
-        return container.start();
+    if (options.volumes) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const { source, target, bindMode } of options.volumes) {
+        container.withBindMount(source, target, bindMode);
+      }
     }
+
+    return container.start();
+  }
 }
