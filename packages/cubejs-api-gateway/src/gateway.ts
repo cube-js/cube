4ec172b3dd27193d145afcdb8d9bf7ef1bd7505d
buntarb <buntarb@gmail.com>
2022-12-08 20:21:54 -0800

    feat: query limits (#5763)

--- packages/cubejs-api-gateway/src/gateway.ts
+++ packages/cubejs-api-gateway/src/gateway.ts
@@ -451,6 +451,8 @@ class ApiGateway {
   public async metaExtended({ context, res }: { context: RequestContext, res: ResponseResultFn }) {
     const requestStarted = new Date();
 
+    // TODO: test and remove this function.
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     function visibilityFilter(item) {
       return getEnv('devMode') || context.signedWithPlaygroundAuthSecret || item.isVisible;
     }
