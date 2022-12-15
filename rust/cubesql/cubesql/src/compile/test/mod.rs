c22f7f659bc04a33cfb38ea696267ec6caf0ea48
Alex Qyoun-ae <4062971+MazterQyou@users.noreply.github.com>
2022-12-16 01:30:43 +0400

    fix(cubesql): Improve Thoughtspot compatibility

--- rust/cubesql/cubesql/src/compile/test/mod.rs
+++ rust/cubesql/cubesql/src/compile/test/mod.rs
@@ -30,6 +30,10 @@ pub fn get_test_meta() -> Vec<V1CubeMeta> {
                     _type: "time".to_string(),
                 },
                 V1CubeMetaDimension {
+                    name: "KibanaSampleDataEcommerce.last_mod".to_string(),
+                    _type: "time".to_string(),
+                },
+                V1CubeMetaDimension {
                     name: "KibanaSampleDataEcommerce.customer_gender".to_string(),
                     _type: "string".to_string(),
                 },
@@ -98,10 +102,16 @@ pub fn get_test_meta() -> Vec<V1CubeMeta> {
         V1CubeMeta {
             name: "Logs".to_string(),
             title: None,
-            dimensions: vec![V1CubeMetaDimension {
-                name: "Logs.read".to_string(),
-                _type: "boolean".to_string(),
-            }],
+            dimensions: vec![
+                V1CubeMetaDimension {
+                    name: "Logs.read".to_string(),
+                    _type: "boolean".to_string(),
+                },
+                V1CubeMetaDimension {
+                    name: "Logs.content".to_string(),
+                    _type: "string".to_string(),
+                },
+            ],
             measures: vec![
                 V1CubeMetaMeasure {
                     name: "Logs.agentCount".to_string(),
