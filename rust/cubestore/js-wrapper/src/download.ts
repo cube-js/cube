20eb3a37cca727beb87822d07b3246936a08aa5b
Dmitry Patsura <talk@dmtry.me>
2022-12-28 00:02:21 +0300

    feat(cubestore): Download binary without GH API

--- rust/cubestore/js-wrapper/src/download.ts
+++ rust/cubestore/js-wrapper/src/download.ts
@@ -60,37 +60,36 @@ function parseInfoFromAssetName(assetName: string): { target: string, type: stri
 export async function downloadBinaryFromRelease() {
   // eslint-disable-next-line global-require
   const { version } = require('../../package.json');
+  const cubestorePath = getCubeStorePath();
+  const currentTarget = getTarget();
 
-  const release = await fetchRelease(version);
-  if (release) {
-    if (release.assets.length === 0) {
-      throw new Error(
-        `There are no artifacts for Cube Store v${version}. Most probably it is still building. Please try again later.`
-      );
-    }
+  const url = `https://github.com/cube-js/cube.js/releases/download/v${version}/cubestored-${currentTarget}.tar.gz`;
 
-    const currentTarget = getTarget();
+  try {
+    await downloadAndExtractFile(url, {
+      cwd: cubestorePath,
+      showProgress: true,
+    });
+  } catch (e: any) {
+    if (e.toString().includes('Not Found')) {
+      const release = await fetchRelease(version);
+      if (release) {
+        if (release.assets.length === 0) {
+          throw new Error(
+            `There are no artifacts for Cube Store v${version}. Most probably it is still building. Please try again later.`
+          );
+        }
 
-    for (const asset of release.assets) {
-      const assetInfo = parseInfoFromAssetName(asset.name);
-      if (assetInfo && assetInfo.target === currentTarget
-        && assetInfo.type === 'static' && assetInfo.format === 'tar.gz'
-      ) {
-        const cubestorePath = getCubeStorePath();
-
-        return downloadAndExtractFile(asset.browser_download_url, {
-          cwd: cubestorePath,
-          showProgress: true,
-        });
+        throw new Error(
+          `Cube Store v${version} Artifact for ${process.platform} is not found. Most probably it is still building. Please try again later.`
+        );
+      } else {
+        throw new Error(
+          `Unable to find Cube Store release v${version}. Most probably it was removed.`
+        );
       }
+    } else {
+      throw e;
     }
-
-    throw new Error(
-      `Cube Store v${version} Artifact for ${process.platform} is not found. Most probably it is still building. Please try again later.`
-    );
   }
-
-  throw new Error(
-    `Unable to find Cube Store release v${version}. Most probably it was removed.`
-  );
 }
