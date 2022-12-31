3ae4e0d7e7e12347f1a5822ef19272b49bf3ea8e
jay-choe <jay0764@naver.com>
2022-12-31 13:01:42 +0900

    add oracle smoke test

---
+++ packages/cubejs-testing/test/smoke-oracle.test.ts
@@ -0,0 +1,70 @@
+import { StartedTestContainer } from 'testcontainers';
+import { OracleDBRunner } from '@cubejs-backend/testing-shared';
+import cubejs, { CubejsApi } from '@cubejs-client/core';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { afterAll, beforeAll, expect, jest } from '@jest/globals';
+import { pausePromise } from '@cubejs-backend/shared';
+import { BirdBox, getBirdbox } from '../src';
+import { DEFAULT_CONFIG, testQueryMeasure } from './smoke-tests';
+
+describe('oracle', () => {
+  jest.setTimeout(60 * 5 * 10000);
+  let db: StartedTestContainer;
+  let birdbox: BirdBox;
+  let client: CubejsApi;
+
+  beforeAll(async () => {
+    db = await OracleDBRunner.startContainer({});
+    birdbox = await getBirdbox(
+      'oracle',
+      {
+        CUBEJS_DB_TYPE: 'oracle',
+
+        CUBEJS_DB_HOST: db.getHost(),
+        CUBEJS_DB_PORT: `${db.getMappedPort(1521)}`,
+        CUBEJS_DB_NAME: 'XEPDB1',
+        CUBEJS_DB_USER: 'test',
+        CUBEJS_DB_PASS: 'test',
+
+        ...DEFAULT_CONFIG,
+      },
+      {
+        schemaDir: 'oracle/schema',
+      }
+    );
+    client = cubejs(async () => 'test', {
+      apiUrl: birdbox.configuration.apiUrl,
+    });
+  });
+
+  afterAll(async () => {
+    await birdbox.stop();
+    await db.stop();
+  });
+
+  test('query measure', () => testQueryMeasure(client));
+
+  test('query dimensions', async () => {
+    const queryDimensions = async () => {
+      const response = await client.load({
+        measures: [
+          'Orders.totalAmount',
+        ],
+        dimensions: [
+          'Orders.status',
+        ],
+      });
+
+      expect(response.rawData()).toMatchSnapshot('dimensions');
+    };
+    await queryDimensions();
+
+    /**
+         * Running a query with 2 seconds delay
+         * preAggregation has 1 second in the refreshKey
+         * Gives times to trigger the action if hasn't been triggered yet.
+         */
+    await pausePromise(2000);
+    await queryDimensions();
+  });
+});
