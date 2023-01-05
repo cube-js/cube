445bd5094346fc10d91fe5cd92c0a8628144cbb1
Evgenii Khramkov <hey@ewgenius.me>
2023-01-05 16:09:48 +0400

    docs: persist js and yaml code tab selection

--- docs/src/components/CodeTabs/CodeTabs.tsx
+++ docs/src/components/CodeTabs/CodeTabs.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import { useState, type FC } from 'react';
+import { useState, useEffect, type FC } from 'react';
 import { langs } from './dictionary';
 
 import classnames from 'classnames/bind';
 import * as classes from './CodeTabs.module.css';
 const cn = classnames.bind(classes);
+
+const STORAGE_KEY = 'cube-docs.default-code-lang';
 
 export interface CodeTabsProps {
   children: Array<{
@@ -18,25 +20,44 @@ export interface CodeTabsProps {
 export const CodeTabs: FC<CodeTabsProps> = ({ children }) => {
   const [selectedTab, setSelectedTab] = useState(0);
 
+  useEffect(() => {
+    const defaultLang = localStorage.getItem(STORAGE_KEY);
+
+    if (defaultLang) {
+      children.some((tab, i) => {
+        if (tab.props['data-language'] === defaultLang) {
+          setSelectedTab(i);
+        }
+      });
+    }
+  }, []);
+
   return (
     <div className={classes.CodeBlock}>
       <div className={classes.CodeBlocks__tabs}>
-        {children.map((tab, i) => {
-          const lang = tab.props['data-language'];
-          return (
-            <div
-              className={cn('CodeBlocks__tab', {
-                [classes.SelectedTab]: i === selectedTab,
-              })}
-              onClick={() => setSelectedTab(i)}
-            >
-              {langs[lang] || lang}
-            </div>
-          );
-        })}
+        {children
+          .filter((tab) => !!tab.props['data-language'])
+          .map((tab, i) => {
+            const lang = tab.props['data-language'];
+            return (
+              <div
+                className={cn('CodeBlocks__tab', {
+                  [classes.SelectedTab]: i === selectedTab,
+                })}
+                onClick={() => {
+                  if (lang === 'javascript' || lang === 'yaml') {
+                    localStorage.setItem(STORAGE_KEY, lang);
+                  }
+                  setSelectedTab(i);
+                }}
+              >
+                {langs[lang] || lang}
+              </div>
+            );
+          })}
       </div>
 
-      {children[selectedTab].props.children}
+      {children && children[selectedTab].props.children}
     </div>
   );
 };
