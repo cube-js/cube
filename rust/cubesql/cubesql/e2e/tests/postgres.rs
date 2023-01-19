146499e26c03ece62db9f0ca6e7cdf3ed2f7d5af
George <g.andronchik@gmail.com>
2023-01-19 11:17:24 -0300

    minor

--- rust/cubesql/cubesql/e2e/tests/postgres.rs
+++ rust/cubesql/cubesql/e2e/tests/postgres.rs
@@ -274,7 +274,7 @@ impl PostgresIntegrationTestSuite {
 
         let cancel_token = client.cancel_token();
         let cancel = async move {
-            sleep(Duration::from_millis(2000)).await;
+            sleep(Duration::from_millis(1000)).await;
 
             cancel_token.cancel_query(NoTls).await
         };
@@ -303,7 +303,7 @@ impl PostgresIntegrationTestSuite {
 
         let cancel_token = client.cancel_token();
         let cancel = async move {
-            sleep(Duration::from_millis(3000)).await;
+            sleep(Duration::from_millis(1000)).await;
 
             cancel_token.cancel_query(NoTls).await
         };
