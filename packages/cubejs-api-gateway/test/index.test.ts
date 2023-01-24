6948a1401641417c601bcfbad144dfdb0e61a54e
Mikhail Nitsenko <mnitsenko@gmail.com>
2023-01-24 16:30:46 +0600

    restored tests

--- packages/cubejs-api-gateway/test/index.test.ts
+++ packages/cubejs-api-gateway/test/index.test.ts
@@ -555,6 +555,7 @@ describe('API Gateway', () => {
         successBody: {
           query: {
             query: 'SELECT * FROM sql-runner',
+            limit: 10000,
             resultFilter: {
               objectLimit: 2,
               stringLimit: 2,
@@ -580,11 +581,30 @@ describe('API Gateway', () => {
         }, {
           result: {
             status: 400,
+            error: 'A user\'s query must contain limit query param and it must be positive number less than 50000.'
+          },
+          body: { query: { query: 'SELECT * FROM sql-runner' } }
+        }, {
+          result: {
+            status: 400,
+            error: 'A user\'s query must contain limit query param and it must be positive number less than 50000.'
+          },
+          body: { query: { query: 'SELECT * FROM sql-runner', limit: 60000 } }
+        }, {
+          result: {
+            status: 400,
+            error: 'A user\'s query must contain limit query param and it must be positive number less than 50000.'
+          },
+          body: { query: { query: 'SELECT * FROM sql-runner', limit: -1 } }
+        }, {
+          result: {
+            status: 400,
             error: 'A query.resultFilter.objectTypes must be an array of strings'
           },
           body: {
             query: {
               query: 'SELECT * FROM sql-runner',
+              limit: 10000,
               resultFilter: {
                 objectTypes: 'text'
               },
