dca0a220fd28a6b2d5647ba8e33ba9b63d5076c8
Alexandr Romanenko <alex.romanenko@cube.dev>
2023-02-07 21:41:27 +0300

    repair index fix

--- rust/cubestore/cubestore/src/scheduler/mod.rs
+++ rust/cubestore/cubestore/src/scheduler/mod.rs
@@ -206,7 +206,10 @@ impl SchedulerImpl {
         )
         .await
         {
-            error!("Error scheduling partitions compaction: {}", e);
+            error!(
+                "Error scheduling deactivation chunks without partitions: {}",
+                e
+            );
         }
 
         if let Err(e) = warn_long_fut(
