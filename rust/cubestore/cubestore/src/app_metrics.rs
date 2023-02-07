1581817b3dc5655623151bf7b1822958da9b6ccc
Dmitry Patsura <talk@dmtry.me>
2023-02-07 21:21:23 +0300

    chore(metrics): Expose cache.size + cache.hit

--- rust/cubestore/cubestore/src/app_metrics.rs
+++ rust/cubestore/cubestore/src/app_metrics.rs
@@ -2,13 +2,15 @@
 //! The convention is to prefix all metrics with `cs.` (short for CubeStore).
 
 use crate::util::metrics;
-use crate::util::metrics::{Counter, Histogram};
+use crate::util::metrics::{Counter, Gauge, Histogram};
 
 /// The number of process startups.
 pub static STARTUPS: Counter = metrics::counter("cs.startup");
 
 /// Incoming SQL queries that do data reads.
 pub static DATA_QUERIES: Counter = metrics::counter("cs.sql.query.data");
+pub static DATA_QUERIES_CACHE_HIT: Counter = metrics::counter("cs.sql.query.data.cache.hit");
+pub static DATA_QUERIES_CACHE_SIZE: Gauge = metrics::gauge("cs.sql.query.data.cache.size");
 pub static DATA_QUERY_TIME_MS: Histogram = metrics::histogram("cs.sql.query.data.ms");
 /// Incoming SQL queries that only read metadata or do trivial computations.
 pub static META_QUERIES: Counter = metrics::counter("cs.sql.query.meta");
