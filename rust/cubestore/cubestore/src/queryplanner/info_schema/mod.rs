bd6d316f3ea762a7443fa07c3afef45b81c80338
Dmitry Patsura <talk@dmtry.me>
2023-02-13 15:59:48 +0300

    feat(cubestore): Introduce information_schema.columns

--- rust/cubestore/cubestore/src/queryplanner/info_schema/mod.rs
+++ rust/cubestore/cubestore/src/queryplanner/info_schema/mod.rs
@@ -1,3 +1,4 @@
+mod info_schema_columns;
 mod info_schema_schemata;
 mod info_schema_tables;
 mod system_cache;
@@ -10,6 +11,7 @@ mod system_replay_handles;
 mod system_snapshots;
 mod system_tables;
 
+pub use info_schema_columns::*;
 pub use info_schema_schemata::*;
 pub use info_schema_tables::*;
 pub use system_cache::*;
