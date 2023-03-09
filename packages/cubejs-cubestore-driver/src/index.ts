22598978c8d5a291759d2c40aa4cad3d5a45c47b
Dmitry Patsura <talk@dmtry.me>
2023-03-09 14:22:58 +0300

    chore: explain motivation

--- packages/cubejs-cubestore-driver/src/index.ts
+++ packages/cubejs-cubestore-driver/src/index.ts
@@ -1,17 +1,21 @@
-// https://github.com/google/flatbuffers/blob/v23.3.3/tsconfig.json
+// https://github.com/google/flatbuffers/issues/6208
 // flatbuffers uses DOM... In browsers TextEncoder/Decoder (Web API) lives in global space
+// https://github.com/google/flatbuffers/blob/v23.3.3/tsconfig.json
+
+// todo: Remove after fix
 if (!global.TextEncoder) {
   // eslint-disable-next-line global-require
-  global.TextEncoder = require("util").TextEncoder;
+  global.TextEncoder = require('util').TextEncoder;
 }
 
+// todo: Remove after fix
 if (!global.TextDecoder) {
   // eslint-disable-next-line global-require
-  global.TextDecoder = require("util").TextDecoder;
+  global.TextDecoder = require('util').TextDecoder;
 }
 
-export * from "./CubeStoreCacheDriver";
-export * from "./CubeStoreDriver";
-export * from "./CubeStoreDevDriver";
-export * from "./CubeStoreQueueDriver";
-export * from "./rexport";
+export * from './CubeStoreCacheDriver';
+export * from './CubeStoreDriver';
+export * from './CubeStoreDevDriver';
+export * from './CubeStoreQueueDriver';
+export * from './rexport';
