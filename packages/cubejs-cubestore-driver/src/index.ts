9292ed3cca6bd35aa0e7701074993305dabb808d
Dmitry Patsura <talk@dmtry.me>
2023-03-09 14:12:30 +0300

    chore: fix flatbuffers compatibility with node

--- packages/cubejs-cubestore-driver/src/index.ts
+++ packages/cubejs-cubestore-driver/src/index.ts
@@ -1,5 +1,15 @@
-export * from './CubeStoreCacheDriver';
-export * from './CubeStoreDriver';
-export * from './CubeStoreDevDriver';
-export * from './CubeStoreQueueDriver';
-export * from './rexport';
+if (!global.TextEncoder) {
+  // eslint-disable-next-line global-require
+  global.TextEncoder = require("util").TextEncoder;
+}
+
+if (!global.TextDecoder) {
+  // eslint-disable-next-line global-require
+  global.TextDecoder = require("util").TextDecoder;
+}
+
+export * from "./CubeStoreCacheDriver";
+export * from "./CubeStoreDriver";
+export * from "./CubeStoreDevDriver";
+export * from "./CubeStoreQueueDriver";
+export * from "./rexport";
