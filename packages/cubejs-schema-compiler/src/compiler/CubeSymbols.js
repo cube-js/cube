f6d98d2df6427dd483d04a703e0014cfefb11ef0
Dmitry Patsura <talk@dmtry.me>
2023-04-05 19:04:06 +0300

    feat(schema-compiler): Support snake_case aliases for fields

--- packages/cubejs-schema-compiler/src/compiler/CubeSymbols.js
+++ packages/cubejs-schema-compiler/src/compiler/CubeSymbols.js
@@ -15,6 +15,38 @@ const CONTEXT_SYMBOLS = {
 
 const CURRENT_CUBE_CONSTANTS = ['CUBE', 'TABLE'];
 
+const CUBE_ALIAS_MAPPING = {
+  sql_alias: 'sqlAlias',
+  sql_table: 'sqlTable',
+  refresh_key: 'refreshKey',
+  pre_aggregations: 'preAggregations',
+  rewrite_queries: 'rewriteQueries',
+  data_source: 'dataSource',
+};
+
+const CUBE_MEASURES_ALIAS_MAPPING = {
+  drill_members: 'drillMembers',
+  rolling_window: 'rollingWindow',
+};
+
+const CUBE_DIMENSIONS_ALIAS_MAPPING = {
+  primary_key: 'primaryKey',
+  propagate_filters_to_sub_query: 'propagateFiltersToSubQuery',
+  sub_query: 'subQuery',
+};
+
+const CUBE_PRE_AGGREGATION_ALIAS_MAPPING = {
+  time_dimension: 'timeDimension',
+  partition_granularity: 'partitionGranularity',
+  refresh_key: 'refreshKey',
+  allow_non_strict_date_range_match: 'allowNonStrictDateRangeMatch',
+  use_original_sql_preaggregations: 'useOriginalSqlPreAggregations',
+  scheduled_refresh: 'scheduledRefresh',
+  build_range_start: 'buildRangeStart',
+  build_range_end: 'buildRangeEnd',
+  union_with_source_data: 'unionWithSourceData',
+};
+
 export class CubeSymbols {
   constructor() {
     this.symbols = {};
@@ -118,6 +150,11 @@ export class CubeSymbols {
       errorReporter.error(`${duplicateNames.join(', ')} defined more than once`);
     }
 
+    this.transformAliases(cube, CUBE_ALIAS_MAPPING, false);
+    this.transformAliases(cube.measures, CUBE_MEASURES_ALIAS_MAPPING);
+    this.transformAliases(cube.dimensions, CUBE_DIMENSIONS_ALIAS_MAPPING);
+    this.transformAliases(cube.preAggregations, CUBE_PRE_AGGREGATION_ALIAS_MAPPING);
+
     this.camelCaseTypes(cube.joins);
     this.camelCaseTypes(cube.measures);
     this.camelCaseTypes(cube.dimensions);
@@ -137,10 +174,36 @@ export class CubeSymbols {
     );
   }
 
+  /**
+   * @private
+   */
+  transformAliases(obj, mapping, nested = true) {
+    if (!obj) {
+      return;
+    }
+
+    if (nested) {
+      for (const field of Object.keys(obj)) {
+        this.transformAliases(obj[field], mapping, false);
+      }
+    } else {
+      for (const field of Object.keys(mapping)) {
+        if (obj[field] !== undefined) {
+          obj[mapping[field]] = obj[field];
+          delete obj[field];
+        }
+      }
+    }
+  }
+
+  /**
+   * @private
+   */
   camelCaseTypes(obj) {
     if (!obj) {
       return;
     }
+
     // eslint-disable-next-line no-restricted-syntax
     for (const member of Object.values(obj)) {
       if (member.type && member.type.indexOf('_') !== -1) {
