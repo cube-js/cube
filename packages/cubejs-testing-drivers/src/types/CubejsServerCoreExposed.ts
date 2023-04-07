16c251a0ce5a8007485d79991c3744ffb2d83734
buntarb <buntarb@gmail.com>
2023-04-07 23:55:14 +0200

    wip

--- packages/cubejs-testing-drivers/src/types/CubejsServerCoreExposed.ts
+++ packages/cubejs-testing-drivers/src/types/CubejsServerCoreExposed.ts
@@ -1,1 +1,12 @@
-export type CubejsServerCoreExposed = any;
+import { CubejsServerCore } from '@cubejs-backend/server-core';
+
+export type CubejsServerCoreExposed = CubejsServerCore & {
+  options: any;
+  optsHandler: any;
+  contextToDbType: any;
+  contextToExternalDbType: any;
+  apiGateway: any;
+  reloadEnvVariables: any;
+  refreshScheduler: any;
+  getRefreshScheduler: any;
+};
