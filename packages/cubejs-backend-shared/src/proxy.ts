b8a3c457d9af50ed00311d86794b03c35a21c3b1
Sergey Garin <sergey.garin@cube.dev>
2023-04-17 17:23:50 +0200

    fix(cubejs-backend-shared): avoid using npm for reading config file

--- packages/cubejs-backend-shared/src/proxy.ts
+++ packages/cubejs-backend-shared/src/proxy.ts
@@ -1,27 +1,10 @@
-import { exec } from 'child_process';
 import HttpsProxyAgent from 'http-proxy-agent';
-
-function getCommandOutput(command: string) {
-  return new Promise<string>((resolve, reject) => {
-    exec(command, (error, stdout) => {
-      if (error) {
-        reject(error.message);
-        return;
-      }
-
-      resolve(stdout);
-    });
-  });
-}
+// @ts-ignore
+import npmConf from 'npm-conf';
 
 export async function getProxySettings() {
-  const [proxy] = (
-    await Promise.all([getCommandOutput('npm config -g get https-proxy'), getCommandOutput('npm config -g get proxy')])
-  )
-    .map((s) => s.trim())
-    .filter((s) => !['null', 'undefined', ''].includes(s));
-
-  return proxy;
+  const conf = npmConf();
+  return conf.get('https-proxy') ?? conf.get('proxy');
 }
 
 export async function getHttpAgentForProxySettings() {
