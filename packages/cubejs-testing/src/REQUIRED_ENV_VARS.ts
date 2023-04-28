3710b113160d4b0f53b40d6b31ae9c901aa51571
Pavel Tiunov <pavel.tiunov@gmail.com>
2023-04-27 23:29:27 -0700

    fix(snowflake-driver): Int is exported to pre-aggregations as decimal… (#6513)
    * fix(snowflake-driver): Int is exported to pre-aggregations as decimal: Internal: Execution error: Internal: Error during planning: Extension planner for ClusterSendNode
    
    * Fix wrong package ordering
    
    * Touch snowflake
    
    * Add client build
    
    * Remove obsolete snapshot
    
    * Fix driver test use incorrect API token
    
    * Fix athena driver test

--- packages/cubejs-testing/src/REQUIRED_ENV_VARS.ts
+++ packages/cubejs-testing/src/REQUIRED_ENV_VARS.ts
@@ -15,6 +15,14 @@ export const REQUIRED_ENV_VARS: {[key: string]: string[]} = {
     'CUBEJS_DB_EXPORT_BUCKET',
     'CUBEJS_DB_BQ_CREDENTIALS',
   ],
+  snowflake: [
+    'CUBEJS_DB_USER',
+    'CUBEJS_DB_PASS',
+    'CUBEJS_DB_NAME',
+    'CUBEJS_DB_SNOWFLAKE_ACCOUNT',
+    'CUBEJS_DB_SNOWFLAKE_REGION',
+    'CUBEJS_DB_SNOWFLAKE_WAREHOUSE',
+  ],
   crate: [],
   firebolt: [
     'CUBEJS_DB_USER',
