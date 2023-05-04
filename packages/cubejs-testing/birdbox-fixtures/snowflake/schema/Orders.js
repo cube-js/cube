7c8b8ded5f7bb16248989bd56b0913180321314d
Pavel Tiunov <pavel.tiunov@gmail.com>
2023-05-04 10:23:26 -0700

    fix(snowflake-driver): Float exported as decimal for pre-aggregations (#6544)

--- packages/cubejs-testing/birdbox-fixtures/snowflake/schema/Orders.js
+++ packages/cubejs-testing/birdbox-fixtures/snowflake/schema/Orders.js
@@ -1,14 +1,14 @@
 cube(`Orders`, {
   sql: `
-  select 1 as id, 100 as amount, 'new' status
+  select 1 as id, 100 as amount, 100.15 as decimal_amount, CAST(100.5 AS FLOAT) as float_amount, 'new' status
   UNION ALL
-  select 2 as id, 200 as amount, 'new' status
+  select 2 as id, 200 as amount, 100.25 as decimal_amount, CAST(200.5 AS FLOAT) as float_amount, 'new' status
   UNION ALL
-  select 3 as id, 300 as amount, 'processed' status
+  select 3 as id, 300 as amount, 100.35 as decimal_amount, CAST(300.5 AS FLOAT) as float_amount, 'processed' status
   UNION ALL
-  select 4 as id, 500 as amount, 'processed' status
+  select 4 as id, 500 as amount, 100.45 as decimal_amount, CAST(500.5 AS FLOAT) as float_amount, 'processed' status
   UNION ALL
-  select 5 as id, 600 as amount, 'shipped' status
+  select 5 as id, 600 as amount, 100.55 as decimal_amount, CAST(600.5 AS FLOAT) as float_amount, 'shipped' status
   `,
   measures: {
     count: {
@@ -16,6 +16,14 @@ cube(`Orders`, {
     },
     totalAmount: {
       sql: `amount`,
+      type: `sum`,
+    },
+    totalDecimalAmount: {
+      sql: `decimal_amount`,
+      type: `sum`,
+    },
+    totalFloatAmount: {
+      sql: `float_amount`,
       type: `sum`,
     },
     toRemove: {
@@ -31,7 +39,7 @@ cube(`Orders`, {
 
   preAggregations: {
     amount: {
-      measures: [totalAmount],
+      measures: [totalAmount, totalFloatAmount, totalDecimalAmount],
       dimensions: [status],
     }
   }
