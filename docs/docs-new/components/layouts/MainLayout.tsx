c395e6899ad1ac76329def9e45c72798083ab070
Hassan Khan <hassan@cube.dev>
2023-05-22 11:28:13 +0100

    docs: fix build errors

--- docs/docs-new/components/layouts/MainLayout.tsx
+++ docs/docs-new/components/layouts/MainLayout.tsx
@@ -3,14 +3,13 @@ import { Children } from 'react';
 
 const FeedbackBlock = dynamic(() => import('@/components/common/FeedbackBlock/FeedbackBlock').then((r) => r.FeedbackBlock), {
   ssr: false
-})
+});
 
 export const MainLayout = (props) => {
   const childrenAsArray = Children.toArray(props.children.props.children);
   const [firstChild, ...restChildren] = childrenAsArray;
   const childrenWithInjectedFeedback = [
     firstChild,
-    // @ts-expect-error
     <FeedbackBlock key='feedback' />,
     ...restChildren,
   ];
