9086be4bc8e727733f11c08240b463f8f64c879d
Dmitry Patsura <talk@dmtry.me>
2023-05-23 16:50:48 +0300

    feat(native): Initial support for cube.py (Python configuration) (#6465)

---
+++ packages/cubejs-backend-native/test/python.test.ts
@@ -0,0 +1,42 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import * as native from '../js';
+import * as assert from 'assert';
+
+describe('Python', () => {
+  async function loadConfigurationFile() {
+      const content = await fs.readFile(path.join(process.cwd(), 'test', 'config.py'), 'utf8');
+      console.log('content', {
+        content
+      });
+
+      const config = await native.pythonLoadConfig(
+        content,
+        {
+          file: 'config.py'
+        }
+      );
+
+      console.log('loaded config', config);
+
+      return config;
+  }
+
+  test('async checkAuth', async () => {
+    const config = await loadConfigurationFile();
+    expect(config).toEqual({
+      schemaPath: 'models',
+      pgSqlPort: 5555,
+      telemetry: false,
+      checkAuth: expect.any(Function),
+      queryRewrite: expect.any(Function),
+    })
+
+    if (config.checkAuth) {
+      await config.checkAuth({ requestId: 'test' }, 'MY_SECRET_TOKEN');
+    } else {
+      throw new Error('checkAuth was defined in config.py')
+    }
+  })
+});
