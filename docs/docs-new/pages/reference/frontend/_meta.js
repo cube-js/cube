7d71514d7b2b90087dc73684091dc8cd47a11b49
Hassan Khan <hassan@cube.dev>
2023-05-31 15:38:05 +0100

    docs: fix various issues with generated documentation

--- docs/docs-new/pages/reference/frontend/_meta.js
+++ docs/docs-new/pages/reference/frontend/_meta.js
@@ -1,4 +1,7 @@
 module.exports = {
-  "cubejs-client-ngx": "@cubejs-client-ngx",
-  "cubejs-client-vue": "@cubejs-client-vue"
+  "cubejs-client-core": "@cubejs-client/core",
+  "cubejs-client-react": "@cubejs-client/react",
+  "cubejs-client-ngx": "@cubejs-client/ngx",
+  "cubejs-client-vue": "@cubejs-client/vue",
+  "cubejs-client-ws-transport": "@cubejs-client/ws-transport"
 }
--- docs/docs-new/scripts/migrate-content.ts
+++ docs/docs-new/scripts/migrate-content.ts
@@ -938,8 +938,11 @@ const overrides: Record<string, Override> = {
     title: "@cubejs-client/ngx",
 
     meta: {
-      "cubejs-client-ngx": "@cubejs-client-ngx",
-      "cubejs-client-vue": "@cubejs-client-vue",
+      "cubejs-client-core": "@cubejs-client/core",
+      "cubejs-client-react": "@cubejs-client/react",
+      "cubejs-client-ngx": "@cubejs-client/ngx",
+      "cubejs-client-vue": "@cubejs-client/vue",
+      "cubejs-client-ws-transport": "@cubejs-client/ws-transport",
     },
   },
 
