912c547b721874b058d0ed84bc03fcf043083dd9
Alexandr Romanenko <alex.romanenko@cube.dev>
2023-06-06 21:27:33 +0300

    update

--- rust/cubestore/cubestore/src/util/memory.rs
+++ rust/cubestore/cubestore/src/util/memory.rs
@@ -2,7 +2,7 @@ use crate::config::injection::DIService;
 use crate::CubeError;
 use std::alloc::{GlobalAlloc, Layout, System};
 use std::fmt::Debug;
-use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
+use std::sync::atomic::{AtomicI64, Ordering::Relaxed};
 use std::sync::Arc;
 
 pub trait MemoryHandler: DIService + Debug + Send + Sync {
@@ -27,35 +27,35 @@ impl MemoryHandlerImpl {
 crate::di_service!(MemoryHandlerImpl, [MemoryHandler]);
 
 struct TrackingAllocator {
-    allocated: AtomicUsize,
+    allocated: AtomicI64,
 }
 
 unsafe impl GlobalAlloc for TrackingAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let ret = System.alloc(layout);
         if !ret.is_null() {
-            self.allocated.fetch_add(layout.size(), Relaxed);
+            self.allocated.fetch_add(layout.size() as i64, Relaxed);
         }
         ret
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         System.dealloc(ptr, layout);
-        self.allocated.fetch_sub(layout.size(), Relaxed);
+        self.allocated.fetch_sub(layout.size() as i64, Relaxed);
     }
 }
 
 impl TrackingAllocator {
-    pub fn allocated(&self) -> usize {
+    pub fn allocated(&self) -> i64 {
         self.allocated.load(Relaxed)
     }
 }
 
 #[global_allocator]
 static ALLOCATOR: TrackingAllocator = TrackingAllocator {
-    allocated: AtomicUsize::new(0),
+    allocated: AtomicI64::new(0),
 };
 
-pub fn get_allocated_memory() -> usize {
+pub fn get_allocated_memory() -> i64 {
     ALLOCATOR.allocated()
 }
