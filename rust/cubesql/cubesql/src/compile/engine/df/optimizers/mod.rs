16407bd6dedac33111e1a97679417f74f928a51b
Alex Qyoun-ae <4062971+MazterQyou@users.noreply.github.com>
2023-06-15 06:12:15 +0400

    feat(cubesql): Support some window functions with CubeScan

--- rust/cubesql/cubesql/src/compile/engine/df/optimizers/mod.rs
+++ rust/cubesql/cubesql/src/compile/engine/df/optimizers/mod.rs
@@ -3,7 +3,9 @@ pub mod utils;
 mod filter_push_down;
 mod limit_push_down;
 mod sort_push_down;
+mod window_aggr_put_projection;
 
 pub use filter_push_down::FilterPushDown;
 pub use limit_push_down::LimitPushDown;
 pub use sort_push_down::SortPushDown;
+pub use window_aggr_put_projection::WindowAggrPutProjection;
