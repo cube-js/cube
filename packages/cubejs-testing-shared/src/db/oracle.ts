422f17135a75b5743fdd9d440719c9334f93fc34
Pavel Tiunov <pavel.tiunov@gmail.com>
2023-06-29 00:41:37 -0700

    Bump oracle start timeout

--- packages/cubejs-testing-shared/src/db/oracle.ts
+++ packages/cubejs-testing-shared/src/db/oracle.ts
@@ -15,9 +15,9 @@ export class OracleDBRunner extends DbRunnerAbstract {
       .withEnv('ORACLE_PASSWORD', 'test')
       .withHealthCheck({
         test: 'healthcheck.sh',
-        interval: 2 * 1000,
+        interval: 5 * 1000,
         timeout: 15 * 1000,
-        retries: 5,
+        retries: 12,
         startPeriod: (isCI() ? 60 : 30) * 1000
       })
       .withWaitStrategy(Wait.forHealthCheck())
