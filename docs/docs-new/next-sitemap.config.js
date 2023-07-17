fd3a5cbbe7e3a9b4702d6b0735ddffaccc4919f6
Hassan Khan <hassan@cube.dev>
2023-07-17 15:27:11 +0100

    docs: fix sitemap URLs

--- docs/docs-new/next-sitemap.config.js
+++ docs/docs-new/next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   exclude: ['*/_meta'],
-  siteUrl: process.env.SITE_URL || 'https://cube.dev/docs-next',
+  siteUrl: process.env.SITE_URL || 'https://cube.dev/docs',
   generateRobotsTxt: true, // (optional),
   robotsTxtOptions: {
     policies: [{ userAgent: '*', disallow: '/' }],
