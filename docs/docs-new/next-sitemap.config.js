879c2f9e16a3237e7a8d2ddedb470c0a1ec5645c
Hassan Khan <hassan@cube.dev>
2023-07-18 07:28:11 +0100

    docs: fix sitemap by excluding sample page

--- docs/docs-new/next-sitemap.config.js
+++ docs/docs-new/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  exclude: ['*/_meta'],
+  exclude: ['*/_meta', '/docs'],
   siteUrl: process.env.SITE_URL || 'https://cube.dev/docs',
   generateRobotsTxt: true, // (optional),
   robotsTxtOptions: {
