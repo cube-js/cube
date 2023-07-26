e15c4591f9e489eaf5b9304c4ea76487449a6b8b
jnomeisky <javonome@gmail.com>
2023-07-26 17:33:41 -0300

    test rollback

--- examples/react-dashboard/dashboard-app/src/serviceWorker.js
+++ examples/react-dashboard/dashboard-app/src/serviceWorker.js
@@ -21,9 +21,9 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  if (/*process.env.NODE_ENV === 'production' &&*/ 'serviceWorker' in navigator) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(/*process.env.PUBLIC_URL*/ '', window.location.href);
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -32,8 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      const swUrl = `service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
