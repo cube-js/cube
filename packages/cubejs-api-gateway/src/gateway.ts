69c319c217eaad4d80407b649ccf2bea3dd67b4c
Alex Vasilev <vaspiring@gmail.com>
2023-09-21 19:05:15 +0200

    move logging message

--- packages/cubejs-api-gateway/src/gateway.ts
+++ packages/cubejs-api-gateway/src/gateway.ts
@@ -2146,12 +2146,6 @@ class ApiGateway {
           req.securityContext = await checkAuthFn(auth, secret);
           req.signedWithPlaygroundAuthSecret = Boolean(internalOptions?.isPlaygroundCheckAuth);
         } catch (e) {
-          this.log({
-            type: (e as Error).message,
-            url: req.url,
-            token: auth,
-            error: (e as Error).stack || (e as Error).toString()
-          }, <any>req);
           if (this.enforceSecurityChecks) {
             throw new CubejsHandlerError(403, 'Forbidden', 'Invalid token');
           }
@@ -2273,6 +2267,13 @@ class ApiGateway {
       }
     } catch (e: unknown) {
       if (e instanceof CubejsHandlerError) {
+        this.log({
+          type: (e as Error).message,
+          url: req.url,
+          token,
+          error: (e as Error).stack || (e as Error).toString()
+        }, <any>req);
+        
         res.status(e.status).json({ error: e.message });
       } else if (e instanceof Error) {
         this.log({
