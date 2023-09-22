0fb0ea7ad67a9546dc1173dd6c7ff6ea1c1e822b
Alex Vasilev <alex@cube.dev>
2023-09-22 19:41:03 +0200

    chore: show original jwt error (#7161)

--- packages/cubejs-api-gateway/src/CubejsHandlerError.ts
+++ packages/cubejs-api-gateway/src/CubejsHandlerError.ts
@@ -2,7 +2,8 @@ export class CubejsHandlerError extends Error {
   public constructor(
     public readonly status: number,
     public readonly type: string,
-    message: string
+    message: string,
+    public readonly originalError?: Error
   ) {
     super(message || type);
   }
