463b20162fcc460f544a1926f46a3c049829b97a
Igor Lukanin <me@igorlukanin.io>
2023-10-02 13:17:17 +0200

    Extract "drilldowns" into a recipe

--- docs/docs-new/pages/guides/recipes/_meta.js
+++ docs/docs-new/pages/guides/recipes/_meta.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "overview": "Overview",
   "analytics": "Analytics",
   "access-control": "Access control",
   "auth": "Authentication & Authorization",
   "data-modeling": "Data modeling",
+  "code-reusability": "Code reusability",
   "data-sources": "Data sources",
   "queries": "Queries",
   "query-acceleration": "Query acceleration",
-  "code-reusability": "Code reusability",
+  "data-exploration": "Data exploration",
   "upgrading-cube": "Upgrading Cube"
 }
