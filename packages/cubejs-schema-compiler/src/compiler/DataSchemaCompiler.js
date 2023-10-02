69e9437deccc2f16442a39ceb95d1583bcea195f
Pavel Tiunov <pavel.tiunov@gmail.com>
2023-10-02 11:59:46 -0700

    feat(python): Evaluate Jinja templates inside `.yml` and `.yaml` (#7182)

--- packages/cubejs-schema-compiler/src/compiler/DataSchemaCompiler.js
+++ packages/cubejs-schema-compiler/src/compiler/DataSchemaCompiler.js
@@ -17,6 +17,8 @@ const NATIVE_IS_SUPPORTED = isNativeSupported();
 
 const moduleFileCache = {};
 
+const JINJA_SYNTAX = /{%|%}|{{|}}/ig;
+
 export class DataSchemaCompiler {
   constructor(repository, options = {}) {
     this.repository = repository;
@@ -122,7 +124,11 @@ export class DataSchemaCompiler {
   }
 
   transpileFile(file, errorsReport) {
-    if (R.endsWith('.jinja', file.fileName)) {
+    if (R.endsWith('.jinja', file.fileName) ||
+      (R.endsWith('.yml', file.fileName) || R.endsWith('.yaml', file.fileName))
+      // TODO do Jinja syntax check with jinja compiler
+      && file.content.match(JINJA_SYNTAX)
+    ) {
       if (NATIVE_IS_SUPPORTED !== true) {
         throw new Error(
           `Native extension is required to process jinja files. ${NATIVE_IS_SUPPORTED.reason}. Read more: ` +
@@ -234,7 +240,12 @@ export class DataSchemaCompiler {
 
     if (R.endsWith('.js', file.fileName)) {
       this.compileJsFile(file, errorsReport, cubes, contexts, exports, asyncModules, toCompile, compiledFiles);
-    } else if (R.endsWith('.yml.jinja', file.fileName) || R.endsWith('.yaml.jinja', file.fileName)) {
+    } else if (R.endsWith('.yml.jinja', file.fileName) || R.endsWith('.yaml.jinja', file.fileName) ||
+      (
+        R.endsWith('.yml', file.fileName) || R.endsWith('.yaml', file.fileName)
+        // TODO do Jinja syntax check with jinja compiler
+      ) && file.content.match(JINJA_SYNTAX)
+    ) {
       this.yamlCompiler.compileYamlWithJinjaFile(
         file,
         errorsReport,
