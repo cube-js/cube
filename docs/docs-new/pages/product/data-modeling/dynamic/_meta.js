6d6f66d0a3e5c43a3d295304c431bffd751d61eb
Igor Lukanin <me@igorlukanin.io>
2023-10-02 18:52:34 +0200

    Rough draft of the "dynamic data models" page

--- docs/docs-new/pages/product/data-modeling/dynamic/_meta.js
+++ docs/docs-new/pages/product/data-modeling/dynamic/_meta.js
@@ -1,6 +1,6 @@
 module.exports = {
   "jinja": "Dynamic data models with Jinja and Python",
-  "dynamic-schema-creation": "Dynamic data models with JavaScript",
+  "javascript": "Dynamic data models with JavaScript",
   "code-reusability-export-and-import": "Export and import",
   "schema-execution-environment": "Execution Environment (JS models)"
 }
