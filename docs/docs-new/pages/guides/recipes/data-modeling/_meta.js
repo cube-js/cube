95b99c5e21add64380109279fded9a94d57fb094
Igor Lukanin <me@igorlukanin.io>
2023-10-02 13:31:09 +0200

    Extract "string time dimensions" into a recipe

--- docs/docs-new/pages/guides/recipes/data-modeling/_meta.js
+++ docs/docs-new/pages/guides/recipes/data-modeling/_meta.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "string-time-dimensions": "Working around string time dimensions",
   "dynamic-union-tables": "Using dynamic union tables",
   "entity-attribute-value": "Implementing Entity-Attribute-Value Model (EAV)",
   "passing-dynamic-parameters-in-a-query": "Passing dynamic parameters in a query",
