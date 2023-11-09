39275b32d21707dfcb0019cfc18da81bb4082a5e
Igor Lukanin <me@igorlukanin.io>
2023-11-09 20:46:43 +0100

    Fix

--- docs/docs-new/pages/guides/_meta.js
+++ docs/docs-new/pages/guides/_meta.js
@@ -2,6 +2,5 @@ module.exports = {
   "dbt": "Using Cube with dbt",
   "recipes": "Recipes",
   "style-guide": "Style guide",
-  "data-store-cost-saving-guide": "Cost saving guide",
-  "examples": "Examples",
+  "data-store-cost-saving-guide": "Cost saving guide"
 }
