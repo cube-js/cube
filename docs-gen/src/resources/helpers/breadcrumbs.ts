c64f5d7e2117df7b2b04fcc68504d01c43db84d6
Igor Lukanin <igor@cube.dev>
2023-11-20 16:16:42 +0100

    docs: Remove old docs platform from the repo (#7387)
    * Remove old docs files and see if anything breaks
    
    * Remove docs-gen
    
    * Remove GitHub workflow files

--- docs-gen/src/resources/helpers/breadcrumbs.ts
+++
@@ -1,7 +0,0 @@
-import { PageEvent } from 'typedoc/dist/lib/output/events';
-
-import MarkdownTheme from '../../theme';
-
-export function breadcrumbs(this: PageEvent) {
-  return MarkdownTheme.handlebars.helpers.breadcrumbs.call(this);
-}
