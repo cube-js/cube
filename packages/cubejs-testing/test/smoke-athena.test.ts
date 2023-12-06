ddcb7110da7404b5232e68156d3ad6fd10bf08e0
Dmitry Patsura <talk@dmtry.me>
2023-12-06 18:56:41 +0100

    chore: remove unused code

--- packages/cubejs-testing/test/smoke-athena.test.ts
+++ packages/cubejs-testing/test/smoke-athena.test.ts
@@ -1,7 +1,6 @@
-import fetch from 'node-fetch';
 import cubejs, { CubejsApi } from '@cubejs-client/core';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { afterAll, beforeAll, jest, expect } from '@jest/globals';
+import { afterAll, beforeAll, jest } from '@jest/globals';
 import { BirdBox, getBirdbox } from '../src';
 import {
   DEFAULT_API_TOKEN,
@@ -10,8 +9,6 @@ import {
   JEST_BEFORE_ALL_DEFAULT_TIMEOUT,
   testQueryMeasure,
 } from './smoke-tests';
-
-const delay = (t: number) => new Promise(resolve => setTimeout(() => resolve(null), t));
 
 describe('athena', () => {
   jest.setTimeout(60 * 5 * 1000);
