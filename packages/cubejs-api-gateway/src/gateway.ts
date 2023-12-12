74b0862800ada31681e702992ff1f7da89924b9f
Dmitry Patsura <talk@dmtry.me>
2023-12-12 15:58:32 +0100

    fix(api-gateway): Fallback to global error middelware for async handlers (#7520)

--- packages/cubejs-api-gateway/src/gateway.ts
+++ packages/cubejs-api-gateway/src/gateway.ts
@@ -15,7 +15,8 @@ import type {
   ErrorRequestHandler,
   NextFunction,
   RequestHandler,
-  Response,
+  Request as ExpressRequest,
+  Response as ExpressResponse,
 } from 'express';
 import {
   QueryType,
@@ -91,9 +92,18 @@ import {
 
 const memberExpressionRegex = /^([a-zA-Z0-9_]+).([a-zA-Z0-9_]+):\(([a-zA-Z0-9_,]+)\):(.*)$/;
 
-/**
- * API gateway server class.
- */
+function userAsyncHandler(handler: (req: Request & { context: ExtendedRequestContext }, res: ExpressResponse) => Promise<void>) {
+  return (req: ExpressRequest, res: ExpressResponse, next: NextFunction) => {
+    handler(req as any, res).catch(next);
+  };
+}
+
+function systemAsyncHandler(handler: (req: Request & { context: ExtendedRequestContext }, res: ExpressResponse) => Promise<void>) {
+  return (req: ExpressRequest, res: ExpressResponse, next: NextFunction) => {
+    handler(req as any, res).catch(next);
+  };
+}
+
 class ApiGateway {
   protected readonly refreshScheduler: any;
 
@@ -216,7 +226,7 @@ class ApiGateway {
           }
         },
       ],
-      async (req, res) => {
+      userAsyncHandler(async (req, res) => {
         const compilerApi = await this.getCompilerApi(req.context);
         let schema = compilerApi.getGraphQLSchema();
         if (!schema) {
@@ -237,14 +247,14 @@ class ApiGateway {
             ? { headerEditorEnabled: true }
             : false,
         })(req, res);
-      }
+      })
     );
 
     /** **************************************************************
      * data scope                                                    *
      *************************************************************** */
 
-    app.get(`${this.basePath}/v1/load`, userMiddlewares, (async (req, res) => {
+    app.get(`${this.basePath}/v1/load`, userMiddlewares, userAsyncHandler(async (req: any, res) => {
       await this.load({
         query: req.query.query,
         context: req.context,
@@ -254,7 +264,7 @@ class ApiGateway {
     }));
 
     const jsonParser = bodyParser.json({ limit: '1mb' });
-    app.post(`${this.basePath}/v1/load`, jsonParser, userMiddlewares, (async (req, res) => {
+    app.post(`${this.basePath}/v1/load`, jsonParser, userMiddlewares, userAsyncHandler(async (req, res) => {
       await this.load({
         query: req.body.query,
         context: req.context,
@@ -263,7 +273,7 @@ class ApiGateway {
       });
     }));
 
-    app.get(`${this.basePath}/v1/subscribe`, userMiddlewares, (async (req, res) => {
+    app.get(`${this.basePath}/v1/subscribe`, userMiddlewares, userAsyncHandler(async (req: any, res) => {
       await this.load({
         query: req.query.query,
         context: req.context,
@@ -272,7 +282,7 @@ class ApiGateway {
       });
     }));
 
-    app.get(`${this.basePath}/v1/sql`, userMiddlewares, (async (req, res) => {
+    app.get(`${this.basePath}/v1/sql`, userMiddlewares, userAsyncHandler(async (req: any, res) => {
       await this.sql({
         query: req.query.query,
         context: req.context,
@@ -280,7 +290,7 @@ class ApiGateway {
       });
     }));
 
-    app.post(`${this.basePath}/v1/sql`, jsonParser, userMiddlewares, (async (req, res) => {
+    app.post(`${this.basePath}/v1/sql`, jsonParser, userMiddlewares, userAsyncHandler(async (req, res) => {
       await this.sql({
         query: req.body.query,
         context: req.context,
@@ -288,7 +298,7 @@ class ApiGateway {
       });
     }));
 
-    app.get(`${this.basePath}/v1/dry-run`, userMiddlewares, (async (req, res) => {
+    app.get(`${this.basePath}/v1/dry-run`, userMiddlewares, userAsyncHandler(async (req: any, res) => {
       await this.dryRun({
         query: req.query.query,
         context: req.context,
@@ -296,7 +306,7 @@ class ApiGateway {
       });
     }));
 
-    app.post(`${this.basePath}/v1/dry-run`, jsonParser, userMiddlewares, (async (req, res) => {
+    app.post(`${this.basePath}/v1/dry-run`, jsonParser, userMiddlewares, userAsyncHandler(async (req, res) => {
       await this.dryRun({
         query: req.body.query,
         context: req.context,
@@ -311,7 +321,7 @@ class ApiGateway {
     app.get(
       `${this.basePath}/v1/meta`,
       userMiddlewares,
-      async (req, res) => {
+      userAsyncHandler(async (req, res) => {
         if ('extended' in req.query) {
           await this.metaExtended({
             context: req.context,
@@ -323,7 +333,7 @@ class ApiGateway {
             res: this.resToResultFn(res),
           });
         }
-      }
+      })
     );
 
     // Used by Rollup Designer
@@ -345,7 +355,7 @@ class ApiGateway {
           }
         },
       ],
-      async (req: Request, res: Response) => {
+      userAsyncHandler(async (req, res) => {
         const { transformedQuery, references } = req.body;
         const compilerApi = await this.getCompilerApi(req.context as RequestContext);
         const canUsePreAggregationForTransformedQuery = compilerApi.canUsePreAggregationForTransformedQuery(
@@ -353,7 +363,7 @@ class ApiGateway {
           references,
         );
         res.json({ canUsePreAggregationForTransformedQuery });
-      }
+      })
     );
 
     /** **************************************************************
@@ -363,19 +373,19 @@ class ApiGateway {
     app.get(
       `${this.basePath}/v1/run-scheduled-refresh`,
       userMiddlewares,
-      async (req, res) => {
+      userAsyncHandler(async (req, res) => {
         await this.runScheduledRefresh({
           queryingOptions: req.query.queryingOptions,
           context: req.context,
           res: this.resToResultFn(res)
         });
-      }
+      })
     );
 
     app.post(
       `${this.basePath}/v1/pre-aggregations/jobs`,
       userMiddlewares,
-      this.preAggregationsJobs.bind(this),
+      userAsyncHandler(this.preAggregationsJobs.bind(this)),
     );
 
     /** **************************************************************
@@ -392,15 +402,15 @@ class ApiGateway {
 
       app.get('/cubejs-system/v1/context', systemMiddlewares, this.createSystemContextHandler(this.basePath));
 
-      app.get('/cubejs-system/v1/pre-aggregations', systemMiddlewares, (async (req, res) => {
+      app.get('/cubejs-system/v1/pre-aggregations', systemMiddlewares, systemAsyncHandler(async (req, res) => {
         await this.getPreAggregations({
-          cacheOnly: req.query.cacheOnly,
+          cacheOnly: !!req.query.cacheOnly,
           context: req.context,
           res: this.resToResultFn(res)
         });
       }));
 
-      app.get('/cubejs-system/v1/pre-aggregations/security-contexts', systemMiddlewares, (async (req, res) => {
+      app.get('/cubejs-system/v1/pre-aggregations/security-contexts', systemMiddlewares, systemAsyncHandler(async (req, res) => {
         const contexts = this.scheduledRefreshContexts ? await this.scheduledRefreshContexts() : [];
         this.resToResultFn(res)({
           securityContexts: contexts
@@ -409,13 +419,13 @@ class ApiGateway {
         });
       }));
 
-      app.get('/cubejs-system/v1/pre-aggregations/timezones', systemMiddlewares, (async (req, res) => {
+      app.get('/cubejs-system/v1/pre-aggregations/timezones', systemMiddlewares, systemAsyncHandler(async (req, res) => {
         this.resToResultFn(res)({
           timezones: this.scheduledRefreshTimeZones || []
         });
       }));
 
-      app.post('/cubejs-system/v1/pre-aggregations/partitions', jsonParser, systemMiddlewares, (async (req, res) => {
+      app.post('/cubejs-system/v1/pre-aggregations/partitions', jsonParser, systemMiddlewares, systemAsyncHandler(async (req, res) => {
         await this.getPreAggregationPartitions({
           query: req.body.query,
           context: req.context,
@@ -423,7 +433,7 @@ class ApiGateway {
         });
       }));
 
-      app.post('/cubejs-system/v1/pre-aggregations/preview', jsonParser, systemMiddlewares, (async (req, res) => {
+      app.post('/cubejs-system/v1/pre-aggregations/preview', jsonParser, systemMiddlewares, systemAsyncHandler(async (req, res) => {
         await this.getPreAggregationPreview({
           query: req.body.query,
           context: req.context,
@@ -431,7 +441,7 @@ class ApiGateway {
         });
       }));
 
-      app.post('/cubejs-system/v1/pre-aggregations/build', jsonParser, systemMiddlewares, (async (req, res) => {
+      app.post('/cubejs-system/v1/pre-aggregations/build', jsonParser, systemMiddlewares, systemAsyncHandler(async (req, res) => {
         await this.buildPreAggregations({
           query: req.body.query,
           context: req.context,
@@ -439,14 +449,14 @@ class ApiGateway {
         });
       }));
 
-      app.post('/cubejs-system/v1/pre-aggregations/queue', jsonParser, systemMiddlewares, (async (req, res) => {
+      app.post('/cubejs-system/v1/pre-aggregations/queue', jsonParser, systemMiddlewares, systemAsyncHandler(async (req, res) => {
         await this.getPreAggregationsInQueue({
           context: req.context,
           res: this.resToResultFn(res)
         });
       }));
 
-      app.post('/cubejs-system/v1/pre-aggregations/cancel', jsonParser, systemMiddlewares, (async (req, res) => {
+      app.post('/cubejs-system/v1/pre-aggregations/cancel', jsonParser, systemMiddlewares, systemAsyncHandler(async (req, res) => {
         await this.cancelPreAggregationsFromQueue({
           query: req.body.query,
           context: req.context,
@@ -527,7 +537,7 @@ class ApiGateway {
     }
   }
 
-  public async metaExtended({ context, res }: { context: RequestContext, res: ResponseResultFn }) {
+  public async metaExtended({ context, res }: { context: ExtendedRequestContext, res: ResponseResultFn }) {
     const requestStarted = new Date();
 
     try {
@@ -750,7 +760,7 @@ class ApiGateway {
    * ```
    * TODO (buntarb): selector object validator.
    */
-  private async preAggregationsJobs(req: Request, res: Response) {
+  private async preAggregationsJobs(req: Request, res: ExpressResponse) {
     const response = this.resToResultFn(res);
     const started = new Date();
     const context = <RequestContext>req.context;
@@ -1895,7 +1905,7 @@ class ApiGateway {
     }
   }
 
-  protected resToResultFn(res: Response) {
+  protected resToResultFn(res: ExpressResponse) {
     return (message, { status }: { status?: number } = {}) => (status ? res.status(status).json(message) : res.json(message));
   }
 
@@ -2262,7 +2272,7 @@ class ApiGateway {
     return undefined;
   }
 
-  protected async checkAuthWrapper(checkAuthFn: CheckAuthFn, req: Request, res: Response, next) {
+  protected async checkAuthWrapper(checkAuthFn: CheckAuthFn, req: Request, res: ExpressResponse, next) {
     const token = this.extractAuthorizationHeaderWithSchema(req);
 
     try {
@@ -2304,7 +2314,7 @@ class ApiGateway {
     await this.checkAuthWrapper(this.checkAuthSystemFn, req, res, next);
   };
   
-  protected requestContextMiddleware: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+  protected requestContextMiddleware: RequestHandler = async (req: Request, res: ExpressResponse, next: NextFunction) => {
     try {
       req.context = await this.contextByReq(req, req.securityContext, getRequestIdFromRequest(req));
       if (next) {
@@ -2319,7 +2329,7 @@ class ApiGateway {
     }
   };
 
-  protected requestLogger: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+  protected requestLogger: RequestHandler = async (req: Request, res: ExpressResponse, next: NextFunction) => {
     const details = requestParser(req, res);
 
     this.log({ type: 'REST API Request', ...details }, req.context);
@@ -2329,7 +2339,7 @@ class ApiGateway {
     }
   };
 
-  protected logNetworkUsage: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+  protected logNetworkUsage: RequestHandler = async (req: Request, res: ExpressResponse, next: NextFunction) => {
     this.log({
       type: 'Incoming network usage',
       service: 'api-http',
@@ -2400,7 +2410,7 @@ class ApiGateway {
     });
   }
 
-  protected healthResponse(res: Response, health: 'HEALTH' | 'DOWN') {
+  protected healthResponse(res: ExpressResponse, health: 'HEALTH' | 'DOWN') {
     res.status(health === 'HEALTH' ? 200 : 500).json({
       health,
     });
@@ -2430,9 +2440,9 @@ class ApiGateway {
     let health: 'HEALTH' | 'DOWN' = 'HEALTH';
 
     if (this.standalone) {
-      const orchestratorApi = await this.adapterApi({} as any);
-
       try {
+        const orchestratorApi = await this.adapterApi({} as any);
+
         // todo: test other data sources
         orchestratorApi.addDataSeenSource('default');
         await orchestratorApi.testConnection();
