d8a273475b12de8bd7e91096ef94d03c8bd1adcc
Igor Lukanin <me@igorlukanin.io>
2023-12-15 19:32:14 +0100

    First draft

--- docs/pages/product/workspace/_meta.js
+++ docs/pages/product/workspace/_meta.js
@@ -1,14 +1,15 @@
 module.exports = {
   "playground": "Playground",
   "data-model": "Data Model",
+  "dev-mode": "Development mode",
   "semantic-layer-sync": "Semantic Layer Sync",
   "sql-runner": "SQL Runner",
   "query-history": "Query History",
   "pre-aggregations": "Pre-Aggregations",
+  "performance": "Performance Insights",
   "access-control": "Access Control",
   "budgets": "Budgets",
   "sso": "Single Sign-On",
-  "dev-mode": "Development mode",
   "preferences": "Preferences",
   "cli": "CLI"
 }
