0240f71ff4b9deff8d9e77776ed99f255516b31e
Jichao Sun <4977515+JichaoS@users.noreply.github.com>
2024-01-11 23:41:48 -0500

    Add support for duckdb database file path

--- packages/cubejs-duckdb-driver/src/DuckDBDriver.ts
+++ packages/cubejs-duckdb-driver/src/DuckDBDriver.ts
@@ -57,8 +57,11 @@ export class DuckDBDriver extends BaseDriver implements DriverInterface {
 
   protected async init(): Promise<InitPromise> {
     const token = getEnv('duckdbMotherDuckToken', this.config);
+    const db_path = getEnv('duckdbDatabasePath', this.config);
 
-    const db = new Database(token ? `md:?motherduck_token=${token}&custom_user_agent=Cube/${version}` : ':memory:');
+    const db_url = db_path ? db_path : (token ? `md:?motherduck_token=${token}&custom_user_agent=Cube/${version}` : ':memory:');
+
+    const db = new Database(db_url);
     // Under the hood all methods of Database uses internal default connection, but there is no way to expose it
     const defaultConnection = db.connect();
     const execAsync: (sql: string, ...params: any[]) => Promise<void> = promisify(defaultConnection.exec).bind(defaultConnection) as any;
