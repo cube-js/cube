3c49d3467c625e66a1d6af81c08e3180a0ca81cd
Pavel Tiunov <pavel.tiunov@gmail.com>
2024-01-11 19:45:37 -0800

    Remove mock calls check as it isn't predictable

--- packages/cubejs-backend-native/test/sql.test.ts
+++ packages/cubejs-backend-native/test/sql.test.ts
@@ -240,7 +240,6 @@ describe('SQLInterface', () => {
         password: null,
       });
 
-      expect(meta.mock.calls.length).toEqual(6);
       expect(meta.mock.calls[0][0]).toEqual({
         request: {
           id: expect.any(String),
