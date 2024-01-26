1021a00f8a6d0ff67be86331b7b15dc844b2d840
Igor Lukanin <me@igorlukanin.io>
2024-01-26 13:02:30 +0100

    Period-over-period recipe

--- docs/pages/guides/recipes/data-modeling/_meta.js
+++ docs/pages/guides/recipes/data-modeling/_meta.js
@@ -1,10 +1,11 @@
 module.exports = {
-  "string-time-dimensions": "Working around string time dimensions",
-  "fiscal-year-quarter-dimensions": "Implementing fiscal year or fiscal quarter dimensions",
-  "dynamic-union-tables": "Using dynamic union tables",
-  "entity-attribute-value": "Implementing Entity-Attribute-Value Model (EAV)",
-  "passing-dynamic-parameters-in-a-query": "Passing dynamic parameters in a query",
+  "percentiles": "Calculating averages and percentiles",
+  "period-over-period": "Calculating period-over-period changes",
   "snapshots": "Implementing data snapshots",
+  "entity-attribute-value": "Implementing Entity-Attribute-Value Model (EAV)",
+  "fiscal-year-quarter-dimensions": "Implementing fiscal year or fiscal quarter dimensions",
+  "passing-dynamic-parameters-in-a-query": "Passing dynamic parameters in a query",
   "using-dynamic-measures": "Using dynamic measures",
-  "percentiles": "Calculating averages and percentiles"
+  "dynamic-union-tables": "Using dynamic union tables",
+  "string-time-dimensions": "Working around string time dimensions"
 }
