73b5c0d6fb5924e38439e7147516e6d30406b3a6
Alex Qyoun-ae <4062971+MazterQyou@users.noreply.github.com>
2024-02-13 03:07:04 +0400

    chore(cubesql): Remove duplicate match pattern

--- rust/cubesql/cubesql/src/compile/rewrite/analysis.rs
+++ rust/cubesql/cubesql/src/compile/rewrite/analysis.rs
@@ -1297,7 +1297,6 @@ impl Analysis<LogicalPlanLanguage> for LogicalPlanAnalysis {
                     c,
                     ScalarValue::Date32(_)
                         | ScalarValue::Date64(_)
-                        | ScalarValue::Int32(_)
                         | ScalarValue::Int64(_)
                         | ScalarValue::Int32(_)
                         | ScalarValue::Float64(_)
