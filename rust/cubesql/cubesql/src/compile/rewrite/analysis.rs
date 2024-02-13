9a32d957276daf4a1aaf51fc0096db95e7081ecb
Pavel Tiunov <pavel.tiunov@gmail.com>
2024-02-12 17:20:35 -0800

    Bring back ScalarValue::Int32(_) as it was already removed

--- rust/cubesql/cubesql/src/compile/rewrite/analysis.rs
+++ rust/cubesql/cubesql/src/compile/rewrite/analysis.rs
@@ -1297,8 +1297,8 @@ impl Analysis<LogicalPlanLanguage> for LogicalPlanAnalysis {
                     c,
                     ScalarValue::Date32(_)
                         | ScalarValue::Date64(_)
-                        | ScalarValue::Int32(_)
                         | ScalarValue::Int64(_)
+                        | ScalarValue::Int32(_)
                         | ScalarValue::Float64(_)
                         | ScalarValue::IntervalYearMonth(_)
                         | ScalarValue::IntervalDayTime(_)
