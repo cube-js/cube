ccb04a3b4566537397646d02fac86ac9de138f9d
Dmitry Patsura <talk@dmtry.me>
2024-04-19 18:51:15 +0200

    chore: fix testing server

--- packages/cubejs-backend-native/test/server.js
+++ packages/cubejs-backend-native/test/server.js
@@ -24,6 +24,12 @@ const meta_fixture = require('./meta');
     throw new Error('load is not implemented');
   };
 
+  const sql = async () => {
+    console.log('[js] sql');
+
+    throw new Error('sql is not implemented');
+  };
+
   const meta = async ({ request, session }) => {
     console.log('[js] meta', {
       request,
@@ -45,24 +51,25 @@ const meta_fixture = require('./meta');
     };
   };
 
-  const checkAuth = async ({ request, user }) => {
+  const checkAuth = async ({ request, user, password }) => {
     console.log('[js] checkAuth', {
       request,
       user,
+      password
     });
 
     if (user) {
       // without password
       if (user === 'wp') {
         return {
-          password: null,
+          password,
           superuser: false,
         };
       }
 
       if (user === 'admin') {
         return {
-          password: null,
+          password,
           superuser: true,
         };
       }
@@ -76,34 +83,49 @@ const meta_fixture = require('./meta');
     throw new Error('Please specify password');
   };
 
-  const sqlGenerators = async ({ request, session, query }) => {
-        console.log('[js] sqlGenerators',  {
-            request,
-            session,
-            query ,
-        });
-
-        return {};
-    }
-
-    native.setupLogger(
-      ({ event }) => console.log(event),
-      'trace',
-    );
-
-    const server = await native.registerInterface({
-      // nonce: '12345678910111213141516'.substring(0, 20),
-      checkAuth,
-      load,
-      sqlApiLoad,
-      meta,
-      stream,
-      sqlGenerators,
-      pgPort: '5555',
+  const sqlGenerators = async ({ request, session }) => {
+    console.log('[js] sqlGenerators', {
+      request,
+      session,
     });
-    console.log({
-      server
-    });
+
+    return {
+      cubeNameToDataSource: {},
+      dataSourceToSqlGenerator: {},
+    };
+  };
+
+  const logLoadEvent = async () => {
+    console.log('[js] logLoadEvent');
+  };
+
+  const canSwitchUserForSession = async () => {
+    console.log('[js] canSwitchUserForSession');
+
+    return true;
+  };
+
+  native.setupLogger(
+    ({ event }) => console.log(event),
+    'trace',
+  );
+
+  const server = await native.registerInterface({
+    // nonce: '12345678910111213141516'.substring(0, 20),
+    checkAuth,
+    load,
+    sql,
+    meta,
+    stream,
+    sqlApiLoad,
+    sqlGenerators,
+    logLoadEvent,
+    canSwitchUserForSession,
+    pgPort: '5555',
+  });
+  console.log({
+    server
+  });
 
   process.on('SIGINT', async () => {
     console.log('SIGINT signal');
