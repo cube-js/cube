2934e9982456f081ff1d721a5db04dbda5c19f2d
Nikita-str <newmynamespace@gmail.com>
2024-05-08 01:50:11 +0600

    inlist pushdown: pullup list if consts only

--- rust/cubesql/cubesql/src/compile/rewrite/rules/wrapper/in_list_expr.rs
+++ rust/cubesql/cubesql/src/compile/rewrite/rules/wrapper/in_list_expr.rs
@@ -4,7 +4,7 @@ use crate::{
         transforming_rewrite, wrapper_pullup_replacer, wrapper_pushdown_replacer,
         LogicalPlanLanguage, WrapperPullupReplacerAliasToCube,
     },
-    var, var_iter,
+    var, var_iter, var_list_iter,
 };
 use egg::{EGraph, Rewrite, Subst};
 
@@ -14,6 +14,34 @@ impl WrapperRules {
         rules: &mut Vec<Rewrite<LogicalPlanLanguage, LogicalPlanAnalysis>>,
     ) {
         rules.extend(vec![
+            transforming_rewrite(
+                "wrapper-in-list-only-consts-push-down",
+                wrapper_pushdown_replacer(
+                    inlist_expr("?expr", "?list", "?negated"),
+                    "?alias_to_cube",
+                    "?ungrouped",
+                    "?in_projection",
+                    "?cube_members",
+                ),
+                inlist_expr(
+                    wrapper_pushdown_replacer(
+                        "?expr",
+                        "?alias_to_cube",
+                        "?ungrouped",
+                        "?in_projection",
+                        "?cube_members",
+                    ),
+                    wrapper_pullup_replacer(
+                        "?list",
+                        "?alias_to_cube",
+                        "?ungrouped",
+                        "?in_projection",
+                        "?cube_members",
+                    ),
+                    "?negated",
+                ),
+                self.transform_in_list_only_consts("?list"),
+            ),
             rewrite(
                 "wrapper-in-list-push-down",
                 wrapper_pushdown_replacer(
@@ -31,7 +59,7 @@ impl WrapperRules {
                         "?in_projection",
                         "?cube_members",
                     ),
-                    wrapper_pullup_replacer(
+                    wrapper_pushdown_replacer(
                         "?list",
                         "?alias_to_cube",
                         "?ungrouped",
@@ -70,6 +98,9 @@ impl WrapperRules {
                 self.transform_in_list_expr("?alias_to_cube"),
             ),
         ]);
+
+        // TODO: support for flatten list
+        Self::expr_list_pushdown_pullup_rules(rules, "wrapper-in-list-exprs", "InListExprList");
     }
 
     fn transform_in_list_expr(
@@ -91,6 +122,23 @@ impl WrapperRules {
                         .templates
                         .contains_key("expressions/in_list")
                     {
+                        return true;
+                    }
+                }
+            }
+            false
+        }
+    }
+
+    fn transform_in_list_only_consts(
+        &self,
+        list_var: &'static str,
+    ) -> impl Fn(&mut EGraph<LogicalPlanLanguage, LogicalPlanAnalysis>, &mut Subst) -> bool {
+        let list_var = var!(list_var);
+        move |egraph, subst| {
+            if let Some(constant_in_list) = &egraph[subst[list_var]].data.constant_in_list {
+                for list in var_list_iter!(egraph[subst[list_var]], InListExprList) {
+                    if list.len() == constant_in_list.len() {
                         return true;
                     }
                 }
