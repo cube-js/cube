abc5a53234769e1df55647da15d0d3d8b2405896
Igor Lukanin <igor@cube.dev>
2024-05-15 17:13:13 +0200

    docs: Chart Prototyping (#8267)

--- docs/pages/product/workspace/_meta.js
+++ docs/pages/product/workspace/_meta.js
@@ -3,6 +3,7 @@ module.exports = {
   "data-model": "Data Model",
   "rollup-designer": "Rollup Designer",
   "dev-mode": "Development mode",
+  "vizard": "Chart Prototyping",
   "semantic-layer-sync": "Semantic Layer Sync",
   "sql-runner": "SQL Runner",
   "query-history": "Query History",
