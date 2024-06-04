49699036dd1c0e35a3ebde73a9d28a4589f1b9ac
Mike Nitsenko <mnitsenko@gmail.com>
2024-06-05 03:34:04 +0500

    docs: MDX API (#8300)
    * mdx api docs
    
    * Update mdx docs
    
    ---------
    
    Co-authored-by: Artyom Keydunov <artyom@cube.dev>

--- docs/pages/product/apis-integrations/_meta.js
+++ docs/pages/product/apis-integrations/_meta.js
@@ -3,6 +3,7 @@ module.exports = {
   "sql-api": "SQL API",
   "rest-api": "REST API",
   "graphql-api": "GraphQL API",
+  "mdx-api": "MDX API",
   "ai-api": "AI API",
   "javascript-sdk": "JavaScript SDK",
   "orchestration-api": "Orchestration API",
