d47311ab061c8c24425f339a5fc8a29a16af5895
Igor Lukanin <me@igorlukanin.io>
2024-06-26 18:18:16 +0200

    docs: Revert "docs: Revert "docs: Add Google Tag Manager (#8275)""
    This reverts commit fbfabea82202ee795375651acf3a83d84303c3e4.

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -15,6 +15,23 @@ const repo = "https://github.com/cube-js/cube";
 const branch = "master";
 const path = "/docs/";
 
+const GoogleTagManager = () => (
+  <>
+    <script dangerouslySetInnerHTML={{ __html: `
+      (function(w,d,s,l,i){
+        w[l]=w[l]||[];
+        w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),
+            dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-52W7VM2');
+    `}} ></script>
+  </>
+)
+
 const config: DocsThemeConfig = {
   logo: LogoWithVersion,
   logoLink: undefined,
@@ -29,6 +46,7 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <GoogleTagManager />
     </>
   ),
   useNextSeoProps: () => {
