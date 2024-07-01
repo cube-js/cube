83b30935fde01a9c721fc8aa42091fcd2d871d64
Igor Lukanin <me@igorlukanin.io>
2024-07-01 15:00:57 +0200

    examples: Remove outdated note on 3rd-party dependencies

--- examples/advanced-pre-aggregations-workshop/cube.js
+++ examples/advanced-pre-aggregations-workshop/cube.js
@@ -68,12 +68,6 @@
  * Pre-aggregations are separated by tenant id
  */
 
-/* ---------------------------------------- */
-// NOTE: third-party dependencies and the use of require(...) are disabled for
-// CubeCloud users by default.  Please contact support if you need them
-// enabled for your account.  You are still allowed to require
-// @cubejs-backend/*-driver packages.
-
 // const { BigQuery } = require('@google-cloud/bigquery');
 // const bigquery = new BigQuery();
 // async function fetchRegionKeys() {
--- examples/multitenancy-workshop/cube.js
+++ examples/multitenancy-workshop/cube.js
@@ -103,11 +103,6 @@ module.exports = {
 };
 
 /* Part 3. Dynamic tenant-aware data schema */
-/* ---------------------------------------- */
-// NOTE: third-party dependencies and the use of require(...) are disabled for
-// CubeCloud users by default.  Please contact support if you need them
-// enabled for your account.  You are still allowed to require
-// @cubejs-backend/*-driver packages.
 
 const { Pool } = require('pg');
 
