4dec79eaddf1e67390820147802420cdd5df8cf4
Alex Qyoun-ae <4062971+MazterQyou@users.noreply.github.com>
2024-08-09 10:30:33 +0400

    fix(cubesql): Split PowerBI count distinct expression

--- rust/cubesql/cubesql/src/compile/mod.rs
+++ rust/cubesql/cubesql/src/compile/mod.rs
@@ -19459,4 +19459,50 @@ LIMIT {{ limit }}{% endif %}"#.to_string(),
         assert!(sql.contains(" AS DOUBLE PRECISION)"));
         assert!(sql.contains(" AS DECIMAL(38,10))"));
     }
+
+    #[tokio::test]
+    async fn test_powerbi_count_distinct_with_max_case() {
+        if !Rewriter::sql_push_down_enabled() {
+            return;
+        }
+        init_testing_logger();
+
+        let logical_plan = convert_select_to_query_plan(
+            r#"
+            select
+                "rows"."customer_gender" as "customer_gender",
+                count(distinct("rows"."countDistinct")) + max(
+                    case
+                        when "rows"."countDistinct" is null then 1
+                        else 0
+                    end
+                ) as "a0"
+            from
+                "public"."KibanaSampleDataEcommerce" "rows"
+            group by
+                "customer_gender"
+            limit
+                1000001
+            ;"#
+            .to_string(),
+            DatabaseProtocol::PostgreSQL,
+        )
+        .await
+        .as_logical_plan();
+
+        assert_eq!(
+            logical_plan.find_cube_scan().request,
+            V1LoadRequestQuery {
+                measures: Some(vec!["KibanaSampleDataEcommerce.countDistinct".to_string()]),
+                dimensions: Some(vec!["KibanaSampleDataEcommerce.customer_gender".to_string()]),
+                segments: Some(vec![]),
+                time_dimensions: None,
+                order: None,
+                limit: Some(1000001),
+                offset: None,
+                filters: None,
+                ungrouped: None,
+            }
+        )
+    }
 }
