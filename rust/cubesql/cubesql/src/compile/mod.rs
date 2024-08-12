67ed82f99822b4c98db4a942bc8120531f5fa794
Dmitry Patsura <talk@dmtry.me>
2024-08-12 14:56:22 +0200

    chore(cubesql): Re-export datafusion from compile module

--- rust/cubesql/cubesql/src/compile/mod.rs
+++ rust/cubesql/cubesql/src/compile/mod.rs
@@ -68,6 +68,10 @@ pub mod service;
 
 pub mod test;
 
+// re-export base deps to minimise version maintenance for crate users such as cloud
+pub use datafusion;
+pub use datafusion::arrow as arrow;
+
 pub use crate::transport::ctx::*;
 use crate::{
     compile::{
