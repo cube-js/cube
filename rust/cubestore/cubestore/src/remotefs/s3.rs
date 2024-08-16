2af606472b10153249369a5cb44ea72b6eee08e9
Hai Pham <haipham23@gmail.com>
2024-08-16 14:32:05 +0200

    chore: rename CUBESTORE_AWS_ROLE to CUBESTORE_AWS_IAM_ROLE

--- rust/cubestore/cubestore/src/remotefs/s3.rs
+++ rust/cubestore/cubestore/src/remotefs/s3.rs
@@ -59,7 +59,7 @@ impl S3RemoteFs {
             ))
         })?;
     
-        let role_name = env::var("CUBESTORE_AWS_ROLE").ok();
+        let role_name = env::var("CUBESTORE_AWS_IAM_ROLE").ok();
         let (access_key, secret_key) = match role_name {
             Some(role_name) => {
                 assume_role(&role_name, &region.to_string()).await
