2fa957397705e40f8500993768932ea1f32e6c29
Rene Heijdens <rene.heijdens@x-ip.nl>
2024-08-19 10:44:15 +0200

    removed logging

--- packages/cubejs-server-core/src/core/logger.js
+++ packages/cubejs-server-core/src/core/logger.js
@@ -81,8 +81,8 @@ export const devLogger = (level) => (type, { error, warning, ...message }) => {
 };
 
 export const prodLogger = (level) => (msg, params) => {
-  const error = params.error;
-  const warning = params.warning;
+  const error = params?.error;
+  const warning = params?.warning;
 
   const logMessage = () => console.log(JSON.stringify({ message: msg, ...params }));
   // eslint-disable-next-line default-case
