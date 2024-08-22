f0bc42e98d871607ce02e9c0dfa2428ae3d5ca19
Konstantin Burkalev <KSDaemon@gmail.com>
2024-08-22 17:16:46 +0300

    Add tests for yaml schema compiler

--- packages/cubejs-schema-compiler/test/unit/yaml-schema.test.ts
+++ packages/cubejs-schema-compiler/test/unit/yaml-schema.test.ts
@@ -128,7 +128,7 @@ describe('Yaml Schema Testing', () => {
     }
   });
 
-  it('unammed measure', async () => {
+  it('unnamed measure', async () => {
     const { compiler } = prepareYamlCompiler(
       `cubes:
   - name: Users
@@ -221,5 +221,94 @@ describe('Yaml Schema Testing', () => {
     expect(segments).toBeDefined();
     expect(segments.length).toBeGreaterThan(0);
     expect(segments.find((segment) => segment.name === 'CubeA.sfUsers').description).toBe('SF users segment from createCubeSchema');
+  });
+
+  describe('Custom dimension granularities: ', () => {
+    it('no granularity name', async () => {
+      const { compiler } = prepareYamlCompiler(
+        `
+        cubes:
+        - name: Orders
+          sql: "select * from tbl"
+          dimensions:
+            - name: created_at
+              sql: created_at
+              type: time
+              granularities:
+                - interval: 6 months
+            - name: status
+              sql: status
+              type: string
+          measures:
+            - name: count
+              type: count
+        `
+      );
+
+      try {
+        await compiler.compile();
+        throw new Error('compile must return an error');
+      } catch (e: any) {
+        expect(e.message).toContain('name isn\'t defined for dimension.granularity');
+      }
+    });
+
+    it('incorrect granularity name', async () => {
+      const { compiler } = prepareYamlCompiler(
+        `
+        cubes:
+        - name: Orders
+          sql: "select * from tbl"
+          dimensions:
+            - name: created_at
+              sql: created_at
+              type: time
+              granularities:
+                - name: 6_months
+            - name: status
+              sql: status
+              type: string
+          measures:
+            - name: count
+              type: count
+        `
+      );
+
+      try {
+        await compiler.compile();
+        throw new Error('compile must return an error');
+      } catch (e: any) {
+        expect(e.message).toContain('(dimensions.created_at.granularities.6_months = [object Object]) is not allowed');
+      }
+    });
+
+    it('granularities as object ', async () => {
+      const { compiler } = prepareYamlCompiler(
+        `
+        cubes:
+        - name: Orders
+          sql: "select * from tbl"
+          dimensions:
+            - name: created_at
+              sql: created_at
+              type: time
+              granularities:
+                name: half_year
+            - name: status
+              sql: status
+              type: string
+          measures:
+            - name: count
+              type: count
+        `
+      );
+
+      try {
+        await compiler.compile();
+        throw new Error('compile must return an error');
+      } catch (e: any) {
+        expect(e.message).toContain('dimension.granularitys must be defined as array');
+      }
+    });
   });
 });
