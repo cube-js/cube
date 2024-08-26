b4754dbd7898d928844558adece42668fe6e728f
Dmitry Patsura <talk@dmtry.me>
2024-08-26 19:46:38 +0200

    feat(cubesql): Upgrade serde, serde_json - performance boost (#8636)

--- packages/cubejs-backend-native/src/transport.rs
+++ packages/cubejs-backend-native/src/transport.rs
@@ -16,7 +16,7 @@ use cubesql::{
     transport::{CubeStreamReceiver, LoadRequestMeta, MetaContext, TransportService},
     CubeError,
 };
-use serde_derive::Serialize;
+use serde::Serialize;
 use std::sync::Arc;
 use uuid::Uuid;
 
