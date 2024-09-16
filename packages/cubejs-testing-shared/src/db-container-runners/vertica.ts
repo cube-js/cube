dfbbd829ad591302c0536aaeb4770a47e4a8d40b
timbrownls20 <timbrownls26@hotmail.com>
2024-09-17 09:33:51 +1000

    fix build

--- packages/cubejs-testing-shared/src/db-container-runners/vertica.ts
+++ packages/cubejs-testing-shared/src/db-container-runners/vertica.ts
@@ -1,17 +1,16 @@
 import { GenericContainer } from 'testcontainers';
-import { LogWaitStrategy } from 'testcontainers/dist/wait-strategy';
 import { DbRunnerAbstract } from './db-runner.abstract';
+import { LogWaitStrategy } from 'testcontainers/build/wait-strategies/log-wait-strategy';
 
 export class VerticaDBRunner extends DbRunnerAbstract {
   public static startContainer() {
     const version = process.env.TEST_VERTICA_VERSION || '11.1.1-0';
 
     const container = new GenericContainer(`vertica/vertica-ce:${version}`)
-      .withEnv('TZ', 'Antarctica/Troll')
-      .withEnv('VERTICA_DB_NAME', 'test')
+      .withEnvironment({'TZ': 'Antarctica/Troll', 'VERTICA_DB_NAME': 'test'})
       .withExposedPorts(5433)
       .withStartupTimeout(60 * 1000)
-      .withWaitStrategy(new LogWaitStrategy("Node Status: v_test_node0001: (UP)"));
+      .withWaitStrategy(new LogWaitStrategy("Node Status: v_test_node0001: (UP)", 1));
 
     return container.start();
   }
