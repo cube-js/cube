148e4cf32890872d8be41b127a05df1aca68179c
Alex Qyoun-ae <4062971+MazterQyou@users.noreply.github.com>
2024-09-17 20:41:39 +0400

    fix(cubesql): Support new QuickSight meta queries

--- rust/cubesql/cubesql/src/compile/engine/information_schema/redshift/mod.rs
+++ rust/cubesql/cubesql/src/compile/engine/information_schema/redshift/mod.rs
@@ -1,15 +1,19 @@
 pub mod late_binding_view_unpacked;
+pub mod pg_external_schema;
 pub mod stl_ddltext;
 pub mod stl_query;
 pub mod stl_querytext;
+pub mod stv_slices;
 pub mod svv_external_schemas;
 pub mod svv_table_info;
 pub mod svv_tables;
 
 pub use late_binding_view_unpacked::*;
+pub use pg_external_schema::*;
 pub use stl_ddltext::*;
 pub use stl_query::*;
 pub use stl_querytext::*;
+pub use stv_slices::*;
 pub use svv_external_schemas::*;
 pub use svv_table_info::*;
 pub use svv_tables::*;
