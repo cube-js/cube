c4bee3bce2ecfe1089ba14ae094ffd5e41ad27e5
Konstantin Burkalev <KSDaemon@gmail.com>
2024-09-24 15:45:17 +0300

    update annotation for query with custom granularity (if it was used) with granularity details

--- packages/cubejs-api-gateway/src/helpers/prepareAnnotation.ts
+++ packages/cubejs-api-gateway/src/helpers/prepareAnnotation.ts
@@ -11,6 +11,14 @@ import { MemberType } from '../types/strings';
 import { MemberType as MemberTypeEnum } from '../types/enums';
 import { MemberExpression } from '../types/query';
 
+type GranularityMeta = {
+  name: string;
+  title: string;
+  interval: string;
+  offset?: string;
+  origin?: string;
+};
+
 /**
  * Annotation item for cube's member.
  */
@@ -23,6 +31,7 @@ type ConfigItem = {
   meta: any;
   drillMembers?: any[];
   drillMembersGrouped?: any;
+  granularities?: GranularityMeta[];
 };
 
 /**
@@ -50,7 +59,10 @@ const annotation = (
     ...(memberType === MemberTypeEnum.MEASURES ? {
       drillMembers: config.drillMembers,
       drillMembersGrouped: config.drillMembersGrouped
-    } : {})
+    } : {}),
+    ...(memberType === MemberTypeEnum.DIMENSIONS && config.granularities ? {
+      granularities: config.granularities || [],
+    } : {}),
   }];
 };
 
@@ -81,25 +93,38 @@ function prepareAnnotation(metaConfig: MetaConfig[], query: any) {
         (query.timeDimensions || [])
           .filter(td => !!td.granularity)
           .map(
-            td => [
-              annotation(
+            td => {
+              const an = annotation(
                 configMap,
                 MemberTypeEnum.DIMENSIONS,
               )(
                 `${td.dimension}.${td.granularity}`
-              )
-            ].concat(
+              );
+
+              if (an && an[1].granularities) {
+                // No need to send all the granularities defined, only those make sense for this query
+                an[1].granularities = an[1].granularities.filter(g => g.name === td.granularity);
+              }
+
               // TODO: deprecated: backward compatibility for
               // referencing time dimensions without granularity
-              dimensions.indexOf(td.dimension) === -1
-                ? [
-                  annotation(
-                    configMap,
-                    MemberTypeEnum.DIMENSIONS
-                  )(td.dimension)
-                ]
-                : []
-            ).filter(a => !!a)
+              if (dimensions.indexOf(td.dimension) !== -1) {
+                return [an].filter(a => !!a);
+              }
+
+              const dimWithoutGranularity = annotation(
+                configMap,
+                MemberTypeEnum.DIMENSIONS
+              )(td.dimension);
+
+              if (dimWithoutGranularity && dimWithoutGranularity[1].granularities) {
+                // no need to populate granularities here
+                dimWithoutGranularity[1].granularities = undefined;
+              }
+
+              return [an].concat([dimWithoutGranularity])
+                .filter(a => !!a);
+            }
           )
       )
     ),
