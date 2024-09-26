347cfc92d11fa2bf138a481e1bf3aa1fe7315ca2
Konstantin Burkalev <KSDaemon@gmail.com>
2024-09-26 23:01:16 +0300

    Remove includeIncrementalSchemaSuite from every test suite besides one + add comment

--- packages/cubejs-testing-drivers/test/snowflake-export-bucket-azure-full.test.ts
+++ packages/cubejs-testing-drivers/test/snowflake-export-bucket-azure-full.test.ts
@@ -1,7 +1,6 @@
 import { testQueries } from '../src/tests/testQueries';
 
 testQueries('snowflake', {
-  includeIncrementalSchemaSuite: true,
   includeHLLSuite: true,
   extendedEnv: 'export-bucket-azure'
 });
