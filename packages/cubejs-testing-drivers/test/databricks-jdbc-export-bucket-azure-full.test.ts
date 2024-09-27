eb2aaf441de10708d3dcd3128740bb3c75fec13c
Konstantin Burkalev <KSDaemon@gmail.com>
2024-09-27 15:30:50 +0300

    remove not needed includeIncrementalSchemaSuite in some tests

--- packages/cubejs-testing-drivers/test/databricks-jdbc-export-bucket-azure-full.test.ts
+++ packages/cubejs-testing-drivers/test/databricks-jdbc-export-bucket-azure-full.test.ts
@@ -1,7 +1,6 @@
 import { testQueries } from '../src/tests/testQueries';
 
 testQueries('databricks-jdbc', {
-  includeIncrementalSchemaSuite: true,
   includeHLLSuite: true,
   extendedEnv: 'export-bucket-azure'
 });
