3789af91c548023595353d9790a0a537f7d43501
Konstantin Burkalev <KSDaemon@gmail.com>
2024-10-22 17:33:59 +0300

    fix redshift export bucket test

--- packages/cubejs-testing-drivers/test/redshift-export-bucket-s3-full.test.ts
+++ packages/cubejs-testing-drivers/test/redshift-export-bucket-s3-full.test.ts
@@ -4,6 +4,5 @@ testQueries('redshift', {
   // NOTICE: It's enough to turn on this flag only once for any one
   // cloud storage integration. Please do not turn it on for every integration test!
   includeIncrementalSchemaSuite: true,
-  includeHLLSuite: true,
   extendedEnv: 'export-bucket-s3'
 });
