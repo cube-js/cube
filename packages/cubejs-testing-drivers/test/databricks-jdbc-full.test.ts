bd08e7706f94dd788e01edf0870260a0c15916db
Mikhail Cheshkov <mcheshkov@gmail.com>
2024-11-01 15:35:32 +0200

    [DO NOT MERGE] add debug calls from node

--- packages/cubejs-testing-drivers/test/databricks-jdbc-full.test.ts
+++ packages/cubejs-testing-drivers/test/databricks-jdbc-full.test.ts
@@ -1,4 +1,19 @@
+import * as https from "node:https";
+
 import { testQueries } from '../src/tests/testQueries';
+
+https.get(
+  'https://ohio.cloud.databricks.com/sql/1.0/warehouses/',
+  (res) => {
+    console.log(
+      'res status and headers',
+      res.statusCode,
+      res.statusMessage,
+      res.headers
+    );
+    res.on('data', (chunk) => console.log('res data', chunk.toString()));
+  }
+);
 
 testQueries('databricks-jdbc', {
   includeIncrementalSchemaSuite: true,
