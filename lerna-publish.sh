d7dc5fe901def8fb7aae776c9a8bdac8d01bc0d9
Dmitry Patsura <talk@dmtry.me>
2024-11-01 18:28:59 +0100

    Revert "chore: release script - push tag and commit manually (#8897)"
    This reverts commit 4157a6d8a6640278ff0fadd7cebca7b06c18c7a8.

--- lerna-publish.sh
+++ lerna-publish.sh
@@ -11,11 +11,4 @@ BUMP=$1
 if [ "x$BUMP" == "x" ]; then
   BUMP=patch
 fi
-
-# In publish workflow we need to detect branch for tags from github.event.base_ref
-# It's possible only when GitHub receives information of tag from separate push
-# Lerna uses single push for commit & tag, that's why --no-push is passed and pushed manually
-yarn lerna version --create-release=github --conventional-commits --exact --no-push $BUMP
-git push -u origin HEAD
-# commitdate doesn't work with lerna tags :( version:refname is not an option, because we have LTS branches
-git push origin v$(cat lerna.json | jq -r '.version')
+yarn lerna version --create-release=github --conventional-commits --exact $BUMP
