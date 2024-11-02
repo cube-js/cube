558de586fc4cebd1374eb199a59e64defa8f1084
Mikhail Cheshkov <mcheshkov@gmail.com>
2024-11-02 04:05:12 +0200

    [DO NOT MERGE] add debug calls from node with token

--- packages/cubejs-testing-drivers/test/databricks-jdbc-full.test.ts
+++ packages/cubejs-testing-drivers/test/databricks-jdbc-full.test.ts
@@ -15,6 +15,24 @@ https.get(
   }
 );
 
+https.get(
+  'https://ohio.cloud.databricks.com/sql/1.0/warehouses/',
+  {
+    headers: {
+      authorization: process.env.DRIVERS_TESTS_CUBEJS_DB_DATABRICKS_TOKEN,
+    }
+  },
+  (res) => {
+    console.log(
+      'res with token status and headers',
+      res.statusCode,
+      res.statusMessage,
+      res.headers
+    );
+    res.on('data', (chunk) => console.log('res with token data', chunk.toString()));
+  }
+);
+
 testQueries('databricks-jdbc', {
   includeIncrementalSchemaSuite: true,
   includeHLLSuite: true,
