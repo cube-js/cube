c166775e12ee96ac2dff93dca2dc10ee61ff97da
Rene Heijdens <rene.heijdens@x-ip.nl>
2024-11-12 15:52:06 +0100

    fix log

--- packages/cubejs-query-orchestrator/src/orchestrator/QueryCache.ts
+++ packages/cubejs-query-orchestrator/src/orchestrator/QueryCache.ts
@@ -1066,7 +1066,7 @@ export class QueryCache {
     const resHash = getCacheHash(JSON.stringify(res));
 
     if (prevResHash !== resHash) {
-      console.log(`Emitting Cube Renewed ${options.renewedCube} with options: ${JSON.stringify(options.requestContext)}`);
+      console.log(`Emitting Cube Renewed ${options.renewedCube} with options: ${options.requestContext?.authInfo?.tenantId}`);
       this.eventEmitter.emit('cubeRenewed', {
         renewedCube: options.renewedCube,
         requestContext: options.requestContext,
