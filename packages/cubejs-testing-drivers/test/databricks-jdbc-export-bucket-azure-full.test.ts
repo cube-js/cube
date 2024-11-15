d59f3fe312f4c0b7fc17befcbc23cd02456f0ff6
Mikhail Cheshkov <mcheshkov@gmail.com>
2024-11-15 18:57:39 +0200

    [DEBUG] Silly commit just to trigger tests

--- packages/cubejs-testing-drivers/test/databricks-jdbc-export-bucket-azure-full.test.ts
+++ packages/cubejs-testing-drivers/test/databricks-jdbc-export-bucket-azure-full.test.ts
@@ -1,5 +1,6 @@
 import { testQueries } from '../src/tests/testQueries';
 
+// TODO just to trigger tests
 testQueries('databricks-jdbc', {
   includeHLLSuite: true,
   extendedEnv: 'export-bucket-azure'
