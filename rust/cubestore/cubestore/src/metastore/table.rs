a9b5fae43a84b43f57cd7c9d3a329028bae566a7
Sam Hughes <sam@cube.dev>
2024-11-27 14:29:22 -0800

    style(cubestore): Ran cargo fmt

--- rust/cubestore/cubestore/src/metastore/table.rs
+++ rust/cubestore/cubestore/src/metastore/table.rs
@@ -11,7 +11,9 @@ use byteorder::{BigEndian, WriteBytesExt};
 use chrono::DateTime;
 use chrono::Utc;
 use datafusion::arrow::datatypes::Schema as ArrowSchema;
-use datafusion::physical_plan::expressions::{sum_return_type, Column as FusionColumn, Max, Min, Sum};
+use datafusion::physical_plan::expressions::{
+    sum_return_type, Column as FusionColumn, Max, Min, Sum,
+};
 use datafusion::physical_plan::{udaf, AggregateExpr, PhysicalExpr};
 use itertools::Itertools;
 
@@ -77,7 +79,12 @@ impl AggregateColumn {
         let res: Arc<dyn AggregateExpr> = match self.function {
             AggregateFunction::SUM => {
                 let input_data_type = col.data_type(schema)?;
-                Arc::new(Sum::new(col.clone(), col.name(), sum_return_type(&input_data_type)?, &input_data_type))
+                Arc::new(Sum::new(
+                    col.clone(),
+                    col.name(),
+                    sum_return_type(&input_data_type)?,
+                    &input_data_type,
+                ))
             }
             AggregateFunction::MAX => {
                 Arc::new(Max::new(col.clone(), col.name(), col.data_type(schema)?))
