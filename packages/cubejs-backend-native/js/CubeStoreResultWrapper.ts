d3d44295d2370f36094c1bce3034e3a4939d4eba
Konstantin Burkalev <KSDaemon@gmail.com>
2024-11-29 20:03:30 +0200

    create CubeStoreResultWrapper class and switch to lazy evaluation of results set

---
+++ packages/cubejs-backend-native/js/CubeStoreResultWrapper.ts
@@ -0,0 +1,77 @@
+import { getCubestoreResult, ResultRow } from './index';
+
+export class CubeStoreResultWrapper {
+  private readonly proxy: any;
+
+  private cache: any;
+
+  public constructor(private readonly nativeReference: any) {
+    this.proxy = new Proxy(this, {
+      get: (target, prop: string | symbol) => {
+        // intercept indexes
+        if (typeof prop === 'string' && !Number.isNaN(Number(prop))) {
+          const array = this.getArray();
+          return array[Number(prop)];
+        }
+
+        // intercept array methods
+        if (typeof prop === 'string' && prop in Array.prototype) {
+          const arrayMethod = (Array.prototype as any)[prop];
+          if (typeof arrayMethod === 'function') {
+            return (...args: any[]) => this.invokeArrayMethod(prop, ...args);
+          }
+        }
+
+        // intercept isNative
+        if (prop === 'isNative') {
+          return true;
+        }
+
+        // intercept array length
+        if (prop === 'length') {
+          return this.getArray().length;
+        }
+
+        // intercept JSON.stringify or toJSON()
+        if (prop === 'toJSON') {
+          return () => this.getArray();
+        }
+
+        return (target as any)[prop];
+      },
+
+      // intercept array length
+      getOwnPropertyDescriptor: (target, prop) => {
+        if (prop === 'length') {
+          const array = this.getArray();
+          return {
+            configurable: true,
+            enumerable: true,
+            value: array.length,
+            writable: false
+          };
+        }
+        return Object.getOwnPropertyDescriptor(target, prop);
+      },
+
+      ownKeys: (target) => {
+        const array = this.getArray();
+        return [...Object.keys(target), ...Object.keys(array), 'length', 'isNative'];
+      }
+    });
+
+    return this.proxy;
+  }
+
+  private getArray(): ResultRow[] {
+    if (!this.cache) {
+      this.cache = getCubestoreResult(this.nativeReference);
+    }
+    return this.cache;
+  }
+
+  private invokeArrayMethod(method: string, ...args: any[]): any {
+    const array = this.getArray();
+    return (array as any)[method](...args);
+  }
+}
--- rust/cubeorchestrator/src/cubestore_message_parser.rs
+++ rust/cubeorchestrator/src/cubestore_message_parser.rs
@@ -1,3 +1,4 @@
+use neon::prelude::Finalize;
 use cubeshared::codegen::{root_as_http_message, HttpCommand};
 
 #[derive(Debug)]
@@ -23,55 +24,61 @@ impl std::fmt::Display for ParseError {
     }
 }
 
-pub struct CubeStoreResult<'a> {
-    pub columns: Vec<&'a str>,
-    pub rows: Vec<Vec<&'a str>>,
-}
-
 impl std::error::Error for ParseError {}
 
-pub fn parse_cubestore_ws_result(msg_data: &[u8]) -> Result<CubeStoreResult, ParseError> {
-    let http_message = root_as_http_message(msg_data).map_err(|_| ParseError::FlatBufferError)?;
+pub struct CubeStoreResult {
+    pub columns: Vec<String>,
+    pub rows: Vec<Vec<String>>,
+}
 
-    let command_type = http_message.command_type();
+impl Finalize for CubeStoreResult {}
 
-    match command_type {
-        HttpCommand::HttpError => {
-            let http_error = http_message
-                .command_as_http_error()
-                .ok_or(ParseError::FlatBufferError)?;
-            let error_message = http_error.error().unwrap_or("Unknown error").to_string();
-            Err(ParseError::ErrorMessage(error_message))
-        }
-        HttpCommand::HttpResultSet => {
-            let result_set = http_message
-                .command_as_http_result_set()
-                .ok_or(ParseError::EmptyResultSet)?;
+impl CubeStoreResult {
+    pub fn new(msg_data: &[u8]) -> Result<Self, ParseError> {
+        let mut result = CubeStoreResult {
+            columns: vec![],
+            rows: vec![],
+        };
 
-            let result_set_columns = result_set.columns().ok_or(ParseError::EmptyResultSet)?;
+        let http_message = root_as_http_message(msg_data)
+            .map_err(|_| ParseError::FlatBufferError)?;
 
-            if result_set_columns.iter().any(|c| c.is_empty()) {
-                return Err(ParseError::ColumnNameNotDefined);
+        match http_message.command_type() {
+            HttpCommand::HttpError => {
+                let http_error = http_message
+                    .command_as_http_error()
+                    .ok_or(ParseError::FlatBufferError)?;
+                let error_message = http_error.error().unwrap_or("Unknown error").to_string();
+                Err(ParseError::ErrorMessage(error_message))
             }
+            HttpCommand::HttpResultSet => {
+                let result_set = http_message
+                    .command_as_http_result_set()
+                    .ok_or(ParseError::EmptyResultSet)?;
 
-            let result_set_rows = result_set.rows().ok_or(ParseError::EmptyResultSet)?;
-            let mut result = CubeStoreResult{
-                columns: result_set_columns.iter().collect(),
-                rows: Vec::with_capacity(result_set_rows.len())
-            };
+                let result_set_columns = result_set.columns().ok_or(ParseError::EmptyResultSet)?;
 
-            for row in result_set_rows.iter() {
-                let values = row.values().ok_or(ParseError::NullRow)?;
-                let row_obj: Vec<_> = values
-                    .iter()
-                    .map(|val| val.string_value().unwrap_or(""))
-                    .collect();
+                if result_set_columns.iter().any(|c| c.is_empty()) {
+                    return Err(ParseError::ColumnNameNotDefined);
+                }
 
-                result.rows.push(row_obj);
-            }
+                result.columns = result_set_columns.iter().map(|val| val.to_owned()).collect();
+                let result_set_rows = result_set.rows().ok_or(ParseError::EmptyResultSet)?;
+                result.rows = Vec::with_capacity(result_set_rows.len());
 
-            Ok(result)
+                for row in result_set_rows.iter() {
+                    let values = row.values().ok_or(ParseError::NullRow)?;
+                    let row_obj: Vec<_> = values
+                        .iter()
+                        .map(|val| val.string_value().unwrap_or("").to_owned())
+                        .collect();
+
+                    result.rows.push(row_obj);
+                }
+
+                Ok(result)
+            },
+            _ => Err(ParseError::UnsupportedCommand),
         }
-        _ => Err(ParseError::UnsupportedCommand),
     }
 }
