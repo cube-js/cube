61db4c6b631db03a7bb30b36d82118bbc7d2f87a
Konstantin Burkalev <KSDaemon@gmail.com>
2024-12-03 13:35:06 +0200

    fix tests: have to add alias, because of too long identifiers

--- packages/cubejs-schema-compiler/test/integration/postgres/sql-generation.test.ts
+++ packages/cubejs-schema-compiler/test/integration/postgres/sql-generation.test.ts
@@ -296,6 +296,7 @@ describe('SQL Generation', () => {
       \${FILTER_PARAMS.visitor_checkins.created_at.filter('created_at')} AND
       \${FILTER_GROUP(FILTER_PARAMS.visitor_checkins.created_at.filter("(created_at - INTERVAL '3 DAY')"), FILTER_PARAMS.visitor_checkins.source.filter('source'))}
       \`,
+      sql_alias: \`vc\`,
 
       rewriteQueries: true,
 
@@ -568,28 +569,28 @@ describe('SQL Generation', () => {
             visitors__created_at_day: '2017-01-02T00:00:00.000Z',
             visitors__visitor_revenue: '100',
             visitors__visitor_count: '1',
-            visitor_checkins__visitor_checkins_count: '3',
+            vc__visitor_checkins_count: '3',
             visitors__per_visitor_revenue: '100'
           },
           {
             visitors__created_at_day: '2017-01-04T00:00:00.000Z',
             visitors__visitor_revenue: '200',
             visitors__visitor_count: '1',
-            visitor_checkins__visitor_checkins_count: '2',
+            vc__visitor_checkins_count: '2',
             visitors__per_visitor_revenue: '200'
           },
           {
             visitors__created_at_day: '2017-01-05T00:00:00.000Z',
             visitors__visitor_revenue: null,
             visitors__visitor_count: '1',
-            visitor_checkins__visitor_checkins_count: '1',
+            vc__visitor_checkins_count: '1',
             visitors__per_visitor_revenue: null
           },
           {
             visitors__created_at_day: '2017-01-06T00:00:00.000Z',
             visitors__visitor_revenue: null,
             visitors__visitor_count: '2',
-            visitor_checkins__visitor_checkins_count: '0',
+            vc__visitor_checkins_count: '0',
             visitors__per_visitor_revenue: null
           }
         ]
@@ -627,7 +628,7 @@ describe('SQL Generation', () => {
   }, [{
     visitors__visitor_revenue: '300',
     visitors__visitor_count: '5',
-    visitor_checkins__visitor_checkins_count: '6',
+    vc__visitor_checkins_count: '6',
     visitors__per_visitor_revenue: '60'
   }]));
 
@@ -748,22 +749,22 @@ describe('SQL Generation', () => {
     {
       visitors__created_at_day: '2017-01-02T00:00:00.000Z',
       visitors__revenue_rolling: null,
-      visitor_checkins__visitor_checkins_count: '3'
+      vc__visitor_checkins_count: '3'
     },
     {
       visitors__created_at_day: '2017-01-04T00:00:00.000Z',
       visitors__revenue_rolling: '100',
-      visitor_checkins__visitor_checkins_count: '2'
+      vc__visitor_checkins_count: '2'
     },
     {
       visitors__created_at_day: '2017-01-05T00:00:00.000Z',
       visitors__revenue_rolling: '200',
-      visitor_checkins__visitor_checkins_count: '1'
+      vc__visitor_checkins_count: '1'
     },
     {
       visitors__created_at_day: '2017-01-06T00:00:00.000Z',
       visitors__revenue_rolling: '500',
-      visitor_checkins__visitor_checkins_count: '0'
+      vc__visitor_checkins_count: '0'
     }
   ]));
 
@@ -979,7 +980,7 @@ describe('SQL Generation', () => {
     return dbRunner.testQuery(query.buildSqlAndParams()).then(res => {
       console.log(JSON.stringify(res));
       expect(res).toEqual(
-        [{ visitor_checkins__revenue_per_checkin: '50' }]
+        [{ vc__revenue_per_checkin: '50' }]
       );
     });
   });
@@ -1000,7 +1001,7 @@ describe('SQL Generation', () => {
     return dbRunner.testQuery(query.buildSqlAndParams()).then(res => {
       console.log(JSON.stringify(res));
       expect(res).toEqual(
-        [{ visitor_checkins__google_sourced_checkins: '1' }]
+        [{ vc__google_sourced_checkins: '1' }]
       );
     });
   });
@@ -1024,7 +1025,7 @@ describe('SQL Generation', () => {
     return dbRunner.testQuery(query.buildSqlAndParams()).then(res => {
       console.log(JSON.stringify(res));
       expect(res).toEqual(
-        [{ visitor_checkins__google_sourced_checkins: '1' }]
+        [{ vc__google_sourced_checkins: '1' }]
       );
     });
   });
@@ -1435,15 +1436,15 @@ describe('SQL Generation', () => {
     }]
   }, [
     {
-      visitor_checkins__cards_count: '0',
+      vc__cards_count: '0',
       visitors__visitor_revenue: '300'
     },
     {
-      visitor_checkins__cards_count: '1',
+      vc__cards_count: '1',
       visitors__visitor_revenue: '100'
     },
     {
-      visitor_checkins__cards_count: null,
+      vc__cards_count: null,
       visitors__visitor_revenue: null
     }
   ]));
@@ -1479,28 +1480,28 @@ describe('SQL Generation', () => {
       expect(res).toEqual(
         [
           {
-            visitor_checkins__id: 3,
-            visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z',
-            visitor_checkins__visitor_checkins_count: 1,
-            visitor_checkins__visitor_checkins_rolling: 1
+            vc__id: 3,
+            vc__created_at_day: '2017-01-04T00:00:00.000Z',
+            vc__visitor_checkins_count: 1,
+            vc__visitor_checkins_rolling: 1
           },
           {
-            visitor_checkins__id: 4,
-            visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z',
-            visitor_checkins__visitor_checkins_count: 1,
-            visitor_checkins__visitor_checkins_rolling: 1
+            vc__id: 4,
+            vc__created_at_day: '2017-01-04T00:00:00.000Z',
+            vc__visitor_checkins_count: 1,
+            vc__visitor_checkins_rolling: 1
           },
           {
-            visitor_checkins__id: 5,
-            visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z',
-            visitor_checkins__visitor_checkins_count: 1,
-            visitor_checkins__visitor_checkins_rolling: 1
+            vc__id: 5,
+            vc__created_at_day: '2017-01-04T00:00:00.000Z',
+            vc__visitor_checkins_count: 1,
+            vc__visitor_checkins_rolling: 1
           },
           {
-            visitor_checkins__id: 6,
-            visitor_checkins__created_at_day: '2017-01-05T00:00:00.000Z',
-            visitor_checkins__visitor_checkins_count: 1,
-            visitor_checkins__visitor_checkins_rolling: 1
+            vc__id: 6,
+            vc__created_at_day: '2017-01-05T00:00:00.000Z',
+            vc__visitor_checkins_count: 1,
+            vc__visitor_checkins_rolling: 1
           }
         ]
       );
@@ -1549,7 +1550,7 @@ describe('SQL Generation', () => {
       expect(res).toEqual(
         [
           {
-            visitor_checkins__source: 'google',
+            vc__source: 'google',
             visitors__created_at_day: '2017-01-02T00:00:00.000Z',
             visitors__per_visitor_revenue: '100'
           }
@@ -1596,7 +1597,7 @@ describe('SQL Generation', () => {
       console.log(JSON.stringify(res));
       expect(res).toEqual(
         [{
-          visitor_checkins__source: 'google',
+          vc__source: 'google',
           visitors__created_at_day: '2017-01-02T00:00:00.000Z',
           visitors__visitor_revenue: '100'
         }]
@@ -1623,7 +1624,7 @@ describe('SQL Generation', () => {
     return dbRunner.testQuery(query.buildSqlAndParams()).then(res => {
       console.log(JSON.stringify(res));
       expect(res).toEqual(
-        [{ visitor_checkins__revenue_per_checkin: '60' }]
+        [{ vc__revenue_per_checkin: '60' }]
       );
     });
   });
@@ -1649,7 +1650,7 @@ describe('SQL Generation', () => {
     return dbRunner.testQuery(query.buildSqlAndParams()).then(res => {
       console.log(JSON.stringify(res));
       expect(res).toEqual(
-        [{ visitor_checkins__revenue_per_checkin: '50' }]
+        [{ vc__revenue_per_checkin: '50' }]
       );
     });
   });
@@ -1694,12 +1695,12 @@ describe('SQL Generation', () => {
     ungrouped: true,
     allowUngroupedWithoutPrimaryKey: true,
   }, [
-    { visitor_checkins__created_at_day: '2017-01-02T00:00:00.000Z', visitor_checkins__google_sourced_checkins: null },
-    { visitor_checkins__created_at_day: '2017-01-03T00:00:00.000Z', visitor_checkins__google_sourced_checkins: null },
-    { visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z', visitor_checkins__google_sourced_checkins: null },
-    { visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z', visitor_checkins__google_sourced_checkins: null },
-    { visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z', visitor_checkins__google_sourced_checkins: null },
-    { visitor_checkins__created_at_day: '2017-01-05T00:00:00.000Z', visitor_checkins__google_sourced_checkins: 1 },
+    { vc__created_at_day: '2017-01-02T00:00:00.000Z', vc__google_sourced_checkins: null },
+    { vc__created_at_day: '2017-01-03T00:00:00.000Z', vc__google_sourced_checkins: null },
+    { vc__created_at_day: '2017-01-04T00:00:00.000Z', vc__google_sourced_checkins: null },
+    { vc__created_at_day: '2017-01-04T00:00:00.000Z', vc__google_sourced_checkins: null },
+    { vc__created_at_day: '2017-01-04T00:00:00.000Z', vc__google_sourced_checkins: null },
+    { vc__created_at_day: '2017-01-05T00:00:00.000Z', vc__google_sourced_checkins: 1 },
   ]));
 
   it('ungrouped filtered distinct count', () => runQueryTest({
@@ -1718,12 +1719,12 @@ describe('SQL Generation', () => {
     ungrouped: true,
     allowUngroupedWithoutPrimaryKey: true,
   }, [
-    { visitor_checkins__created_at_day: '2017-01-02T00:00:00.000Z', visitor_checkins__unique_google_sourced_checkins: null },
-    { visitor_checkins__created_at_day: '2017-01-03T00:00:00.000Z', visitor_checkins__unique_google_sourced_checkins: null },
-    { visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z', visitor_checkins__unique_google_sourced_checkins: null },
-    { visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z', visitor_checkins__unique_google_sourced_checkins: null },
-    { visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z', visitor_checkins__unique_google_sourced_checkins: null },
-    { visitor_checkins__created_at_day: '2017-01-05T00:00:00.000Z', visitor_checkins__unique_google_sourced_checkins: 1 },
+    { vc__created_at_day: '2017-01-02T00:00:00.000Z', vc__unique_google_sourced_checkins: null },
+    { vc__created_at_day: '2017-01-03T00:00:00.000Z', vc__unique_google_sourced_checkins: null },
+    { vc__created_at_day: '2017-01-04T00:00:00.000Z', vc__unique_google_sourced_checkins: null },
+    { vc__created_at_day: '2017-01-04T00:00:00.000Z', vc__unique_google_sourced_checkins: null },
+    { vc__created_at_day: '2017-01-04T00:00:00.000Z', vc__unique_google_sourced_checkins: null },
+    { vc__created_at_day: '2017-01-05T00:00:00.000Z', vc__unique_google_sourced_checkins: 1 },
   ]));
 
   it('ungrouped ratio measure', () => runQueryTest({
@@ -1742,12 +1743,12 @@ describe('SQL Generation', () => {
     ungrouped: true,
     allowUngroupedWithoutPrimaryKey: true,
   }, [
-    { visitor_checkins__created_at_day: '2017-01-02T00:00:00.000Z', visitor_checkins__unique_sources_per_checking: 1 },
-    { visitor_checkins__created_at_day: '2017-01-03T00:00:00.000Z', visitor_checkins__unique_sources_per_checking: 1 },
-    { visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z', visitor_checkins__unique_sources_per_checking: 1 },
-    { visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z', visitor_checkins__unique_sources_per_checking: 1 },
-    { visitor_checkins__created_at_day: '2017-01-04T00:00:00.000Z', visitor_checkins__unique_sources_per_checking: 1 },
-    { visitor_checkins__created_at_day: '2017-01-05T00:00:00.000Z', visitor_checkins__unique_sources_per_checking: 1 },
+    { vc__created_at_day: '2017-01-02T00:00:00.000Z', vc__unique_sources_per_checking: 1 },
+    { vc__created_at_day: '2017-01-03T00:00:00.000Z', vc__unique_sources_per_checking: 1 },
+    { vc__created_at_day: '2017-01-04T00:00:00.000Z', vc__unique_sources_per_checking: 1 },
+    { vc__created_at_day: '2017-01-04T00:00:00.000Z', vc__unique_sources_per_checking: 1 },
+    { vc__created_at_day: '2017-01-04T00:00:00.000Z', vc__unique_sources_per_checking: 1 },
+    { vc__created_at_day: '2017-01-05T00:00:00.000Z', vc__unique_sources_per_checking: 1 },
   ]));
 
   it('builds geo dimension', () => runQueryTest({
@@ -2175,7 +2176,7 @@ describe('SQL Generation', () => {
       }],
       order: []
     }, [
-      { visitor_checkins__visitor_checkins_count: '4' }
+      { vc__visitor_checkins_count: '4' }
     ])
   );
 
@@ -2209,7 +2210,7 @@ describe('SQL Generation', () => {
       }],
       order: []
     }, [
-      { visitor_checkins__visitor_checkins_count: '4' }
+      { vc__visitor_checkins_count: '4' }
     ])
   );
 
@@ -2227,7 +2228,7 @@ describe('SQL Generation', () => {
       }],
       order: []
     }, [
-      { visitor_checkins__visitor_checkins_count: '4' }
+      { vc__visitor_checkins_count: '4' }
     ])
   );
 
@@ -2263,7 +2264,7 @@ describe('SQL Generation', () => {
       }],
       order: []
     }, [
-      { visitor_checkins__visitor_checkins_count: '1' }
+      { vc__visitor_checkins_count: '1' }
     ])
   );
 
@@ -2303,7 +2304,7 @@ describe('SQL Generation', () => {
       }],
       order: []
     }, [
-      { visitor_checkins__visitor_checkins_count: '1' }
+      { vc__visitor_checkins_count: '1' }
     ])
   );
 
@@ -2770,11 +2771,11 @@ describe('SQL Generation', () => {
     [{
       visitors__percentage_of_total: 9,
       visitors__revenue: '100',
-      visitor_checkins__source: 'google'
+      vc__source: 'google'
     }, {
       visitors__percentage_of_total: 91,
       visitors__revenue: '1000',
-      visitor_checkins__source: null
+      vc__source: null
     }]
   ));
 
