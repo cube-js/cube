f6cace5e642e77a78a36cf845f07ddd075dfbf42
Mikhail Cheshkov <mcheshkov@gmail.com>
2024-12-14 01:48:43 +0200

    refactor(cubesql): Fix manual_is_ascii_check warning

--- rust/cubesql/cubesql/src/compile/parser.rs
+++ rust/cubesql/cubesql/src/compile/parser.rs
@@ -23,8 +23,8 @@ impl Dialect for MySqlDialectWithBackTicks {
         // See https://dev.mysql.com/doc/refman/8.0/en/identifiers.html.
         // We don't yet support identifiers beginning with numbers, as that
         // makes it hard to distinguish numeric literals.
-        ('a'..='z').contains(&ch)
-            || ('A'..='Z').contains(&ch)
+        ch.is_ascii_lowercase()
+            || ch.is_ascii_uppercase()
             || ch == '_'
             || ch == '$'
             || ch == '@'
@@ -32,7 +32,7 @@ impl Dialect for MySqlDialectWithBackTicks {
     }
 
     fn is_identifier_part(&self, ch: char) -> bool {
-        self.is_identifier_start(ch) || ('0'..='9').contains(&ch)
+        self.is_identifier_start(ch) || ch.is_ascii_digit()
     }
 }
 
