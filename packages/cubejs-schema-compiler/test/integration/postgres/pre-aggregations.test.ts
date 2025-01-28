7d49c3be7b1682be4c3e884bfa180f5f627f831a
Konstantin Burkalev <KSDaemon@gmail.com>
2025-01-28 23:47:32 +0200

    test for custom granularity match

--- packages/cubejs-schema-compiler/test/integration/postgres/pre-aggregations.test.ts
+++ packages/cubejs-schema-compiler/test/integration/postgres/pre-aggregations.test.ts
@@ -94,7 +94,13 @@ describe('PreAggregations', () => {
         },
         createdAt: {
           type: 'time',
-          sql: 'created_at'
+          sql: 'created_at',
+          granularities: {
+            hourTenMinOffset: {
+              interval: '1 hour',
+              offset: '10 minutes'
+            }
+          }
         },
         signedUpAt: {
           type: 'time',
@@ -223,6 +229,11 @@ describe('PreAggregations', () => {
           timeDimensionReference: createdAt,
           granularity: 'hour',
           partitionGranularity: 'month'
+        },
+        countCustomGranularity: {
+          measures: [count],
+          timeDimension: createdAt,
+          granularity: 'hourTenMinOffset'
         },
         sourceAndIdRollup: {
           measures: [count],
@@ -584,6 +595,49 @@ describe('PreAggregations', () => {
           {
             visitors__count: '2',
             visitors__created_at_hour: '2017-01-06T16:00:00.000Z',
+          },
+        ]
+      );
+    });
+  }));
+
+  it('simple pre-aggregation with custom granularity (exact match)', () => compiler.compile().then(() => {
+    const query = new PostgresQuery({ joinGraph, cubeEvaluator, compiler }, {
+      measures: [
+        'visitors.count'
+      ],
+      timeDimensions: [{
+        dimension: 'visitors.createdAt',
+        dateRange: ['2017-01-01 00:10:00.000', '2017-01-29 22:09:59.999'],
+        granularity: 'hourTenMinOffset',
+      }],
+      timezone: 'America/Los_Angeles',
+      preAggregationsSchema: ''
+    });
+
+    const queryAndParams = query.buildSqlAndParams();
+    console.log(queryAndParams);
+    expect(query.preAggregations?.preAggregationForQuery?.canUsePreAggregation).toEqual(true);
+    expect(queryAndParams[0]).toMatch(/visitors_count_custom_granularity/);
+
+    return dbRunner.evaluateQueryWithPreAggregations(query).then(res => {
+      expect(res).toEqual(
+        [
+          {
+            visitors__count: '1',
+            visitors__created_at_hourTenMinOffset: '2017-01-02T15:10:00.000Z',
+          },
+          {
+            visitors__count: '1',
+            visitors__created_at_hourTenMinOffset: '2017-01-04T15:10:00.000Z',
+          },
+          {
+            visitors__count: '1',
+            visitors__created_at_hourTenMinOffset: '2017-01-05T15:10:00.000Z',
+          },
+          {
+            visitors__count: '2',
+            visitors__created_at_hourTenMinOffset: '2017-01-06T15:10:00.000Z',
           },
         ]
       );
