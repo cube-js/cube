4c8de882b3cc57e2b0c29c33ca4ee91377176e85
Mikhail Cheshkov <mcheshkov@gmail.com>
2025-02-04 15:18:48 +0200

    fix(cubesql): Avoid panics during filter rewrites (#9166)

--- rust/cubesql/cubesql/src/compile/test/test_filters.rs
+++ rust/cubesql/cubesql/src/compile/test/test_filters.rs
@@ -1,4 +1,5 @@
 use cubeclient::models::{V1LoadRequestQuery, V1LoadRequestQueryFilterItem};
+use datafusion::physical_plan::displayable;
 use pretty_assertions::assert_eq;
 
 use crate::compile::{
@@ -59,4 +60,76 @@ GROUP BY
             ..Default::default()
         }
     );
+}
+
+#[tokio::test]
+async fn test_filter_dim_in_null() {
+    if !Rewriter::sql_push_down_enabled() {
+        return;
+    }
+    init_testing_logger();
+
+    let query_plan = convert_select_to_query_plan(
+        // language=PostgreSQL
+        r#"
+        SELECT
+            dim_str0
+        FROM
+            MultiTypeCube
+        WHERE dim_str1 IN (NULL)
+        "#
+        .to_string(),
+        DatabaseProtocol::PostgreSQL,
+    )
+    .await;
+
+    let physical_plan = query_plan.as_physical_plan().await.unwrap();
+    println!(
+        "Physical plan: {}",
+        displayable(physical_plan.as_ref()).indent()
+    );
+
+    // For now this tests only that query is rewritable
+    // TODO support this as "notSet" filter
+
+    assert!(query_plan
+        .as_logical_plan()
+        .find_cube_scan_wrapped_sql()
+        .wrapped_sql
+        .sql
+        .contains(r#"\"expr\":\"${MultiTypeCube.dim_str1} IN (NULL)\""#));
+}
+
+#[tokio::test]
+async fn test_filter_superset_is_null() {
+    if !Rewriter::sql_push_down_enabled() {
+        return;
+    }
+    init_testing_logger();
+
+    let query_plan = convert_select_to_query_plan(
+        // language=PostgreSQL
+        r#"
+SELECT dim_str0 FROM MultiTypeCube WHERE (dim_str1 IS NULL OR dim_str1 IN (NULL) AND (1<>1))
+        "#
+        .to_string(),
+        DatabaseProtocol::PostgreSQL,
+    )
+    .await;
+
+    let physical_plan = query_plan.as_physical_plan().await.unwrap();
+    println!(
+        "Physical plan: {}",
+        displayable(physical_plan.as_ref()).indent()
+    );
+
+    // For now this tests only that query is rewritable
+    // TODO support this as "notSet" filter
+
+    assert!(query_plan
+        .as_logical_plan()
+        .find_cube_scan_wrapped_sql()
+        .wrapped_sql
+        .sql
+        .contains(r#"\"expr\":\"((${MultiTypeCube.dim_str1} IS NULL) OR (${MultiTypeCube.dim_str1} IN (NULL) AND FALSE))\""#));
 }
