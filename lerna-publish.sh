df1c9ac6f5dead69e1b47dea74c2a50ade57b4a6
Konstantin Burkalev <KSDaemon@gmail.com>
2025-02-06 15:57:23 +0200

    chore(dev-scripts): update lerna publishing to force publish all packages

--- lerna-publish.sh
+++ lerna-publish.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 . .gh-token
 
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if [[ "$BRANCH" != "master" ]]; then
-  echo 'Must be run from the master branch';
-  exit 1;
-fi
-
 BUMP=$1
 if [ "x$BUMP" == "x" ]; then
   BUMP=patch
 fi
-yarn lerna version --create-release=github --conventional-commits --exact $BUMP
+yarn lerna version --create-release=github --conventional-commits --force-publish --exact $BUMP
