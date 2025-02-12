6c1170e75331a578dd1472ebdbbae7a81c2bad3f
Igor Lukanin <igor@cube.dev>
2025-02-12 14:35:39 +0100

    Move SQL API reference

--- docs/pages/reference/_meta.js
+++ docs/pages/reference/_meta.js
@@ -2,7 +2,6 @@ module.exports = {
   "configuration": "Configuration",
   "data-model": "Data modeling",
   "python": "Python packages",
-  "sql-api": "SQL API",
   "rest-api": "REST API",
   "graphql-api": "GraphQL API",
   "ai-api": "AI API",
