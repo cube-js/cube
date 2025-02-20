dd0abdbb9d6efb110c29c587431cf75fd8eac26b
Mikhail Cheshkov <mcheshkov@gmail.com>
2025-02-20 13:28:16 +0200

    [DEBUG] print free space in vertica tests

--- .github/actions/integration/vertica.sh
+++ .github/actions/integration/vertica.sh
@@ -7,6 +7,10 @@ export DEBUG=testcontainers
 export TEST_VERTICA_VERSION=12.0.4-0
 
 echo "::group::Vertica ${TEST_VERTICA_VERSION}"
+echo "Before pull" && df -h
 docker pull vertica/vertica-ce:${TEST_VERTICA_VERSION}
+echo "After pull" && df -h
+bash -c 'sleep 5 && echo "5 sec after pull" && df -h' &
 yarn lerna run --concurrency 1 --stream --no-prefix integration:vertica
+echo "After tests" && df -h
 echo "::endgroup::"
