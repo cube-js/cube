741fb5de87beb3651d0f8b064ee6742afc88a562
Michael Astashkevich <asmisha@users.noreply.github.com>
2025-02-28 11:31:39 +0100

    Update packages/cubejs-server-core/src/core/server.ts
    Co-authored-by: Konstantin Burkalev <KSDaemon@gmail.com>

--- packages/cubejs-server-core/src/core/server.ts
+++ packages/cubejs-server-core/src/core/server.ts
@@ -712,7 +712,7 @@ export class CubejsServerCore {
         sqlCache: this.options.sqlCache,
         standalone: this.standalone,
         allowNodeRequire: options.allowNodeRequire,
-        fastReloadEnabled: getEnv('fastReloadEnabled'),
+        fastReloadEnabled: options.fastReloadEnabled || getEnv('fastReloadEnabled'),
       },
     );
   }
