6db887f1ea503325764c50a6e8bc848224c368f9
Konstantin Burkalev <KSDaemon@gmail.com>
2025-03-04 19:40:03 +0200

    revert mongobi

--- .github/actions/integration/mongobi.sh
+++ .github/actions/integration/mongobi.sh
@@ -2,10 +2,10 @@
 set -eo pipefail
 
 # Debug log for test containers
-export DEBUG=testcontainers*
+export DEBUG=testcontainers
 
 export TEST_MONGO_TAG=6.0
-export TEST_MONGOBI_VERSION=mongodb-bi-linux-x86_64-ubuntu2204-v2.14.21
+export TEST_MONGOBI_VERSION=mongodb-bi-linux-x86_64-ubuntu2004-v2.14.8
 
 echo "::group::MongoBI"
 yarn lerna run --concurrency 1 --stream --no-prefix integration:mongobi
