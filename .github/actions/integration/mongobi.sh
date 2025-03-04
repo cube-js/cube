74821e3558f605597e0d5fd4286db6034e2daf99
Konstantin Burkalev <KSDaemon@gmail.com>
2025-03-04 16:27:45 +0200

    fix mongodb-bi version

--- .github/actions/integration/mongobi.sh
+++ .github/actions/integration/mongobi.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 export DEBUG=testcontainers
 
 export TEST_MONGO_TAG=6.0
-export TEST_MONGOBI_VERSION=mongodb-bi-linux-x86_64-ubuntu2004-v2.14.8
+export TEST_MONGOBI_VERSION=mongodb-bi-linux-x86_64-ubuntu2204-v2.14.21
 
 echo "::group::MongoBI"
 yarn lerna run --concurrency 1 --stream --no-prefix integration:mongobi
