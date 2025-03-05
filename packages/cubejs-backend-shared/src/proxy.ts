77e704fead50305fa76feb9373896954dd0f739b
Konstantin Burkalev <KSDaemon@gmail.com>
2025-03-05 19:37:25 +0200

    Cache npm proxy

--- packages/cubejs-backend-shared/src/proxy.ts
+++ packages/cubejs-backend-shared/src/proxy.ts
@@ -2,6 +2,9 @@ import { exec } from 'child_process';
 import { ProxyAgent } from 'proxy-agent';
 import { HttpsProxyAgent } from 'https-proxy-agent';
 
+let npmProxy: string;
+let npmProxyInitialized = false;
+
 function getCommandOutput(command: string) {
   return new Promise<string>((resolve, reject) => {
     exec(command, (error, stdout) => {
@@ -19,22 +22,26 @@ function getCommandOutput(command: string) {
  * @deprecated
  * use ProxyAgent instead
  */
-export async function getProxySettings() {
+export async function getProxySettings(): Promise<string> {
   const [proxy] = (
     await Promise.all([getCommandOutput('npm config -g get https-proxy'), getCommandOutput('npm config -g get proxy')])
   )
     .map((s) => s.trim())
     .filter((s) => !['null', 'undefined', ''].includes(s));
 
+  npmProxyInitialized = true;
+
   return proxy;
 }
 
 export async function getHttpAgentForProxySettings() {
-  const proxy = await getProxySettings();
+  if (!npmProxyInitialized) {
+    npmProxy = await getProxySettings();
+  }
 
-  if (proxy) {
+  if (npmProxy) {
     console.warn('Npm proxy settings are deprecated. Please use HTTP_PROXY, HTTPS_PROXY environment variables instead.');
-    return new HttpsProxyAgent(proxy);
+    return new HttpsProxyAgent(npmProxy);
   }
 
   return new ProxyAgent();
