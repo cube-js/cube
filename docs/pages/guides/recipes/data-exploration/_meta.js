9551b18c724ccb31eca74e68d8e38e01e8b66445
Igor Lukanin <igor@cube.dev>
2025-03-19 00:05:10 +0100

    docs: Query History export

--- docs/pages/guides/recipes/data-exploration/_meta.js
+++ docs/pages/guides/recipes/data-exploration/_meta.js
@@ -1,4 +1,5 @@
 module.exports = {
   "drilldowns": "Building UI with drilldowns",
-  "cast-numerics": "Retrieving numeric values on the front-end"
+  "cast-numerics": "Retrieving numeric values on the front-end",
+  "query-history-export": "Analyzing data from Query History export"
 }
