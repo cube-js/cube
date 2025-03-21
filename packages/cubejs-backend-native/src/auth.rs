64ab46661bef93cec8ceec626e9ea347d45153e9
Dmitry Patsura <talk@dmtry.me>
2025-03-21 13:34:15 +0100

    refactor(cubesql): Use correct naming for checkSqlAuth

--- packages/cubejs-backend-native/src/auth.rs
+++ packages/cubejs-backend-native/src/auth.rs
@@ -17,14 +17,14 @@ use crate::channel::call_js_with_channel_as_callback;
 #[derive(Debug)]
 pub struct NodeBridgeAuthService {
     channel: Arc<Channel>,
-    check_auth: Arc<Root<JsFunction>>,
+    check_sql_auth: Arc<Root<JsFunction>>,
 }
 
 impl NodeBridgeAuthService {
-    pub fn new(channel: Channel, check_auth: Root<JsFunction>) -> Self {
+    pub fn new(channel: Channel, check_sql_auth: Root<JsFunction>) -> Self {
         Self {
             channel: Arc::new(channel),
-            check_auth: Arc::new(check_auth),
+            check_sql_auth: Arc::new(check_sql_auth),
         }
     }
 }
@@ -36,14 +36,14 @@ pub struct TransportRequest {
 }
 
 #[derive(Debug, Serialize)]
-struct CheckAuthRequest {
+struct CheckSQLAuthRequest {
     request: TransportRequest,
     user: Option<String>,
     password: Option<String>,
 }
 
 #[derive(Debug, Deserialize)]
-struct CheckAuthResponse {
+struct CheckSQLAuthResponse {
     password: Option<String>,
     superuser: bool,
     #[serde(rename = "securityContext", skip_serializing_if = "Option::is_none")]
@@ -76,7 +76,7 @@ impl SqlAuthService for NodeBridgeAuthService {
 
         let request_id = Uuid::new_v4().to_string();
 
-        let extra = serde_json::to_string(&CheckAuthRequest {
+        let extra = serde_json::to_string(&CheckSQLAuthRequest {
             request: TransportRequest {
                 id: format!("{}-span-1", request_id),
                 meta: None,
@@ -84,9 +84,9 @@ impl SqlAuthService for NodeBridgeAuthService {
             user: user.clone(),
             password: password.clone(),
         })?;
-        let response: CheckAuthResponse = call_js_with_channel_as_callback(
+        let response: CheckSQLAuthResponse = call_js_with_channel_as_callback(
             self.channel.clone(),
-            self.check_auth.clone(),
+            self.check_sql_auth.clone(),
             Some(extra),
         )
         .await?;
