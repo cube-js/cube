cc43ebb638e1e3ebbb3d07788bf2fcae28c941e1
Konstantin Burkalev <KSDaemon@gmail.com>
2025-03-27 16:17:50 +0200

    code polish

--- packages/cubejs-cubestore-driver/src/CubeStoreQueueDriver.ts
+++ packages/cubejs-cubestore-driver/src/CubeStoreQueueDriver.ts
@@ -83,7 +83,7 @@ class CubestoreQueueDriverConnection implements QueueDriverConnectionInterface {
     values.push(JSON.stringify(data));
 
     const rows = await this.driver.query(`QUEUE ADD PRIORITY ?${options.orphanedTimeout ? ' ORPHANED ?' : ''} ? ?`, values);
-    if (rows && rows.length) {
+    if (rows?.length) {
       return [
         rows[0].added === 'true' ? 1 : 0,
         rows[0].id ? parseInt(rows[0].id, 10) : null,
@@ -104,7 +104,7 @@ class CubestoreQueueDriverConnection implements QueueDriverConnectionInterface {
       // queryKeyHash as compatibility fallback
       queueId || this.prefixKey(hash),
     ]);
-    if (rows && rows.length) {
+    if (rows?.length) {
       return this.decodeQueryDefFromRow(rows[0], 'cancelQuery');
     }
 
@@ -149,7 +149,7 @@ class CubestoreQueueDriverConnection implements QueueDriverConnectionInterface {
     const rows = await this.driver.query('CACHE INCR ?', [
       `${this.options.redisQueuePrefix}:PROCESSING_COUNTER`
     ]);
-    if (rows && rows.length) {
+    if (rows?.length) {
       return rows[0].value;
     }
 
@@ -186,7 +186,7 @@ class CubestoreQueueDriverConnection implements QueueDriverConnectionInterface {
     const rows = await this.driver.query('QUEUE RESULT ?', [
       this.prefixKey(this.redisHash(queryKey)),
     ]);
-    if (rows && rows.length) {
+    if (rows?.length) {
       return this.decodeQueryDefFromRow(rows[0], 'getResult');
     }
 
@@ -245,7 +245,7 @@ class CubestoreQueueDriverConnection implements QueueDriverConnectionInterface {
     const rows = await this.driver.query('QUEUE GET ?', [
       queueId || this.prefixKey(hash),
     ]);
-    if (rows && rows.length) {
+    if (rows?.length) {
       return this.decodeQueryDefFromRow(rows[0], 'getQueryDef');
     }
 
@@ -271,7 +271,7 @@ class CubestoreQueueDriverConnection implements QueueDriverConnectionInterface {
       this.options.concurrency,
       this.prefixKey(hash),
     ]);
-    if (rows && rows.length) {
+    if (rows?.length) {
       const active = rows[0].active ? (rows[0].active).split(',') as unknown as QueryKeyHash[] : [];
       const pending = parseInt(rows[0].pending, 10);
 
@@ -302,7 +302,7 @@ class CubestoreQueueDriverConnection implements QueueDriverConnectionInterface {
       // queryKeyHash as compatibility fallback
       queueId || this.prefixKey(hash),
     ]);
-    if (rows && rows.length) {
+    if (rows?.length) {
       return this.decodeQueryDefFromRow(rows[0], 'getResultBlocking');
     }
 
@@ -348,8 +348,8 @@ export class CubeStoreQueueDriver implements QueueDriverInterface {
       return this.connection;
     }
 
-    // eslint-disable-next-line no-return-assign
-    return this.connection = await this.driverFactory();
+    this.connection = await this.driverFactory();
+    return this.connection;
   }
 
   public async createConnection(): Promise<CubestoreQueueDriverConnection> {
