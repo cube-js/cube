031a7942fef98e248afe21ae132a8029843f5f52
Konstantin Burkalev <KSDaemon@gmail.com>
2025-03-28 16:12:39 +0200

    update node-check

--- packages/cubejs-backend-shared/src/node-check.ts
+++ packages/cubejs-backend-shared/src/node-check.ts
@@ -1,36 +1,36 @@
 import process from 'process';
 import color from '@oclif/color';
 
+const leastSupportedVersion = 20;
 const currentNodeVersion = process.versions.node;
 const semver = currentNodeVersion.split('.');
 const major = parseInt(<string> semver[0], 10);
-// const _minor = parseInt(<string> semver[1], 10);
 
-if (major < 18) {
+if (major < leastSupportedVersion) {
   console.error(
     color.red(
       `You are running Node.js ${currentNodeVersion}.\n` +
-      'Cube.js CLI requires Node.js 20 or higher \n' +
+      `Cube.js CLI requires Node.js ${leastSupportedVersion} or higher.\n` +
       'Please update your Node.js version.'
     )
   );
   process.exit(1);
 }
 
-if (major === 19) {
+if (major === (leastSupportedVersion + 1)) {
   process.emitWarning(
     color.red(
       `You are running Node.js ${currentNodeVersion}.\n` +
-      'Support for Node.js 19 not guaranty. Please upgrade to Node.js 20 or higher.'
+      `Support for Node.js ${leastSupportedVersion + 1} not guaranteed. Please upgrade to Node.js ${leastSupportedVersion + 2} or higher.`
     )
   );
 }
 
-if (major === 18) {
+if (major === leastSupportedVersion) {
   process.emitWarning(
     color.red(
       `You are running Node.js ${currentNodeVersion}.\n` +
-      'Support for Node.js 18 will be removed soon. Please upgrade to Node.js 20 or higher.'
+      `Support for Node.js ${leastSupportedVersion} will be removed soon. Please upgrade to Node.js ${leastSupportedVersion + 2} or higher.`
     )
   );
 }
