e1d3b5c067f05c57e684b2b4973afa81d5dce143
Dmitriy Rusov <rusov.d.s@gmail.com>
2025-04-13 13:21:23 +0200

    chore: metaOnly flag in pre-aggregations api (#9461)

--- packages/cubejs-api-gateway/src/gateway.ts
+++ packages/cubejs-api-gateway/src/gateway.ts
@@ -440,6 +440,7 @@ class ApiGateway {
       app.get('/cubejs-system/v1/pre-aggregations', systemMiddlewares, systemAsyncHandler(async (req, res) => {
         await this.getPreAggregations({
           cacheOnly: !!req.query.cacheOnly,
+          metaOnly: !!req.query.metaOnly,
           context: req.context,
           res: this.resToResultFn(res)
         });
@@ -619,7 +620,7 @@ class ApiGateway {
     }
   }
 
-  public async getPreAggregations({ cacheOnly, context, res }: { cacheOnly?: boolean, context: RequestContext, res: ResponseResultFn }) {
+  public async getPreAggregations({ cacheOnly, metaOnly, context, res }: { cacheOnly?: boolean, metaOnly?: boolean, context: RequestContext, res: ResponseResultFn }) {
     const requestStarted = new Date();
     try {
       const compilerApi = await this.getCompilerApi(context);
@@ -634,6 +635,7 @@ class ApiGateway {
               preAggregations: preAggregations.map(p => ({
                 id: p.id,
                 cacheOnly,
+                metaOnly
               }))
             },
           )
