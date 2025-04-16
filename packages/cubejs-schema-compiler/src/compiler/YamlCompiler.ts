0a8cf07f20351a0e591b658f02f243510bf78eae
Konstantin Burkalev <KSDaemon@gmail.com>
2025-04-16 17:41:29 +0300

    fix(schema-compiler): Allow escaping of curly braces in yaml models (#9469)
    * fix(schema-compiler): Allow escaping of curly braces in yaml models
    
    * add tests
    
    * remove obsolete
    
    * Add docs
    
    ---------
    
    Co-authored-by: Igor Lukanin <igor@cube.dev>

--- packages/cubejs-schema-compiler/src/compiler/YamlCompiler.ts
+++ packages/cubejs-schema-compiler/src/compiler/YamlCompiler.ts
@@ -230,11 +230,11 @@ export class YamlCompiler {
       } else if (str[i] === '`' && peek().inStr) {
         result.push(str[i]);
         stateStack.pop();
-      } else if (str[i] === '{' && str[i + 1] === '{' && peek()?.inFormattedStr) {
-        result.push('{{');
+      } else if (str[i] === '\\' && str[i + 1] === '{' && stateStack.length === 0) {
+        result.push('\\{');
         i += 1;
-      } else if (str[i] === '}' && str[i + 1] === '}' && peek()?.inFormattedStr) {
-        result.push('}}');
+      } else if (str[i] === '\\' && str[i + 1] === '}' && stateStack.length === 0) {
+        result.push('\\}');
         i += 1;
       } else if (str[i] === '{' && peek()?.inFormattedStr) {
         result.push(str[i]);
