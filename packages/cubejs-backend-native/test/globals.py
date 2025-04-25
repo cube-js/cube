d4d461544fded710a2ed636acf379db0f52ebc18
Konstantin Burkalev <KSDaemon@gmail.com>
2025-04-25 15:33:36 +0300

    more tests

--- packages/cubejs-backend-native/test/globals.py
+++ packages/cubejs-backend-native/test/globals.py
@@ -1,6 +1,7 @@
 from cube import TemplateContext
 import os
 from utils import answer_to_main_question
+from subdir_for_test.meta import main_question
 
 
 template = TemplateContext()
@@ -11,4 +12,5 @@ template.add_variable('value_or_none', value_or_none)
 value_or_default = os.getenv('MY_OTHER_ENV_VAR', 'my_default_value')
 template.add_variable('value_or_default', value_or_default)
 
+template.add_variable('main_question', main_question())
 template.add_variable('answer_to_main_question', answer_to_main_question())
