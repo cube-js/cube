f7d20f47cd15c621e612fcec3eb98f90a19b1bae
Sam Hughes <sam@cube.dev>
2025-04-26 01:11:42 -0700

    chore(cubestore): Upgrade DF 46: Make DataFrame rendering use correct decimal scale

--- rust/cubestore/cubestore/src/metastore/mod.rs
+++ rust/cubestore/cubestore/src/metastore/mod.rs
@@ -459,18 +459,10 @@ impl ColumnType {
     pub fn target_scale(&self) -> i32 {
         match self {
             ColumnType::Decimal { scale, .. } => {
-                if *scale > 5 {
-                    10
-                } else {
-                    *scale
-                }
+                *scale
             }
             ColumnType::Decimal96 { scale, .. } => {
-                if *scale > 5 {
-                    10
-                } else {
-                    *scale
-                }
+                *scale
             }
             x => panic!("target_scale called on {:?}", x),
         }
