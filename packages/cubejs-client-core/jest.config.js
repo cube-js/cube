36d5c2cc26712d74d83112a343b95353dd3fb013
Konstantin Burkalev <KSDaemon@gmail.com>
2025-05-16 11:41:25 +0300

    some configs polishment

--- packages/cubejs-client-core/jest.config.js
+++ packages/cubejs-client-core/jest.config.js
@@ -1,17 +1,11 @@
-const base = require('../../jest.base.config');
+const base = require('../../jest.base-ts.config');
 
 /** @type {import('jest').Config} */
 module.exports = {
   ...base,
-  preset: 'ts-jest',
   rootDir: '.',
-  testMatch: ['<rootDir>/test/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
-  },
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/**/*.d.ts',
+    ...base.collectCoverageFrom,
+    '!<rootDir>/src/index.umd.ts',
   ],
 };
