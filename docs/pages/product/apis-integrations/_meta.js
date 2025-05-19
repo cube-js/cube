6f5f3bce42753afa48a385327eca8e597cfc74bd
Michael Irvine <mirvine@cube.dev>
2025-05-19 09:30:20 -0400

    docs: Remove docs for AI API and AI Assistant (#9574)
    * docs: removes AI API and AI Assistant
    
    * Update image
    
    ---------
    
    Co-authored-by: Michael Irvine <michaelirvine@Michaels-MacBook-Pro.local>
    Co-authored-by: Igor Lukanin <igor@cube.dev>

--- docs/pages/product/apis-integrations/_meta.js
+++ docs/pages/product/apis-integrations/_meta.js
@@ -8,7 +8,6 @@ module.exports = {
   "sql-api": "SQL API",
   "rest-api": "REST API",
   "graphql-api": "GraphQL API",
-  "ai-api": "AI API",
   "javascript-sdk": "JavaScript SDK",
   "orchestration-api": "Orchestration API",
 };
