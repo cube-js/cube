57fec0f8a0670d588a15f4ced72bbf3f58919609
Konstantin Burkalev <KSDaemon@gmail.com>
2025-05-20 12:39:00 +0300

    chore(dev-tools): Add safeguard to lerna publish script (#9591)

--- lerna-publish.sh
+++ lerna-publish.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+set -e
+
 . .gh-token
+
+echo "Running yarn install..."
+yarn install
+
+if [[ -n $(git status --porcelain) ]]; then
+  echo "Error: the git working tree is not clean after yarn install. Please review the changes and fix it or commit."
+  git status
+  exit 1
+fi
 
 BUMP=$1
 if [ "x$BUMP" == "x" ]; then
