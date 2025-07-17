45809724588de0385595f8c385016b82d0a50eb6
Igor Lukanin <igor@cube.dev>
2025-07-17 18:24:26 +0200

    docs: Calendar cubes

--- docs/pages/product/data-modeling/concepts/_meta.js
+++ docs/pages/product/data-modeling/concepts/_meta.js
@@ -1,6 +1,7 @@
 module.exports = {
   "calculated-members": "Calculated members",
   "multi-stage-calculations": "Multi-stage calculations",
+  "calendar-cubes": "Calendar cubes",
   "code-reusability-extending-cubes": "Extension",
   "polymorphic-cubes": "Polymorphic cubes",
   "data-blending": "Data blending",
