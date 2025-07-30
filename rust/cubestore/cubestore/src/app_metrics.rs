2294b7f1b85cd40c29d06ba0d2d14e43e95a0a03
Alexandr Romanenko <alex.romanenko@cube.dev>
2025-07-30 15:24:43 +0200

    in work

--- rust/cubestore/cubestore/src/app_metrics.rs
+++ rust/cubestore/cubestore/src/app_metrics.rs
@@ -8,7 +8,7 @@ use crate::util::metrics::{Counter, Gauge, Histogram};
 pub static STARTUPS: Counter = metrics::counter("cs.startup");
 
 /// Errots on reciving from subprocesses.
-pub static WORKER_POOL_ERROR: Counter = metrics::counter("cs.worker_pool.recive_error");
+pub static WORKER_POOL_ERROR: Counter = metrics::counter("cs.worker_pool.errors");
 
 /// Incoming SQL queries that do data reads.
 pub static DATA_QUERIES: Counter = metrics::counter("cs.sql.query.data");
