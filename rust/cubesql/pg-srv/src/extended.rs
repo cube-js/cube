8a614bb715f4e7baf597a03a1134702ea6e3c286
Dmitry Patsura <talk@dmtry.me>
2025-08-05 00:53:00 +0200

    feat(cubesql): Support timestamp parameter binding, fix #9784 (#9847)

--- rust/cubesql/pg-srv/src/extended.rs
+++ rust/cubesql/pg-srv/src/extended.rs
@@ -1,10 +1,15 @@
 //! Implementation for Extended Query
 
+#[cfg(feature = "with-chrono")]
+use crate::TimestampValue;
+
 #[derive(Debug, PartialEq)]
 pub enum BindValue {
     String(String),
     Int64(i64),
     Float64(f64),
     Bool(bool),
+    #[cfg(feature = "with-chrono")]
+    Timestamp(TimestampValue),
     Null,
 }
