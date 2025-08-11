9dd4c29520cdb952c3555faa263de7eed0184ad3
Konstantin Burkalev <KSDaemon@gmail.com>
2025-08-11 21:15:09 +0300

    fix templates.statements.generated_time_series_with_cte_range_source

--- packages/cubejs-databricks-jdbc-driver/src/DatabricksQuery.ts
+++ packages/cubejs-databricks-jdbc-driver/src/DatabricksQuery.ts
@@ -192,6 +192,17 @@ export class DatabricksQuery extends BaseQuery {
       '  FROM (SELECT explode(sequence(\n' +
       '    from_utc_timestamp({{ start }}, \'UTC\'), from_utc_timestamp({{ end }}, \'UTC\'), INTERVAL {{ granularity }}\n' +
       '  )) AS d)';
+    templates.statements.generated_time_series_with_cte_range_source =
+    'SELECT d AS date_from,\n' +
+    '(d + INTERVAL {{ granularity }}) - INTERVAL 1 MILLISECOND AS date_to\n' +
+    'FROM {{ range_source }}\n' +
+    'LATERAL VIEW explode(\n' +
+    '    sequence(\n' +
+    '        CAST({{ min_name }} AS TIMESTAMP),\n' +
+    '        CAST({{ max_name }} AS TIMESTAMP),\n' +
+    '        INTERVAL {{ granularity }}\n' +
+    '    )\n' +
+    ') dates AS d';
 
     // TODO: Databricks has `TIMESTAMP_NTZ` with logic similar to Pg's `TIMESTAMP`
     // but that requires Runtime 13.3+. Should this be enabled?
