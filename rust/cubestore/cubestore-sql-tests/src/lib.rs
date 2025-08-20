9fb4076bdcbc201e645f81efc500512a5f389d68
Dmitry Patsura <talk@dmtry.me>
2025-08-20 12:52:40 +0200

    chore: fix

--- rust/cubestore/cubestore-sql-tests/src/lib.rs
+++ rust/cubestore/cubestore-sql-tests/src/lib.rs
@@ -7,12 +7,12 @@ use async_trait::async_trait;
 use cubestore::sql::{QueryPlans, SqlQueryContext, SqlService};
 use cubestore::store::DataFrame;
 use cubestore::CubeError;
+use itertools::Itertools;
 use std::env;
 use std::panic::RefUnwindSafe;
 use std::sync::Arc;
 use test::TestFn::DynTestFn;
 use test::{ShouldPanic, TestDesc, TestDescAndFn, TestName, TestType};
-use itertools::Itertools;
 use tests::sql_tests;
 
 mod benches;
@@ -67,7 +67,10 @@ pub fn run_sql_tests(
         .collect();
 
     test::test_main(
-        &env::args().chain(extra_args).unique().collect::<Vec<String>>(),
+        &env::args()
+            .chain(extra_args)
+            .unique()
+            .collect::<Vec<String>>(),
         tests,
         None,
     );
