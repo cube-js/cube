dd11638f1378c0c4342dc8266206dbbc9d885398
Igor Lukanin <igor@cube.dev>
2025-08-29 13:26:22 +0200

    Revert "docs: Prefer literal over folded strings in YAML"
    This reverts commit 3368ba4820a52d8ecf164d9458371daefaf844a9.

--- docs/pages/product/data-modeling/dynamic/_meta.js
+++ docs/pages/product/data-modeling/dynamic/_meta.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "jinja": "YAML, Jinja, and Python",
-  "javascript": "JavaScript",
+  "jinja": "Dynamic data models with Jinja and Python",
+  "javascript": "Dynamic data models with JavaScript",
   "code-reusability-export-and-import": "Export and import",
-  "schema-execution-environment": "Execution environment (JavaScript)"
+  "schema-execution-environment": "Execution environment (JavaScript models)"
 }
