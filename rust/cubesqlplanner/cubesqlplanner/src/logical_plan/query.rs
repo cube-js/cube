ed22c12507bb0b58a056e78c72d436ea835a6b11
Alexandr Romanenko <alex.romanenko@cube.dev>
2025-09-10 15:20:37 +0200

    fix

--- rust/cubesqlplanner/cubesqlplanner/src/logical_plan/query.rs
+++ rust/cubesqlplanner/cubesqlplanner/src/logical_plan/query.rs
@@ -3,41 +3,6 @@ use cubenativeutils::CubeError;
 use std::rc::Rc;
 
 #[derive(Clone)]
-pub enum QuerySource {
-    LogicalJoin(Rc<LogicalJoin>),
-    FullKeyAggregate(Rc<FullKeyAggregate>),
-    PreAggregation(Rc<PreAggregation>),
-}
-
-impl QuerySource {
-    fn as_plan_node(&self) -> PlanNode {
-        match self {
-            Self::LogicalJoin(item) => item.as_plan_node(),
-            Self::FullKeyAggregate(item) => item.as_plan_node(),
-            Self::PreAggregation(item) => item.as_plan_node(),
-        }
-    }
-    fn with_plan_node(&self, plan_node: PlanNode) -> Result<Self, CubeError> {
-        Ok(match self {
-            Self::LogicalJoin(_) => Self::LogicalJoin(plan_node.into_logical_node()?),
-            Self::FullKeyAggregate(_) => Self::FullKeyAggregate(plan_node.into_logical_node()?),
-            Self::PreAggregation(_) => Self::PreAggregation(plan_node.into_logical_node()?),
-        })
-    }
-}
-
-impl PrettyPrint for QuerySource {
-    fn pretty_print(&self, result: &mut PrettyPrintResult, state: &PrettyPrintState) {
-        match self {
-            QuerySource::LogicalJoin(join) => join.pretty_print(result, state),
-            QuerySource::FullKeyAggregate(full_key) => full_key.pretty_print(result, state),
-            QuerySource::PreAggregation(pre_aggregation) => {
-                pre_aggregation.pretty_print(result, state)
-            }
-        }
-    }
-}
-#[derive(Clone)]
 pub struct Query {
     pub multistage_members: Vec<Rc<LogicalMultiStageMember>>,
     pub schema: Rc<LogicalSchema>,
