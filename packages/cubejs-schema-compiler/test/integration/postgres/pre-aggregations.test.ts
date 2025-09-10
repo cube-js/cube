078a8e36c6adb2b50f4a0410638c80fdcf96a41f
Konstantin Burkalev <KSDaemon@gmail.com>
2025-09-10 13:59:35 +0300

    fix tests

--- packages/cubejs-schema-compiler/test/integration/postgres/pre-aggregations.test.ts
+++ packages/cubejs-schema-compiler/test/integration/postgres/pre-aggregations.test.ts
@@ -2427,7 +2427,7 @@ describe('PreAggregations', () => {
           [
             { visitors__source: 'google', vc__count: '1' },
             { visitors__source: 'some', vc__count: '5' },
-            { visitors__source: null, vc__count: '0' },
+            { visitors__source: null, vc__count: null },
           ],
         );
       });
@@ -2474,7 +2474,7 @@ describe('PreAggregations', () => {
             { visitors__source: 'google', cards__visitor_id: 3, vc__count: '1' },
             { visitors__source: 'some', cards__visitor_id: 1, vc__count: '3' },
             { visitors__source: 'some', cards__visitor_id: null, vc__count: '2' },
-            { visitors__source: null, cards__visitor_id: null, vc__count: '0' },
+            { visitors__source: null, cards__visitor_id: null, vc__count: null },
           ],
         );
       });
