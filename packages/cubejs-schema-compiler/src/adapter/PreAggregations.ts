b536a733c6115ea11c426c8ba9176e35b052195b
Konstantin Burkalev <KSDaemon@gmail.com>
2025-09-29 20:04:37 +0300

    expose joinGraph from transformQueryToCanUseForm()

--- packages/cubejs-schema-compiler/src/adapter/PreAggregations.ts
+++ packages/cubejs-schema-compiler/src/adapter/PreAggregations.ts
@@ -510,6 +510,7 @@ export class PreAggregations {
       allValuesEq1(filterDimensionsSingleValueEqual) ? new Set(filterDimensionsSingleValueEqual?.keys()) : null;
 
     return {
+      joinGraph: query.join,
       sortedDimensions,
       sortedTimeDimensions,
       timeDimensions,
