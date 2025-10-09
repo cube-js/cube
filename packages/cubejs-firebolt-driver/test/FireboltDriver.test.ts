f92e496009fdd0e57d2447f582d0e1201374beaf
Alexander Chiu <alexanderchiu@users.noreply.github.com>
2025-10-09 12:46:05 +0200

    feat(firebolt-driver): Pass CUBEJS_DB_QUERY_TIMEOUT to Firebolt driver (#10043)

--- packages/cubejs-firebolt-driver/test/FireboltDriver.test.ts
+++ packages/cubejs-firebolt-driver/test/FireboltDriver.test.ts
@@ -1,6 +1,7 @@
 import { DriverTests } from '@cubejs-backend/testing-shared';
 
 import { FireboltDriver } from '../src';
+import { getEnv } from '@cubejs-backend/shared';
 
 describe('FireboltDriver', () => {
   let tests: DriverTests;
@@ -22,4 +23,22 @@ describe('FireboltDriver', () => {
   test('stream', async () => {
     await tests.testStream();
   });
+
+  test('query should fail on timeout', async () => {
+    const slowQuery = 'SELECT checksum(*) FROM GENERATE_SERIES(1, 1000000000000)';
+    // Set query timeout to 2 seconds
+    process.env.CUBEJS_DB_QUERY_TIMEOUT = '2';
+    const driver = new FireboltDriver({});
+
+    let timedOut = false;
+    try {
+      await driver.query(slowQuery);
+    } catch (e) {
+      if (String(e).toLowerCase().includes('timeout expired (2000 ms)')) {
+        timedOut = true;
+      }
+    }
+
+    expect(timedOut).toBe(true);
+  }, 10000);
 });
