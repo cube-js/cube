fbd2bf0d234b3169f67e036cac732a316b2624fd
Alexandr Romanenko <alex.romanenko@cube.dev>
2025-10-16 15:23:14 +0200

    test

---
+++ packages/cubejs-schema-compiler/test/unit/switch-dimension.test.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+
+import { CubeToMetaTransformer } from 'src/compiler/CubeToMetaTransformer';
+import { prepareYamlCompiler } from './PrepareCompiler';
+
+describe('Switch Dimension', () => {
+  it('Switch dimension meta type', async () => {
+    const modelContent = fs.readFileSync(
+      path.join(process.cwd(), '/test/unit/fixtures/switch-dimension.yml'),
+      'utf8'
+    );
+    const prepared = prepareYamlCompiler(modelContent);
+    const { metaTransformer } = prepared;
+    const { compiler } = prepared;
+    await compiler.compile();
+
+    const cube = metaTransformer.cubes[0];
+    const numberDim = cube.config.dimensions.find((d) => d.name === 'orders.number');
+    const statusDim = cube.config.dimensions.find((d) => d.name === 'orders.status');
+    const currencyDim = cube.config.dimensions.find((d) => d.name === 'orders.currency');
+
+    expect(numberDim.type).toBe('number');
+    expect(statusDim.type).toBe('string');
+    expect(currencyDim.type).toBe('string');
+  });
+});
