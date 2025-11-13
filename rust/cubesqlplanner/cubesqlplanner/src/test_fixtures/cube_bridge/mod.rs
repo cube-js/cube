40fbd8a1b61c9d9feb6e180cd269505ee52154da
Aleksandr Romanenko <alex.romanenko@cube.dev>
2025-11-13 17:14:57 +0100

    dimension mock

--- rust/cubesqlplanner/cubesqlplanner/src/test_fixtures/cube_bridge/mod.rs
+++ rust/cubesqlplanner/cubesqlplanner/src/test_fixtures/cube_bridge/mod.rs
@@ -1,6 +1,12 @@
 #[macro_use]
 mod macros;
 
+mod mock_case_definition;
+mod mock_case_else_item;
+mod mock_case_item;
+mod mock_case_switch_definition;
+mod mock_case_switch_else_item;
+mod mock_case_switch_item;
 mod mock_dimension_definition;
 mod mock_geo_item;
 mod mock_member_sql;
@@ -8,7 +14,12 @@ mod mock_security_context;
 mod mock_sql_utils;
 mod mock_timeshift_definition;
 
-pub use mock_dimension_definition::MockDimensionDefinition;
+pub use mock_case_definition::MockCaseDefinition;
+pub use mock_case_else_item::MockCaseElseItem;
+pub use mock_case_item::MockCaseItem;
+pub use mock_case_switch_definition::MockCaseSwitchDefinition;
+pub use mock_case_switch_else_item::MockCaseSwitchElseItem;
+pub use mock_case_switch_item::MockCaseSwitchItem;
 pub use mock_geo_item::MockGeoItem;
 pub use mock_member_sql::MockMemberSql;
 pub use mock_security_context::MockSecurityContext;
