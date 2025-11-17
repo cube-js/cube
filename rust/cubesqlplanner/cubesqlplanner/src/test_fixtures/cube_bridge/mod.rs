c31a47fbc62be25f9bb66eddcf8861bd96c4ae7d
Aleksandr Romanenko <alex.romanenko@cube.dev>
2025-11-17 14:57:30 +0100

    chore

--- rust/cubesqlplanner/cubesqlplanner/src/test_fixtures/cube_bridge/mod.rs
+++ rust/cubesqlplanner/cubesqlplanner/src/test_fixtures/cube_bridge/mod.rs
@@ -32,6 +32,10 @@ mod mock_sql_utils;
 mod mock_struct_with_sql_member;
 mod mock_timeshift_definition;
 
+#[cfg(test)]
+mod mock_join_graph_tests;
+
+pub use base_query_options::{members_from_strings, MockBaseQueryOptions};
 pub use mock_base_tools::MockBaseTools;
 pub use mock_case_definition::MockCaseDefinition;
 pub use mock_case_else_item::MockCaseElseItem;
