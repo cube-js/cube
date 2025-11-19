3da7a4785318d00104ba7d5d3b1f2f870a298ca9
Dennis Zhang <dennis@gorgias.com>
2025-11-19 10:22:51 -0800

    make env var for cubestore metrics bind address

--- rust/cubestore/cubestore/src/bin/cubestored.rs
+++ rust/cubestore/cubestore/src/bin/cubestored.rs
@@ -31,12 +31,14 @@ fn main() {
         ),
         Err(_) => metrics::Compatibility::StatsD,
     };
+    let metrics_bind_address =
+        std::env::var("CUBESTORE_METRICS_BIND_ADDRESS").unwrap_or("127.0.0.1".to_string());
     let metrics_addr =
         std::env::var("CUBESTORE_METRICS_ADDRESS").unwrap_or("127.0.0.1".to_string());
     let metrics_port = std::env::var("CUBESTORE_METRICS_PORT").unwrap_or("8125".to_string());
     let metrics_server_address = format!("{}:{}", metrics_addr, metrics_port);
 
-    init_metrics("0.0.0.0:0", metrics_server_address, metrics_format, vec![]);
+    init_metrics(format!("{}:0", metrics_bind_address), metrics_server_address, metrics_format, vec![]);
     let telemetry_env = std::env::var("CUBESTORE_TELEMETRY")
         .or(std::env::var("CUBEJS_TELEMETRY"))
         .unwrap_or("true".to_string());
