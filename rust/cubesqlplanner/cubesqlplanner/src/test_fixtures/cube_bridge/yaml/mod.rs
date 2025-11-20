2cb27bb45796b9807c23eb0b9c3c0369d4e83d71
Aleksandr Romanenko <alex.romanenko@cube.dev>
2025-11-20 14:22:14 +0100

    mock schema yaml

--- rust/cubesqlplanner/cubesqlplanner/src/test_fixtures/cube_bridge/yaml/mod.rs
+++ rust/cubesqlplanner/cubesqlplanner/src/test_fixtures/cube_bridge/yaml/mod.rs
@@ -1,5 +1,13 @@
 pub mod case;
 pub mod dimension;
 pub mod measure;
+pub mod schema;
 pub mod segment;
 pub mod timeshift;
+
+pub use case::{YamlCaseDefinition, YamlCaseSwitchDefinition, YamlCaseVariant};
+pub use dimension::YamlDimensionDefinition;
+pub use measure::YamlMeasureDefinition;
+pub use schema::YamlSchema;
+pub use segment::YamlSegmentDefinition;
+pub use timeshift::YamlTimeShiftDefinition;
