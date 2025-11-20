e18355093c0ad4b792b2371a9677f812510b6d2e
Konstantin Burkalev <KSDaemon@gmail.com>
2025-11-20 21:43:45 +0200

    rewrite byId in CubeDictionary to Map<string, Cube>

--- packages/cubejs-schema-compiler/src/compiler/CubeDictionary.ts
+++ packages/cubejs-schema-compiler/src/compiler/CubeDictionary.ts
@@ -8,24 +8,28 @@ export interface Cube {
 }
 
 export class CubeDictionary implements TranspilerCubeResolver, CompilerInterface {
-  public byId: Record<string, Cube>;
+  private byId: Map<string, Cube>;
 
   public constructor() {
-    this.byId = {};
+    this.byId = new Map<string, Cube>();
   }
 
   public compile(cubes: Cube[], _errorReporter?: ErrorReporter): void {
-    this.byId = {};
+    this.byId = new Map<string, Cube>();
     for (const cube of cubes) {
-      this.byId[cube.name] = cube;
+      this.byId.set(cube.name, cube);
     }
   }
 
-  public resolveCube(name: string): boolean {
-    return !!this.byId[name];
+  public resolveCube(name: string): Cube | undefined {
+    return this.byId.get(name);
   }
 
   public free(): void {
-    this.byId = {};
+    this.byId = new Map<string, Cube>();
+  }
+
+  public cubeNames(): string[] {
+    return Array.from(this.byId.keys());
   }
 }
