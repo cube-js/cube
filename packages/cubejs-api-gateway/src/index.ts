62715203c70b71e371bf2ccad83609ea4b9ce2d1
Dmitry Patsura <talk@dmtry.me>
2025-12-09 18:02:17 +0100

    fix: Improve WS request sanitization (#10231)

--- packages/cubejs-api-gateway/src/index.ts
+++ packages/cubejs-api-gateway/src/index.ts
@@ -3,5 +3,8 @@ export * from './sql-server';
 export * from './interfaces';
 export * from './cubejs-handler-error';
 export * from './user-error';
+
 export { getRequestIdFromRequest } from './request-parser';
 export { TransformDataRequest } from './types/responses';
+
+export type { SubscriptionServer } from './ws';
