010cedfb05c47c3357cc688f91b7aaaa935def13
Artyom Keydunov <artyom@cube.dev>
2025-12-15 13:43:07 -0800

    docs: update APIs section (#10248)
    * Update chat api docs
    
    * Fixes
    
    * fixes

---
+++ docs/pages/product/apis-integrations/core-data-apis/_meta.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "queries": "Queries",
+  "sql-api": "SQL API",
+  "dax-api": "DAX API",
+  "mdx-api": "MDX API",
+  "rest-api": "REST API",
+  "graphql-api": "GraphQL API"
+};
+
