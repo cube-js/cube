0e925a4bf75eb1f6117bf149c0a909f1431df7a7
Artyom Keydunov <artyom.keydunov@gmail.com>
2025-12-15 16:59:56 -0800

    docs: update access policies docs

--- docs/pages/product/auth/_meta.js
+++ docs/pages/product/auth/_meta.js
@@ -3,6 +3,5 @@ module.exports = {
   "row-level-security": "Row-level security",
   "member-level-security": "Member-level security",
   "methods": "Cube Core Authentication",
-  "context": "Security context",
-  "recipes": "Recipes"
+  "context": "Security context"
 }
