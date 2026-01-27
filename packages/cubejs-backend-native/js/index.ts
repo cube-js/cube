1bc88f9e2be8fbf1fe8099f58e66df25987b5549
Dmitry Patsura <talk@dmtry.me>
2026-01-27 12:55:22 +0100

    feat(backend-native): Add JSON format logging for production environment (#10338)
    Previusly, Rust logger output simple strings in stdout in production env, while Node.js logger output JSON.
    Let's align it to usage of JSON.

--- packages/cubejs-backend-native/js/index.ts
+++ packages/cubejs-backend-native/js/index.ts
@@ -353,9 +353,9 @@ function wrapNativeFunctionWithStream(
 
 type LogLevel = 'error' | 'warn' | 'info' | 'debug' | 'trace';
 
-export const setupLogger = (logger: (extra: any) => unknown, logLevel: LogLevel): void => {
+export const setupLogger = (logger: (extra: any) => unknown, logLevel: LogLevel, prodLogger: boolean = false): void => {
   const native = loadNative();
-  native.setupLogger({ logger: wrapNativeFunctionWithChannelCallback(logger), logLevel });
+  native.setupLogger({ logger: wrapNativeFunctionWithChannelCallback(logger), logLevel, prodLogger });
 };
 
 /// Reset local to default implementation, which uses STDOUT
