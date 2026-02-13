295583c5503f84112283522793d493f9be9a539e
Dmitry Patsura <talk@dmtry.me>
2026-02-13 14:08:06 +0100

    chore: upgrade cratedb to 5.10.16

--- packages/cubejs-testing-shared/src/db-container-runners/crate.ts
+++ packages/cubejs-testing-shared/src/db-container-runners/crate.ts
@@ -2,14 +2,20 @@ import { GenericContainer, Wait } from 'testcontainers';
 
 import { DbRunnerAbstract, DBRunnerContainerOptions } from './db-runner.abstract';
 
+const DEFAULT_VERSION = '5.10.16';
+
 export class CrateDBRunner extends DbRunnerAbstract {
   public static startContainer(options: DBRunnerContainerOptions) {
-    const version = process.env.TEST_CRATE_DB_VERSION || '5.0.0';
+    const version = process.env.TEST_CRATE_DB_VERSION || DEFAULT_VERSION;
 
     const container = new GenericContainer(`crate/crate:${version}`)
       .withExposedPorts(5432)
       .withWaitStrategy(Wait.forLogMessage('started'))
-      .withStartupTimeout(15 * 1000);
+      .withStartupTimeout(30 * 1000);
+
+    if (process.platform === 'darwin' && process.arch === 'arm64' && version === DEFAULT_VERSION) {
+      container.withPlatform('linux/amd64');
+    }
 
     if (options.volumes) {
       const binds = options.volumes.map(v => ({ source: v.source, target: v.target, mode: v.bindMode }));
