f1a7f6c566c467dd94103a490d1c54e55b458da3
Igor Lukanin <igor@cube.dev>
2026-02-18 21:00:59 +0100

    Add header links: logo with version, GitHub, Slack, and Get Started button

--- docs_v4/app/layout.jsx
+++ docs_v4/app/layout.jsx
@@ -3,6 +3,20 @@ import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
 import { Footer } from '../components/Footer'
+import { LogoWithVersion } from '../components/LogoWithVersion'
+import { GetStartedButton } from '../components/GetStartedButton'
+
+const SlackIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path d="M5.042 15.165a2.528 2.528 0 0 1-2.52 2.523A2.528 2.528 0 0 1 0 15.165a2.527 2.527 0 0 1 2.522-2.52h2.52v2.52zM6.313 15.165a2.527 2.527 0 0 1 2.521-2.52 2.527 2.527 0 0 1 2.521 2.52v6.313A2.528 2.528 0 0 1 8.834 24a2.528 2.528 0 0 1-2.521-2.522v-6.313zM8.834 5.042a2.528 2.528 0 0 1-2.521-2.52A2.528 2.528 0 0 1 8.834 0a2.528 2.528 0 0 1 2.521 2.522v2.52H8.834zM8.834 6.313a2.528 2.528 0 0 1 2.521 2.521 2.528 2.528 0 0 1-2.521 2.521H2.522A2.528 2.528 0 0 1 0 8.834a2.528 2.528 0 0 1 2.522-2.521h6.312zM18.956 8.834a2.528 2.528 0 0 1 2.522-2.521A2.528 2.528 0 0 1 24 8.834a2.528 2.528 0 0 1-2.522 2.521h-2.522V8.834zM17.688 8.834a2.528 2.528 0 0 1-2.523 2.521 2.527 2.527 0 0 1-2.52-2.521V2.522A2.527 2.527 0 0 1 15.165 0a2.528 2.528 0 0 1 2.523 2.522v6.312zM15.165 18.956a2.528 2.528 0 0 1 2.523 2.522A2.528 2.528 0 0 1 15.165 24a2.527 2.527 0 0 1-2.52-2.522v-2.522h2.52zM15.165 17.688a2.527 2.527 0 0 1-2.52-2.523 2.526 2.526 0 0 1 2.52-2.52h6.313A2.527 2.527 0 0 1 24 15.165a2.528 2.528 0 0 1-2.522 2.523h-6.313z" />
+  </svg>
+)
 
 export const metadata = {
   title: 'Cube Documentation',
@@ -11,8 +25,15 @@ export const metadata = {
 
 const navbar = (
   <Navbar
-    logo={<b>Cube</b>}
-  />
+    logo={<LogoWithVersion />}
+    logoLink={false}
+    align="left"
+    projectLink="https://github.com/cube-js/cube"
+    chatLink="https://slack.cube.dev"
+    chatIcon={<SlackIcon />}
+  >
+    <GetStartedButton />
+  </Navbar>
 )
 
 const footer = <Footer />
