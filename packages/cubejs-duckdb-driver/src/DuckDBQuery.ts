cafa818a6e5be4114077b5a40be3645f6fd00e2c
Peter Leiwakabessy <93518481+pjleiwa6g@users.noreply.github.com>
2026-02-23 15:23:40 +0000

    Added templates.expressions.timestamp_literal for MySQL and DuckDB to have timestamp between quotes and in thhe right format.

--- packages/cubejs-duckdb-driver/src/DuckDBQuery.ts
+++ packages/cubejs-duckdb-driver/src/DuckDBQuery.ts
@@ -64,6 +64,7 @@ export class DuckDBQuery extends BaseQuery {
     templates.functions.LEAST = 'LEAST({{ args_concat }})';
     templates.functions.GREATEST = 'GREATEST({{ args_concat }})';
     templates.functions.STRING_AGG = 'STRING_AGG({% if distinct %}DISTINCT {% endif %}{{ args[0] }}, COALESCE({{ args[1] }}, \'\'))';
+    templates.expressions.timestamp_literal = '\'{{ value }}\'';
     return templates;
   }
 
