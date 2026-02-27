b2535549aec31a85415321d2698a7f80d7779fed
Dmitry Patsura <talk@dmtry.me>
2026-02-27 22:45:50 +0100

    chore: Align to jest ^29

--- packages/cubejs-client-vue3/jest.config.js
+++ packages/cubejs-client-vue3/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
-  preset: '@vue/cli-plugin-unit-jest',
   transform: {
     '^.+\\.vue$': '@vue/vue3-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
@@ -13,6 +12,7 @@ module.exports = {
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
   testEnvironment: 'jsdom',
-  testURL: 'http://localhost/',
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  testEnvironmentOptions: {
+    url: 'http://localhost/',
+  },
 };
