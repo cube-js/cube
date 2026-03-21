2ed7d031433e80e1bf2f763a35930134279d7a6e
Aleksandr Romanenko <alex.romanenko@cube.dev>
2026-03-21 20:03:13 +0100

    rolling window step 2

--- rust/cubesqlplanner/cubesqlplanner/src/tests/integration/rolling_window/mod.rs
+++ rust/cubesqlplanner/cubesqlplanner/src/tests/integration/rolling_window/mod.rs
@@ -1,6 +1,11 @@
 mod aggregation_types;
 mod basic_types;
+mod calculated_measures;
+mod different_granularities;
+mod filtered_rolling_measures;
 mod mixed_measures;
 mod multiple_rolling;
+mod time_dimension_variations;
 mod with_dimensions;
 mod with_filters;
+mod with_joins;
