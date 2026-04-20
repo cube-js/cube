936a65feaee99d2f1b6e29b382c6bcaada413776
Dmitry Patsura <talk@dmtry.me>
2026-04-20 12:48:07 +0200

    feat(cubestore-driver): Upgrade flatbuffers to 25.9.23
    Bumps flatbuffers from 23.3.3 to 25.9.23 and regenerates the TS
    bindings from http_message.fbs. Also switches the generator flag
    from the deprecated --ts-flat-files to --gen-all.
    
    Notable runtime improvement: createString now uses Uint8Array.set
    instead of a per-byte loop, which is ~2-3x faster for large payloads
    such as the csv_rows field on inline tables.

--- packages/cubejs-cubestore-driver/codegen/generate.sh
+++ packages/cubejs-cubestore-driver/codegen/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-flatc --ts ../../../rust/cubeshared/src/codegen/http_message.fbs --ts-flat-files
+flatc --ts ../../../rust/cubeshared/src/codegen/http_message.fbs --gen-all
 mv http_message.ts index.ts
