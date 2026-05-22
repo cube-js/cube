85933927db85baea2a96c178da6f81c833956093
Sascha John Hesse <sascha.hesse@danfoss.com>
2026-05-22 10:52:23 +0200

    fix(tests): reorder test module declarations for consistency after linting

--- rust/cubesql/cubesql/src/compile/test/mod.rs
+++ rust/cubesql/cubesql/src/compile/test/mod.rs
@@ -31,11 +31,11 @@ pub mod test_bi_workarounds;
 #[cfg(test)]
 pub mod test_cube_join;
 #[cfg(test)]
-pub mod test_database_meta;
-#[cfg(test)]
 pub mod test_cube_join_grouped;
 #[cfg(test)]
 pub mod test_cube_scan;
+#[cfg(test)]
+pub mod test_database_meta;
 #[cfg(test)]
 pub mod test_df_execution;
 #[cfg(test)]
