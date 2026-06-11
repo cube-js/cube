dbd4f612b2f3145134dac8c849a8060c2430eaeb
Dmitry Patsura <talk@dmtry.me>
2026-06-11 16:26:13 +0200

    chore: fix

--- packages/cubejs-testing/birdbox-fixtures/multidb/cube.js
+++ packages/cubejs-testing/birdbox-fixtures/multidb/cube.js
@@ -1,16 +1,21 @@
 module.exports = {
   driverFactory: ({ dataSource }) => {
     switch (dataSource) {
-      case 'suppliers': return { type: 'postgres' };
-      case 'products': return {
-        type: 'mysql',
-        host: process.env.CUBEJS_DB_HOST2,
-        port: process.env.CUBEJS_DB_PORT2,
-        database: process.env.CUBEJS_DB_NAME2,
-        user: process.env.CUBEJS_DB_USER2,
-        password: process.env.CUBEJS_DB_PASS2,
-      };
-      default: throw new Error(`driverFactory: Invalid dataSource '${dataSource}'`);
+      case 'suppliers':
+        return { type: 'postgres' };
+      case 'products':
+        return {
+          type: 'mysql',
+          host: process.env.CUBEJS_DB_HOST2,
+          port: process.env.CUBEJS_DB_PORT2,
+          database: process.env.CUBEJS_DB_NAME2,
+          user: process.env.CUBEJS_DB_USER2,
+          password: process.env.CUBEJS_DB_PASS2,
+        };
+      case 'default':
+        return { type: 'postgres' };
+      default:
+        throw new Error(`driverFactory: Invalid dataSource '${dataSource}'`);
     }
   },
 };
