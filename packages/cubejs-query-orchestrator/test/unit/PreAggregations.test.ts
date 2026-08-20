cube-js/cube

commit 75167a0e92028dbdd6f24aca85331b84be8ec3c3

feat: Execute refreshKeys in externalDb (only for every) (#3061)
Internal:
First, there is an internal change, that we started to pass query options as 3rd parameter in Query tuple.

Motivation
We are going to execute refresh keys (only for every, just now) in an external database. In the feature, we are planning to improve support for every via changes in the cube store (support more useful SQL things).

packages/cubejs-query-orchestrator/src/orchestrator/QueryCache.ts
@@ -10,7 +10,12 @@ import { CacheDriverInterface } from './cache-driver.interface';
 import { DriverFactory, DriverFactoryByDataSource } from './DriverFactory';
 import { BaseDriver } from '../driver';
 
-type QueryWithParams = [string, any[]] | string;
+type QueryOptions = {
+  external?: boolean,
+  renewalThreshold?: number,
+};
+export type QueryTuple = [sql: string, params: unknown[], options?: QueryOptions];
+export type QueryWithParams = QueryTuple | string;
 type Query = {
   requestId?: string;
   dataSource: string;
@@ -74,7 +79,6 @@ export class QueryCache {
       this.cacheKeyQueriesFrom(queryBody).map(replacePreAggregationTableNames);
 
     const renewalThreshold = queryBody.cacheKeyQueries && queryBody.cacheKeyQueries.renewalThreshold;
-    const refreshKeyRenewalThresholds = this.getRefreshKeyRenewalThresholds(queryBody);
 
     const expireSecs = this.getExpireSecs(queryBody);
 
@@ -97,7 +101,6 @@ export class QueryCache {
         external: queryBody.external,
         requestId: queryBody.requestId,
         dataSource: queryBody.dataSource,
-        refreshKeyRenewalThresholds
       });
     }
 
@@ -106,7 +109,6 @@ export class QueryCache {
         external: queryBody.external,
         requestId: queryBody.requestId,
         dataSource: queryBody.dataSource,
-        refreshKeyRenewalThresholds,
         skipRefreshKeyWaitForRenew: true
       });
 
@@ -114,7 +116,6 @@ export class QueryCache {
         external: queryBody.external,
         requestId: queryBody.requestId,
         dataSource: queryBody.dataSource,
-        refreshKeyRenewalThresholds
       });
 
       return resultPromise;
@@ -140,7 +141,6 @@ export class QueryCache {
         external: queryBody.external,
         requestId: queryBody.requestId,
         dataSource: queryBody.dataSource,
-        refreshKeyRenewalThresholds
       });
     }
 
@@ -148,11 +148,6 @@ export class QueryCache {
       data: await mainPromise,
       lastRefreshTime: await this.lastRefreshTime(cacheKey)
     };
-  }
-
-  private getRefreshKeyRenewalThresholds(queryBody) {
-    return queryBody.cacheKeyQueries &&
-      queryBody.cacheKeyQueries.refreshKeyRenewalThresholds;
   }
 
   private getExpireSecs(queryBody): number {
@@ -178,12 +173,12 @@ export class QueryCache {
   }
 
   public static replacePreAggregationTableNames(queryAndParams: QueryWithParams, preAggregationsTablesToTempTables) {
-    const [keyQuery, params] = Array.isArray(queryAndParams) ? queryAndParams : [queryAndParams, []];
+    const [keyQuery, params, queryOptions] = Array.isArray(queryAndParams) ? queryAndParams : [queryAndParams, []];
     const replacedKeqQuery = preAggregationsTablesToTempTables.reduce(
       (query, [tableName, { targetTableName }]) => QueryCache.replaceAll(tableName, targetTableName, query),
       keyQuery
     );
-    return Array.isArray(queryAndParams) ? [replacedKeqQuery, params] : replacedKeqQuery;
+    return Array.isArray(queryAndParams) ? [replacedKeqQuery, params, queryOptions] : replacedKeqQuery;
   }
 
   public queryWithRetryAndRelease(query, values, {
@@ -300,7 +295,6 @@ export class QueryCache {
     requestId?: string,
     skipRefreshKeyWaitForRenew?: boolean,
     external?: boolean,
-    refreshKeyRenewalThresholds?: Array<number>
     dataSource: string
   }) {
     this.renewQuery(
@@ -318,7 +312,6 @@ export class QueryCache {
     requestId?: string,
     skipRefreshKeyWaitForRenew?: boolean,
     external?: boolean,
-    refreshKeyRenewalThresholds?: Array<number>
     dataSource: string
   }) {
     options = options || { dataSource: 'default' };
@@ -356,11 +349,16 @@ export class QueryCache {
   }
 
   public async loadRefreshKeysFromQuery(query: Query) {
-    return Promise.all(this.loadRefreshKeys(this.cacheKeyQueriesFrom(query), this.getExpireSecs(query), {
-      requestId: query.requestId,
-      dataSource: query.dataSource,
-      refreshKeyRenewalThresholds: this.getRefreshKeyRenewalThresholds(query)
-    }));
+    return Promise.all(
+      this.loadRefreshKeys(
+        this.cacheKeyQueriesFrom(query),
+        this.getExpireSecs(query),
+        {
+          requestId: query.requestId,
+          dataSource: query.dataSource,
+        }
+      )
+    );
   }
 
   public loadRefreshKeys(
@@ -369,27 +367,28 @@ export class QueryCache {
     options: {
       requestId?: string;
       skipRefreshKeyWaitForRenew?: boolean;
-      refreshKeyRenewalThresholds?: Array<number>;
       dataSource: string
     }
   ) {
-    return cacheKeyQueries.map((q, i) => this.cacheQueryResult(
-      Array.isArray(q) ? q[0] : q,
-      Array.isArray(q) ? q[1] : [],
-      q,
-      expireSecs,
-      {
-        renewalThreshold:
-          this.options.refreshKeyRenewalThreshold ||
-          (options.refreshKeyRenewalThresholds || [])[i] ||
-          2 * 60,
-        renewalKey: q,
-        waitForRenew: !options.skipRefreshKeyWaitForRenew,
-        requestId: options.requestId,
-        dataSource: options.dataSource,
-        useInMemory: true
-      },
-    ));
+    return cacheKeyQueries.map((q, i) => {
+      const [query, values, queryOptions]: QueryTuple = Array.isArray(q) ? q : [q, [], {}];
+
+      return this.cacheQueryResult(
+        query,
+        values,
+        [query, values],
+        expireSecs,
+        {
+          renewalThreshold: this.options.refreshKeyRenewalThreshold || queryOptions?.renewalThreshold || 2 * 60,
+          renewalKey: q,
+          waitForRenew: !options.skipRefreshKeyWaitForRenew,
+          requestId: options.requestId,
+          dataSource: options.dataSource,
+          useInMemory: true,
+          external: queryOptions?.external,
+        },
+      );
+    });
   }
 
   public withLock = <T = any>(

packages/cubejs-query-orchestrator/src/orchestrator/PreAggregations.ts
@@ -6,7 +6,7 @@ import { getEnv } from '@cubejs-backend/shared';
 import { cancelCombinator, SaveCancelFn } from '../driver/utils';
 import { RedisCacheDriver } from './RedisCacheDriver';
 import { LocalCacheDriver } from './LocalCacheDriver';
-import { QueryCache } from './QueryCache';
+import { QueryCache, QueryTuple, QueryWithParams } from './QueryCache';
 import { ContinueWaitError } from './ContinueWaitError';
 import { DriverFactory, DriverFactoryByDataSource } from './DriverFactory';
 import { CacheDriverInterface } from './cache-driver.interface';
@@ -267,28 +267,29 @@ class PreAggregationLoadCache {
     return this.versionEntries[redisKey];
   }
 
-  protected async keyQueryResult(keyQuery, waitForRenew, priority, renewalThreshold) {
-    if (!this.queryResults[this.queryCache.queryRedisKey(keyQuery)]) {
-      this.queryResults[this.queryCache.queryRedisKey(keyQuery)] = await this.queryCache.cacheQueryResult(
-        Array.isArray(keyQuery) ? keyQuery[0] : keyQuery,
-        Array.isArray(keyQuery) ? keyQuery[1] : [],
-        keyQuery,
+  protected async keyQueryResult(sqlQuery: QueryWithParams, waitForRenew, priority) {
+    const [query, values, queryOptions]: QueryTuple = Array.isArray(sqlQuery) ? sqlQuery : [sqlQuery, [], {}];
+
+    if (!this.queryResults[this.queryCache.queryRedisKey([query, values])]) {
+      this.queryResults[this.queryCache.queryRedisKey([query, values])] = await this.queryCache.cacheQueryResult(
+        query,
+        values,
+        [query, values],
         60 * 60,
         {
-          renewalThreshold:
-            this.queryCache.options.refreshKeyRenewalThreshold ||
-            renewalThreshold ||
-            2 * 60,
-          renewalKey: keyQuery,
+          renewalThreshold: this.queryCache.options.refreshKeyRenewalThreshold
+            || queryOptions?.renewalThreshold || 2 * 60,
+          renewalKey: [query, values],
           waitForRenew,
           priority,
           requestId: this.requestId,
           dataSource: this.dataSource,
-          useInMemory: true
+          useInMemory: true,
+          external: queryOptions?.external
         }
       );
     }
-    return this.queryResults[this.queryCache.queryRedisKey(keyQuery)];
+    return this.queryResults[this.queryCache.queryRedisKey([query, values])];
   }
 
   protected hasKeyQueryResult(keyQuery) {
@@ -412,12 +413,12 @@ class PreAggregationLoader {
         });
         return this.targetTableName(versionEntryByStructureVersion);
       } else {
-        // no rollup has been built yet - build it syncronously as part of responding to this request
+        // no rollup has been built yet - build it synchronously as part of responding to this request
         return this.loadPreAggregationWithKeys();
       }
     } else {
       // either we have no data cached for this rollup or waitForRenew is true, either way,
-      // syncronously renew what data is needed so that the most current data will be returned for the current request
+      // synchronously renew what data is needed so that the most current data will be returned for the current request
       return {
         targetTableName: await this.loadPreAggregationWithKeys(),
         refreshKeyValues: await this.getInvalidationKeyValues()
@@ -532,15 +533,9 @@ class PreAggregationLoader {
 
   protected getInvalidationKeyValues() {
     return Promise.all(
-      (this.preAggregation.invalidateKeyQueries || [])
-        .map(
-          (keyQuery, i) => this.loadCache.keyQueryResult(
-            keyQuery,
-            this.waitForRenew,
-            this.priority(10),
-            (this.preAggregation.refreshKeyRenewalThresholds || [])[i]
-          )
-        )
+      (this.preAggregation.invalidateKeyQueries || []).map(
+        (sqlQuery) => this.loadCache.keyQueryResult(sqlQuery, this.waitForRenew, this.priority(10))
+      )
     );
   }
 

packages/cubejs-query-orchestrator/test/unit/PreAggregations.test.js
@@ -68,21 +68,29 @@ describe('PreAggregations', () => {
   let mockDriverReadOnlyFactory = null;
   let mockExternalDriverFactory = null;
   let queryCache = null;
+
   const basicQuery = {
     query: 'SELECT "orders__created_at_week" "orders__created_at_week", sum("orders__count") "orders__count" FROM (SELECT * FROM stb_pre_aggregations.orders_number_and_count20191101) as partition_union  WHERE ("orders__created_at_week" >= ($1::timestamptz::timestamptz AT TIME ZONE \'UTC\') AND "orders__created_at_week" <= ($2::timestamptz::timestamptz AT TIME ZONE \'UTC\')) GROUP BY 1 ORDER BY 1 ASC LIMIT 10000',
     values: ['2019-11-01T00:00:00Z', '2019-11-30T23:59:59Z'],
     cacheKeyQueries: {
       renewalThreshold: 21600,
-      queries: [['SELECT date_trunc(\'hour\', (NOW()::timestamptz AT TIME ZONE \'UTC\')) as current_hour', []]]
+      queries: [['SELECT date_trunc(\'hour\', (NOW()::timestamptz AT TIME ZONE \'UTC\')) as current_hour', [], {
+        renewalThreshold: 10,
+        external: false,
+      }]]
     },
     preAggregations: [{
       preAggregationsSchema: 'stb_pre_aggregations',
       tableName: 'stb_pre_aggregations.orders_number_and_count20191101',
       loadSql: ['CREATE TABLE stb_pre_aggregations.orders_number_and_count20191101 AS SELECT\n      date_trunc(\'week\', ("orders".created_at::timestamptz AT TIME ZONE \'UTC\')) "orders__created_at_week", count("orders".id) "orders__count", sum("orders".number) "orders__number"\n    FROM\n      public.orders AS "orders"\n  WHERE ("orders".created_at >= $1::timestamptz AND "orders".created_at <= $2::timestamptz) GROUP BY 1', ['2019-11-01T00:00:00Z', '2019-11-30T23:59:59Z']],
-      invalidateKeyQueries: [['SELECT date_trunc(\'hour\', (NOW()::timestamptz AT TIME ZONE \'UTC\')) as current_hour', []]]
+      invalidateKeyQueries: [['SELECT date_trunc(\'hour\', (NOW()::timestamptz AT TIME ZONE \'UTC\')) as current_hour', [], {
+        renewalThreshold: 10,
+        external: false,
+      }]]
     }],
     requestId: 'basic'
   };
+
   const basicQueryExternal = R.clone(basicQuery);
   basicQueryExternal.preAggregations[0].external = true;
   const basicQueryWithRenew = R.clone(basicQuery);

packages/cubejs-query-orchestrator/test/unit/QueryOrchestrator.test.js
@@ -565,8 +565,9 @@ describe('QueryOrchestrator', () => {
         preAggregationsSchema: 'stb_pre_aggregations',
         tableName: 'stb_pre_aggregations.orders_d20181103',
         loadSql: ['CREATE TABLE stb_pre_aggregations.orders_d20181103 AS SELECT * FROM public.orders', []],
-        invalidateKeyQueries: [['SELECT NOW()', []]],
-        refreshKeyRenewalThresholds: [0.001]
+        invalidateKeyQueries: [['SELECT NOW()', [], {
+          renewalThreshold: 0.001
+        }]],
       }]
     };
 
@@ -617,18 +618,22 @@ describe('QueryOrchestrator', () => {
       query: 'SELECT * FROM orders',
       values: [],
       cacheKeyQueries: {
-        refreshKeyRenewalThresholds: [21600, 120],
         queries: [
-          ['SELECT NOW()', []],
-          ['SELECT date_trunc(\'hour\', (NOW()::timestamptz AT TIME ZONE \'UTC\'))', []]
+          ['SELECT NOW()', [], {
+            renewalThreshold: 21600,
+          }],
+          ['SELECT date_trunc(\'hour\', (NOW()::timestamptz AT TIME ZONE \'UTC\'))', [], {
+            renewalThreshold: 120,
+          }]
         ]
       },
       preAggregations: [{
         preAggregationsSchema: 'stb_pre_aggregations',
         tableName: 'stb_pre_aggregations.orders_d20201103',
         loadSql: ['CREATE TABLE stb_pre_aggregations.orders_d20201103 AS SELECT * FROM public.orders', []],
-        invalidateKeyQueries: [['SELECT NOW() as now', []]],
-        refreshKeyRenewalThresholds: [86400]
+        invalidateKeyQueries: [['SELECT NOW() as now', [], {
+          renewalThreshold: 86400,
+        }]]
       }],
       requestId: 'in memory cache',
     };
@@ -637,17 +642,17 @@ describe('QueryOrchestrator', () => {
     await queryOrchestrator.fetchQuery(query);
     expect(
       queryOrchestrator.queryCache.memoryCache.has(
-        queryOrchestrator.queryCache.queryRedisKey(query.cacheKeyQueries.queries[0])
+        queryOrchestrator.queryCache.queryRedisKey(query.cacheKeyQueries.queries[0].slice(0, 2))
       )
     ).toBe(true);
     expect(
       queryOrchestrator.queryCache.memoryCache.has(
-        queryOrchestrator.queryCache.queryRedisKey(query.cacheKeyQueries.queries[1])
+        queryOrchestrator.queryCache.queryRedisKey(query.cacheKeyQueries.queries[1].slice(0, 2))
       )
     ).toBe(false);
     expect(
       queryOrchestrator.queryCache.memoryCache.has(
-        queryOrchestrator.queryCache.queryRedisKey(query.preAggregations[0].invalidateKeyQueries[0])
+        queryOrchestrator.queryCache.queryRedisKey(query.preAggregations[0].invalidateKeyQueries[0].slice(0, 2))
       )
     ).toBe(true);
   });
@@ -745,5 +750,64 @@ describe('QueryOrchestrator', () => {
       structure_version: 'ezlvkhjl',
       naming_version: 2
     });
+  });
+
+  test('loadRefreshKeys', async () => {
+    const preAggregationsLoadCacheByDataSource = {};
+    const preAggregationExternalRefreshKey = {
+      preAggregationsLoadCacheByDataSource,
+      cacheKeyQueries: {
+        renewalThreshold: 21600,
+        queries: [
+          ['SELECT refreshKey in source database', [], {
+            renewalThreshold: 21600,
+            external: false,
+          }],
+          ['SELECT refreshKey in external database', [], {
+            renewalThreshold: 21600,
+            external: true,
+          }]
+        ]
+      },
+      preAggregations: [],
+      renewQuery: true,
+      requestId: 'loadRefreshKeys should respect external flag'
+    };
+
+    const driverQueryMock = jest.spyOn(mockDriver, 'query').mockImplementation(async () => []);
+    const externalDriverQueryMock = jest.spyOn(externalMockDriver, 'query').mockImplementation(async () => []);
+
+    const refreshKeys = await queryOrchestrator.loadRefreshKeys(preAggregationExternalRefreshKey);
+    console.log(refreshKeys);
+
+    expect(driverQueryMock.mock.calls.length).toEqual(1);
+    expect(driverQueryMock.mock.calls[0]).toEqual([
+      'SELECT refreshKey in source database',
+      [],
+      {
+        queryKey: [
+          'SELECT refreshKey in source database',
+          [],
+        ],
+        query: 'SELECT refreshKey in source database',
+        values: [],
+        requestId: preAggregationExternalRefreshKey.requestId,
+      }
+    ]);
+
+    expect(externalDriverQueryMock.mock.calls.length).toEqual(1);
+    expect(externalDriverQueryMock.mock.calls[0]).toEqual([
+      'SELECT refreshKey in external database',
+      [],
+      {
+        queryKey: [
+          'SELECT refreshKey in external database',
+          [],
+        ],
+        query: 'SELECT refreshKey in external database',
+        values: [],
+        requestId: preAggregationExternalRefreshKey.requestId,
+      }
+    ]);
   });
 });

commit 18666adf63e509dbaac304cf20cefcb19a6b9cb0

fix(query-orchestrator): make refresh key memo lookups hit again
`PreAggregationLoadCache` writes resolved refresh key values under
`queryRedisKey([query, values])` but `hasKeyQueryResult` looked them up under
`queryRedisKey(keyQuery)` — the full `[sql, params, options]` tuple every producer
emits. `getCacheHash` md5s `JSON.stringify`, so array length is part of the hash and
the lookup could never hit.

Regression from #3061, which moved refresh key metadata from a parallel
`refreshKeyRenewalThresholds` array into a third tuple element and updated only the
store side. Consequence: `notLoadedKey` was truthy whenever a pre-aggregation had any
invalidate key query, so `waitForRenew: false` requests always took the deferred path
and the "keys are already warm in this load cache, confirm freshness inline" branch has
been dead for four years. It failed safe — data was never stale, we just skipped a free
freshness win.

`hasKeyQueryResult` now keys on `[sql, params]` exactly like `keyQueryResult` already
stored it — the store side was correct all along. The `options` element stays out
deliberately: `replacePartitionSqlAndParams` recomputes `renewalThreshold` from
`new Date()` for incremental keys, so a key hashed over it would never be stable.

Re-enabling that branch needed a guard. The `externalRefresh` handling
(`throwOnMissingPartition` / `return null`) lives only in the deferred branch, and the
constructor forces `waitForRenew = false` when it is set, so those requests always
landed there. With the hash fixed and warm keys they would fall through to
`loadPreAggregationWithKeys()` -> `executeInQueue`, building pre-aggregations on an
instance explicitly told not to. The branch condition now excludes `externalRefresh`,
keeping its behaviour identical to today.

Also fixes the twin asymmetry from the same commit: `QueryCache.loadRefreshKey` still
passed `renewalKey: q` (3-tuple) while writing to `queryRedisKey([query, values])`,

packages/cubejs-query-orchestrator/src/orchestrator/PreAggregationLoadCache.ts
@@ -215,8 +215,11 @@ export class PreAggregationLoadCache {
     return this.queryResults[this.queryCache.queryRedisKey([query, values])];
   }
 
-  public hasKeyQueryResult(keyQuery) {
-    return !!this.queryResults[this.queryCache.queryRedisKey(keyQuery)];
+  public hasKeyQueryResult(keyQuery: QueryWithParams) {
+    // `options` is excluded because `replacePartitionSqlAndParams` recomputes `renewalThreshold`
+    // from `new Date()` for incremental refresh keys, so a key covering it would never be stable.
+    const [query, values] = keyQuery;
+    return !!this.queryResults[this.queryCache.queryRedisKey([query, values])];
   }
 
   public async getQueryStage(stageQueryKey) {

packages/cubejs-query-orchestrator/src/orchestrator/PreAggregationLoader.ts
@@ -131,14 +131,14 @@ export class PreAggregationLoader {
   public async loadPreAggregation(
     throwOnMissingPartition: boolean,
   ): Promise<null | LoadPreAggregationResult> {
-    const notLoadedKey = (this.preAggregation.invalidateKeyQueries || [])
-      .find(keyQuery => !this.loadCache.hasKeyQueryResult(keyQuery));
+    const invalidationKeysLoaded = (this.preAggregation.invalidateKeyQueries || [])
+      .every(keyQuery => this.loadCache.hasKeyQueryResult(keyQuery));
 
-    if (this.isJob || !(notLoadedKey && !this.waitForRenew)) {
-      // Case 1: pre-agg build job processing.
-      // Case 2: either we have no data cached for this rollup or waitForRenew
-      // is true, either way, synchronously renew what data is needed so that
-      // the most current data will be returned fo the current request.
+    // `externalRefresh` must stay on the Case 3 path below: it owns the "partition is not built
+    // yet" handling and must never enqueue a build on this instance.
+    if (this.isJob || this.waitForRenew || (invalidationKeysLoaded && !this.externalRefresh)) {
+      // Renew synchronously so the current request returns the most current data, unlike Case 3
+      // below which serves what already exists and refreshes in the background.
       const result = await this.loadPreAggregationWithKeys();
       const refreshKeyValues = await this.getInvalidationKeyValues();
       return {

packages/cubejs-query-orchestrator/src/orchestrator/QueryCache.ts
@@ -871,7 +871,9 @@ export class QueryCache {
       expireSecs,
       {
         renewalThreshold: this.options.refreshKeyRenewalThreshold || queryOptions?.renewalThreshold || 2 * 60,
-        renewalKey: q,
+        // Must match what PreAggregationLoadCache.keyQueryResult stores: the same refresh key can
+        // be resolved through either path and both write to the same cache entry.
+        renewalKey: [query, values],
         waitForRenew: !options.skipRefreshKeyWaitForRenew,
         requestId: options.requestId,
         dataSource: options.dataSource,

packages/cubejs-query-orchestrator/test/unit/PreAggregations.test.ts
@@ -280,6 +280,108 @@ describe('PreAggregations', () => {
     });
   });
 
+  describe('refresh key memoization', () => {
+    let preAggregations: PreAggregations;
+
+    const preAggregation = {
+      preAggregationsSchema: 'stb_pre_aggregations',
+      tableName: 'stb_pre_aggregations.orders_memo',
+      dataSource: 'default',
+      external: false,
+      loadSql: ['CREATE TABLE stb_pre_aggregations.orders_memo AS SELECT 1', []],
+      invalidateKeyQueries: [defaultCacheKeyQuery],
+    };
+
+    const createLoadCache = () => new PreAggregationLoadCache(
+      mockDriverFactory as any,
+      queryCache!,
+      preAggregations,
+      { dataSource: 'default' },
+    );
+
+    const createPreAggLoader = (loadCache: PreAggregationLoadCache, options: Record<string, any> = {}) => new PreAggregationLoader(
+      mockDriverFactory as any,
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      () => {},
+      queryCache!,
+      preAggregations,
+      preAggregation,
+      [],
+      loadCache,
+      { requestId: 'refresh-key-memo', ...options },
+    );
+
+    beforeEach(() => {
+      preAggregations = new PreAggregations(
+        'TEST',
+        mockDriverFactory as any,
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        () => {},
+        queryCache!,
+        {
+          queueOptions: async () => ({
+            executionTimeout: 1,
+            concurrency: 2,
+          }),
+        },
+      );
+    });
+
+    test('hasKeyQueryResult ignores the options element of the refresh key tuple', async () => {
+      const loadCache = createLoadCache();
+
+      expect(loadCache.hasKeyQueryResult(defaultCacheKeyQuery)).toBe(false);
+      await loadCache.keyQueryResult(defaultCacheKeyQuery, false, 10);
+
+      // Callers legitimately hold differing options objects: replacePartitionSqlAndParams rebuilds
+      // that element on every call, with a clock-dependent renewalThreshold for incremental keys.
+      expect(loadCache.hasKeyQueryResult(defaultCacheKeyQuery)).toBe(true);
+      expect(loadCache.hasKeyQueryResult(defaultCacheKeyQuery.slice(0, 2) as any)).toBe(true);
+      expect(loadCache.hasKeyQueryResult([defaultCacheKeyQuery[0], [], { renewalThreshold: 1 }])).toBe(true);
+      expect(loadCache.hasKeyQueryResult(['SELECT NOW() as unrelated', [], {}])).toBe(false);
+    });
+
+    test('both refresh key paths store the same renewal key', async () => {
+      const redisKey = queryCache!.queryRedisKey(defaultCacheKeyQuery.slice(0, 2));
+      const storedRenewalKey = async () => (await queryCache!.getCacheDriver().get(redisKey)).renewalKey;
+
+      await queryCache!.loadRefreshKey(defaultCacheKeyQuery, 3600, { dataSource: 'default', requestId: 'loadRefreshKey' });
+      const throughLoadRefreshKey = await storedRenewalKey();
+
+      // Each path has to write the entry itself, otherwise the second one just reads what the
+      // first stored and any disagreement stays invisible.
+      (queryCache!.getCacheDriver() as LocalCacheDriver).reset();
+      await createLoadCache().keyQueryResult(defaultCacheKeyQuery, false, 10);
+      const throughKeyQueryResult = await storedRenewalKey();
+
+      // A cube level cacheKeyQuery and a pre-aggregation invalidateKeyQuery can be the same SQL and
+      // then share this entry. Differing renewal keys make each path look stale to the other, so
+      // they re-fetch on every touch.
+      expect(throughLoadRefreshKey).toEqual(throughKeyQueryResult);
+    });
+
+    test('warm invalidation keys are confirmed synchronously', async () => {
+      const loadCache = createLoadCache();
+      await loadCache.keyQueryResult(defaultCacheKeyQuery, false, 10);
+
+      const result = await createPreAggLoader(loadCache, { waitForRenew: false }).loadPreAggregation(true);
+
+      // A populated refreshKeyValues is the marker of the inline path; the deferred one reports [].
+      expect(result!.refreshKeyValues.length).toEqual(1);
+    });
+
+    test('warm invalidation keys do not let externalRefresh build a pre-aggregation', async () => {
+      const loadCache = createLoadCache();
+      await loadCache.keyQueryResult(defaultCacheKeyQuery, false, 10);
+
+      // externalRefresh owns the "partition is not built yet" handling, so it must stay on the
+      // deferred path even when nothing has to be queried to confirm the refresh keys.
+      await expect(createPreAggLoader(loadCache, { externalRefresh: true }).loadPreAggregation(true))
+        .rejects.toThrowError(/No pre-aggregation partitions were built yet/);
+      expect(mockDriver!.tables).toEqual([]);
+    });
+  });
+
   describe('loadAllPreAggregationsIfNeeded', () => {
     let preAggregations: PreAggregations | null = null;
 